9c3d32e6734b7bd6056d4b932837a10baf8aa15f
Naoya Maruyama <nmaruyama@nvidia.com>
2023-03-15 14:19:51 -0700

    Add clang-format off/on to runtime files
    Note that clang-format is entirely disabled for those files that are
    mostly just copied from external code such as basic_type_traits.cu

--- runtime/tensor.cu
+++ runtime/tensor.cu
@@ -1,8 +1,10 @@
+// clang-format off
 /*
  * SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  */
+// clang-format on
 template <typename T, int N>
 struct Tensor {
   __device__ T& operator[](nvfuser_index_t ind) {
