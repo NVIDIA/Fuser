c2f5258b6dea147027e8411f8df08a90173153ed
Xiang Gao <qasdfgtyuiop@gmail.com>
2023-04-06 20:46:45 -0700

    Fix ldmatrix bank conflict factor

--- csrc/lower_bank_conflict.cpp
+++ csrc/lower_bank_conflict.cpp
@@ -79,19 +79,68 @@ inline int64_t getPhaseSize(int64_t word_size_bytes) {
   return 32;
 }
 
+// Doc for ldmatrix can be found at:
+// https://docs.nvidia.com/cuda/parallel-thread-execution/index.html#warp-level-matrix-load-instruction-ldmatrix
+//
+// According to this doc, ldmatrix always do vectorize 8 load, which might be
+// different from the consumer vectorization factor. The vectorization factor of
+// the ldmatrix consumer is not the load vector size (which is always 8),
+// instead, it is the number of items each thread will eventually get. This
+// vectorization factor determines the .num modifier of ldmatrix, which further
+// determines the number of addresses used by ldmatrix. For ldmatrix.x1, there
+// are 8 addresses. For ldmatrix.x2, there are 16 addresses. For ldmatrix.x4,
+// there are 32 addresses.
+int64_t getLdMatrixNumThreads(int64_t word_size) {
+  switch (word_size) {
+    case 2:
+      // If the consumer has vector 2, then each thread of the warp get 2 items.
+      // So there are in total 32*2 = 64 items for the warp. Each vector has 8
+      // elements, so there are 64/8 = 8 vectors, i.e. this is a .x1 ldmatrix
+      // and the first 8 threads contain useful addresses.
+      return 8;
+    case 4:
+      // If the consumer has vector 4, then each thread of the warp get 4 items.
+      // So there are in total 32*4 = 128 items for the warp. Each vector has 8
+      // elements, so there are 128/8 = 16 vectors, i.e. this is a .x2 ldmatrix
+      // and the first 16 threads contain useful addresses.
+      return 16;
+    case 8:
+      // If the consumer has vector 8, then each thread of the warp get 8 items.
+      // So there are in total 32*8 = 256 items for the warp. Each vector has 8
+      // elements, so there are 256/8 = 32 vectors, i.e. this is a .x4 ldmatrix
+      // and all the 32 threads contain useful addresses.
+      return 32;
+    default:
+      TORCH_INTERNAL_ASSERT(false, "Invalid word size for ldmatrix");
+  }
+}
+
 std::vector<int64_t> evaluateAddressesOnFirstPhase(
-    kir::TensorIndex* ti,
     const std::vector<kir::ForLoop*>& for_loops,
     c10::optional<LaunchParams> launch_params,
-    const ExpressionEvaluator& expr_eval_common) {
+    const ExpressionEvaluator& expr_eval_common,
+    LoadStoreOp* ldst,
+    bool is_producer) {
   std::vector<int64_t> addresses;
-  const auto word_size_bytes =
-      dataTypeSize(*(ti->getDataType())) * getVectorizeSize(ti);
-  int64_t phase_size = getPhaseSize((int64_t)word_size_bytes);
-
-  if (launch_params.has_value()) {
-    phase_size = std::min<int64_t>(phase_size, launch_params->nThreads());
+  auto consumer = ldst->output(0)->as<kir::TensorIndex>();
+  auto ti = (is_producer ? ldst->input(0)->as<kir::TensorIndex>() : consumer);
+  int64_t word_size = -1;
+  int64_t num_threads = -1;
+  if (ir_utils::isLdMatrixOp(ldst)) {
+    // See the comment of getLdMatrixNumThreads for why ldmatrix is handled
+    // differently.
+    word_size = 8;
+    num_threads = getLdMatrixNumThreads(getVectorizeSize(consumer));
+  } else {
+    word_size = getVectorizeSize(consumer);
+    num_threads =
+        (launch_params.has_value()
+             ? std::min<int64_t>(32l, launch_params->nThreads())
+             : 32l);
   }
+  int64_t word_size_bytes = dataTypeSize(*(ti->getDataType())) * word_size;
+  int64_t phase_size =
+      std::min(num_threads, getPhaseSize((int64_t)word_size_bytes));
 
   for (int64_t linear_tidx : c10::irange(phase_size)) {
     int64_t tidx = linear_tidx;
@@ -242,45 +291,16 @@ class BankConflictInfo : public kir::IrVisitor {
       return;
     }
 
-    if (expr->isA<UnaryOp>()) {
-      auto uop = expr->as<UnaryOp>();
-      if (uop->getUnaryOpType() != UnaryOpType::Set) {
-        return;
-      }
-      std::pair<int, int> conflict_ways{0, 0};
-      if (isSmemTensorIndex(uop->in())) {
-        conflict_ways.first = getConflictWays(evaluateAddressesOnFirstPhase(
-            uop->in()->as<kir::TensorIndex>(),
-            for_loops_,
-            launch_params_,
-            expr_eval_common_));
-      }
-      if (isSmemTensorIndex(uop->out())) {
-        conflict_ways.second = getConflictWays(evaluateAddressesOnFirstPhase(
-            uop->out()->as<kir::TensorIndex>(),
-            for_loops_,
-            launch_params_,
-            expr_eval_common_));
-      }
-      if (conflict_ways.first > 1 || conflict_ways.second > 1) {
-        bank_conflict_info_[expr] = conflict_ways;
-      }
-    } else if (expr->isA<LoadStoreOp>()) {
+    if (expr->isA<LoadStoreOp>()) {
       auto ldst = expr->as<LoadStoreOp>();
       std::pair<int, int> conflict_ways{0, 0};
       if (isSmemTensorIndex(ldst->in())) {
         conflict_ways.first = getConflictWays(evaluateAddressesOnFirstPhase(
-            ldst->in()->as<kir::TensorIndex>(),
-            for_loops_,
-            launch_params_,
-            expr_eval_common_));
+            for_loops_, launch_params_, expr_eval_common_, ldst, true));
       }
       if (isSmemTensorIndex(ldst->out())) {
         conflict_ways.second = getConflictWays(evaluateAddressesOnFirstPhase(
-            ldst->out()->as<kir::TensorIndex>(),
-            for_loops_,
-            launch_params_,
-            expr_eval_common_));
+            for_loops_, launch_params_, expr_eval_common_, ldst, false));
       }
       if (conflict_ways.first > 1 || conflict_ways.second > 1) {
         bank_conflict_info_[expr] = conflict_ways;
