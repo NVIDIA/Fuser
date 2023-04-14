b01c96f9a5d7180f5e0d17451116309e751ad77a
Ryan Spring <rdspring1@gmail.com>
2023-04-14 11:55:33 -0700

    remove

--- nvfuser_python_utils/__init__.py
+++ nvfuser_python_utils/__init__.py
@@ -1,12 +1,3 @@
-import os
 from .patch_nvfuser import patch_installation
 
 __all__ = ["patch_installation"]
-
-cmake_prefix_path = os.path.join(
-    os.path.dirname(os.path.dirname(__file__)),
-    "nvfuser",
-    "share",
-    "cmake",
-    "nvfuser",
-)
