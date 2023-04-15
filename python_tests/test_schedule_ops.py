1529d93035c22af7028cf7f7ef4c68e65d3301d8
Kevin Stephano <kevin.stephano@gmail.com>
2023-04-14 20:40:44 -0700

    Added more schedule op testing and renamed test file.

--- python_tests/test_schedule_ops.py
+++ python_tests/test_schedule_ops.py
@@ -30,13 +30,13 @@ def is_pre_volta():
 
 @unittest.skipIf(not RUN_NVFUSER, "requires CUDA")
 @unittest.skipIf(is_pre_volta(), "Only supported on Volta and newer devices.")
-class TestUserSchedules(TestCase):
+class TestScheduleOps(TestCase):
     def sched_op_in_definition_error(self, sched_op_fn: Callable):
         """
         Common function to test for an error when a schedule op is used in a definition
         """
         inputs = [
-            torch.randn(4, 4, 4, device="cuda"),
+            torch.randn(8, 8, 8, device="cuda"),
         ]
 
         def fusion_fn(fd: FusionDefinition):
@@ -55,12 +55,12 @@ class TestUserSchedules(TestCase):
             fd = DefError()
             _ = fd.execute(inputs)
 
-    def check_input_error(self, sched_fn: Callable, error_msg: str):
+    def check_input_error(self, sched_fn: Callable, error_msg: str, error_type=RuntimeError):
         """
         Common function to test for an input error to a schedule op
         """
         inputs = [
-            torch.randn(4, 4, 4, device="cuda"),
+            torch.randn(8, 8, 8, device="cuda"),
         ]
 
         def fusion_fn(fd: FusionDefinition):
@@ -75,7 +75,7 @@ class TestUserSchedules(TestCase):
             def schedule(self):
                 sched_fn(self)
 
-        with self.assertRaisesRegex(RuntimeError, error_msg):
+        with self.assertRaisesRegex(error_type, error_msg):
             fd = InputError()
             _ = fd.execute(inputs)
 
@@ -84,7 +84,7 @@ class TestUserSchedules(TestCase):
         Common function to test op works in a common case
         """
         inputs = [
-            torch.randn(4, 4, 4, device="cuda"),
+            torch.randn(8, 8, 8, device="cuda"),
         ]
 
         def fusion_fn(fd: FusionDefinition):
@@ -109,24 +109,90 @@ class TestUserSchedules(TestCase):
 
     def test_merge_op(self):
         self.sched_op_in_definition_error(lambda fd: fd.sched.merge(fd.t1, 1))
+
+        # Erorr check merge dimension. The dimension to merge is +1 from
+        # the relative dimension indicated
+        self.check_input_error(
+            lambda fd: fd.sched.merge(fd.t1, 1),
+            "Merging IterDomains requires that their iteration types match."
+        )
+        self.check_input_error(
+            lambda fd: fd.sched.merge(fd.t1, 2),
+            "Invalid merge detected, either one or both axes are outside of TensorView's range.",
+        )
+        # TODO: I am not sure why this error doesn't match the previous error
+        # The previous error seems like it should match as they represent the
+        # same merge position -1 and 2 in a 3 dimensional tensor
+        # https://github.com/NVIDIA/Fuser/issues/171
+        self.check_input_error(
+            lambda fd: fd.sched.merge(fd.t1, -1),
+            "Merging IterDomains requires that their iteration types match",
+        )
+        self.check_input_error(
+            lambda fd: fd.sched.merge(fd.t1, -4),
+            "Cannot merge axes within compute at position. Either axis -1 or 0 are within computePosition = 0",
+        )
+
         self.valid_use(lambda fd: fd.sched.merge(fd.t1, 0))
+        self.valid_use(lambda fd: fd.sched.merge(fd.t1, -3))
 
     def test_reduction_factor_op(self):
         self.sched_op_in_definition_error(
             lambda fd: fd.sched.reduction_factor(fd.t1, [-1])
         )
 
+        def error1_fn(fd: FusionDefinition):
+            fd.sched.split(fd.t1, 2, 2)
+            fd.sched.reduction_factor(fd.t1, [1])
+        self.check_input_error(
+            error1_fn,
+            "Cannot rfactor axes that are not reduction axes."
+        )
+        def error2_fn(fd: FusionDefinition):
+            fd.sched.split(fd.t1, 2, 2)
+            fd.sched.reduction_factor(fd.t1, [-3])
+        self.check_input_error(
+            error2_fn,
+            "Cannot rfactor axes that are not reduction axes."
+        )
+        def error3_fn(fd: FusionDefinition):
+            fd.sched.split(fd.t1, 2, 2)
+            fd.sched.reduction_factor(fd.t1, [2, 3])
+        self.check_input_error(
+            error3_fn,
+            "Must have at least one reduction axis not marked as rfactor."
+        )
+
         def sched_fn(fd: FusionDefinition):
             fd.sched.split(fd.t1, 2, 2)
             fd.sched.reduction_factor(fd.t1, [2])
-
         self.valid_use(sched_fn)
+
+        # Donut whole factoring of reduction dims
+        def sched1_fn(fd: FusionDefinition):
+            fd.sched.split(fd.t1, 2, 4)
+            fd.sched.split(fd.t1, 2, 2)
+            fd.sched.reduction_factor(fd.t1, [2, 4])
+        self.valid_use(sched1_fn)
+        def sched2_fn(fd: FusionDefinition):
+            fd.sched.split(fd.t1, 2, 4)
+            fd.sched.split(fd.t1, 2, 2)
+            fd.sched.reduction_factor(fd.t1, [3])
+        self.valid_use(sched2_fn)
+       
+        # NOTE: The binding function for the "rfactor" alias is identical so
+        # only proof of existence is needed
+        def sched_fn_alias(fd: FusionDefinition):
+            fd.sched.split(fd.t1, 2, 2)
+            fd.sched.rfactor(fd.t1, [2])
+        self.valid_use(sched_fn_alias)
 
     def test_reorder_op(self):
         self.sched_op_in_definition_error(
             lambda fd: fd.sched.reorder(fd.t1, {0: 1, 1: 0})
         )
-
+        
+        # Error checks of reorder dict
         self.check_input_error(
             lambda fd: fd.sched.reorder(fd.t1, {0: 3}),
             "Reorder axes are not within the number of dimensions of the provided domain",
@@ -151,7 +217,34 @@ class TestUserSchedules(TestCase):
 
     def test_split_op(self):
         self.sched_op_in_definition_error(lambda fd: fd.sched.split(fd.t1, 1, 2))
+
+        # Error checking split dimension
+        self.check_input_error(
+            lambda fd: fd.sched.split(fd.t1, 3, 2),
+            "Tried to access position . in domain",
+        )
+        self.check_input_error(
+            lambda fd: fd.sched.split(fd.t1, -4, 2),
+            "Split axis is less than 0 even after adjusting for nDims",
+        )
+
+        # Error checking split factor.
+        # NOTE: ceildiv will always turn a split greater than the dimension
+        # size into 1.
+        self.check_input_error(
+            lambda fd: fd.sched.split(fd.t1, 1, 0),
+            "Expected rhs != 0 to be true, but got false",
+        )
+        # NOTE: While a negative split is not allowed, it does not make sense
+        # why the error is a TypeError given -1 is a valid int
+        self.check_input_error(
+            lambda fd: fd.sched.split(fd.t1, 1, -1),
+            "incompatible function arguments",
+            TypeError,
+        )
+
         self.valid_use(lambda fd: fd.sched.split(fd.t1, 1, 2))
+        self.valid_use(lambda fd: fd.sched.split(fd.t1, -1, 2))
 
 
 if __name__ == "__main__":
