c0432e576cdc8049ede1c2f7bae294c72c37b415
Xiang Gao <qasdfgtyuiop@gmail.com>
2023-04-26 13:56:05 -0700

    save

--- test/test_gpu_tensorcore.cpp
+++ test/test_gpu_tensorcore.cpp
@@ -698,7 +698,7 @@ TEST_F(NVFuserTest, FusionAmpereMatmulBFloat16_CUDA) {
     fusion.addInput(tv0);
     fusion.addInput(tv1);
 
-    auto tv2 = matmul(tv0, tv1, layout);
+    auto tv2 = matmul(tv0, tv1, layout, true);
 
     fusion.addOutput(tv2);
 
