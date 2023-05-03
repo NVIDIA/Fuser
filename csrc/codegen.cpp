a53bf0451558dd557ccaf2151c7aefcf07503a0e
Xiang Gao <qasdfgtyuiop@gmail.com>
2023-05-03 13:00:28 -0700

    fix

--- csrc/codegen.cpp
+++ csrc/codegen.cpp
@@ -227,8 +227,7 @@ class CudaKernelGenerator : private OptOutConstDispatch {
     for (auto allocate : kernel_summary.global_allocations) {
       TORCH_INTERNAL_ASSERT(allocate->buffer()->isA<TensorView>());
       const auto tv = allocate->buffer()->as<TensorView>();
-      const auto& maybe_rfactor_domain =
-          tv->hasRFactor() ? tv->getRFactorDomain() : tv->getRootDomain();
+      const auto& maybe_rfactor_domain = tv->getMaybeRFactorDomain();
       const auto nDims = std::count_if(
           maybe_rfactor_domain.begin(),
           maybe_rfactor_domain.end(),
