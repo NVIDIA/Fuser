c5f6740d345974972481f3e6a436e215d5dbaa64
Xiang Gao <qasdfgtyuiop@gmail.com>
2023-05-09 08:31:30 -0700

    libnccl-dev

--- tools/setup-env.sh
+++ tools/setup-env.sh
@@ -4,7 +4,7 @@
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
-sudo apt-get -y install cuda-toolkit-12-1
+sudo apt-get -y install cuda-toolkit-12-1 libnccl-dev
 
 # cmake environment variables
 export CUDAARCHS=86
