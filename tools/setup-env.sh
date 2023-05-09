0884e8630e3c5739222913df77f156531b04860b
Xiang Gao <qasdfgtyuiop@gmail.com>
2023-05-09 08:25:59 -0700

    more bg job

--- tools/setup-env.sh
+++ tools/setup-env.sh
@@ -2,16 +2,16 @@
 
 # Install pytorch in background
 pip3 install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu121 &
-pip_pid=$!
+torch_pid=$!
 
 # Install cuda
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
-sudo apt-get -y install cuda-compiler-12-1 cuda-command-line-tools-12-1
+sudo apt-get -y install cuda-compiler-12-1 cuda-command-line-tools-12-1 &
 
 # wait for PyTorch installation to finish
-wait $pip_pid
+wait $torch_pid
 
 # cmake environment variables
 export CUDAARCHS=86
@@ -21,3 +21,6 @@ export CUDA_INSTALL_PATH=/usr/local/cuda
 
 # Install other requirements
 pip install -r requirements.txt
+
+# wait for CUDA installation to finish
+wait
