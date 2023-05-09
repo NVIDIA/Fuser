34170fa61484234dc266604cc234c5d24a2f8358
Xiang Gao <qasdfgtyuiop@gmail.com>
2023-05-09 08:20:17 -0700

    install torch in background

--- tools/setup-env.sh
+++ tools/setup-env.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+# Install pytorch in background
+pip3 install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu121 &
+pip_pid=$!
+
 # Install cuda
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
 sudo apt-get -y install cuda-compiler-12-1
 
+# wait for PyTorch installation to finish
+wait $pip_pid
+
 # cmake environment variables
 export CUDAARCHS=86
 export CUDACXX=/usr/local/cuda/bin/nvcc
 export PATH=/usr/local/cuda/bin:${PATH}
 export CUDA_INSTALL_PATH=/usr/local/cuda
-
-# Install pytorch
-pip3 install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu121
 
 # Install other requirements
 pip install -r requirements.txt
