2a41cd766e2898d27357da2b13d54f316d2dcd11
Xiang Gao <qasdfgtyuiop@gmail.com>
2023-05-11 06:37:47 -0700

    save

--- tools/pip-install-things.sh
+++ tools/pip-install-things.sh
@@ -3,6 +3,6 @@
 pip3 install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu121
 
 # PyTorch's has a buildin Modules_CUDA which is super old. Remove it.
-rm -rf /opt/hostedtoolcache/Python/3.10.11/x64/lib/python3.10/site-packages/torch/share/cmake/Caffe2/Modules_CUDA_fix
+rm -rf $(python -c 'import torch; print(torch.__path__[0])')/share/cmake/Caffe2/Modules_CUDA_fix
 
 pip install -r requirements.txt
