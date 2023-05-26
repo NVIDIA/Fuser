6fece9a78ffc8acf227a5e81cf00c31e90699947
jjsjann123 <jiej@nvidia.com>
2023-05-26 16:10:43 -0700

    filling in implementation

--- csrc/optimization/consecutive_cast_pass.cpp
+++ csrc/optimization/consecutive_cast_pass.cpp
@@ -12,87 +12,133 @@ namespace nvfuser::optimization {
 
 namespace {
 
+bool isSameDtypeCategory(const DataType& input_t, const DataType& output_t) {
+  if ((isIntegralType(input_t) && isIntegralType(out_dtype)) ||
+      (isFloatingPointType(input_t) && isFloatingPointType(out_dtype)) ||
+      (isComplexType(input_t) && isComplexType(out_dtype))) {
+    return true;
+  }
+  return false;
+}
+
+// check if type1 is a wider type than type0
+// Which indicates a cast from type0 -> type1 -> type0 should be bit-wise identical
+bool isWiderType(const DataType& type0, const DataType& type1) {
+  if (type0 == type1) {
+    return true;
+  } else if (type0 == DataType::Double && (type1 == DataType::Float || type1 == DataType::Half || type1 == DataType::BFloat16)) {
+    return true;
+  } else if (type0 == DataType::Float && (type1 == DataType::Half || type1 == DataType::BFloat16)) {
+    return true;
+  } else if (type0 == DataType::Int && type1 == DataType::Int32) {
+    return true;
+  } else if (type0 == DataType::ComplexDouble && type1 == DataType::ComplexFloat) {
+    return true;
+  }
+  return false;
+}
+
+// note: returns
+//  - -1 : v0 contains strictly less information than v1;
+//  - 0  : a complex case, where each v0 and v1 isn't a super set of the other;
+//  - 1  : v0 and v1 has the same dtype;
+//  - 2  : v0 contains strictly more information than v1;
+int checkInformationLoss(Val* v0, Val* v1) {
+  auto dtype0 = v0->getDataType().value();
+  auto dtype1 = v1->getDataType().value();
+  if (dtype0 == dtype1) {
+    return 1;
+  }
+  if ((dtype0 == DataType::BFloat16 && dtype1 == DataType::Half) ||
+      (dtype1 == DataType::BFloat16 && dtype0 == DataType::Half)) {
+    return 0;
+  } 
+  if (isWiderType(dtype0, dtype1)) {
+    return -1;
+  }
+  TORCH_INTERNAL_ASSERT(isWiderType(dtype1, dtype0), "unrecognized cast category is encountered");
+  return 2;
+}
+
 void castOptimizationPass(Fusion* fusion) {
-  auto is_cast_op = [](Expr* expr) {
+  auto is_foldable_cast_op = [](Expr* expr) {
     if (expr != nullptr && expr->isA<UnaryOp>()) {
       auto op = expr->as<UnaryOp>();
-      if (op->getUnaryOpType() == UnaryOpType::Cast) {
+      if (op->getUnaryOpType() == UnaryOpType::Cast && 
+	isSameDtypeCategory(expr->input(0)->getDataType().value(), expr->output(0)->getDataType().value())) {
         return true;
       }
     }
     return false;
   };
 
-  // NOTE: not the most efficient pass
+  // TODO: Traveral implies topological order on returns exprs, we can leverage that to improve the effieciency of the pass. In the case of a straight line casts, we are doing a lot of meaningless work here on mutating intermediate casts that would have been done again at the end of the chain.
   for (auto expr : fusion->exprs()) {
-    if (is_cast_op(expr)) {
+    if (is_foldable_cast_op(expr)) {
       bool mutated = false;
       std::vector<Val*> chain_casts;
       auto prev_expr = expr->input(0)->definition();
-      while (prev_expr != nullptr && is_cast_op(prev_expr)) {
-	chain_casts.push_back(prev_expr->output(0));
+      while (prev_expr != nullptr && is_foldable_cast_op(prev_expr)) {
+	auto intermediate_cast = prev_expr->output(0);
+        // Note, if the output f prev_expr
+        //   is used by other operation(s); or
+        //   is a direct output from fusion
+        // we skip the casting chaining
+        if (intermediate_cast->isFusionOutput() ||
+            intermediate_cast->uses().size() > 1) {
+          break;
+        }
+
+        // in the loop, we just repetitively chaining consecutive casts.
+	chain_casts.push_back(intermediate_cast);
         prev_expr = prev_expr->input(0)->definition();
       }
 
+      // Note, chain_casts has a straight-line use without branches
       if (!chain_casts.empty()) {
-      }
+        auto lo_anchor = chain_casts[0]->definition()->input(0);
+	auto starting_anchor = lo_anchor;
+	for (auto val : chain_casts) {
+	  auto info = checkInformationLoss(anchor, val);
+          if (info >= 0) {
+            if (info == 0) {
+	      // we run into a lemon case where we are casting between two types that can't be folded away. i.e. bf16 & fp16.
+	      auto tmp_expr = val->definition();
+	      if (lo_anchor != tmp_expr->input(0)) {
+                tmp_expr = nvfuser::ir_utils::replaceValInExpr(tmp_expr, tmp_expr->input(0), lo_anchor);
+	      }
+	      // move starting_anchor past the ambiguous case
+	      starting_anchor = val;
+	    }
+	    // updating lo_anchor
+	    lo_anchor = val;
+	  }
+	}
 
-      while (true) {
-        // in the loop, we just repetitively skip consecutive casts.
-        auto intermediate_cast = expr->input(0);
-        auto prev_expr = intermediate_cast->definition();
-        if (prev_expr == nullptr || !is_cast_op(prev_expr)) {
-          break;
-        }
-        // Note, if intermediate_cast:
-        //   is used by other none-cast operations; or
-        //   is a direct output from fusion
-        // we skip the casting
-        if (intermediate_cast->isFusionOutput() ||
-            !std::all_of(
-                intermediate_cast->uses().begin(),
-                intermediate_cast->uses().end(),
-                is_cast_op)) {
-          break;
-        }
-        auto original_dtype = prev_expr->input(0)->getDataType().value();
-        auto intermediate_dtype = intermediate_cast->getDataType().value();
-        auto out_dtype = expr->output(0)->getDataType().value();
-        // cases where skipping the intermediate cast is relatively safe,
-        // two conditions:
-        //   1. original_dtype is the same as out_dtype; or
-        //   2. we support direct cast from original_dtype to out_dtype.
-        // and
-        //   1. intermediate_dtype is the same type category as with
-        //   out_dtype
-        if ((original_dtype == out_dtype ||
-             cast_func_str({original_dtype, out_dtype}).has_value()) &&
-            ((isIntegralType(intermediate_dtype) &&
-              isIntegralType(out_dtype)) ||
-             (isFloatingPointType(intermediate_dtype) &&
-              isFloatingPointType(out_dtype)) ||
-             (isComplexType(intermediate_dtype) && isComplexType(out_dtype)))) {
+	auto info = checkInformationLoss(lo_anchor, expr->output(0));
+	if (info == 1) {
+          // replacing output with lo_anchor in the fusion
+          ir_utils::replaceValue(fusion, {{expr->output(0), lo_anchor}});
+          if (expr->output(0)->isFusionOutput()) {
+            fusion->replaceOutput(expr->output(0), lo_anchor);
+          }
+	} else if (info == -1 || info == 0) {
+          // expr output has either:
+	  //   higher precision than lo_anchor; or
+	  //   incompatible precision
+          // in either case, we can't fold away lo_anchor, we'll just re-wire the input to expr to lo_anchor
+          expr = nvfuser::ir_utils::replaceValInExpr(
+              expr, expr->input(0), lo_anchor);
+	} else if (info == 2) {
+          // if expr has lower precision than lo_anchor, we'll just fold away to the starting_anchor instead
+          expr = nvfuser::ir_utils::replaceValInExpr(
+              expr, expr->input(0), starting_anchor);
+        } else {
+          TORCH_INTERNAL_ASSERT(false, "checkInformationLoss returns a flag that's not recognized");
+	}
+
           expr = nvfuser::ir_utils::replaceValInExpr(
               expr, intermediate_cast, prev_expr->input(0));
-          mutated = true;
-        } else {
-          break;
-        }
-      }
-
-      if (mutated) {
-        // quick short-wire to skip current cast node if it's trivially
-        // casting to the same type
-        if (expr->input(0)->getDataType().value() ==
-            expr->output(0)->getDataType().value()) {
-          // replacing output with input in the fusion
-          ir_utils::replaceValue(fusion, {{expr->output(0), expr->input(0)}});
-          // NOTE: if current output is a fusion output, DCE won't kick in and
-          // we'll ended up with an illegal cast.
-          if (expr->output(0)->isFusionOutput()) {
-            fusion->replaceOutput(expr->output(0), expr->input(0));
-          }
-        }
       }
     }
   }
