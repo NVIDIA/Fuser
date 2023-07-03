61b70b06eb5fb6f3c6c1a9a6f5f7f10f58971dcc
Jacob Hinkle <jhinkle@nvidia.com>
2023-07-03 10:18:56 -0400

    Add capture_debug_output and get_debug_output to frontend

--- python_tests/test_python_frontend.py
+++ python_tests/test_python_frontend.py
@@ -2359,6 +2359,27 @@ class TestNvFuserFrontend(TestCase):
         # Just test that this executes, not that it's correct
         nvf_out, _ = self.exec_nvfuser(fusion_func, inputs)
 
+    def test_debug_output(self):
+        inputs = [
+            torch.randn((3,), dtype=torch.float32, device="cuda:0"),
+            0.1,
+        ]
+
+        with FusionDefinition() as fd:
+            T0 = fd.from_pytorch(inputs[0])
+            S1 = fd.define_scalar()
+            T1 = fd.ops.mul(T0, S1)
+            fd.add_output(T1)
+
+        out1 = fd.execute(inputs)
+        self.assertIsNone(fd.get_debug_output())
+
+        # If debug output is captured, getDebugOutput() will not return None.
+        # The output will depend on the NVFUSER_DUMP environment variable in
+        # such case
+        out2 = fd.execute(inputs, capture_debug_output=True)
+        self.assertIsNotNone(fd.get_debug_output())
+
 
 if __name__ == "__main__":
     run_tests()
