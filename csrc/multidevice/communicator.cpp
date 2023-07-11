6404fb1a77171da45a3ea6faba04fd6e4151bc08
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2023-07-11 14:06:38 -0700

    [war] link ucc

--- csrc/multidevice/communicator.cpp
+++ csrc/multidevice/communicator.cpp
@@ -96,9 +96,7 @@ c10::intrusive_ptr<c10d::Backend> createBackend(
 
 #ifdef USE_C10D_UCC
   if (backend == CommunicatorBackend::ucc) {
-    auto pg_opts = c10d::ProcessGroupUCC::Options::create();
-    return c10::make_intrusive<::c10d::ProcessGroupUCC>(
-        store, rank, size, pg_opts);
+    return c10::make_intrusive<::c10d::ProcessGroupUcc>(store, rank, size);
   }
 #endif
   TORCH_CHECK(false, "no distributed backend available");
