71892a5ebc7ec669a2ac80e2bcf85f70a5c684bb
jjsjann123 <jiej@nvidia.com>
2023-07-20 14:53:38 -0700

    clean up TS test disable decorator

--- python_tests/test_torchscript.py
+++ python_tests/test_torchscript.py
@@ -873,8 +873,7 @@ class TestCudaFuser(JitTestCase):
         GRAPH_EXECUTOR != ProfilingMode.PROFILING,
         "Requires fusion optimization pass to be effective",
     )
-    # Disabled due to change in torch.testing._internal.jit_metaprogramming_utils
-    # See https://github.com/NVIDIA/Fuser/issues/624
+    @unittest.skipIf(True, "TS issue: https://github.com/NVIDIA/Fuser/issues/624")
     def _test_category_rule(self):
         def run_tensor(x, z):
             def t(x: torch.Tensor, z: torch.Tensor):
@@ -3264,8 +3263,7 @@ class TestCudaFuser(JitTestCase):
         GRAPH_EXECUTOR != ProfilingMode.PROFILING,
         "Requires fusion optimization pass to be effective",
     )
-    # Disabled due to change in received graph.
-    # See https://github.com/NVIDIA/Fuser/issues/624
+    @unittest.skipIf(True, "TS issue: https://github.com/NVIDIA/Fuser/issues/624")
     def _test_linear(self):
         in_feature = 2
         out_feature = 8
@@ -4116,8 +4114,7 @@ class TestCudaFuser(JitTestCase):
         GRAPH_EXECUTOR != ProfilingMode.PROFILING,
         "Requires fusion optimization pass to be effective",
     )
-    # Disabled due to change in received graph.
-    # See https://github.com/NVIDIA/Fuser/issues/624
+    @unittest.skipIf(True, "TS issue: https://github.com/NVIDIA/Fuser/issues/624")
     def _test_conv2d_bias(self):
         def t(x: torch.Tensor, w: torch.Tensor, bias: torch.Tensor):
             o = torch.nn.functional.conv2d(x, w, bias)
@@ -4509,8 +4506,7 @@ class TestCudaFuser(JitTestCase):
         GRAPH_EXECUTOR != ProfilingMode.PROFILING,
         "Requires fusion optimization pass to be effective",
     )
-    # Disabled due to change in received graph.
-    # See https://github.com/NVIDIA/Fuser/issues/624
+    @unittest.skipIf(True, "TS issue: https://github.com/NVIDIA/Fuser/issues/624")
     def _test_view(self):
         torch._C._jit_set_nvfuser_guard_mode(True)
         self._bias_view_relu_helper([2, 3, 4, 5], [-1, 4, 5], torch.float, "cuda", 1e-6)
@@ -4595,8 +4591,7 @@ class TestCudaFuser(JitTestCase):
         GRAPH_EXECUTOR != ProfilingMode.PROFILING,
         "Requires fusion optimization pass to be effective",
     )
-    # Disabled due to change in received graph.
-    # See https://github.com/NVIDIA/Fuser/issues/624
+    @unittest.skipIf(True, "TS issue: https://github.com/NVIDIA/Fuser/issues/624")
     def _test_flatten(self):
         torch._C._jit_set_nvfuser_guard_mode(True)
         self._bias_flatten_relu_helper([2, 3, 4, 5], 0, -1, torch.float, "cuda", 1e-6)
@@ -4693,8 +4688,7 @@ class TestCudaFuser(JitTestCase):
         GRAPH_EXECUTOR != ProfilingMode.PROFILING,
         "Requires fusion optimization pass to be effective",
     )
-    # Disabled due to change in received graph.
-    # See https://github.com/NVIDIA/Fuser/issues/624
+    @unittest.skipIf(True, "TS issue: https://github.com/NVIDIA/Fuser/issues/624")
     def _test_nested_view(self):
         self._ltc_helper([256, 128, 1024], torch.float, "cuda", 1e-6)
 
@@ -4755,8 +4749,7 @@ class TestCudaFuser(JitTestCase):
         GRAPH_EXECUTOR != ProfilingMode.PROFILING,
         "Requires fusion optimization pass to be effective",
     )
-    # Disabled due to change in received graph.
-    # See https://github.com/NVIDIA/Fuser/issues/624
+    @unittest.skipIf(True, "TS issue: https://github.com/NVIDIA/Fuser/issues/624")
     def _test_squeeze(self):
         self._bias_squeeze_relu_helper([1, 6, 1, 2, 2, 5, 1], torch.float, "cuda", 1e-6)
         self._alias_bias_squeeze_relu_helper(
@@ -4769,8 +4762,7 @@ class TestCudaFuser(JitTestCase):
         GRAPH_EXECUTOR != ProfilingMode.PROFILING,
         "Requires fusion optimization pass to be effective",
     )
-    # Disabled due to change in received graph.
-    # See https://github.com/NVIDIA/Fuser/issues/624
+    @unittest.skipIf(True, "TS issue: https://github.com/NVIDIA/Fuser/issues/624")
     def _test_squeeze_zero(self):
         x = torch.tensor(1.0, dtype=torch.float, device="cuda")
 
@@ -4848,8 +4840,7 @@ class TestCudaFuser(JitTestCase):
         GRAPH_EXECUTOR != ProfilingMode.PROFILING,
         "Requires fusion optimization pass to be effective",
     )
-    # Disabled due to change in received graph.
-    # See https://github.com/NVIDIA/Fuser/issues/624
+    @unittest.skipIf(True, "TS issue: https://github.com/NVIDIA/Fuser/issues/624")
     def _test_unsqueeze(self):
         self._bias_unsqueeze_relu_helper([2, 3, 4, 5], torch.float, "cuda", 1e-6)
         self._alias_bias_unsqueeze_relu_helper([2, 3, 4, 5], torch.float, "cuda", 1e-6)
@@ -4859,8 +4850,7 @@ class TestCudaFuser(JitTestCase):
         GRAPH_EXECUTOR != ProfilingMode.PROFILING,
         "Requires fusion optimization pass to be effective",
     )
-    # Disabled due to change in received graph.
-    # See https://github.com/NVIDIA/Fuser/issues/624
+    @unittest.skipIf(True, "TS issue: https://github.com/NVIDIA/Fuser/issues/624")
     def _test_alias_pass_fix(self):
         x = torch.randn(4, 24, 2, 2, dtype=torch.float, device="cuda")
         w = torch.randn(24, 24, 1, 1, dtype=torch.float, device="cuda")
@@ -4879,8 +4869,7 @@ class TestCudaFuser(JitTestCase):
         GRAPH_EXECUTOR != ProfilingMode.PROFILING,
         "Requires fusion optimization pass to be effective",
     )
-    # Disabled due to change in received graph.
-    # See https://github.com/NVIDIA/Fuser/issues/624
+    @unittest.skipIf(True, "TS issue: https://github.com/NVIDIA/Fuser/issues/624")
     def _test_squeeze_negative_dim(self):
         x = torch.randn(4, 24, 1, 2, dtype=torch.float, device="cuda")
 
@@ -4958,8 +4947,7 @@ class TestCudaFuser(JitTestCase):
         GRAPH_EXECUTOR != ProfilingMode.PROFILING,
         "Requires fusion optimization pass to be effective",
     )
-    # Disabled due to change in received graph.
-    # See https://github.com/NVIDIA/Fuser/issues/624
+    @unittest.skipIf(True, "TS issue: https://github.com/NVIDIA/Fuser/issues/624")
     def _test_index_select_shape_expression(self):
         x = torch.randn([68, 128], dtype=torch.float, device="cuda")
         y = torch.randn_like(x)
@@ -5241,8 +5229,7 @@ class TestCudaFuser(JitTestCase):
         GRAPH_EXECUTOR != ProfilingMode.PROFILING,
         "Requires fusion optimization pass to be effective",
     )
-    # Disabled due to change in received graph.
-    # See https://github.com/NVIDIA/Fuser/issues/624
+    @unittest.skipIf(True, "TS issue: https://github.com/NVIDIA/Fuser/issues/624")
     def _test_shape_expression(self):
         x = torch.randn(4, 2, 1, 3, device="cuda")
 
@@ -5415,8 +5402,7 @@ class TestCudaFuser(JitTestCase):
         GRAPH_EXECUTOR != ProfilingMode.PROFILING,
         "Requires fusion optimization pass to be effective",
     )
-    # Disabled due to change in received graph.
-    # See https://github.com/NVIDIA/Fuser/issues/624
+    @unittest.skipIf(True, "TS issue: https://github.com/NVIDIA/Fuser/issues/624")
     def _test_view_copy_graph_guard(self):
         x = torch.randn(4, 2, 3, device="cuda").permute([1, 2, 0])
         y = [4, 6]
@@ -5437,8 +5423,7 @@ class TestCudaFuser(JitTestCase):
         GRAPH_EXECUTOR != ProfilingMode.PROFILING,
         "Requires fusion optimization pass to be effective",
     )
-    # Disabled due to change in received graph.
-    # See https://github.com/NVIDIA/Fuser/issues/624
+    @unittest.skipIf(True, "TS issue: https://github.com/NVIDIA/Fuser/issues/624")
     def _test_view_copy_graph_guard_double_fusion(self):
         x = torch.randn(2, 2, 5, device="cuda")
         w = torch.randn(5, 5, device="cuda")
