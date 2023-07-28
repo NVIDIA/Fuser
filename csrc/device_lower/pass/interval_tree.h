9e415370e84095189382c8248e7314c86716a20e
Jacob Hinkle <jhinkle@nvidia.com>
2023-07-28 14:09:16 -0400

    Add constructor, start of printing

--- csrc/device_lower/pass/interval_tree.h
+++ csrc/device_lower/pass/interval_tree.h
@@ -7,6 +7,11 @@
 // clang-format on
 #pragma once
 
+#include <c10/util/irange.h>
+
+#include <memory>
+#include <ostream>
+#include <sstream>
 #include <vector>
 
 namespace nvfuser {
@@ -29,26 +34,35 @@ namespace nvfuser {
 template <typename TimeType, typename IndexType = int16_t>
 class CenteredIntervalTree {
  public:
-  //! Get a vector of indices of intervals that overlap a single point
-  std::vector<IndexType> getIntervalsOverlappingPoint(TimeType x) const {
+  CenteredIntervalTree(
+      const std::vector<std::pair<TimeType, TimeType>>& intervals) {
+    std::vector<IndexType> all_indices;
+    all_indices.reserve(intervals.size());
+    for (IndexType i : c10::irange(intervals.size())) {
+      all_indices.push_back(i);
+    }
+    root_ = std::make_unique<CenteredIntervalTreeNode>(intervals, all_indices);
+  }
+
+  //! Get a vector of indices of intervals that contain a point
+  std::vector<IndexType> getIntervalsContainingPoint(TimeType x) const {
     std::vector<IndexType> output;
-    root_.insertIntervalsOverlappingPoint(output, x);
+    root_->insertIntervalsContainingPoint(output, x);
     return output;
   }
 
   //! Get a vector of indices of intervals that overlap an arbitrary given
   //! interval
-  std::vector<IndexType> getIntervalsOverlappingPoint(
-      TimeType start,
-      TimeType stop) const {
+  std::vector<IndexType> getOverlappingIntervals(TimeType start, TimeType stop)
+      const {
     std::vector<IndexType> output;
-    root_.insertIntervalsOverlappingPoint(output, start, stop);
+    root_->insertOverlappingIntervals(output, start, stop);
     return output;
   }
 
   //! Get a vector of indices of intervals that overlap a given interval in the
   //! original set of intervals.
-  std::vector<IndexType> getIntervalsOverlappingPoint(
+  std::vector<IndexType> getIntervalsOverlappingKnownInterval(
       IndexType interval_index) const {
     TORCH_CHECK(
         interval_index < root_.intervals_.size() &&
@@ -58,12 +72,95 @@ class CenteredIntervalTree {
         ", inclusive. Found ",
         interval_index);
     auto [start, stop] = root_.intervals_.at(interval_index);
-    return getIntervalsOverlappingPoint(start, stop);
+    return getOverlappingIntervals(start, stop);
+  }
+
+  void print(std::ostream& out) const {
+    out << "CenteredIntervalTree {\n";
+    root_->print(out, /* indent */ 1);
+    out << "}";
+  }
+
+  std::string toString() const {
+    std::stringstream ss;
+    print(ss);
+    return ss.str();
   }
 
  private:
   class CenteredIntervalTreeNode {
    public:
+    //! Construct a node with global interval list intervals restricted to the
+    //! given subset of indices
+    CenteredIntervalTreeNode(
+        const std::vector<std::pair<TimeType, TimeType>>& intervals,
+        const std::vector<IndexType>& indices)
+        : intervals_(intervals) {
+      TORCH_CHECK(
+          indices.size() > 0, "Cannot initialize node with zero intervals");
+
+      TORCH_CHECK(
+          intervals_.size() < std::numeric_limits<IndexType>::max(),
+          "IndexType is too small to fit ",
+          intervals_.size(),
+          " intervals.");
+
+      // Decide on center point.
+      selectCenterPoint(indices);
+
+      // Create left, right, and center index lists
+      std::vector<IndexType> left_indices;
+      std::vector<IndexType> right_indices;
+      for (const auto ind : indices) {
+        TORCH_CHECK(ind < (IndexType)intervals_.size());
+        const auto [start, stop] = intervals_.at(ind);
+        TORCH_CHECK(start <= stop);
+        if (stop < center_point_) {
+          left_indices.push_back(ind);
+        } else if (start > center_point_) {
+          right_indices.push_back(ind);
+        } else {
+          // append to the center subset
+          local_to_global_.push_back(ind);
+        }
+      }
+
+      // sort center endpoints
+      center_start_sorted_.insert(
+          center_start_sorted_.end(),
+          local_to_global_.begin(),
+          local_to_global_.end());
+      std::sort(
+          center_start_sorted_.begin(),
+          center_start_sorted_.end(),
+          [this](IndexType const& a, IndexType const& b) -> bool {
+            return intervals_.at(a).first < intervals_.at(b).first;
+          });
+
+      center_stop_reverse_sorted_.insert(
+          center_stop_reverse_sorted_.end(),
+          local_to_global_.begin(),
+          local_to_global_.end());
+      std::sort(
+          center_stop_reverse_sorted_.begin(),
+          center_stop_reverse_sorted_.end(),
+          [this](IndexType const& a, IndexType const& b) -> bool {
+            return intervals_.at(a).second > intervals_.at(b).second;
+          });
+
+      // Recurse to initialize left and right
+      if (!left_indices.empty()) {
+        left_ =
+            std::make_unique<CenteredIntervalTreeNode>(intervals, left_indices);
+        left_indices.clear(); // early free to make more space for allocations
+                              // in right branch
+      }
+      if (!right_indices.empty()) {
+        right_ =
+            std::make_unique<CenteredIntervalTreeNode>(intervals, left_indices);
+      }
+    }
+
     //! Inserts any intervals [a, b] in this subtree such that x \in [a, b].
     //! Notice that intervals are treated as closed intervals in this
     //! comparison.
@@ -93,7 +190,7 @@ class CenteredIntervalTree {
           }
           output.push_back(global_idx);
         }
-        child_node = left_;
+        child_node = left_.get();
       } else {
         for (auto local_idx : center_stop_reverse_sorted_) {
           auto global_idx = local_to_global_.at(local_idx);
@@ -103,12 +200,12 @@ class CenteredIntervalTree {
           }
           output.push_back(global_idx);
         }
-        child_node = right_;
+        child_node = right_.get();
       }
 
       // Recurse to left or right subset and merge results
       if (child_node) {
-        child_node->insertInterval(output, x);
+        child_node->insertIntervalsContainingPoint(output, x);
       }
     }
 
@@ -122,11 +219,32 @@ class CenteredIntervalTree {
         TimeType start,
         TimeType stop) const {}
 
-   private:
-    TimeType center_point_;
-    CenteredIntervalTreeNode* left_;
-    CenteredIntervalTreeNode* right_;
+    void print(std::ostream& out, int indent = 0) const {
+      out << "";
+    }
 
+   private:
+    //! This method picks the approximate midpoint of the span of all intervals
+    //! in a subset as the center point. A more detailed approach would result
+    //! in a more balanced and efficient tree.
+    void selectCenterPoint(const std::vector<IndexType>& indices) {
+      TimeType min_start = 0;
+      TimeType max_stop = 0;
+      bool init = false;
+      for (const auto ind : indices) {
+        const auto [start, stop] = intervals_.at(ind);
+        if (init) {
+          min_start = start;
+          max_stop = stop;
+        } else {
+          min_start = std::min(start, min_start);
+          max_stop = std::min(stop, max_stop);
+        }
+      }
+      center_point_ = (min_start + max_stop) / 2;
+    }
+
+   private:
     //! Reference to the global vector of intervals
     const std::vector<std::pair<TimeType, TimeType>>& intervals_;
 
@@ -139,10 +257,15 @@ class CenteredIntervalTree {
     //! Stop values are reverse sorted so that we can insert results starting
     //! with the first elements before breaking early.
     std::vector<IndexType> center_stop_reverse_sorted_;
+
+    std::unique_ptr<CenteredIntervalTreeNode> left_ = nullptr;
+    std::unique_ptr<CenteredIntervalTreeNode> right_ = nullptr;
+
+    TimeType center_point_ = 0;
   };
 
-  CenteredIntervalTreeNode root_;
+ private:
+  std::unique_ptr<CenteredIntervalTreeNode> root_ = nullptr;
 };
 
 } // namespace nvfuser
-
--- test/test_allocation_reuse.cpp
+++ test/test_allocation_reuse.cpp
@@ -21,7 +21,26 @@ class AllocationReuse : public NVFuserTest {};
 
 // Simple exercises for CenteredIntervalTree
 TEST_F(AllocationReuse, CenteredIntervalTree) {
-  //CenteredIntervalTree cit;
+  // For this test case, we will have the following intervals
+  //
+  //     ----------
+  //  -----
+  //        ------
+  //          -----
+  // ---------------
+  using TimeType = int16_t;
+  std::vector<std::pair<TimeType, TimeType>> intervals = {
+      {2, 9}, {1, 3}, {4, 7}, {5, 9}, {0, 10}};
+
+  auto cit = CenteredIntervalTree<TimeType>(intervals);
+
+  std::cout << cit.toString() << std::endl;
+
+  auto overlaps = cit.getIntervalsContainingPoint(5);
+
+  std::cout << "Intervals containing 5: " << std::to_string(overlaps.size())
+            << std::endl;
+  TORCH_CHECK(overlaps.size() == 1);
 }
 
 } // namespace nvfuser
