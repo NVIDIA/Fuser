13a0a772ad87b53b7b81ebd3c51a6dfa6c0b2f70
Jacob Hinkle <jhinkle@nvidia.com>
2023-07-28 12:25:46 -0400

    Add test file

---
+++ test/test_allocation_reuse.cpp
@@ -0,0 +1,27 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+#include <gmock/gmock-matchers.h>
+#include <gtest/gtest.h>
+
+#include <device_lower/pass/alias_memory.h>
+
+#include <test/utils.h>
+#include <test/validator.h>
+
+#include <torch/torch.h>
+
+namespace nvfuser {
+
+class AllocationReuse : public NVFuserTest {};
+
+// Simple exercises for CenteredIntervalTree
+TEST_F(AllocationReuse, CenteredIntervalTree) {
+  //CenteredIntervalTree cit;
+}
+
+} // namespace nvfuser
