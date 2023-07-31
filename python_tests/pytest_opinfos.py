8dc035ac2392268ae97b72cb070d6f96f0eecb08
Ryan Spring <rdspring1@gmail.com>
2023-07-30 21:26:02 -0700

    lint

--- python_tests/pytest_opinfos.py
+++ python_tests/pytest_opinfos.py
@@ -182,7 +182,10 @@ elementwise_unary_ops.append(erfc_opinfo)
 erfcinv_opinfo = OpInfo(
     lambda fd: fd.ops.erfcinv,
     "erfcinv",
-    dtypes=(torch.float32, torch.float64,),
+    dtypes=(
+        torch.float32,
+        torch.float64,
+    ),
     domain=Domain(0.3, 0.7),
     sample_input_generator=elementwise_unary_generator,
     reference=_elementwise_unary_torch(lambda x: torch.erfinv(1 - x)),
