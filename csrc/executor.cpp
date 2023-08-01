c64d05077da5cae5279c9359de3c17320544be07
Gao, Xiang <qasdfgtyuiop@gmail.com>
2023-07-31 18:23:04 -0700

    refactor `getKernelArgument` and `getTensorArgBuffer` (#662)
    1. `getTensorArgBuffer` is renamed as `polymorphicValueToBytes`. In the
    past, it was specifically to convert tensor metadata to byte buffer. Now
    it is used to convert all polymorphic values to byte buffers. This will
    allow us to support using arbitrary `PolymorphicValue` as kernel
    parameter.
    2. `getKernelArgument` is refactored to use `polymorphicValueToBytes`

--- csrc/executor.cpp
+++ csrc/executor.cpp
@@ -1835,12 +1835,17 @@ float FusionExecutor::runRtc(
   std::vector<void*> pointers;
 
   for (const auto& input : args) {
+    DataType metadata_type = globalTensorMetaData(
+        aten_to_data_type(input.scalar_type()), input.dim());
+
     Struct<PolymorphicValue> concrete_value;
     concrete_value["data"] = PolymorphicValue(
         Pointer(input.data_ptr(), aten_to_data_type(input.scalar_type())));
     concrete_value["logical_size"] = PolymorphicValue(input.sizes().vec());
     concrete_value["alloc_stride"] = PolymorphicValue(input.strides().vec());
-    data.emplace_back(getTensorArgBuffer(concrete_value, index_type));
+
+    data.emplace_back(
+        polymorphicValueToBytes(concrete_value, metadata_type, index_type));
     pointers.emplace_back(data.back().data());
   }
 
