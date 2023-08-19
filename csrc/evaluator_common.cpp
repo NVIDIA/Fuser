4f917bb763a60becb884de07687e623579a4e70b
Xiang Gao <qasdfgtyuiop@gmail.com>
2023-08-18 17:40:21 -0700

    do not bind tensor in PrecomputedValues

--- csrc/evaluator_common.cpp
+++ csrc/evaluator_common.cpp
@@ -165,12 +165,13 @@ void PrecomputedValues::bindInputs(const KernelArgumentHolder& args) {
   for (const auto i : c10::irange((int64_t)inputs.size())) {
     const auto input = inputs[i];
     TORCH_INTERNAL_ASSERT(input != nullptr);
-    bindValue(input->evaluatorIndex(), *args[i]);
     if (auto tensor_input = dynamic_cast<TensorView*>(input)) {
       const auto& tensor = args[i]->as<at::Tensor>();
       if (!tensor.is_cpu()) {
         bindTensorMetaData(tensor_input, tensor);
       }
+    } else {
+      bindValue(input->evaluatorIndex(), *args[i]);
     }
   }
 }
