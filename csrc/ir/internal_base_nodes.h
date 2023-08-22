a35da65d2c4c94c399188258a9860af8fb31100f
jjsjann123 <jiej@nvidia.com>
2023-08-22 15:48:49 -0700

    adding constructor TensorDomain::TensorDomain(const TensorDomain*)

--- csrc/ir/internal_base_nodes.h
+++ csrc/ir/internal_base_nodes.h
@@ -456,6 +456,8 @@ class TORCH_CUDA_CU_API TensorDomain : public Val {
 
   TensorDomain(const TensorDomain* src, IrCloner* ir_cloner);
 
+  TensorDomain(const TensorDomain* src);
+
   NVFUSER_DECLARE_CLONE
 
   bool operator==(const TensorDomain& other) const;
