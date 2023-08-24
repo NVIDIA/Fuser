fa04740819aed58bdce7f44fc485d3efcf812035
Jacob Hinkle <jhinkle@nvidia.com>
2023-08-24 09:56:37 -0400

    Update comment on AllocationInfoMap

--- csrc/device_lower/pass/alias_memory.cpp
+++ csrc/device_lower/pass/alias_memory.cpp
@@ -607,7 +607,9 @@ class AllocationAliasModifier;
 //!       T1 = ...            <--  Inner Live Interval of T1 begin
 //!       T2 = ...
 //!       T3 = T1 + ...    <-- Inner Live Interval of T1 end
-//!       T5 = T3 + ...    <-------  Outer Live Interval of T1 end
+//!       T5 = T3 + ...
+//!     EndFor Idx2 ...
+//!   EndFor Idx1 ... <-------  Outer Live Interval of T1 end
 //!
 //!   Alloc(T4, register)
 //!   For Idx3 ...
@@ -618,7 +620,10 @@ class AllocationAliasModifier;
 //!  write and last read of the buffer. Outer interval marks the beginning of
 //!  the loop of first write and end of the loop of last read, at the same loop
 //!  level as the buffer allocation. Note that the end of a ForLoop is marked by
-//!  the last expression within it.
+//!  the last expression within it. In the case of an outer live interval, if
+//!  the end point is the end of a for loop, it is given a position at which
+//!  that expression would reside, but no actual `Expr` is associated with that
+//!  position.
 class AllocationInfoMap : private kir::IrVisitor {
  public:
   // Alias local memory if it exceeds this threshold
