23f0d1256c2c47ee3683bce944b73362cb05c690
Jacob Hinkle <jhinkle@nvidia.com>
2023-08-25 11:03:27 -0400

    Silence shellcheck

--- tools/compare_codegen.sh
+++ tools/compare_codegen.sh
@@ -13,7 +13,7 @@ then
 fi
 
 comparetoref=${1:-origin/main}
-comparecommit=$(git describe --always --long $comparetoref)
+comparecommit=$(git describe --always --long "$comparetoref")
 outdir=${2:-codegen_comparison}
 
 if [[ $(git status --porcelain --untracked-files=no) ]]
@@ -28,21 +28,21 @@ origcommit=$currentcommit
 orighead=$(git symbolic-ref --short HEAD)
 
 movecudafiles() {
-    find . -maxdepth 1 -name '__tmp_kernel*.cu' -exec mv '{}' $1 \;
+    find . -maxdepth 1 -name '__tmp_kernel*.cu' -exec mv '{}' "$1" \;
 }
 
 cleanup() {
     numkernels=$(find . -maxdepth 1 -name '__tmp_kernel*.cu' | wc -l)
 
-    if (( $numkernels > 0 ))
+    if (( numkernels > 0 ))
     then
         backupdir=$outdir/${currentcommit}-interrupted
         echo "Interrupted. Backing up $numkernels .cu files to $backupdir"
-        mkdir -p $backupdir
-        movecudafiles $backupdir
+        mkdir -p "$backupdir"
+        movecudafiles "$backupdir"
     fi
 
-    git switch $orighead
+    git switch "$orighead"
 }
 
 trap "cleanup" INT TERM EXIT
@@ -51,14 +51,14 @@ collect_kernels() {
     outdir=$1
     commit=$2
 
-    git checkout $commit
+    git checkout "$commit"
     currentcommit=$commit
 
     testdir=$outdir/$commit/test
     benchdir=$outdir/$commit/bench
 
     # build in release mode so that everything runs a bit faster
-    if [[ -d $testdir/cuda && -d $benchdir/cuda ]]
+    if [[ -d "$testdir/cuda" && -d "$benchdir/cuda" ]]
     then
         # Test for output directories and return early if they exist. This
         # avoids rebuilds when we are changing code and comparing repeatedly to
@@ -74,44 +74,44 @@ collect_kernels() {
     # run tests and benchmarks with cuda_to_file and dump output to files
     export NVFUSER_DUMP=cuda_to_file
 
-    mkdir -p $outdir/$commit
+    mkdir -p "$outdir/$commit"
 
-    if [[ ! -d $testdir/cuda ]]
+    if [[ ! -d "$testdir/cuda" ]]
     then
-        mkdir -p $testdir
+        mkdir -p "$testdir"
         build/nvfuser_tests --gtest_color=yes | \
-            tee $testdir/stdout-$(date +%Y%m%d_%H%M%S).log
-        mkdir -p $testdir/cuda
-        movecudafiles $testdir/cuda
+            tee "$testdir/stdout-$(date +%Y%m%d_%H%M%S).log"
+        mkdir -p "$testdir/cuda"
+        movecudafiles "$testdir/cuda"
     else
         echo "Skipping tests since $testdir/cuda exists"
     fi
 
-    if [[ ! -d $benchdir/cuda ]]
+    if [[ ! -d "$benchdir/cuda" ]]
     then
-        mkdir -p $benchdir
+        mkdir -p "$benchdir"
         build/nvfuser_bench \
             --benchmark_repetitions=1 \
             --benchmark_min_time=0 \
             --benchmark_enable_random_interleaving=false \
             --benchmark_color=true \
-         | tee $benchdir/stdout-$(date +%Y%m%d_%H%M%S).log
-        mkdir -p $benchdir/cuda
-        movecudafiles $benchdir/cuda
+         | tee "$benchdir/stdout-$(date +%Y%m%d_%H%M%S).log"
+        mkdir -p "$benchdir/cuda"
+        movecudafiles "$benchdir/cuda"
     else
         echo "Skipping benchmarks since $benchdir/cuda exists"
     fi
 }
 
-collect_kernels $outdir $origcommit
-collect_kernels $outdir $comparecommit
+collect_kernels "$outdir" "$origcommit"
+collect_kernels "$outdir" "$comparecommit"
 
 cleanup
 
 # Print mismatching files. Note that logs are expected to differ since timings are included
-diffs=$(diff -qr -x '*.log' $outdir/$origcommit $outdir/$comparecommit)
-echo $diffs
+diffs=$(diff -qr -x '*.log' "$outdir/$origcommit" "$outdir/$comparecommit")
+echo "$diffs"
 
 # Return number of mismatched cuda files. Success=0
 num_mismatches=$(echo "$diffs" | wc -l)
-exit $num_mismatches
+exit "$num_mismatches"
