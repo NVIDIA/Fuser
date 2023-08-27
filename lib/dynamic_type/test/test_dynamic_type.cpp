2b0bcb9fe85467682cc830cecc14225e996daabb
Xiang Gao <qasdfgtyuiop@gmail.com>
2023-08-27 14:45:46 -0700

    print

--- lib/dynamic_type/test/test_dynamic_type.cpp
+++ lib/dynamic_type/test/test_dynamic_type.cpp
@@ -34,28 +34,6 @@ namespace dynamic_type {
 #pragma clang diagnostic ignored "-Wunused-lambda-capture"
 #endif
 
-TEST_F(DynamicTypeTest, Printing) {
-  std::stringstream ss;
-  ss << DoubleInt64Bool(299792458L) << ", " << DoubleInt64Bool(3.14159) << ", "
-     << DoubleInt64Bool(true);
-  EXPECT_EQ(ss.str(), "299792458, 3.14159, 1");
-
-  std::stringstream ss2;
-  static_assert(opcheck<std::stringstream&> << opcheck<IntSomeType>);
-  ss2 << IntSomeType(299792458);
-  EXPECT_EQ(ss2.str(), "299792458");
-
-  EXPECT_THAT(
-      [&]() { ss << IntSomeType(); },
-      ::testing::ThrowsMessage<std::runtime_error>(
-          ::testing::HasSubstr("Can not print")));
-  EXPECT_THAT(
-      [&]() { ss << IntSomeType(SomeType{}); },
-      ::testing::ThrowsMessage<std::runtime_error>(
-          ::testing::HasSubstr("Can not print")));
-  static_assert(!(opcheck<std::stringstream&> << opcheck<SomeTypes>));
-}
-
 #if defined(__clang__)
 #pragma clang diagnostic pop
 #endif
