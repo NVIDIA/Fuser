10a5d0eca3b31cb7f8a60d0643bf96f8b7741de8
Xiang Gao <qasdfgtyuiop@gmail.com>
2023-08-27 14:38:07 -0700

    unary ops

--- lib/dynamic_type/test/test_dynamic_type.cpp
+++ lib/dynamic_type/test/test_dynamic_type.cpp
@@ -34,54 +34,6 @@ namespace dynamic_type {
 #pragma clang diagnostic ignored "-Wunused-lambda-capture"
 #endif
 
-#define TEST_UNARY_OP(name, op, int_or_bool)                                  \
-  TEST_F(DynamicTypeTest, name) {                                             \
-    static_assert(op opcheck<DoubleInt64Bool>);                               \
-    static_assert(op opcheck<DoubleInt64BoolVec>);                            \
-    static_assert((op DoubleInt64Bool(2L)).as<decltype(op 2L)>() == (op 2L)); \
-    EXPECT_EQ((op DoubleInt64BoolVec(2L)).as<decltype(op 2L)>(), (op 2L));    \
-    EXPECT_THAT(                                                              \
-        [&]() { op DoubleInt64Bool(); },                                      \
-        ::testing::ThrowsMessage<std::runtime_error>(                         \
-            ::testing::HasSubstr("Cannot compute ")));                        \
-    EXPECT_THAT(                                                              \
-        [&]() { op DoubleInt64BoolVec(std::vector<DoubleInt64BoolVec>{}); },  \
-        ::testing::ThrowsMessage<std::runtime_error>(                         \
-            ::testing::HasSubstr("Cannot compute ")));                        \
-    static_assert(op opcheck<int_or_bool##SomeType>);                         \
-    static_assert(!(op opcheck<SomeTypes>));                                  \
-    EXPECT_THAT(                                                              \
-        [&]() { op int_or_bool##SomeType(SomeType{}); },                      \
-        ::testing::ThrowsMessage<std::runtime_error>(                         \
-            ::testing::HasSubstr("Cannot compute ")));                        \
-  }
-
-TEST_UNARY_OP(Positive, +, Int);
-TEST_UNARY_OP(Negative, -, Int);
-TEST_UNARY_OP(BinaryNot, ~, Int);
-TEST_UNARY_OP(LogicalNot, !, Bool);
-
-#undef TEST_UNARY_OP
-
-TEST_F(DynamicTypeTest, UnaryOpAdvancedTyping) {
-  struct Type1 {};
-  struct Type2 {
-    Type1 operator+() const {
-      return Type1{};
-    }
-  };
-  // not defined compile time because +Type2 is not in type list
-  static_assert(!(+opcheck<DynamicType<NoContainers, Type2, SomeType>>));
-  // defined compile time because +int is in type list
-  static_assert(+opcheck<DynamicType<NoContainers, Type2, int>>);
-  // runtime error because +Type2 is not in type list
-  auto bad = [&]() { +DynamicType<NoContainers, Type2, int>(Type2{}); };
-  EXPECT_THAT(
-      bad,
-      ::testing::ThrowsMessage<std::runtime_error>(
-          ::testing::HasSubstr("Cannot compute ")));
-}
-
 TEST_F(DynamicTypeTest, Printing) {
   std::stringstream ss;
   ss << DoubleInt64Bool(299792458L) << ", " << DoubleInt64Bool(3.14159) << ", "
