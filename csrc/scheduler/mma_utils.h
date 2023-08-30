0a29cc6f30a21a440279c054d6d32a0dc25f7b1d
Jacob Hinkle <jhinkle@nvidia.com>
2023-08-30 07:42:40 -0400

    Reformat comment

--- csrc/scheduler/mma_utils.h
+++ csrc/scheduler/mma_utils.h
@@ -294,18 +294,18 @@ TORCH_CUDA_CU_API ProblemIterDomainsOpt getProblemIterDomains(Fusion* fusion);
 TORCH_CUDA_CU_API RolesMapOpt getTensorsRoles(Fusion* fusion);
 
 //! Return pair of whether use shared memory epilogue or not and whether to
-//! reuse shared memory for the prologue at the expense of an additional block
-//! sync.
-//!  Returns true in first position if using shared memory epilogue won't cause
-//!  the decrease of occupancy ratio. The occupancy ratio is
-//!  estimated using register and shared memory usage.
-//!  If ignore_occupancy_drop is set to true, returns true if
-//!  there is enough shared memory to launch the kernel without
-//!  considering the occupancy, useful for debug and validate
-//!  shared memory epilogue implementation.
+//!  reuse shared memory for the prologue at the expense of an additional block
+//!  sync.
+//!
+//! Returns true in first position if using shared memory epilogue won't cause
+//!  the decrease of occupancy ratio. The occupancy ratio is estimated using
+//!  register and shared memory usage.  If ignore_occupancy_drop is set to true,
+//!  returns true if there is enough shared memory to launch the kernel without
+//!  considering the occupancy, useful for debug and validate shared memory
+//!  epilogue implementation.
 //!
 //! Returns true in the second position if reusing shared memory for the
-//! epilogue does not increase occupancy.
+//!  epilogue does not increase occupancy.
 TORCH_CUDA_CU_API std::pair<bool, bool> generateSharedMemoryEpilogueHeuristics(
     const MatMulTileOptions& gemm_tile,
     const int smem_double_buffer_stage,
