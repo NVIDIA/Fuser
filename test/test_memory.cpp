c923306137a9cdc7564489ddede0b01f9a6f04e6
Xiang Gao <qasdfgtyuiop@gmail.com>
2023-09-01 15:18:24 -0700

    test cleanup

--- test/test_memory.cpp
+++ test/test_memory.cpp
@@ -17,21 +17,6 @@ class TMATest : public NVFuserTest {
   // TODO: assert hopper
 };
 
-// Tuning guide:
-// https://docs.nvidia.com/cuda/hopper-tuning-guide/index.html#tensor-memory-accelerator
-
-// TODO:
-// Example usage of TMA
-// https://github.com/NVIDIA/cutlass/blob/87349d349605c1e24366fcbe8f04d0141dcb617b/include/cutlass/gemm/collective/sm90_mma_tma_gmma_rs_warpspecialized.hpp#L311-L414
-// https://github.com/NVIDIA/cutlass/blob/87349d349605c1e24366fcbe8f04d0141dcb617b/include/cutlass/epilogue/collective/sm90_epilogue_tma_warpspecialized.hpp#L516-L545
-// https://github.com/NVIDIA/cutlass/blob/87349d349605c1e24366fcbe8f04d0141dcb617b/include/cutlass/gemm/collective/sm90_mma_tma_gmma_ss.hpp
-
-// TODO: smem fence?
-
-// https://developer.nvidia.com/blog/controlling-data-movement-to-boost-performance-on-ampere-architecture/
-
-// https://github.com/NVIDIA/cutlass/blob/main/include/cutlass/gemm/kernel/sm90_gemm_tma.hpp
-
 TEST_F(TMATest, Store1D_CUDA) {
   Fusion fusion;
   FusionGuard fg(&fusion);
@@ -47,15 +32,10 @@ TEST_F(TMATest, Store1D_CUDA) {
   tv3->definition()->as<LoadStoreOp>()->setOpType(
       LoadStoreOpType::CpAsyncBulkTensorTile);
 
-  for (auto tv : {tv1, tv2, tv3}) {
-    tv->split(0, 32);
-    tv->axis(0)->parallelize(ParallelType::BIDx);
-  }
-  tv3->axis(1)->parallelize(ParallelType::Bulk);
-  // tv2->axis(1)->parallelize(ParallelType::Vectorize);
+  tv3->axis(0)->parallelize(ParallelType::Bulk);
 
   auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
-  auto t0 = at::randn({1024}, options);
+  auto t0 = at::randn({32}, options);
   FusionExecutor fe;
   fe.compileFusion(&fusion, {t0}, {}, {DataType::Int32});
   auto cg_outputs = fe.runFusion({t0});
@@ -77,18 +57,11 @@ TEST_F(TMATest, Store2D_CUDA) {
   tv3->definition()->as<LoadStoreOp>()->setOpType(
       LoadStoreOpType::CpAsyncBulkTensorTile);
 
-  for (auto tv : {tv1, tv2, tv3}) {
-    tv->split(1, 4);
-    tv->axis(1)->parallelize(ParallelType::BIDx);
-    tv->split(0, 4);
-    tv->axis(0)->parallelize(ParallelType::BIDy);
-  }
+  tv3->axis(0)->parallelize(ParallelType::Bulk);
   tv3->axis(1)->parallelize(ParallelType::Bulk);
-  tv3->axis(3)->parallelize(ParallelType::Bulk);
-  tv2->axis(3)->parallelize(ParallelType::Vectorize);
 
   auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
-  auto t0 = at::randn({32, 32}, options);
+  auto t0 = at::randn({4, 4}, options);
   FusionExecutor fe;
   fe.compileFusion(&fusion, {t0}, {}, {DataType::Int32});
   auto cg_outputs = fe.runFusion({t0});
@@ -110,21 +83,12 @@ TEST_F(TMATest, Store3D_CUDA) {
   tv3->definition()->as<LoadStoreOp>()->setOpType(
       LoadStoreOpType::CpAsyncBulkTensorTile);
 
-  for (auto tv : {tv1, tv2, tv3}) {
-    tv->split(2, 4);
-    tv->axis(2)->parallelize(ParallelType::BIDx);
-    tv->split(1, 4);
-    tv->axis(1)->parallelize(ParallelType::BIDy);
-    tv->split(0, 4);
-    tv->axis(0)->parallelize(ParallelType::BIDz);
-  }
+  tv3->axis(0)->parallelize(ParallelType::Bulk);
   tv3->axis(1)->parallelize(ParallelType::Bulk);
-  tv3->axis(3)->parallelize(ParallelType::Bulk);
-  tv3->axis(5)->parallelize(ParallelType::Bulk);
-  tv2->axis(5)->parallelize(ParallelType::Vectorize);
+  tv3->axis(2)->parallelize(ParallelType::Bulk);
 
   auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
-  auto t0 = at::randn({16, 16, 16}, options);
+  auto t0 = at::randn({4, 4, 4}, options);
   FusionExecutor fe;
   fe.compileFusion(&fusion, {t0}, {}, {DataType::Int32});
   auto cg_outputs = fe.runFusion({t0});
@@ -146,24 +110,13 @@ TEST_F(TMATest, Store4D_CUDA) {
   tv3->definition()->as<LoadStoreOp>()->setOpType(
       LoadStoreOpType::CpAsyncBulkTensorTile);
 
-  for (auto tv : {tv1, tv2, tv3}) {
-    tv->split(3, 4);
-    tv->axis(3)->parallelize(ParallelType::TIDx);
-    tv->split(2, 4);
-    tv->axis(2)->parallelize(ParallelType::BIDx);
-    tv->split(1, 4);
-    tv->axis(1)->parallelize(ParallelType::BIDy);
-    tv->split(0, 4);
-    tv->axis(0)->parallelize(ParallelType::BIDz);
-  }
+  tv3->axis(0)->parallelize(ParallelType::Bulk);
   tv3->axis(1)->parallelize(ParallelType::Bulk);
+  tv3->axis(2)->parallelize(ParallelType::Bulk);
   tv3->axis(3)->parallelize(ParallelType::Bulk);
-  tv3->axis(5)->parallelize(ParallelType::Bulk);
-  tv3->axis(7)->parallelize(ParallelType::Bulk);
-  tv2->axis(7)->parallelize(ParallelType::Vectorize);
 
   auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
-  auto t0 = at::randn({16, 16, 16, 16}, options);
+  auto t0 = at::randn({4, 4, 4, 4}, options);
   FusionExecutor fe;
   fe.compileFusion(&fusion, {t0}, {}, {DataType::Int32});
   auto cg_outputs = fe.runFusion({t0});
@@ -185,27 +138,14 @@ TEST_F(TMATest, Store5D_CUDA) {
   tv3->definition()->as<LoadStoreOp>()->setOpType(
       LoadStoreOpType::CpAsyncBulkTensorTile);
 
-  for (auto tv : {tv1, tv2, tv3}) {
-    tv->split(4, 4);
-    tv->axis(4)->parallelize(ParallelType::TIDx);
-    tv->split(3, 4);
-    tv->axis(3)->parallelize(ParallelType::TIDy);
-    tv->split(2, 4);
-    tv->axis(2)->parallelize(ParallelType::BIDx);
-    tv->split(1, 4);
-    tv->axis(1)->parallelize(ParallelType::BIDy);
-    tv->split(0, 4);
-    tv->axis(0)->parallelize(ParallelType::BIDz);
-  }
+  tv3->axis(0)->parallelize(ParallelType::Bulk);
   tv3->axis(1)->parallelize(ParallelType::Bulk);
+  tv3->axis(2)->parallelize(ParallelType::Bulk);
   tv3->axis(3)->parallelize(ParallelType::Bulk);
-  tv3->axis(5)->parallelize(ParallelType::Bulk);
-  tv3->axis(7)->parallelize(ParallelType::Bulk);
-  tv3->axis(9)->parallelize(ParallelType::Bulk);
-  tv2->axis(9)->parallelize(ParallelType::Vectorize);
+  tv3->axis(4)->parallelize(ParallelType::Bulk);
 
   auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
-  auto t0 = at::randn({16, 16, 16, 16, 16}, options);
+  auto t0 = at::randn({4, 4, 4, 4, 4}, options);
   FusionExecutor fe;
   fe.compileFusion(&fusion, {t0}, {}, {DataType::Int32});
   auto cg_outputs = fe.runFusion({t0});
