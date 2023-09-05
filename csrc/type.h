4d69a8facf25efb23b3183d6f786836e604a91c6
Xiang Gao <qasdfgtyuiop@gmail.com>
2023-09-04 20:48:27 -0700

    Support empty array

--- csrc/type.h
+++ csrc/type.h
@@ -488,6 +488,14 @@ inline bool hasCompatibleDataType(
     }
     auto ptr = std::get<PointerType>(dtype.type);
     return dataTypeSize(*ptr.type) == value.as<Pointer>().size();
+  } else if (std::holds_alternative<ArrayType>(dtype.type)) {
+    if (!value.is<std::vector>()) {
+      return false;
+    }
+    if (std::get<ArrayType>(dtype.type).size == 0 &&
+        value.as<std::vector>().empty()) {
+      return true;
+    }
   }
   return isCompatibleDataType(getDataType(value), dtype);
 }
@@ -954,7 +962,6 @@ inline PolymorphicValue castToDtype(
   // Cast the given value to the given data type. This enables interface
   // like: IrBuilder::create<Val>(0, DataType::Double) where value is
   // an integer but the desired data type is double.
-  auto value_dtype = getDataType(value);
   if (!hasCompatibleDataType(value, dtype)) {
     PolymorphicValue::for_all_types([&](auto _) {
       using T = typename decltype(_)::type;
