79d5f958f92f574260794f38922558a5257875f4
Jingyue Wu <wujingyue@gmail.com>
2023-09-08 17:52:02 -0700

    LoadStoreOp takes a CacheOp. Second try.
    This reverts commit b3edc818037a48624caa94d096ef3ed903bbe5bc.

---
+++ test/test_memory.cpp
@@ -0,0 +1,74 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+#include <gmock/gmock-matchers.h>
+#include <gtest/gtest.h>
+
+#include <regex>
+
+#include <fusion.h>
+#include <inlining.h>
+#include <ir/utils.h>
+#include <ops/alias.h>
+#include <ops/arith.h>
+#include <ops/utils.h>
+#include <test/utils.h>
+#include <test/validator.h>
+#include <type.h>
+
+namespace nvfuser {
+
+class MemoryTest : public NVFuserTest {};
+
+TEST_F(MemoryTest, LoadCache) {
+  Fusion fusion;
+  FusionGuard fg(&fusion);
+
+  TensorView* tv0 = makeContigTensor(1);
+  fusion.addInput(tv0);
+  TensorView* tv1 =
+      ops::newValLike(tv0, tv0->getDataType().value())->as<TensorView>();
+  IrBuilder::create<LoadStoreOp>(
+      LoadStoreOpType::Set, tv1, tv0, CacheOp::AllLevels);
+  TensorView* tv2 = add(tv1, IrBuilder::create<Val>(1.0));
+  TensorView* tv3 = set(tv2);
+  fusion.addOutput(tv3);
+
+  tv1->split(0, 4);
+  tv1->split(0, 32);
+  TransformPropagatorWithCheck propagator(tv1);
+  MaxRootDomainInfoSpanningTree(tv1).traverse(&propagator);
+
+  // Parallelize LoadStoreOps. Other TensorViews don't support vectorization.
+  tv1->axis(0)->parallelize(ParallelType::BIDx);
+  tv1->axis(1)->parallelize(ParallelType::TIDx);
+  tv1->axis(2)->parallelize(ParallelType::Vectorize);
+  scheduler_utils::parallelizeAllLike(tv1, {tv3});
+
+  inlineMost();
+
+  at::Tensor input = at::randn(
+      {1024}, at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0));
+  at::Tensor expected_output = input + 1.0f;
+
+  FusionExecutor fe;
+  fe.setSaveCompiledBinaryFlag(true);
+  fe.compileFusion(&fusion, {input});
+  std::vector<char> compiled_ptx = fe.compiledPtx();
+  std::string ptx(compiled_ptx.begin(), compiled_ptx.end());
+
+  std::regex regex(R"(ld\.global\.ca\.\S+)");
+  std::smatch match;
+  std::regex_search(ptx, match, regex);
+  EXPECT_EQ(match.size(), 1);
+
+  std::vector<at::Tensor> actual_ts = fe.runFusion({input});
+  testValidate(
+      &fusion, actual_ts, {input}, {expected_output}, __LINE__, __FILE__);
+}
+
+} // namespace nvfuser
