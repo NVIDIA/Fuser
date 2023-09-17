e42a8544257e6b83debcf281470810bfdb3871c6
Liqiang Lu <liqiangxl@gmail.com>
2023-09-17 06:08:19 -0700

    remove not required head files

--- csrc/scheduler/normalization_outer.cpp
+++ csrc/scheduler/normalization_outer.cpp
@@ -6,25 +6,12 @@
  */
 // clang-format on
 #include <ATen/cuda/CUDAContext.h>
-#include <c10/util/irange.h>
-#include <disjoint_set.h>
-#include <executor_utils.h>
-#include <expr_evaluator.h>
 #include <instrumentation.h>
-#include <ir/iostream.h>
-#include <ir/utils.h>
-#include <root_domain_map.h>
 #include <scheduler/debug_utils.h>
-#include <scheduler/matmul_utils.h>
 #include <scheduler/normalization_outer.h>
 #include <scheduler/normalization_utils.h>
-#include <scheduler/pointwise.h>
 #include <scheduler/reduction_utils.h>
-#include <scheduler/registry_utils.h>
-#include <scheduler/transpose.h>
 #include <scheduler/utils.h>
-#include <tensor_metadata.h>
-#include <limits>
 
 namespace nvfuser {
 
