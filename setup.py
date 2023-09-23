5169d129cea2dfe4cca1314cec885f2170b0b25b
Jingyue Wu <wujingyue@gmail.com>
2023-09-23 11:18:05 -0700

    Add an option to build with asan. (#933)
    Tested:
    
    ```
    $ pip install -e .
    $ nm -an bin/nvfuser_tests | grep asan
    <empty>
    $ python setup.py develop --build-with-asan
    $ nm -an bin/nvfuser_tests | grep asan
    ...
    00000000024f3840 B __odr_asan._ZN7nvfuser38NVFuserTest_FusionDefinition_CUDA_Test10test_info_E
    00000000024f38e0 B __odr_asan._ZN7nvfuser35NVFuserTest_PyFusionCache_CUDA_Test10test_info_E
    00000000024f3980 B __odr_asan._ZN7nvfuser43NVFuserTest_RecordFunctorEquality_CUDA_Test10test_info_E
    0000000002528fa0 B __odr_asan._ZN5torch8autograd47_GLOBAL__N__551c3bf8_14_rng_kernels_cu_8fdfe6fb13graph_task_idE
    ```

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ NO_TEST = False
 NO_BENCHMARK = False
 NO_NINJA = False
 BUILD_WITH_UCC = False
+BUILD_WITH_ASAN = False
 PATCH_NVFUSER = True
 OVERWRITE_VERSION = False
 VERSION_TAG = None
@@ -86,6 +87,9 @@ for i, arg in enumerate(sys.argv):
         continue
     if arg == "--build-with-ucc":
         BUILD_WITH_UCC = True
+        continue
+    if arg == "--build-with-asan":
+        BUILD_WITH_ASAN = True
         continue
     if arg == "--debug":
         BUILD_TYPE = "Debug"
@@ -296,6 +300,8 @@ def cmake(build_dir: str = "", install_prefix: str = "./nvfuser"):
         cmd_str.append(f"-DPython_EXECUTABLE={sys.executable}")
     if not NO_BENCHMARK:
         cmd_str.append("-DBUILD_NVFUSER_BENCHMARK=ON")
+    if BUILD_WITH_ASAN:
+        cmd_str.append("-DNVFUSER_BUILD_WITH_ASAN=ON")
     cmd_str.append(".")
 
     print(f"Configuring CMake with {' '.join(cmd_str)}")
