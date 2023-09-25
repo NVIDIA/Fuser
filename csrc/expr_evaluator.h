223982b8ad72b0e3285767e1a97504a7e8567aba
Priya Mishra <52657555+Priya2698@users.noreply.github.com>
2023-09-25 09:05:37 -0700

    Add a `const evaluate` to `ExpressionEvaluator` (#881)

--- csrc/expr_evaluator.h
+++ csrc/expr_evaluator.h
@@ -56,6 +56,9 @@ class ExpressionEvaluator {
   //! Try to evaluate a parallel dimension
   const PolymorphicValue& evaluate(ParallelType pt);
 
+  //! Try to evaluate a known value using const evaluator ref
+  PolymorphicValue evaluate(const Val* value) const;
+
   //! Debugging helper, prints all the currently known values
   void print() const;
 
@@ -77,7 +80,13 @@ class ExpressionEvaluator {
   ExpressionEvaluator clone(IrCloner& ir_cloner) const;
 
  private:
-  const PolymorphicValue& getValue(const Val* value);
+  const PolymorphicValue& getValue(
+      const Val* value,
+      const std::unordered_map<const Val*, PolymorphicValue>&
+          additional_known_values) const;
+  const PolymorphicValue& evaluateHelper(
+      const Val* value,
+      std::unordered_map<const Val*, PolymorphicValue>& known_values) const;
 
  private:
   // TODO: Consider make this const. It can't be const as bind() of
