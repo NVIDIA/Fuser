5285338093ddf2fd34541a24c0b1bd6283531f14
root <root@nvdl-a112-d001.nvidia.com>
2023-09-30 17:00:09 -0700

    Start to adding CUPTI kernel timing.

---
+++ csrc/profiler.h
@@ -0,0 +1,23 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+
+namespace nvfuser {
+
+struct FusionProfile {
+
+};
+struct SegmentProfile {
+
+};
+
+// Singleton
+class Profiler {
+
+};
+
+} // namespace nvfuser
