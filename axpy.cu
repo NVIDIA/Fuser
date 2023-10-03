65a8354f32ce38ce1b37c1cbdb8c4b4f8f11af43
Jingyue Wu <wujingyue@gmail.com>
2023-10-03 15:27:14 -0700

    Do not flush L2 cache ourselves; rely on ncu.

--- axpy.cu
+++ axpy.cu
@@ -32,42 +32,6 @@ __global__ void Memzero(float* x) {
   __stwt(out, 0.0f);
 }
 
-class L2CacheFlusher {
- public:
-  L2CacheFlusher() {
-    int device_id;
-    CHECK_CUDA_ERROR(cudaGetDevice(&device_id));
-
-    CHECK_CUDA_ERROR(cudaDeviceGetAttribute(
-        &l2_cache_size_, cudaDevAttrL2CacheSize, device_id));
-    std::cerr << "L2 cache size = " << l2_cache_size_ << std::endl;
-    if (l2_cache_size_ <= 0) {
-      std::cerr << "The L2 cache size is expected to be positive. Got "
-                << l2_cache_size_ << std::endl;
-      abort();
-    }
-
-    CHECK_CUDA_ERROR(cudaMalloc(&buffer_, l2_cache_size_));
-  }
-
-  void Flush(cudaStream_t stream) {
-    constexpr int kWarpSize = 32;
-    const int num_elements = l2_cache_size_ / sizeof(float);
-    Memzero<<<num_elements / kWarpSize, kWarpSize, 0, stream>>>(
-        reinterpret_cast<float*>(buffer_));
-  }
-
-  ~L2CacheFlusher() {
-    if (buffer_ != nullptr) {
-      CHECK_CUDA_ERROR(cudaFree(buffer_));
-    }
-  }
-
- private:
-  void* buffer_ = nullptr;
-  int l2_cache_size_ = 0;
-};
-
 enum class CacheOp {
   Global,
   Streaming,
@@ -119,8 +83,6 @@ int main(int argc, char* argv[]) {
   }
   std::vector<float> host_y(kSize);
 
-  L2CacheFlusher l2_cache_flusher;
-
   cudaStream_t stream;
   CHECK_CUDA_ERROR(cudaStreamCreate(&stream));
 
@@ -138,13 +100,11 @@ int main(int argc, char* argv[]) {
 
   constexpr int kIterations = 3;
   for (int i = 0; i < kIterations; i++) {
-    l2_cache_flusher.Flush(stream);
     Axpy<CacheOp::Global>
         <<<kRows, kColumns / 4, 0, stream>>>(alpha, device_x, device_y);
   }
   // Switching to __ldcs and __stcs slows the kernel down from 501us to 534us.
   for (int i = 0; i < kIterations; i++) {
-    l2_cache_flusher.Flush(stream);
     Axpy<CacheOp::Streaming>
         <<<kRows, kColumns / 4, 0, stream>>>(alpha, device_x, device_y);
   }
