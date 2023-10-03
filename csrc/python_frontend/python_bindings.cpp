3b1de6a49400e3806c470be4b9387ade93310968
jjsjann123 <jiej@nvidia.com>
2023-10-03 14:34:57 -0700

    fixing direct list init

--- csrc/python_frontend/python_bindings.cpp
+++ csrc/python_frontend/python_bindings.cpp
@@ -191,10 +191,10 @@ std::tuple<std::vector<std::optional<bool>>, std::vector<int64_t>> computeTensor
       sizes.size() == strides.size(),
       "compute_contiguity: Sizes and strides must have the same number of dimensions");
   std::vector<DimInfo> dim_info_vec(sizes.size());
-  for (auto i : c10::irange(sizes.size())) {
+  for (int64_t i : c10::irange(sizes.size())) {
     // NOTE: not supporting negative stride yet.
     NVF_CHECK(strides[i] < 0, "negative stride on tensor is not supported");
-    dim_info_vec.emplace_back({i, sizes[i], strides[i]});
+    dim_info_vec.emplace_back(DimInfo{i, sizes[i], strides[i]});
   }
   // sort by stride
   std::sort(dim_info_vec.begin(), dim_info_vec.end(), [](const auto& l, const auto& r) {
