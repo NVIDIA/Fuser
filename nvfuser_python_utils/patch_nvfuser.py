3aece1f626110173756c6b551789cba20b9cacf4
Jirka <jirka.borovec@seznam.cz>
2023-10-06 19:46:55 +0200

    make patch-nvfuser verbose for debugging

--- nvfuser_python_utils/patch_nvfuser.py
+++ nvfuser_python_utils/patch_nvfuser.py
@@ -1,27 +1,28 @@
 import os
 
+_ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+
 
 def patch_pytorch_nvfuser_binaries(torch_lib):
-    nvfuser_lib = os.path.join(
-        os.path.dirname(os.path.dirname(__file__)), "nvfuser", "lib"
-    )
+    nvfuser_lib = os.path.join(_ROOT_DIR, "nvfuser", "lib")
     import shutil
 
     for f_name in ["libnvfuser_codegen.so"]:
-        shutil.copyfile(
-            os.path.join(nvfuser_lib, f_name),
-            os.path.join(torch_lib, f_name),
-        )
+        src_file = os.path.join(nvfuser_lib, f_name)
+        tgt_file = os.path.join(torch_lib, f_name)
+        print(f"Copy `{src_file}` -> `{tgt_file}`")
+        shutil.copyfile(src_file, tgt_file)
 
 
 def remove_nvfuser_python_module(installed_nvfuser_dir):
     # only remove if installed nvfuser is in a different path
-    if installed_nvfuser_dir != os.path.join(
-        os.path.dirname(os.path.dirname(__file__)), "nvfuser"
-    ):
-        import shutil
+    if installed_nvfuser_dir == os.path.join(_ROOT_DIR, "nvfuser"):
+        print(f"nvFuser [{installed_nvfuser_dir}] is in among installed packages; no action required.")
+        return
+    import shutil
 
-        shutil.rmtree(installed_nvfuser_dir)
+    print(f"Removing installed nvFuser at `{installed_nvfuser_dir}`")
+    shutil.rmtree(installed_nvfuser_dir)
 
 
 def patch_installation():
