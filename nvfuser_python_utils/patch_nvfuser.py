5649221fa77eead1f02da220dae7a5697cbb8f1e
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2023-10-10 16:32:05 -0700

    lint

--- nvfuser_python_utils/patch_nvfuser.py
+++ nvfuser_python_utils/patch_nvfuser.py
@@ -4,7 +4,8 @@ import warnings
 def patch_installation():
     warnings.warn(
         "`patch-nvfuser` is no longere needed after nvfuser==0.1.0. "
-        "This binary will throw fatal error in the next minor release and be removed after that. "
+        "This binary will throw fatal error in the next minor release and be removed after that. ",
+        stacklevel=2,
     )
 
 
