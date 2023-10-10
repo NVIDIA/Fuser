77a26bf3e107f81a485b34f68e8df92390caf570
Liqiang Lu <liqiangxl@gmail.com>
2023-10-10 06:33:32 -0700

    fix

--- runtime/warp.cu
+++ runtime/warp.cu
@@ -36,6 +36,7 @@ template <typename T>
 __device__ __forceinline__ std::complex<T> warp_broadcast(
     unsigned int participating_mask,
     std::complex<T> var,
+    int srcLane,
     int width = 32) {
   T real = __shfl_sync(participating_mask, var.real(), srcLane, width);
   T imag = __shfl_sync(participating_mask, var.imag(), srcLane, width);
@@ -58,6 +59,9 @@ __device__ void warpReduceTIDX(
     bool read_write_pred,
     T init_val) {
   constexpr int WARP_SIZE = 32;
+  // unpadded version only support 1D thread block.
+  assert(blockDim.y == 1);
+  assert(blockDim.z == 1);
 
   T reduce_val = init_val;
 
@@ -71,13 +75,12 @@ __device__ void warpReduceTIDX(
   unsigned int lane_idx = threadIdx.x % WARP_SIZE;
   unsigned int reduction_size = blockDim.x;
   unsigned int num_of_warps = (reduction_size + WARP_SIZE - 1) / WARP_SIZE;
-  bool launch_condition = threadIdx.x < blockDim.x;
   bool is_last_warp = warp_idx == num_of_warps - 1;
   // The mask sets the participating threads, e.g. for a block with 35 threads,
   // the active threads of the last warp is [32, 33, 34], the mask is
   // [00,...,00111].
   const unsigned int mask =
-      is_last_warp ? __ballot_sync(0xffffffff, launch_condition) : 0xffffffff;
+      is_last_warp ? __ballot_sync(0xffffffff, true) : 0xffffffff;
 
   // The shfl offset begins at np2, which represents the greatest power of 2
   // less than valid_lanes. For instance, consider a block of 35 threads: warp_0
@@ -92,25 +95,20 @@ __device__ void warpReduceTIDX(
   const unsigned int valid_lanes =
       is_last_warp ? reduction_size - warp_idx * WARP_SIZE : WARP_SIZE;
   int np2 = valid_lanes > 1 ? 1 << (31 - __clz(valid_lanes - 1)) : 0;
-  if (launch_condition) {
-    for (int offset = np2; offset >= 1; offset /= 2) {
-      T other_val = shfl_xor(mask, reduce_val, offset);
-      bool other_valid = !is_last_warp || (offset ^ lane_idx) < valid_lanes;
-      if (other_valid) {
-        reduction_op(reduce_val, other_val);
-      }
+  for (int offset = np2; offset >= 1; offset /= 2) {
+    T other_val = shfl_xor(mask, reduce_val, offset);
+    bool other_valid = !is_last_warp || (offset ^ lane_idx) < valid_lanes;
+    if (other_valid) {
+      reduction_op(reduce_val, other_val);
     }
   }
-  // Reduce across warp if needed
-  // Load value to shared mem
-  if (!SINGLE_WARP) {
-    unsigned int reduce_group_id = threadIdx.z * blockDim.y + threadIdx.y;
-    bool is_warp_head = lane_idx == 0;
-    unsigned int smem_offset = reduce_group_id * num_of_warps;
+  // Reduce across warp if needed, sometimes codegen can't detect it is a single
+  // warp. Directly check blockDim.x to make sure it is really multiple warps.
+  if (!SINGLE_WARP && blockDim.x > 32) {
     block_sync::sync<Aligned>();
 
-    if (is_warp_head) {
-      shared_mem[smem_offset + warp_idx] = reduce_val;
+    if (lane_idx == 0) {
+      shared_mem[warp_idx] = reduce_val;
     }
     block_sync::sync<Aligned>();
 
@@ -118,8 +116,7 @@ __device__ void warpReduceTIDX(
       assert(num_of_warps <= 32);
       const bool launch_condition = lane_idx < num_of_warps;
       const unsigned int mask = __ballot_sync(0xffffffff, launch_condition);
-      reduce_val =
-          launch_condition ? shared_mem[smem_offset + lane_idx] : init_val;
+      reduce_val = launch_condition ? shared_mem[lane_idx] : init_val;
       if (launch_condition) {
         int np2 = num_of_warps > 1 ? 1 << (31 - __clz(num_of_warps - 1)) : 0;
         for (int offset = np2; offset >= 1; offset /= 2) {
@@ -131,7 +128,7 @@ __device__ void warpReduceTIDX(
         }
       }
     }
-    if (is_warp_head) {
+    if (lane_idx == 0) {
       reduction_op(out, reduce_val);
     }
     // needs sync, otherwise other warps may access shared memory before this
@@ -164,7 +161,7 @@ __device__ void warpReduceTIDX<SINGLE_WARP, Aligned, true>(
 
   // Reduce within each warp
   for (int i = 16; i >= 1; i /= 2) {
-    reduction_op(reduce_val, shfl_xor(reduce_val, i, WARP_SIZE));
+    reduction_op(reduce_val, shfl_xor(0xffffffff, reduce_val, i, WARP_SIZE));
   }
 
   // Reduce across warp if needed
@@ -196,7 +193,7 @@ __device__ void warpReduceTIDX<SINGLE_WARP, Aligned, true>(
 
       // Reduce within warp 0
       for (int i = 16; i >= 1; i /= 2) {
-        reduction_op(reduce_val, shfl_xor(reduce_val, i, 32));
+        reduction_op(reduce_val, shfl_xor(0xffffffff, reduce_val, i, 32));
       }
     }
 
