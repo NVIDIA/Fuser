316f1ca71aa193e262af04f730bdb07a988d753d
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2023-10-11 17:36:16 -0700

    restore patch-nvfuser

--- nvfuser_python_utils/patch_nvfuser.py
+++ nvfuser_python_utils/patch_nvfuser.py
@@ -1,12 +1,40 @@
-import warnings
+import os
+
+
+def patch_pytorch_nvfuser_binaries(torch_lib):
+    nvfuser_lib = os.path.join(
+        os.path.dirname(os.path.dirname(__file__)), "nvfuser", "lib"
+    )
+    import shutil
+
+    for f_name in ["libnvfuser_codegen.so"]:
+        shutil.copyfile(
+            os.path.join(nvfuser_lib, f_name),
+            os.path.join(torch_lib, f_name),
+        )
+
+
+def remove_nvfuser_python_module(installed_nvfuser_dir):
+    # only remove if installed nvfuser is in a different path
+    if installed_nvfuser_dir != os.path.join(
+        os.path.dirname(os.path.dirname(__file__)), "nvfuser"
+    ):
+        import shutil
+
+        shutil.rmtree(installed_nvfuser_dir)
 
 
 def patch_installation():
-    warnings.warn(
-        "`patch-nvfuser` is no longer needed after nvfuser==0.1.0. "
-        "This binary will throw fatal error in the next minor release and be removed after that. ",
-        stacklevel=2,
-    )
+    from importlib import util
+
+    torch_dir = os.path.dirname(util.find_spec("torch").origin)
+    torch_lib = os.path.join(torch_dir, "lib")
+
+    installed_nvfuser_dir = os.path.join(os.path.dirname(torch_dir), "nvfuser")
+
+    patch_pytorch_nvfuser_binaries(torch_lib)
+    if os.path.exists(installed_nvfuser_dir):
+        remove_nvfuser_python_module(installed_nvfuser_dir)
 
 
 if __name__ == "__main__":
