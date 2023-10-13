3bb96926c421c95647651e4ce6067e094f9cc809
Naoya Maruyama <nmaruyama@nvidia.com>
2023-10-13 10:06:50 -0700

    minor cleanup

--- csrc/id_model/id_graph.cpp
+++ csrc/id_model/id_graph.cpp
@@ -660,17 +660,17 @@ bool IdGraph::exprsMap(Expr* first, Expr* second, bool forward) const {
     auto extent_1o = merge1->outer()->extent();
     auto extent_1i = merge1->inner()->extent();
 
-    auto extent_0_match = extent_0o->sameAs(extent_1o) ||
+    auto extent_o_match = extent_0o->sameAs(extent_1o) ||
         (extent_0o->isConstInt() && extent_1o->isConstInt() &&
          extent_0o->evaluateInt() == extent_1o->evaluateInt()) ||
         disjointIdSets().permissiveAreMapped(merge0->outer(), merge1->outer());
 
-    auto extent_1_match = extent_0i->sameAs(extent_1i) ||
+    auto extent_i_match = extent_0i->sameAs(extent_1i) ||
         (extent_0i->isConstInt() && extent_1i->isConstInt() &&
          extent_0i->evaluateInt() == extent_1i->evaluateInt()) ||
         disjointIdSets().permissiveAreMapped(merge0->inner(), merge1->inner());
 
-    if (!(extent_0_match || extent_1_match)) {
+    if (!(extent_o_match || extent_i_match)) {
       return false;
     }
   }
