07d9e47425c369ed1b20eaa5702aacbf8d005759
jjsjann123 <jiej@nvidia.com>
2023-10-18 16:53:08 -0700

    removing includes

--- test/test_gpu2.cpp
+++ test/test_gpu2.cpp
@@ -30,7 +30,6 @@
 #include <kernel_ir.h>
 #include <kernel_ir_dispatch.h>
 #include <ops/all_ops.h>
-#include <register_interface.h>
 #include <root_domain_map.h>
 #include <scheduler/all_schedulers.h>
 #include <scheduler/reduction_utils.h>
