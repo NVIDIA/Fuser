3053e68d832bf7192f912bc74393602e79046e8f
root <26priya11@gmail.com>
2023-10-20 14:49:50 -0700

    instance norm bwd benchmark

--- python_benchmarks/normalization.py
+++ python_benchmarks/normalization.py
@@ -52,8 +52,8 @@ def norm_fwd_fusion(
     rev_momentum = fd.define_scalar(1-momentum, dtype=DataType.Double)
     momentum = fd.define_scalar(momentum, dtype=DataType.Double)
     
-    running_mean = fd.ops.add(fd.ops.mul(momentum, mean), fd.ops.mul(rev_momentum, running_mean))
-    running_var = fd.ops.add(fd.ops.mul(momentum, var), fd.ops.mul(rev_momentum, running_var))
+    # running_mean = fd.ops.add(fd.ops.mul(momentum, mean), fd.ops.mul(rev_momentum, running_mean))
+    # running_var = fd.ops.add(fd.ops.mul(momentum, var), fd.ops.mul(rev_momentum, running_var))
 
     if dtype in PROMOTE_DTYPES:
         output = fd.ops.cast(output, dtype=dtype)
@@ -74,10 +74,14 @@ def norm_bwd_fusion(
     batch_dim = 0
     channel_dim = 1 if not channels_last else num_dims - 1
     bcast_mask = [True if i != channel_dim else False for i in range(num_dims)]
+    channels_only_bcast_mask = [True if i != channel_dim else False for i in range(num_dims)]
+
     reduction_axes = [i for i in range(num_dims) if i != channel_dim]
     if norm == "instance_norm":
         reduction_axes.remove(batch_dim)
         bcast_mask[batch_dim] = False 
+        
+    num_spatial_dims = num_dims - len(reduction_axes)
 
     input = fd.define_tensor(shape=[-1] * num_dims, contiguity=[True] * num_dims, dtype=dtype, is_cpu=False)
     grad = fd.define_tensor(shape=[-1] * num_dims, contiguity=[True] * num_dims, dtype=dtype, is_cpu=False)
@@ -85,8 +89,9 @@ def norm_bwd_fusion(
 
     running_mean= fd.define_tensor(shape=[-1], contiguity=[True], dtype=DataType.Float, is_cpu=False)
     running_var = fd.define_tensor(shape=[-1], contiguity=[True], dtype=DataType.Float, is_cpu=False)
-    mean= fd.define_tensor(shape=[-1], contiguity=[True], dtype=DataType.Float, is_cpu=False)
-    invstd = fd.define_tensor(shape=[-1], contiguity=[True], dtype=DataType.Float, is_cpu=False)
+    
+    mean= fd.define_tensor(shape=[-1] * num_spatial_dims, contiguity=[True] * num_spatial_dims, dtype=DataType.Float, is_cpu=False)
+    invstd = fd.define_tensor(shape=[-1] * num_spatial_dims, contiguity=[True] * num_spatial_dims, dtype=DataType.Float, is_cpu=False)
     
     if dtype in PROMOTE_DTYPES:
         input = fd.ops.cast(input, dtype=DataType.Float)
@@ -100,23 +105,24 @@ def norm_bwd_fusion(
     norm = fd.ops.reciprocal(num_features)
 
     mean = fd.ops.broadcast(mean, bcast_mask)
-    invstd = fd.ops.broadcast(invstd, bcast_mask)
     
-    grad_sum = fd.ops.sum(grad, axes=reduction_axes, keepdim = True)
+    grad_sum = fd.ops.sum(grad, axes=reduction_axes, keepdim = False)
     
     x_sub_mean = fd.ops.sub(input, mean)
-    dot_p = fd.ops.sum(fd.ops.mul(grad, x_sub_mean), axes=reduction_axes, keepdim=True)
+    dot_p = fd.ops.sum(fd.ops.mul(grad, x_sub_mean), axes=reduction_axes, keepdim=False)
     
-    grad_mean = fd.ops.mul(grad_sum, norm)
+    grad_mean = fd.ops.broadcast(fd.ops.mul(grad_sum, norm), bcast_mask)
     proj_scale = fd.ops.mul(fd.ops.mul(dot_p, norm), fd.ops.mul(invstd, invstd))
-    
-    weight = fd.ops.broadcast(weight, bcast_mask)
-    grad_scale = fd.ops.mul(weight, invstd)
+    proj_scale = fd.ops.broadcast(proj_scale, bcast_mask)
+
+    invstd_bcast = fd.ops.broadcast(invstd, bcast_mask)
+    weight = fd.ops.broadcast(weight, channels_only_bcast_mask)
+    grad_scale = fd.ops.mul(weight, invstd_bcast)
     proj = fd.ops.mul(proj_scale, x_sub_mean)
     
     grad_input = fd.ops.mul(fd.ops.sub(fd.ops.sub(grad, proj), grad_mean), grad_scale)
-    grad_weight = fd.ops.mul(dot_p, invstd)
-    grad_bias = grad_sum
+    grad_weight = fd.ops.sum(fd.ops.mul(dot_p, invstd), batch_dim)
+    grad_bias = fd.ops.sum(grad_sum, batch_dim)
 
     if dtype in PROMOTE_DTYPES:
         grad_input = fd.ops.cast(grad_input, dtype=dtype)
--- python_benchmarks/test_instancenorm.py
+++ python_benchmarks/test_instancenorm.py
@@ -54,7 +54,7 @@ def instancenorm_fwd_fusion(
     fd.add_output(mean)
     fd.add_output(invstd)
 
-@pytest.mark.parametrize("size", generate_input_sizes(dims=4))
+@pytest.mark.parametrize("size", [(128, 64, 32, 16)])
 @pytest.mark.parametrize("dtype", FLOAT_DTYPES)
 @pytest.mark.parametrize("channels_last", [False])
 def test_instancenorm_fwd_benchmark(
@@ -95,6 +95,62 @@ def test_instancenorm_fwd_benchmark(
         )
         assert torch.allclose(nvf_output[0], eager_output, rtol=1e-3, atol=1e-3),\
                               f"{torch.max(nvf_output[0] - eager_output)}"
+    
 
     if not disable_benchmarking:
         run_benchmark(benchmark, fd.execute, [inputs, weight, bias, running_mean, running_var])
+
+@pytest.mark.parametrize("size", [(128, 64, 32, 16)])
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
+@pytest.mark.parametrize("channels_last", [False])
+def test_instancenorm_bwd_benchmark(
+    benchmark,
+    size: tuple,
+    dtype: torch.dtype,
+    channels_last: bool,
+    disable_validation: bool,
+    disable_benchmarking: bool,
+    eps: float = 1e-5,
+):  
+    # Size is assumed to be in the order N, C, ...
+    input_size = size
+    if channels_last:
+        input_size = (size[0], *size[2:], size[1])
+    num_dims = len(size)
+
+    inputs = torch.randn(*input_size, device="cuda", dtype=dtype, requires_grad=True)
+    grads = torch.randn(*input_size, device="cuda", dtype=dtype)
+    weight = torch.randn(size[1], device="cuda", dtype=dtype, requires_grad=True)
+    bias = torch.randn(size[1], device="cuda", dtype=dtype, requires_grad=True)
+
+    # CPP benchmarks assume mean and variance to be of type Float
+    running_mean = torch.zeros(size[1], device="cuda", dtype=torch.float)
+    running_var = torch.ones(size[1], device="cuda", dtype=torch.float)
+
+    channel_dim = 1 if not channels_last else len(size) - 1
+    reduction_axes = [i for i in range(1, len(size)) if i != channel_dim]
+    mean = inputs.to(torch.float).mean(dim = reduction_axes)
+    var = inputs.to(torch.float).var(dim = reduction_axes, unbiased=False)
+    invstd = (1.0 / torch.sqrt(var + eps))
+
+    with FusionDefinition() as fd:
+        norm_bwd_fusion(fd = fd, 
+            dtype = torch_dtype_to_nvfuser_dtype(dtype), 
+            norm = "instance_norm", 
+            num_dims=num_dims, 
+            channels_last=channels_last)
+
+    if not disable_validation:
+        nvf_output = fd.execute([inputs, grads, weight, running_mean, running_var, mean, invstd])
+        # PyTorch expects running mean and variance to be of same type as input.
+        eager_output = torch.nn.functional.instance_norm(
+            inputs, running_mean.to(dtype), running_var.to(dtype), weight=weight, bias=bias,
+        )
+        eager_output.backward(grads)
+        assert torch.allclose(nvf_output[0], inputs.grad, rtol=1e-3, atol=1e-3),\
+                              f"{torch.max(nvf_output[0] - inputs.grad)}"
+        assert nvf_output[1].shape == weight.shape
+        assert nvf_output[2].shape == bias.shape
+
+    if not disable_benchmarking:
+        run_benchmark(benchmark, fd.execute, [inputs, grads, weight, running_mean, running_var, mean, invstd])
