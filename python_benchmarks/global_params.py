a55c2df1d45f0fb5030b068ab7755d0527163e6b
Priya Mishra <52657555+Priya2698@users.noreply.github.com>
2023-10-24 21:31:50 -0700

    Normalization benchmarks (#1111)
    Adds `batchnorm` and `instancenorm` benchmarks (in training mode).

--- python_benchmarks/global_params.py
+++ python_benchmarks/global_params.py
@@ -17,6 +17,22 @@ def generate_input_sizes(dims: Union[int, List] = 2) -> List[Tuple]:
             range_inner = [32 * 1024 * 2**i for i in range(11)]
             inputs.extend([(i, j) for i in range_outer for j in range_inner])
             inputs.extend([(j, i) for i in range_outer for j in range_inner])
+        elif dim == 3:
+            dim_range = [2**i for i in range(1, 10)]
+            inputs.extend(
+                [(i, j, k) for i in dim_range for j in dim_range for k in dim_range]
+            )
+        elif dim == 4:
+            dim_range = [2**i for i in range(1, 10)]
+            inputs.extend(
+                [
+                    (i, j, k, l)
+                    for i in dim_range
+                    for j in dim_range
+                    for k in dim_range
+                    for l in dim_range
+                ]
+            )
         else:
             raise NotImplementedError(
                 f"Generating input sizes of dimension {dim} is not implemented"
@@ -25,7 +41,8 @@ def generate_input_sizes(dims: Union[int, List] = 2) -> List[Tuple]:
 
 
 # Datatypes to benchmark
-FLOAT_DTYPES = [torch.bfloat16, torch.float16, torch.float32]
+# TODO: Add torch.bfloat16 after adding support for variable thresholds
+FLOAT_DTYPES = [torch.float16, torch.float32]
 
 # Datatypes that will be promoted to Datatype.Float in Fusion Definitions
 PROMOTE_DTYPES = [DataType.BFloat16, DataType.Half]
