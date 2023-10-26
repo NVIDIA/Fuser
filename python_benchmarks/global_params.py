bb25316eeab90974328cb5928cb28fd781248ba1
root <26priya11@gmail.com>
2023-10-26 13:08:48 -0700

    Add validate method to fusion definition

--- python_benchmarks/global_params.py
+++ python_benchmarks/global_params.py
@@ -60,8 +60,7 @@ def generate_input_sizes(dims: Union[int, List] = 2) -> List[Tuple]:
 
 
 # Datatypes to benchmark
-# TODO: Add torch.bfloat16 after adding support for variable thresholds
-FLOAT_DTYPES = [torch.float16, torch.float32]
+FLOAT_DTYPES = [torch.bfloat16, torch.float16, torch.float32]
 
 # Datatypes that will be promoted to Datatype.Float in Fusion Definitions
 PROMOTE_DTYPES = [DataType.BFloat16, DataType.Half]
