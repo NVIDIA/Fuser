7f3b12838165838acce862eb60381d4e28310122
Jacob Hinkle <jhinkle@nvidia.com>
2023-10-27 10:02:19 -0400

    Add test SplitExpandedBroadcast

--- test/test_gpu_view.cpp
+++ test/test_gpu_view.cpp
@@ -2478,4 +2478,28 @@ TEST_F(GpuViewTest, ExpandedBroadcast) {
       __FILE__);
 }
 
+// See https://github.com/NVIDIA/Fuser/issues/1126
+TEST_F(GpuViewTest, SplitExpandedBroadcast) {
+  Fusion fusion;
+  FusionGuard fg(&fusion);
+
+  TensorView* in = makeContigConcreteTensor({4, 5});
+  fusion.addInput(in);
+  TensorView* out = broadcast(in, {false, false, true});
+  out = expand(
+      out,
+      {IrBuilder::create<Val>(4),
+       IrBuilder::create<Val>(5),
+       IrBuilder::create<Val>(6)});
+  out = reshape(out, {4, 5, 6}, {4, 5, 2, 3});
+  fusion.addOutput(out);
+
+  FusionExecutor fe;
+  at::Tensor in_tensor =
+      at::randn({4, 5}, at::dtype(at::kFloat).device(at::kCUDA, 0));
+  fe.compileFusion(&fusion, {in_tensor});
+  at::Tensor out_tensor = fe.runFusion({in_tensor})[0];
+  EXPECT_THAT(out_tensor.strides(), testing::ElementsAre(5, 1, 0, 0));
+}
+
 } // namespace nvfuser
