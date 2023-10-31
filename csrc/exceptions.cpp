48528ddb05e06b6010e6c34a5153798b6439bfe2
root <root@nvdl-a112-d004.nvidia.com>
2023-10-30 17:22:36 -0700

    Change location of demangle description from cpp file to header file.

--- csrc/exceptions.cpp
+++ csrc/exceptions.cpp
@@ -120,12 +120,6 @@ std::optional<FrameInformation> parse_frame_information(
 std::string demangle(const char* name) {
   int status = -1;
 
-  // This function will demangle the mangled function name into a more human
-  // readable format, e.g. _Z1gv -> g().
-  // More information:
-  // https://github.com/gcc-mirror/gcc/blob/master/libstdc%2B%2B-v3/libsupc%2B%2B/cxxabi.h
-  // NOTE: `__cxa_demangle` returns a malloc'd string that we have to free
-  // ourselves.
   std::unique_ptr<char, std::function<void(char*)>> demangled(
       abi::__cxa_demangle(
           name,
