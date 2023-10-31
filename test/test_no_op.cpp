bf6d4269809260f77e5bc3eade73beae880f2aab
Jingyue Wu <wujingyue@gmail.com>
2023-10-30 20:36:46 -0700

    Add a unit test.

--- test/test_no_op.cpp
+++ test/test_no_op.cpp
@@ -190,4 +190,45 @@ TEST_F(NoOpTest, FusionEmpty) {
   }
 }
 
+TEST_F(NoOpTest, View) {
+  auto fusion = std::make_unique<Fusion>();
+  FusionGuard fg(fusion.get());
+
+  const std::vector<int64_t> in_shape({2, 3, 4});
+  const std::vector<int64_t> out_shape({2, 12});
+
+  TensorView* in = makeContigConcreteTensor(in_shape);
+  fusion->addInput(in);
+  TensorView* out = reshape(in, in_shape, out_shape);
+  fusion->addOutput(out);
+  fusion->aliasOutputToInput(out, in, AliasType::PointerCast);
+
+  FusionExecutor fe;
+  at::Tensor in_tensor =
+      at::randn({2, 3, 4}, at::dtype(at::kFloat).device(at::kCUDA, 0));
+  fe.compileFusion(fusion.get(), {in_tensor});
+  at::Tensor out_tensor = fe.runFusion({in_tensor})[0];
+  EXPECT_EQ(in_tensor.data_ptr<float>(), out_tensor.data_ptr<float>());
+  testValidate(
+      fusion.get(),
+      {out_tensor},
+      {in_tensor},
+      {in_tensor.view({2, 12})},
+      __LINE__,
+      __FILE__);
+
+  FusionExecutorCache fec(std::move(fusion));
+  std::vector<at::Tensor> out_tensors = fec.runFusionWithInputs({in_tensor});
+  ASSERT_EQ(out_tensors.size(), 1);
+  out_tensor = out_tensors[0];
+  EXPECT_EQ(in_tensor.data_ptr<float>(), out_tensor.data_ptr<float>());
+
+  const std::vector<SegmentedGroup*>& groups =
+      fec.getMostRecentKernelRuntime()->fusionSegments()->groups();
+  ASSERT_EQ(groups.size(), 1);
+  SegmentedGroup* group = groups[0];
+
+  EXPECT_EQ(group->heuristic(), ScheduleHeuristic::NoOp);
+}
+
 } // namespace nvfuser
