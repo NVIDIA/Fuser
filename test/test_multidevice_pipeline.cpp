035204b48200639426707502f6161410be1df914
snordmann <snordmann@nvidia.com>
2023-11-02 08:01:54 -0700

    move tests

--- test/test_multidevice_pipeline.cpp
+++ test/test_multidevice_pipeline.cpp
@@ -181,6 +181,217 @@ INSTANTIATE_TEST_SUITE_P(
         ::testing::Values(true),
         ::testing::Values(true)));
 
+
+TEST_F(PipelineTest, Pipeline_Reduce) {
+  FusionGuard fg(fusion.get());
+
+  TensorView* tv0 = makeContigTensor(4);
+  fusion->addInput(tv0);
+  TensorView* tv1 = sum(tv0, {1});
+  TensorView* tv2 = sum(tv1, {0});
+  TensorView* tv3 = sum(tv2, {0});
+  fusion->addOutput(tv3);
+
+  tv0->axis(0)->parallelize(ParallelType::DIDx);
+  tv1->axis(0)->parallelize(ParallelType::DIDx);
+
+  PipelineStageDescriptor stage0(false), stage1(false);
+  stage0.addVal({tv0, tv1});
+  stage1.addVal({tv2, tv3});
+
+  stage0.mesh = {0, 1, 2, 3};
+  stage1.mesh = {0, 1};
+
+  PipelineDescriptor descriptor{
+      .stage_descriptors{std::move(stage0), std::move(stage1)}};
+
+  pipeline = std::make_unique<Pipeline>(fusion.get(), std::move(descriptor));
+
+  inputs = {at::ones({4, 3, 1, 2}, tensor_options) * (communicator->deviceId() + 1)};
+
+  validate();
+}
+
+TEST_F(PipelineTest, Pipeline_ReduceToExternalRoot) {
+  FusionGuard fg(fusion.get());
+
+  TensorView* tv0 = makeContigTensor(4);
+  fusion->addInput(tv0);
+  TensorView* tv1 = sum(tv0, {1});
+  TensorView* tv2 = sum(tv1, {0});
+  TensorView* tv3 = sum(tv2, {0});
+  fusion->addOutput(tv3);
+
+  tv0->axis(0)->parallelize(ParallelType::DIDx);
+  tv1->axis(0)->parallelize(ParallelType::DIDx);
+
+  PipelineStageDescriptor stage0(false), stage1(false);
+  stage0.addVal({tv0, tv1});
+  stage1.addVal({tv2, tv3});
+
+  stage0.mesh = {0, 1};
+  stage1.mesh = {2};
+
+  PipelineDescriptor descriptor{
+      .stage_descriptors{std::move(stage0), std::move(stage1)}};
+
+  pipeline = std::make_unique<Pipeline>(fusion.get(), std::move(descriptor));
+
+  inputs = {at::ones({2, 3, 1, 2}, tensor_options) * (communicator->deviceId() + 1)};
+
+  validate();
+}
+
+TEST_F(PipelineTest, Pipeline_Allreduce) {
+  FusionGuard fg(fusion.get());
+
+  TensorView* tv0 = makeContigTensor(4);
+  fusion->addInput(tv0);
+  TensorView* tv1 = sum(tv0, {1});
+  TensorView* tv2 = sum(tv1, {0});
+  TensorView* tv3 = sum(tv2, {0});
+  fusion->addOutput(tv3);
+
+  tv0->axis(0)->parallelize(ParallelType::DIDx);
+  tv1->axis(0)->parallelize(ParallelType::DIDx);
+
+  PipelineStageDescriptor stage0(false), stage1(false);
+  stage0.addVal({tv0, tv1});
+  stage1.addVal({tv2, tv3});
+
+  stage0.mesh = {0, 1, 2, 3};
+  stage1.mesh = {0, 1, 2, 3};
+
+  PipelineDescriptor descriptor{
+      .stage_descriptors{std::move(stage0), std::move(stage1)}};
+
+  pipeline = std::make_unique<Pipeline>(fusion.get(), std::move(descriptor));
+
+  inputs = {at::ones({4, 3, 1, 2}, tensor_options) * (communicator->deviceId() + 1)};
+
+  validate();
+}
+
+TEST_F(PipelineTest, Pipeline_ReduceScatter) {
+  FusionGuard fg(fusion.get());
+
+  TensorView* tv0 = makeContigTensor(4);
+  fusion->addInput(tv0);
+  TensorView* tv1 = sum(tv0, {3});
+  TensorView* tv2 = sum(tv1, {0});
+  TensorView* tv3 = sum(tv2, {1});
+  fusion->addOutput(tv3);
+
+  tv0->axis(0)->parallelize(ParallelType::DIDx);
+  tv1->axis(0)->parallelize(ParallelType::DIDx);
+  tv2->axis(1)->parallelize(ParallelType::DIDx); //axis(0) is the "reduce" axis from previous tensor
+  tv3->axis(0)->parallelize(ParallelType::DIDx);
+
+  PipelineStageDescriptor stage0(false), stage1(false);
+  stage0.addVal({tv0, tv1});
+  stage1.addVal({tv2, tv3});
+
+  stage0.mesh = {0, 1, 2, 3};
+  stage1.mesh = {0, 1, 2, 3};
+
+  PipelineDescriptor descriptor{
+      .stage_descriptors{std::move(stage0), std::move(stage1)}};
+
+  pipeline = std::make_unique<Pipeline>(fusion.get(), std::move(descriptor));
+
+  inputs = {at::ones({4, 4, 1, 2}, tensor_options) * (communicator->deviceId() + 1)};
+
+  validate();
+}
+
+
+TEST_F(PipelineTest, Overlap) {
+  // In this example we demonstrate how we can apply the optimization
+  // described in 
+  // Overlap Communication with Dependent Computation via Decomposition in Large Deep Learning Models (acm.org)
+  // https://dl.acm.org/doi/pdf/10.1145/3567955.3567959
+  // We simplify the setting as much as possible by considering a multi-device Pipeline with
+  // a simple "Gather" followed by a dependent compute. The paper suggest to slice those
+  // two operation and to interleave them to achieve better overlap. Consider the following Pipeline:
+
+  // /* Stage 0 */
+  // TensorView* tv0 = makeContigTensor(3);
+  // fusion->addInput(tv0);
+  // TensorView* tv1 = sum(tv0, {2});
+  // /* Stage 1 */
+  // TensorView* tv2 = set(tv1); // is lowered to a "Gather" communication
+  // TensorView* tv3 = sum(tv2, {1});
+  // fusion->addOutput(tv3);
+
+  // tv0->axis(0)->parallelize(ParallelType::DIDx);
+  // tv1->axis(0)->parallelize(ParallelType::DIDx);
+
+  // PipelineStageDescriptor stage0(false), stage1(false);
+  // stage0.addVal({tv0, tv1});
+  // stage1.addVal({tv2, tv3});
+
+  // stage0.mesh = {0, 1, 2, 3, 4, 5, 6, 7};
+  // stage1.mesh = {0};
+
+  const int64_t number_of_devices = communicator->size();
+  constexpr int64_t number_of_slices = 4;
+  constexpr int64_t extent_of_axis2 = 1024;
+  constexpr int64_t extent_of_slice = extent_of_axis2 / number_of_slices;
+  const std::vector<int64_t> input_extents = {number_of_devices, 7, extent_of_axis2, 3};
+  assert(!(extent_of_axis2 % number_of_slices)); // for simplicity
+
+  FusionGuard fg(fusion.get());
+
+  PipelineStageDescriptor stage0(false), stage1(false);
+  // containers used later for adding ranges of tvs directly to the stages
+  std::unordered_set<Val*> from_stage0, from_stage1;
+  std::vector<Val*> to_stage0, to_stage1;
+
+  TensorView* tv0 = makeConcreteTensor(input_extents);
+  fusion->addInput(tv0);
+  TensorView* tv1 = sum(tv0, {3});
+  from_stage0.insert(tv0);
+  tv0->axis(0)->parallelize(ParallelType::DIDx);
+  tv1->axis(0)->parallelize(ParallelType::DIDx);
+
+  TensorView *tv1x, *tv2x, *tv3x;
+  std::vector<TensorView*> tv3_slices;
+  std::vector<Slice> slices {3};
+  for (int i = 0; i < number_of_slices; i++) {
+    slices.at(2).start = IrBuilder::create<Val>(i * extent_of_slice);
+    slices.at(2).stop = IrBuilder::create<Val>((i+1) * extent_of_slice);
+    tv1x = slice(tv1, slices);
+    tv1x->axis(0)->parallelize(ParallelType::DIDx);
+    to_stage0.push_back(tv1x);
+
+    tv2x = set(tv1x);
+    from_stage1.insert(tv2x);
+    tv3x = sum(tv2x, {1});
+    tv3_slices.push_back(tv3x);
+  }
+  TensorView* tv3 = cat(tv3_slices, 1);
+  fusion->addOutput(tv3);
+  to_stage1.push_back(tv3);
+
+  //instead of using "slice/cat" it would be nicer to split the dimension and use "select/stack", but "stack" is not implemented in nvFuser at the moment
+
+  stage0.addRange(fusion.get(), from_stage0, to_stage0);
+  stage1.addRange(fusion.get(), from_stage1, to_stage1);
+  std::vector<DeviceIdxType> devices(number_of_devices);
+  std::iota(devices.begin(), devices.end(), 0);
+  stage0.mesh = devices;
+  stage1.mesh = {0};
+
+  PipelineDescriptor descriptor {
+      .stage_descriptors{std::move(stage0), std::move(stage1)}};
+
+  pipeline = std::make_unique<Pipeline>(fusion.get(), std::move(descriptor));
+
+  inputs = {at::ones(input_extents, tensor_options) * (communicator->deviceId() + 1)};
+
+  validate();
+}
+
 } // namespace nvfuser
 
 #endif
