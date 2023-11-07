38ba2d62d9ba84e829f4633dda8a026847cba897
root <26priya11@gmail.com>
2023-11-07 15:43:16 -0800

    pointwise op benchmark

--- python_benchmarks/test_many_pointwise_ops.py
+++ python_benchmarks/test_many_pointwise_ops.py
@@ -1,58 +1,57 @@
 import pytest
 from nvfuser import FusionDefinition, DataType
 from nvfuser.pytorch_utils import torch_dtype_to_nvfuser_dtype
-from core import run_benchmark
+from .core import run_benchmark
 import torch
-from global_params import generate_input_sizes, FLOAT_DTYPES, PROMOTE_DTYPES
+from .global_params import PROMOTE_DTYPES
 
 def pointwise_ops_fusion(
     fd: FusionDefinition,
     dtype: DataType,
     num_iters: int
 ):
-    x = fd.define_tensor(shape=[-1, -1], contiguity=[True, True], dtype=dtype, is_cpu=False)
+    x = fd.define_tensor(shape=[-1], contiguity=[True], dtype=dtype, is_cpu=False)
+    y = fd.define_tensor(shape=[-1], contiguity=[True], dtype=dtype, is_cpu=False)
+
     if dtype in PROMOTE_DTYPES:
         x = fd.ops.cast(x, dtype=DataType.Float)
+        y = fd.ops.cast(y, dtype=DataType.Float)
+
+    a = fd.ops.add(x, y)
     for _ in range(num_iters):
-        x = fd.ops.add(x, x)
+        x = fd.ops.cos(a)
+        y = fd.ops.sin(a)
+        a = fd.ops.add(x, y)
+
     if dtype in PROMOTE_DTYPES:
-        x = fd.ops.cast(x, dtype=dtype)
-    fd.add_output(x)
+        a = fd.ops.cast(a, dtype=dtype)
+    fd.add_output(a)
 
-# @pytest.mark.parametrize("size", [(2, 4)])
-# @pytest.mark.parametrize("dtype", [torch.float16])
-# @pytest.mark.parametrize("num_iters", [16])
-# def test_pointwise_ops_benchmark(
-#     benchmark,
-#     size: tuple,
-#     dtype: torch.dtype,
-#     num_iters: int,
-#     disable_validation: bool,
-#     disable_benchmarking: bool,
-# ):
-#     inputs = torch.randn(*size, device="cuda", dtype=dtype)
-#     with FusionDefinition() as fd:
-#         pointwise_ops_fusion(fd, torch_dtype_to_nvfuser_dtype(dtype), num_iters)
+#NOTE: num_iters restricted due to issue #1234.
+@pytest.mark.parametrize("num_iters", [2, 4, 8, 16])
+def test_pointwise_ops_benchmark(
+    benchmark,
+    num_iters: int,
+    disable_validation: bool,
+    disable_benchmarking: bool,
+):
+    inputs = [torch.randn(13, device="cuda", dtype=torch.float16) for _ in range(2)]
+    with FusionDefinition() as fd:
+        pointwise_ops_fusion(fd, torch_dtype_to_nvfuser_dtype(torch.float16), num_iters)
     
-#     if not disable_validation:
-#         nvf_output = fd.execute([inputs])
-#         eager_output = inputs
+    if not disable_validation:
+        nvf_output = fd.execute([inputs])
+        eager_output = inputs[0] + inputs[1]
+        for _ in range(num_iters):
+            x = torch.cos(eager_output)
+            y = torch.sin(eager_output)
+            eager_output = x + y
 
-#         for _ in range(num_iters):
-#             eager_output += eager_output
+        assert torch.allclose(nvf_output[0], eager_output, rtol=1e-3, atol=1e-3)
 
-#         assert torch.allclose(nvf_output[0], eager_output, rtol=1e-3, atol=1e-3)
+    if not disable_benchmarking:
+        run_benchmark(benchmark, fd.execute, [inputs])
 
-#     if not disable_benchmarking:
-#         run_benchmark(benchmark, fd.execute, [inputs])
-
-#     torch.cuda.empty_cache()
-
-size = [2, 4]
-dtype = torch.float16
-inputs = torch.randn(*size, device="cuda", dtype=dtype)
-with FusionDefinition() as fd:
-    pointwise_ops_fusion(fd, torch_dtype_to_nvfuser_dtype(dtype), 16)
-nvf_output = fd.execute([inputs])
+    torch.cuda.empty_cache()
 
     
