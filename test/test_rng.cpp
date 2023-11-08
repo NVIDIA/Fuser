73e1510afde70cde2aef1fe500f0ba42542aa3f2
Xiang Gao <qasdfgtyuiop@gmail.com>
2023-11-07 16:25:12 -0800

    Don't include pytorch in rng_kernels.cu

--- test/test_rng.cpp
+++ test/test_rng.cpp
@@ -22,9 +22,61 @@
 
 namespace nvfuser {
 
-at::Tensor generate_uniform(int64_t size, at::ScalarType dtype);
+enum RNGTest_t {
+  Uniform,
+  Normal,
+};
 
-at::Tensor generate_normal(int64_t size, at::ScalarType dtype);
+template <typename T>
+void lanuch_generate_random_numbers_kernel(
+    cudaStream_t stream,
+    T* output,
+    int64_t size,
+    at::PhiloxCudaState philox_args,
+    RNGTest_t rng_test);
+
+at::Tensor generate_random_numbers(
+    int64_t size,
+    at::ScalarType dtype,
+    RNGTest_t rng_test) {
+  auto options = at::TensorOptions().dtype(dtype).device(at::kCUDA, 0);
+  auto result = at::empty({size}, options);
+
+  auto gen = at::get_generator_or_default<at::CUDAGeneratorImpl>(
+      c10::nullopt, at::cuda::detail::getDefaultCUDAGenerator());
+  at::PhiloxCudaState rng_engine_inputs;
+  {
+    // See Note [Acquire lock when using random generators]
+    std::lock_guard<std::mutex> lock(gen->mutex_);
+    rng_engine_inputs = gen->philox_cuda_state(4);
+  }
+
+  if (dtype == at::kFloat) {
+    lanuch_generate_random_numbers_kernel(
+        at::cuda::getCurrentCUDAStream(),
+        result.data_ptr<float>(),
+        size,
+        rng_engine_inputs,
+        rng_test);
+  } else {
+    NVF_CHECK(dtype == at::kDouble);
+    lanuch_generate_random_numbers_kernel(
+        at::cuda::getCurrentCUDAStream(),
+        result.data_ptr<double>(),
+        size,
+        rng_engine_inputs,
+        rng_test);
+  }
+  return result;
+}
+
+at::Tensor generate_uniform(int64_t size, at::ScalarType dtype) {
+  return generate_random_numbers(size, dtype, RNGTest_t::Uniform);
+}
+
+at::Tensor generate_normal(int64_t size, at::ScalarType dtype) {
+  return generate_random_numbers(size, dtype, RNGTest_t::Normal);
+}
 
 class RNGTest : public NVFuserTest {};
 
