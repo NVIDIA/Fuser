c27211b226dc9d971f001325c0494bf884336bde
Jingyue Wu <wujingyue@gmail.com>
2023-11-14 09:35:07 -0800

    Use std::move to avoid copies..
    Co-authored-by: Gao, Xiang <qasdfgtyuiop@gmail.com>

--- csrc/linked_hash_map.h
+++ csrc/linked_hash_map.h
@@ -55,8 +55,8 @@ std::pair<V, typename LinkedHashMap<K, V>::const_iterator> LinkedHashMap<K, V>::
     erase(const K& key) {
   const_iterator index = key_to_index_.at(key);
   key_to_index_.erase(key);
-  V value = index->second;
-  return {value, order_.erase(index)};
+  V value = std::move(index->second);
+  return {std::move(value), order_.erase(index)};
 }
 
 template <typename K, typename V>
