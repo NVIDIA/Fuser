b7e05ddd703e8f755df57a29d76e85ba29bc58c9
Jacob Hinkle <jhinkle@nvidia.com>
2023-11-27 15:14:10 -0500

    Demangle first two components to sanitize _ZN* strings

--- tools/codediff/diff_report.py
+++ tools/codediff/diff_report.py
@@ -646,23 +646,23 @@ def sanitize_ptx_lines(lines: list[str]) -> list[str]:
         # or
         #   _ZN76_GLOBAL__N__00000000_37___tmp_kernel_4_cu_8995cef2_3255329nvfuser_4ENS_6TensorIfLi2ELi2EEES1_S1_
         # with
-        #   _ZN11_kernelfile9kernelENS_6TensorIfLi2ELi2EEES1_S1_
-        # get the length of the name
+        #   _ZN11kernelscope6kernelENS_6TensorIfLi2ELi2EEES1_S1_
 
-        m = re.match(r"^(?P<prefix>^.*\b_Z?ZN)(?P<namelen>\d+)_", l)
+        # demangle first two parts after _ZN and replace with "kernelscope" and "kernel"
+        m = re.match(r"^(?P<prefix>^.*\b_Z?ZN)(?P<scopenamelen>\d+)_", l)
         if m is not None:
             d = m.groupdict()
             namelen = int(d["namelen"])
-            l = (
-                d["prefix"]
-                + "11_kernelfile"
-                + l[(len(d["prefix"]) + len(d["namelen"]) + namelen) :]
-            )
-
-        # Doctor the function or variable name
-        l = re.sub(
-            r"(_tmp_kernel|nvfuser)(_[a-z]+)?_(\d+|f\d+_c\d*_r\d+_g\d+)", "kernel", l
-        )
+            # demangle second part in remainder after scope name
+            remainder = l[(len(d["prefix"]) + len(d["namelen"]) + namelen) :]
+            mrem = re.match(r"^(?P<varnamelen>\d+)", remainder)
+            if mrem is not None:
+                drem = mrem.groupdict()
+                varnamelen = int(drem["varnamelen"])
+                remainder = (
+                    "6kernel" + remainder[len(drem["varnamelen"]) + varnamelen :]
+                )
+            l = d["prefix"] + "11kernelscope" + remainder
 
         # Remove comments. This fixes mismatches in PTX "callseq" comments, which appear to be non-repeatable.
         l = re.sub(r"//.*$", "", l)
