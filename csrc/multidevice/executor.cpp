b6e719c6b2bd2295364692b77fc6c6acf104c046
snordmann <snordmann@nvidia.com>
2023-12-07 07:59:47 -0800

    handling outputs with sf

--- csrc/multidevice/executor.cpp
+++ csrc/multidevice/executor.cpp
@@ -116,8 +116,8 @@ std::vector<at::Tensor> PipelineExecutor::runWithInput(
 
   // Collect global outputs from context
   std::vector<at::Tensor> outputs;
-  for (auto output_val : runtime_.pipeline_->outputs()) {
-    outputs.push_back(val_to_IValue_.at(output_val->as<PipelineVal>()->getOriginalVal()).toTensor());
+  for (auto output_val : runtime_.pipeline_->sf_->outputs()) {
+    outputs.push_back(val_to_IValue_.at(output_val).toTensor());
   }
 
   return outputs;
