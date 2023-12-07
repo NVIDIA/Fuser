24c433499145037c5336fd6320499c76cc05bd44
Gao, Xiang <qasdfgtyuiop@gmail.com>
2023-12-06 17:08:57 -0800

    Split tests

--- manual_ci.sh
+++ manual_ci.sh
@@ -19,8 +19,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 run_test './bin/lib/dynamic_type/test_dynamic_type_17'
 run_test './bin/lib/dynamic_type/test_dynamic_type_20'
-run_test './bin/lib/dynamic_type/test_dynamic_type_23'
 run_test './bin/nvfuser_tests'
+run_test './bin/test_rng'
+# run_test './bin/test_multidevice'
 run_test 'pytest python_tests/pytest_ops.py'
 run_test 'python python_tests/test_python_frontend.py'
 run_test 'python python_tests/test_schedule_ops.py'
