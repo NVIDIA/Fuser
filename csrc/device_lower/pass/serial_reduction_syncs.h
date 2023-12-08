ebec0e5eb39932a1f8b8f44be9eba7a710236e4d
Jacob Hinkle <jhinkle@nvidia.com>
2023-12-08 16:58:33 +0000

    Call insertGridSerializationSyncs pass

---
+++ csrc/device_lower/pass/serial_reduction_syncs.h
@@ -0,0 +1,24 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+#pragma once
+
+#include <c10/macros/Export.h>
+#include <exceptions.h>
+
+#include <ir/all_nodes.h>
+#include <kernel_ir.h>
+
+#include <vector>
+
+namespace nvfuser {
+
+//! Insert syncs
+std::vector<Expr*> insertSerialGridReductionSyncs(const std::vector<Expr*>& exprs);
+
+} // namespace nvfuser
+
