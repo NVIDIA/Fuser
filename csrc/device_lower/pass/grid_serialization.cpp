e96a57f1c6e77b948c97184f5692e513da1f487f
Jacob Hinkle <jhinkle@nvidia.com>
2023-12-11 17:42:08 +0000

    Fix sync insertion in lowering pass.
    Still missing allocation/indexing of work buffer

--- csrc/device_lower/pass/grid_serialization.cpp
+++ csrc/device_lower/pass/grid_serialization.cpp
@@ -33,16 +33,86 @@ class GridSerializationSyncInserter : kir::ExprMutator {
   }
 
  private:
+  using kir::ExprMutator::dispatch;
   using kir::ExprMutator::handle;
 
-  void handle(ReductionOp* rop) final {
+  //! Record cur_expr_sync_pattern_ if this is a serial grid reduction
+  void handle(ReductionOp* rop) override {
     if (rop->serialGridReductionRequested()) {
+      ParallelTypeBitmap sync_pattern;
+      auto out = rop->out()->as<TensorView>();
+      NVF_ERROR(out != nullptr);
+      for (int i : c10::irange((int)out->nDims())) {
+        IterDomain* ax = out->axis(i);
+        if (!ax->isReduction()) {
+          continue;
+        }
+        NVF_ERROR(
+            !ax->isThreadDim(),
+            "Serial grid reduction cannot be applied with block reductions: ",
+            rop->toString());
+        if (ax->isBlockDim()) {
+          sync_pattern.set(ax->getParallelType());
+        }
+      }
+
+      if (!sync_pattern.hasBID()) {
+        // Don't set cur_expr_sync_pattern_ since this is not actually a grid
+        // reduction
+        return;
+      }
+
+      if (cur_expr_sync_pattern_.has_value()) {
+        NVF_ERROR(
+            cur_expr_sync_pattern_.value() == sync_pattern,
+            "Reduction op ",
+            rop->toString(),
+            " has requested serial grid reduction, but pattern ",
+            sync_pattern.toString(),
+            " conflicts with previous pattern: ",
+            cur_expr_sync_pattern_.value().toString());
+      } else {
+        cur_expr_sync_pattern_ = sync_pattern;
+      }
     }
   }
 
-  //! Determine whether a ForLoop is a top-level (in generated kernel) loop
-  //! before handling.
-  void handle(kir::ForLoop* loop) final {}
+  void dispatch(Expr* expr) override {
+    if (auto loop = dynamic_cast<kir::ForLoop*>(expr);
+        cur_top_level_expr_ || (loop && loop->isTrivial())) {
+      // Never sync around trivial loops. Also avoid redefining
+      // cur_top_level_expr_ if it is already set
+      kir::ExprMutator::dispatch(expr);
+      return;
+    }
+    // Any other expr, i.e. non-trivial loops or regular Exprs, can be synced if
+    // it is top-level and either is or contains a serial grid reduction
+    cur_top_level_expr_ = expr;
+    kir::ExprMutator::dispatch(expr);
+    // If a serial grid reduction was found in expr, then cur_expr_sync_pattern_
+    // will be set
+    if (cur_expr_sync_pattern_.has_value()) {
+      insertSyncs();
+    }
+    // reset state variables
+    cur_top_level_expr_ = nullptr;
+    cur_expr_sync_pattern_ = std::nullopt;
+  }
+
+  void insertSyncs() {
+    NVF_ERROR(cur_top_level_expr_ != nullptr);
+    NVF_ERROR(cur_expr_sync_pattern_.has_value());
+    kir::Allocate* alloc = lower_utils::allocGlobalBufferForGridComm(
+        lower_utils::getGridSyncBufferSize(cur_expr_sync_pattern_.value()),
+        DataType::Int,
+        true);
+    auto wait = IrBuilder::create<kir::BlockSerializeWait>(
+        cur_expr_sync_pattern_.value(), alloc->buffer());
+    registerInsertBefore(cur_top_level_expr_, wait);
+    auto release = IrBuilder::create<kir::BlockSerializeRelease>(
+        cur_expr_sync_pattern_.value(), alloc->buffer());
+    registerInsertAfter(cur_top_level_expr_, release);
+  }
 
  private:
   //! Which Expr* is the current top-level containing the current Expr in the
@@ -72,9 +142,10 @@ class GridSerializationSyncInserter : kir::ExprMutator {
   //! will likely be run before any IfThenElse are placed in the kernel anyway.
   Expr* cur_top_level_expr_ = nullptr;
 
-  //! If a serial grid reduction is found for the current expr,
-  std::optional<std::tuple<bool, bool, bool>> current_expr_sync_pattern_ =
-      std::nullopt;
+  //! If a serial grid reduction is found for the current expr, this indicates
+  //! parallel axes that are mapped to reduction domains in the serial
+  //! reduction.
+  std::optional<ParallelTypeBitmap> cur_expr_sync_pattern_ = std::nullopt;
 };
 
 } // namespace
