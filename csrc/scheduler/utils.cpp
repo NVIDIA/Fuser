5c7e0c4b9333f9abed0b6938150d93fa98042b79
Liqiang Lu <116412316+liqiangxl@users.noreply.github.com>
2023-12-14 20:55:14 -0500

    Revert "adjust when should the persistent buffuers being projected to inputs" (#1529)
    Reverts NVIDIA/Fuser#1408
    Fails when the persistent buffer itself has a broadcast dim.

--- csrc/scheduler/utils.cpp
+++ csrc/scheduler/utils.cpp
@@ -473,19 +473,6 @@ class PersistentBufferResolution : public IterVisitor {
 
 } // namespace
 
-// Returns all broadcast tvs from the given vector of Vals.
-std::vector<TensorView*> getBroadcastTvs(const std::vector<Val*>& dep_vals) {
-  std::vector<TensorView*> broadcast_tvs;
-  for (auto val : dep_vals) {
-    if (auto tv = dynamic_cast<TensorView*>(val)) {
-      if (tv->hasBroadcast()) {
-        broadcast_tvs.push_back(tv);
-      }
-    }
-  }
-  return broadcast_tvs;
-}
-
 PersistentBufferInfo persistentBuffers(Fusion* fusion) {
   FusionGuard fg(fusion);
   PersistentBufferInfo persistent_buffer_info;
@@ -555,13 +542,12 @@ PersistentBufferInfo persistentBuffers(Fusion* fusion) {
     if (persistent_buffer->isFusionInput()) {
       continue;
     }
-    // If the buffer doesn't depends on reduction tvs or there is a broadcasted
-    // tv between reduction and persistent buffer. The re-calculation of the
-    // persistent buffer doesn't need additional reductions and this buffer is
-    // projecatable.
-    const auto& dep_vals = DependencyCheck::getAllValsBetween(
+    auto dep_vals = DependencyCheck::getAllValsBetween(
         {reduction_tvs.begin(), reduction_tvs.end()}, {persistent_buffer});
-    if (dep_vals.empty() || !getBroadcastTvs(dep_vals).empty()) {
+
+    // If there's a reduction between a persistent buffer and the inputs, it
+    // can't be projected backwards.
+    if (dep_vals.empty()) {
       persistent_buffer_info.projectable_persistent_buffers.push_back(
           persistent_buffer);
     }
