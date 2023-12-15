8c895d44f980522ee6c927c97a253b41d8ddce14
jjsjann123 <jiej@nvidia.com>
2023-12-14 21:57:20 -0800

    clangformat + comment

--- csrc/executor.cpp
+++ csrc/executor.cpp
@@ -908,11 +908,13 @@ std::pair<std::vector<int64_t>, std::vector<int64_t>> inferShapeOfOutput(
   auto pair_size_stride =
       inferShape(tv, symbolic_sizes, expand_flags, expr_eval);
   if (tv->hasAllocation()) {
-    // TODO(jiej): clean up
     auto options =
         c10::TensorOptions().device(c10::Device(c10::DeviceType::Meta));
     auto meta_tensor = at::empty_strided(
         pair_size_stride.first, pair_size_stride.second, options);
+    // TODO(jiej): we should refactor it here, there's no need to use
+    // meta_tensor at all, size + stride should be used directly in the
+    // `transformOutputFromAllocationToRFactor`
     meta_tensor =
         transformOutputFromAllocationToRFactor(meta_tensor, tv, expr_eval);
     return {meta_tensor.sizes().vec(), meta_tensor.strides().vec()};
