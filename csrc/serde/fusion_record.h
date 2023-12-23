d8bb1f3facc5e032b3c14f153c3f1171cc52e5a0
Ryan Spring <rdspring1@gmail.com>
2023-12-22 18:41:35 -0800

    Refactor namespace

--- csrc/serde/fusion_record.h
+++ csrc/serde/fusion_record.h
@@ -10,6 +10,7 @@
 #include <python_frontend/fusion_record.h>
 #include <serde/factory.h>
 
+namespace nvf = nvfuser;
 namespace nvfuser::serde {
 
 // Forward definition for RecordFunctor
@@ -18,103 +19,70 @@ struct RecordFunctor;
 // OpRecord Function Signatures
 // ========================================================================
 // Unary Functions
-typedef std::function<nvfuser::TensorView*(nvfuser::TensorView*)> unary_tv_fn;
-typedef std::function<nvfuser::Val*(nvfuser::Val*)> unary_val_fn;
+typedef std::function<nvf::TensorView*(nvf::TensorView*)> unary_tv_fn;
+typedef std::function<nvf::Val*(nvf::Val*)> unary_val_fn;
 
 // ========================================================================
 // Binary Functions
-typedef std::function<
-    nvfuser::TensorView*(nvfuser::TensorView*, nvfuser::TensorView*)>
+typedef std::function<nvf::TensorView*(nvf::TensorView*, nvf::TensorView*)>
     binary_tv_fn;
-typedef std::function<nvfuser::Val*(nvfuser::Val*, nvfuser::Val*)>
-    binary_val_fn;
-typedef std::function<nvfuser::TensorView*(nvfuser::TensorView*, nvfuser::Val*)>
+typedef std::function<nvf::Val*(nvf::Val*, nvf::Val*)> binary_val_fn;
+typedef std::function<nvf::TensorView*(nvf::TensorView*, nvf::Val*)>
     binary_tv_val_fn;
-typedef std::function<nvfuser::TensorView*(nvfuser::Val*, nvfuser::TensorView*)>
+typedef std::function<nvf::TensorView*(nvf::Val*, nvf::TensorView*)>
     binary_val_tv_fn;
 
 // ========================================================================
 // Ternary Functions
 // Binary with Alpha Functions
-typedef std::function<nvfuser::TensorView*(
-    nvfuser::TensorView*,
-    nvfuser::TensorView*,
-    nvfuser::TensorView*)>
+typedef std::function<
+    nvf::TensorView*(nvf::TensorView*, nvf::TensorView*, nvf::TensorView*)>
     ternary_tv_fn;
-typedef std::function<
-    nvfuser::Val*(nvfuser::Val*, nvfuser::Val*, nvfuser::Val*)>
+typedef std::function<nvf::Val*(nvf::Val*, nvf::Val*, nvf::Val*)>
     ternary_val_fn;
-typedef std::function<nvfuser::TensorView*(
-    nvfuser::TensorView*,
-    nvfuser::TensorView*,
-    nvfuser::Val*)>
+typedef std::function<
+    nvf::TensorView*(nvf::TensorView*, nvf::TensorView*, nvf::Val*)>
     ternary_tv_tv_val_fn;
-typedef std::function<nvfuser::TensorView*(
-    nvfuser::TensorView*,
-    nvfuser::Val*,
-    nvfuser::TensorView*)>
+typedef std::function<
+    nvf::TensorView*(nvf::TensorView*, nvf::Val*, nvf::TensorView*)>
     ternary_tv_val_tv_fn;
-typedef std::function<nvfuser::TensorView*(
-    nvfuser::Val*,
-    nvfuser::TensorView*,
-    nvfuser::TensorView*)>
+typedef std::function<
+    nvf::TensorView*(nvf::Val*, nvf::TensorView*, nvf::TensorView*)>
     ternary_val_tv_tv_fn;
-typedef std::function<
-    nvfuser::TensorView*(nvfuser::Val*, nvfuser::Val*, nvfuser::TensorView*)>
+typedef std::function<nvf::TensorView*(nvf::Val*, nvf::Val*, nvf::TensorView*)>
     ternary_val_val_tv_fn;
-typedef std::function<
-    nvfuser::TensorView*(nvfuser::TensorView*, nvfuser::Val*, nvfuser::Val*)>
+typedef std::function<nvf::TensorView*(nvf::TensorView*, nvf::Val*, nvf::Val*)>
     ternary_tv_val_val_fn;
-typedef std::function<
-    nvfuser::TensorView*(nvfuser::Val*, nvfuser::TensorView*, nvfuser::Val*)>
+typedef std::function<nvf::TensorView*(nvf::Val*, nvf::TensorView*, nvf::Val*)>
     ternary_val_tv_val_fn;
 
 // ========================================================================
 // Ternary with Alpha Functions
-typedef std::function<nvfuser::TensorView*(
-    nvfuser::TensorView*,
-    nvfuser::TensorView*,
-    nvfuser::TensorView*,
-    nvfuser::Val*)>
+typedef std::function<nvf::TensorView*(
+    nvf::TensorView*,
+    nvf::TensorView*,
+    nvf::TensorView*,
+    nvf::Val*)>
     ternary_alpha_tv_fn;
-typedef std::function<
-    nvfuser::Val*(nvfuser::Val*, nvfuser::Val*, nvfuser::Val*, nvfuser::Val*)>
+typedef std::function<nvf::Val*(nvf::Val*, nvf::Val*, nvf::Val*, nvf::Val*)>
     ternary_alpha_val_fn;
-typedef std::function<nvfuser::TensorView*(
-    nvfuser::TensorView*,
-    nvfuser::TensorView*,
-    nvfuser::Val*,
-    nvfuser::Val*)>
+typedef std::function<
+    nvf::TensorView*(nvf::TensorView*, nvf::TensorView*, nvf::Val*, nvf::Val*)>
     ternary_alpha_tv_tv_val_fn;
-typedef std::function<nvfuser::TensorView*(
-    nvfuser::TensorView*,
-    nvfuser::Val*,
-    nvfuser::TensorView*,
-    nvfuser::Val*)>
+typedef std::function<
+    nvf::TensorView*(nvf::TensorView*, nvf::Val*, nvf::TensorView*, nvf::Val*)>
     ternary_alpha_tv_val_tv_fn;
-typedef std::function<nvfuser::TensorView*(
-    nvfuser::Val*,
-    nvfuser::TensorView*,
-    nvfuser::TensorView*,
-    nvfuser::Val*)>
+typedef std::function<
+    nvf::TensorView*(nvf::Val*, nvf::TensorView*, nvf::TensorView*, nvf::Val*)>
     ternary_alpha_val_tv_tv_fn;
-typedef std::function<nvfuser::TensorView*(
-    nvfuser::Val*,
-    nvfuser::Val*,
-    nvfuser::TensorView*,
-    nvfuser::Val*)>
+typedef std::function<
+    nvf::TensorView*(nvf::Val*, nvf::Val*, nvf::TensorView*, nvf::Val*)>
     ternary_alpha_val_val_tv_fn;
-typedef std::function<nvfuser::TensorView*(
-    nvfuser::TensorView*,
-    nvfuser::Val*,
-    nvfuser::Val*,
-    nvfuser::Val*)>
+typedef std::function<
+    nvf::TensorView*(nvf::TensorView*, nvf::Val*, nvf::Val*, nvf::Val*)>
     ternary_alpha_tv_val_val_fn;
-typedef std::function<nvfuser::TensorView*(
-    nvfuser::Val*,
-    nvfuser::TensorView*,
-    nvfuser::Val*,
-    nvfuser::Val*)>
+typedef std::function<
+    nvf::TensorView*(nvf::Val*, nvf::TensorView*, nvf::Val*, nvf::Val*)>
     ternary_alpha_val_tv_val_fn;
 // ========================================================================
 
@@ -124,8 +92,7 @@ typedef std::function<nvfuser::TensorView*(
 class RecordFunctorFactory
     : public Factory<RecordFunctor, python_frontend::RecordFunctor*> {
  public:
-  RecordFunctorFactory()
-      : Factory((nvfuser::toUnderlying(RecordType::MAX) + 1)) {
+  RecordFunctorFactory() : Factory((nvf::toUnderlying(RecordType::MAX) + 1)) {
     setupFunctionMaps();
     registerAllParsers();
   }
