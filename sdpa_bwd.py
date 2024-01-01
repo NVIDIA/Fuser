b1e63e3d49abaa6b32d0bd8044b4c6fea2b86e78
Jingyue Wu <wujingyue@gmail.com>
2024-01-01 23:41:45 +0000

    Test torch MHA and update the config to xl.

--- sdpa_bwd.py
+++ sdpa_bwd.py
@@ -1,19 +1,61 @@
 import cudnn
 import math
 import torch
+from dataclasses import dataclass
 
 
-def test_sdpa_backward():
-    b = 16
-    h = 12
-    s = 128
-    d = 64
-    qkv_shape = (b, h, s, d)
+@dataclass
+class Config:
+    b: int
+    h: int
+    s: int
+    d: int
 
+
+def make_nanogpt_gpt2xl_config() -> Config:
+    return Config(16, 25, 128, 64)
+
+
+def test_torch_sdpa_backward():
+    config = make_nanogpt_gpt2xl_config()
+
+    qkv_shape = (config.b, config.h, config.s, config.d)
+    q_tensor = torch.randn(qkv_shape, dtype=torch.bfloat16).cuda()
+    k_tensor = torch.randn(qkv_shape, dtype=torch.bfloat16).cuda()
+    v_tensor = torch.randn(qkv_shape, dtype=torch.bfloat16).cuda()
+    o_tensor = torch.randn(qkv_shape, dtype=torch.bfloat16).cuda()
+    do_tensor = torch.randn(qkv_shape, dtype=torch.bfloat16).cuda()
+    # The logsumexp L returned by the forward pass. See https://arxiv.org/pdf/2307.08691.pdf
+    stats_tensor = torch.randn(
+        config.b, config.h, config.s, 1, dtype=torch.float32
+    ).cuda()
+    philox_seed = torch.randint(10, ())
+    philox_offset = torch.randint(10, ())
+
+    torch.ops.aten._scaled_dot_product_efficient_attention_backward(
+        do_tensor,
+        q_tensor,
+        k_tensor,
+        v_tensor,
+        None,
+        o_tensor,
+        stats_tensor,
+        philox_seed,
+        philox_offset,
+        0.0,
+        [False, False, False, False],
+    )
+    torch.cuda.synchronize()
+
+
+def test_cudnn_sdpa_backward():
+    config = make_nanogpt_gpt2xl_config()
+
+    qkv_shape = (config.b, config.h, config.s, config.d)
     stride_d = 1
-    stride_h = d * stride_d
-    stride_s = h * stride_h * 3
-    stride_b = s * stride_s
+    stride_h = config.d * stride_d
+    stride_s = config.h * stride_h * 3
+    stride_b = config.s * stride_s
     out_strides = (stride_b, stride_h, stride_s, stride_d)
 
     q_tensor = torch.randn(qkv_shape, dtype=torch.bfloat16).cuda()
@@ -23,14 +65,16 @@ def test_sdpa_backward():
     dqkv_tensor = torch.empty(math.prod(qkv_shape) * 3, dtype=torch.bfloat16).cuda()
     dq_tensor = torch.as_strided(dqkv_tensor, qkv_shape, out_strides, storage_offset=0)
     dk_tensor = torch.as_strided(
-        dqkv_tensor, qkv_shape, out_strides, storage_offset=h * stride_h
+        dqkv_tensor, qkv_shape, out_strides, storage_offset=config.h * stride_h
     )
     dv_tensor = torch.as_strided(
-        dqkv_tensor, qkv_shape, out_strides, storage_offset=h * stride_h * 2
+        dqkv_tensor, qkv_shape, out_strides, storage_offset=config.h * stride_h * 2
     )
     o_tensor = torch.randn(qkv_shape, dtype=torch.bfloat16).cuda()
     do_tensor = torch.randn(qkv_shape, dtype=torch.bfloat16).cuda()
-    stats_tensor = torch.randn(b, h, s, 1, dtype=torch.float32).cuda()
+    stats_tensor = torch.randn(
+        config.b, config.h, config.s, 1, dtype=torch.float32
+    ).cuda()
 
     graph = cudnn.pygraph(
         io_data_type=cudnn.data_type.BFLOAT16,
