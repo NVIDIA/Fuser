a709a6eae3482f305f0775c4ae607b23be4b93ab
Jingyue Wu <wujingyue@gmail.com>
2024-01-01 22:32:21 +0000

    Reformat.

--- sdpa_bwd.py
+++ sdpa_bwd.py
@@ -20,27 +20,23 @@ def test_sdpa_backward():
     k_tensor = torch.randn(qkv_shape, dtype=torch.bfloat16).cuda()
     v_tensor = torch.randn(qkv_shape, dtype=torch.bfloat16).cuda()
 
-    dqkv_tensor = torch.empty(math.prod(qkv_shape) * 3,
-                              dtype=torch.bfloat16).cuda()
-    dq_tensor = torch.as_strided(dqkv_tensor,
-                                 qkv_shape,
-                                 out_strides,
-                                 storage_offset=0)
-    dk_tensor = torch.as_strided(dqkv_tensor,
-                                 qkv_shape,
-                                 out_strides,
-                                 storage_offset=h * stride_h)
-    dv_tensor = torch.as_strided(dqkv_tensor,
-                                 qkv_shape,
-                                 out_strides,
-                                 storage_offset=h * stride_h * 2)
+    dqkv_tensor = torch.empty(math.prod(qkv_shape) * 3, dtype=torch.bfloat16).cuda()
+    dq_tensor = torch.as_strided(dqkv_tensor, qkv_shape, out_strides, storage_offset=0)
+    dk_tensor = torch.as_strided(
+        dqkv_tensor, qkv_shape, out_strides, storage_offset=h * stride_h
+    )
+    dv_tensor = torch.as_strided(
+        dqkv_tensor, qkv_shape, out_strides, storage_offset=h * stride_h * 2
+    )
     o_tensor = torch.randn(qkv_shape, dtype=torch.bfloat16).cuda()
     do_tensor = torch.randn(qkv_shape, dtype=torch.bfloat16).cuda()
     stats_tensor = torch.randn(b, h, s, 1, dtype=torch.float32).cuda()
 
-    graph = cudnn.pygraph(io_data_type=cudnn.data_type.BFLOAT16,
-                          intermediate_data_type=cudnn.data_type.FLOAT,
-                          compute_data_type=cudnn.data_type.FLOAT)
+    graph = cudnn.pygraph(
+        io_data_type=cudnn.data_type.BFLOAT16,
+        intermediate_data_type=cudnn.data_type.FLOAT,
+        compute_data_type=cudnn.data_type.FLOAT,
+    )
     q = graph.tensor_like(q_tensor)
     k = graph.tensor_like(k_tensor)
     v = graph.tensor_like(v_tensor)
@@ -49,12 +45,9 @@ def test_sdpa_backward():
     stats = graph.tensor_like(stats_tensor)
 
     dq, dk, dv = graph.sdpa_backward(q, k, v, o, do, stats)
-    dq.set_output(True).set_dim(dq_tensor.size()).set_stride(
-        dq_tensor.stride())
-    dk.set_output(True).set_dim(dk_tensor.size()).set_stride(
-        dk_tensor.stride())
-    dv.set_output(True).set_dim(dv_tensor.size()).set_stride(
-        dv_tensor.stride())
+    dq.set_output(True).set_dim(dq_tensor.size()).set_stride(dq_tensor.stride())
+    dk.set_output(True).set_dim(dk_tensor.size()).set_stride(dk_tensor.stride())
+    dv.set_output(True).set_dim(dv_tensor.size()).set_stride(dv_tensor.stride())
 
     graph.validate()
     graph.build_operation_graph()
@@ -62,8 +55,7 @@ def test_sdpa_backward():
     graph.check_support()
     graph.build_plans()
 
-    workspace = torch.empty(graph.get_workspace_size(),
-                            dtype=torch.uint8).cuda()
+    workspace = torch.empty(graph.get_workspace_size(), dtype=torch.uint8).cuda()
     graph.execute(
         {
             q: q_tensor,
@@ -74,6 +66,8 @@ def test_sdpa_backward():
             stats: stats_tensor,
             dq: dq_tensor,
             dk: dk_tensor,
-            dv: dv_tensor
-        }, workspace)
+            dv: dv_tensor,
+        },
+        workspace,
+    )
     torch.cuda.synchronize()
