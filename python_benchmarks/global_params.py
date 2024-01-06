6f7535fabc23cfa9db85ebf29735873bdbc3a4fb
root <26priya11@gmail.com>
2024-01-06 14:22:53 -0800

    reduce hidden size for batch size = 16384

--- python_benchmarks/global_params.py
+++ python_benchmarks/global_params.py
@@ -36,14 +36,25 @@ def generate_input_sizes(dims: Union[int, List] = 2) -> List[Tuple]:
 
     for dim in dims:
         if dim == 2:
-            # max_batch_range: set according to max size that fits in GPU memory
-            batch_range = [2**i for i in range(4, 15)]  # {16, 16384}
+            input_ranges = []
             step_size = 256
+
+            # max_batch_range: set according to max size that fits in GPU memory
+            batch_range = [2**i for i in range(4, 14)]  # {16, 8192}
             # max_hidden_size = 4 * d_model_max (max hidden size in feedforward layers)
             hidden_range = np.arange(
                 D_MODEL_MIN, 4 * D_MODEL_MAX + 1, step_size
             )  # (768, 4*18432)
-            inputs.extend([(i, j) for i in batch_range for j in hidden_range])
+            input_ranges.append((batch_range, hidden_range))
+
+            # Reduce max hidden size for largest batch size (16384) to avoid OOM in RMSNorm.
+            hidden_range = np.arange(
+                D_MODEL_MIN, 2 * D_MODEL_MAX + 1, step_size
+            )  # (768, 2*18432)
+            input_ranges.append(([16384], hidden_range))
+
+            for batch_range, hidden_range in input_ranges:
+                inputs.extend([(i, j) for i in batch_range for j in hidden_range])
         elif dim == 3:
             dim_range = [2**i for i in range(1, 10)]
             inputs.extend(
