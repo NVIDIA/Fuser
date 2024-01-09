785ace67f87eb55ea87458ae354eef1e1b9c80eb
root <26priya11@gmail.com>
2024-01-09 12:35:23 -0800

    fix errors in fd

--- python_benchmarks/test_dropout_layernorm_bwd.py
+++ python_benchmarks/test_dropout_layernorm_bwd.py
@@ -5,18 +5,15 @@ from .core import run_benchmark, clear_cuda_cache
 import torch
 from .global_params import generate_input_sizes, FLOAT_DTYPES, PROMOTE_DTYPES
 
-
 def dropout_layernorm_bwd_fusion(
     fd: FusionDefinition, dtype: DataType, dropout_p: float
 ) -> None:
-
     T1 = fd.define_tensor(shape=[-1, -1], contiguity=[True, True], dtype=DataType.Bool, is_cpu=False) # mask
     T2 = fd.define_tensor(shape=[-1], contiguity=[True], dtype=DataType.Float, is_cpu=False) # mean
     T3 = fd.define_tensor(shape=[-1, 1], contiguity=[True, None], dtype=DataType.Float, is_cpu=False) # invstd
     T4 = fd.define_tensor(shape=[-1, -1], contiguity=[True, True], dtype=dtype, is_cpu=False) # grads
     T5 = fd.define_tensor(shape=[-1], contiguity=[True], dtype=dtype, is_cpu=False) # weights
     T6 = fd.define_tensor(shape=[-1, -1], contiguity=[True, True], dtype=dtype, is_cpu=False) # inputs
-
     if dtype in PROMOTE_DTYPES:
         T1 = fd.ops.cast(T1, dtype=DataType.Float)
         T4 = fd.ops.cast(T4, dtype=DataType.Float)
@@ -30,11 +27,9 @@ def dropout_layernorm_bwd_fusion(
     
     V15 = fd.define_vector([T6.size(0), 1], dtype=DataType.Int)
     T16 = fd.ops.broadcast_in_dim(T2, shape=V15, broadcast_dims=[0])
-
     V19 = T6.shape()
     T20 = fd.ops.broadcast_in_dim(T16, shape=V19, broadcast_dims=[0, 1])
     T21 = fd.ops.sub(T12, T20)
-
     T25 = fd.ops.broadcast_in_dim(T3, shape=V19, broadcast_dims=[0, 1])
     T26 = fd.ops.mul(T21, T25)
     T30 = fd.ops.broadcast_in_dim(T5, shape=V19, broadcast_dims=[1])
@@ -47,11 +42,9 @@ def dropout_layernorm_bwd_fusion(
     T41 = fd.ops.mul(T37, T25)
     T42 = fd.ops.mul(T37, T21)
     T43 = fd.ops.sum(T42, axes=[1], keepdim=False, dtype=DataType.Null)
-
     T47 = fd.ops.broadcast_in_dim(T43, shape=V15, broadcast_dims=[0])
     T48 = fd.ops.neg(T41)
     T49 = fd.ops.sum(T48, axes=[1], keepdim=False, dtype=DataType.Null)
-
     T53 = fd.ops.broadcast_in_dim(T49, shape=V15, broadcast_dims=[0])
     S54 = fd.define_scalar(-0.500000, dtype=DataType.Double)
     T55 = fd.ops.mul(S54, T47)
@@ -60,10 +53,8 @@ def dropout_layernorm_bwd_fusion(
     T58 = fd.ops.mul(T55, T57)
     T61 = fd.ops.sum(T53, axes=[1], keepdim=False, dtype=DataType.Null)
     T62 = fd.ops.sum(T58, axes=[1], keepdim=False, dtype=DataType.Null)
-
     T66 = fd.ops.broadcast_in_dim(T62, shape=V15, broadcast_dims=[0])
     T70 = fd.ops.broadcast_in_dim(T66, shape=V19, broadcast_dims=[0, 1])
-
     T74 = fd.ops.broadcast_in_dim(T2, shape=V15, broadcast_dims=[0])
     T78 = fd.ops.broadcast_in_dim(T74, shape=V19, broadcast_dims=[0, 1])
     S79 = fd.define_scalar(2.00000, dtype=DataType.Double)
@@ -72,7 +63,6 @@ def dropout_layernorm_bwd_fusion(
     T82 = fd.ops.mul(T80, T81)
     S84 = fd.ops.reciprocal(T6.size(1))
     T85 = fd.ops.mul(T82, S84)
-
     T89 = fd.ops.broadcast_in_dim(T61, shape=V15, broadcast_dims=[0])
     T93 = fd.ops.broadcast_in_dim(T89, shape=V19, broadcast_dims=[0, 1])
     T95 = fd.ops.mul(S84, T93)
@@ -80,17 +70,14 @@ def dropout_layernorm_bwd_fusion(
     T97 = fd.ops.add(T41, T96)
     T101 = fd.ops.mul(T97, T1)
     T102 = fd.ops.add(T97, T101)
-
     if dtype in PROMOTE_DTYPES:
         T35 = fd.ops.cast(T35, dtype=dtype)
         T39 = fd.ops.cast(T39, dtype=dtype)
         T102 = fd.ops.cast(T102, dtype=dtype)
-
     fd.add_output(T102)
     fd.add_output(T39)
     fd.add_output(T35)
     
-
 @pytest.mark.parametrize("size", generate_input_sizes(dims=2))
 @pytest.mark.parametrize("dtype", FLOAT_DTYPES)
 def test_layernorm_bwd_benchmark(
@@ -102,38 +89,34 @@ def test_layernorm_bwd_benchmark(
     eps: float = 1e-5,
 ):
     clear_cuda_cache()
-
     inputs = torch.randn(*size, device="cuda", dtype=dtype, requires_grad=True)
     grads = torch.randn(*size, device="cuda", dtype=dtype)
     weights = torch.randn(size[1], device="cuda", dtype=dtype, requires_grad=True)
     bias = torch.randn(size[1], device="cuda", dtype=dtype, requires_grad=True)
-
-    mean = inputs.to(torch.float).mean(dim=-1)
-    variance = inputs.to(torch.float).var(dim=-1, unbiased=False)
-    invstd = (1.0 / torch.sqrt(variance + eps)).unsqueeze(1)
     dropout_p = 0.0
-
-    dropout_mask = torch.lt(torch.rand(*size, device="cuda"), dropout_p)
-
+    dropout_mask = torch.lt(torch.rand(*size, device="cuda"), 1 - dropout_p)
+    x = inputs + torch.nn.functional.dropout(inputs, dropout_p)
+    mean = x.to(torch.float).mean(dim=-1)
+    variance = x.to(torch.float).var(dim=-1, unbiased=False)
+    invstd = (1.0 / torch.sqrt(variance + eps)).unsqueeze(1)
+    
     with FusionDefinition() as fd:
         dropout_layernorm_bwd_fusion(
             fd, torch_dtype_to_nvfuser_dtype(dtype), dropout_p=dropout_p
         )
-
     if not disable_validation:
         eager_output = torch.nn.functional.layer_norm(
-            inputs.to(torch.double)
-            + torch.nn.functional.dropout(inputs.to(torch.double), dropout_p),
+            x.to(torch.double),
             inputs.shape[1:],
             weight=weights.to(torch.double),
             bias=bias.to(torch.double),
         )
+        
         eager_output.backward(grads.to(torch.double))
         fd.validate(
             [dropout_mask, mean, invstd, grads, weights, inputs],
             [inputs.grad, weights.grad, bias.grad]
         )
-
     if not disable_benchmarking:
         run_benchmark(
             benchmark, fd.execute, [dropout_mask, mean, invstd, grads, weights, inputs]
--- python_benchmarks/test_dropout_layernorm_fwd.py
+++ python_benchmarks/test_dropout_layernorm_fwd.py
@@ -5,7 +5,6 @@ from .core import run_benchmark, clear_cuda_cache
 import torch
 from .global_params import generate_input_sizes, FLOAT_DTYPES, PROMOTE_DTYPES
 
-
 def dropout_layernorm_fwd_fusion(
     fd: FusionDefinition, dtype: DataType, dropout_p: float, eps: float = 1e-5
 ) -> None:
@@ -14,54 +13,43 @@ def dropout_layernorm_fwd_fusion(
     )
     T1 = fd.define_tensor(shape=[-1], contiguity=[True], dtype=dtype, is_cpu=False)
     T0 = fd.define_tensor(shape=[-1], contiguity=[True], dtype=dtype, is_cpu=False)
-
     S3 = fd.define_scalar(0.00000, dtype=DataType.Double)
     S4 = fd.define_scalar(1.00000, dtype=DataType.Double)
     T8 = fd.ops.uniform(S3, S4, shape=T2.shape(), dtype=dtype)
-    S9 = fd.define_scalar(dropout_p, dtype=DataType.Double)
+    S9 = fd.define_scalar(1 - dropout_p, dtype=DataType.Double)
     T10 = fd.ops.lt(T8, S9)
-
     if dtype in PROMOTE_DTYPES:
         T0 = fd.ops.cast(T0, dtype=DataType.Float)
         T1 = fd.ops.cast(T1, dtype=DataType.Float)
         T2 = fd.ops.cast(T2, dtype=DataType.Float)
         T10 = fd.ops.cast(T10, dtype=DataType.Float)
-
     # Dropout + Add
     T13 = fd.ops.mul(T2, T10)
-
     S14 = fd.define_scalar(1 / (1 - dropout_p), dtype=DataType.Double)
     T15 = fd.ops.mul(T13, S14)
     T16 = fd.ops.add(T2, T15)
-
     # Layernorm
     T17, T18 = fd.ops.var_mean(T16, axes=[1], correction=0, keepdim=False)
-
     V21 = fd.define_vector([T2.size(0), 1], dtype=DataType.Int)
     T22 = fd.ops.broadcast_in_dim(T17, shape=V21, broadcast_dims=[0])
     T26 = fd.ops.broadcast_in_dim(T18, shape=V21, broadcast_dims=[0])
     S27 = fd.define_scalar(eps, dtype=DataType.Double)
     T28 = fd.ops.add(T22, S27)
     T29 = fd.ops.rsqrt(T28)
-
     T33 = fd.ops.broadcast_in_dim(T26, shape=T2.shape(), broadcast_dims=[0, 1])
     T34 = fd.ops.sub(T16, T33)
     T38 = fd.ops.broadcast_in_dim(T29, shape=T2.shape(), broadcast_dims=[0, 1])
     T39 = fd.ops.mul(T34, T38)
-
     T43 = fd.ops.broadcast_in_dim(T1, shape=T2.shape(), broadcast_dims=[1])
     T45 = fd.ops.mul(T39, T43)
     T49 = fd.ops.broadcast_in_dim(T0, shape=T2.shape(), broadcast_dims=[1])
     T51 = fd.ops.add(T45, T49)
-
     if dtype in PROMOTE_DTYPES:
         T51 = fd.ops.cast(T51, dtype=dtype)
-
     fd.add_output(T51)
     fd.add_output(T18)
     fd.add_output(T29)
     fd.add_output(T10)
-
 
 @pytest.mark.parametrize("size", generate_input_sizes(dims=2))
 @pytest.mark.parametrize("dtype", FLOAT_DTYPES)
@@ -74,37 +62,31 @@ def test_dropout_layernorm_fwd_benchmark(
     eps: float = 1e-5,
 ):
     clear_cuda_cache()
-
     inputs = [
         torch.randn(*size, device="cuda", dtype=dtype),
-        torch.randn(size[1], device="cuda", dtype=dtype),
-        torch.randn(size[1], device="cuda", dtype=dtype),
+        torch.ones(size[1], device="cuda", dtype=dtype),
+        torch.zeros(size[1], device="cuda", dtype=dtype),
     ]
     dropout_p = 0.0
-    dropout_mask = torch.lt(torch.rand(*size, device="cuda"), dropout_p)
-
+    dropout_mask = torch.lt(torch.rand(*size, device="cuda"), 1 - dropout_p)
     with FusionDefinition() as fd:
         dropout_layernorm_fwd_fusion(fd, torch_dtype_to_nvfuser_dtype(dtype), dropout_p)
-
     if not disable_validation:
+        # dropout + add
+        x = inputs[0] + torch.nn.functional.dropout(inputs[0], dropout_p)
+        # layernorm
         eager_output = torch.nn.functional.layer_norm(
-            inputs[0], inputs[0].shape[1:], weight=inputs[1], bias=inputs[2]
-        )
-
-        eager_output = torch.nn.functional.layer_norm(
-            inputs[0] + torch.nn.functional.dropout(inputs[0], dropout_p),
+            x,
             inputs[0].shape[1:],
             weight=inputs[1],
             bias=inputs[2],
         )
-
-        mean = inputs[0].to(torch.float).mean(dim=-1)
-        variance = inputs[0].to(torch.float).var(dim=-1, unbiased=False)
+        # mean and invstd are computed for the output of dropout + add
+        mean = x.to(torch.float).mean(dim=-1)
+        variance = x.to(torch.float).var(dim=-1, unbiased=False)
         invstd = (1.0 / torch.sqrt(variance + eps)).unsqueeze(1)
-
         fd.validate(
-            inputs, [eager_output.to(dtype), mean, invstd, dropout_mask.to(torch.float)]
+            inputs, [eager_output.to(dtype), mean, invstd, dropout_mask]
         )
-
     if not disable_benchmarking:
         run_benchmark(benchmark, fd.execute, inputs)
