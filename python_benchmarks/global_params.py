356de5d0c438b84d2c23d7e34de5839ef74d919a
Priya Mishra <52657555+Priya2698@users.noreply.github.com>
2024-01-10 11:24:25 -0800

    Attention benchmarks (#1602)

--- python_benchmarks/global_params.py
+++ python_benchmarks/global_params.py
@@ -5,27 +5,40 @@ from .core import DEVICE_PROPERTIES
 import numpy as np
 import itertools
 
+# Datatypes to benchmark
+FLOAT_DTYPES = [torch.float32]
+# Run only one of float16 / bfloat16.
+if DEVICE_PROPERTIES["gpu_compute_capability_major"] >= 8:
+    FLOAT_DTYPES.append(torch.bfloat16)
+else:
+    FLOAT_DTYPES.append(torch.float16)
+
+# Datatypes that will be promoted to Datatype.Float in Fusion Definitions
+PROMOTE_DTYPES = [DataType.BFloat16, DataType.Half]
+
 # Model Parameters from LLMs (GPT2/3, PaLM, LLama)
 
 # Embedding size: d_model, d_ff = 4 * d_model
 D_MODEL_MIN = 768
 D_MODEL_MAX = 18432
 
-# Actual d_model sizes seen in models.
-D_MODEL_SIZES = [
-    768,  # GPT-2 (124M), GPT-3 (125M)
-    1024,  # GPT-2 (350M), GPT-3 (350M)
-    1280,  # GPT-2 (774M)
-    1536,  # GPT-3 (760M), GPT-3 (760M)
-    1600,  # GPT-2 (1558M)
-    2048,  # GPT-3 (1.3B)
-    2560,  # GPT-3 (2.7B)
-    4096,  # LLaMA (7B), PaLM (8B), GPT-3 (6.7B)
-    5120,  # LLaMA (13B), GPT-3 (13B)
-    6656,  # LLaMA (30B)
-    8192,  # LLaMA (65B), PaLM (63B)
-    12288,  # GPT-3 (175B)
-    18432,  # PaLM (540B)
+# (num_heads, n_embd) configurations seen in models.
+LLM_CONFIGS = [
+    (12, 768),  # GPT-2 (124M), GPT-3 (125M)
+    (16, 1024),  # GPT-2 (350M), GPT-3 (350M)
+    (20, 1280),  # GPT-2 (774M)
+    (16, 1536),  # GPT-3 (760M)
+    (25, 1600),  # GPT-2 (1558M)
+    (24, 2048),  # GPT-3 (1.3B)
+    (32, 2560),  # GPT-3 (2.7B)
+    (16, 4096),  # PaLM (8B)
+    (32, 4096),  # LLaMA (7B), GPT-3 (6.7B)
+    (40, 5120),  # LLaMA (13B), GPT-3 (13B)
+    (52, 6656),  # LLaMA (30B)
+    (32, 8192),  # PaLM (63B)
+    (64, 8192),  # LLaMA (65B)
+    (96, 12288),  # GPT-3 (175B)
+    (48, 18432),  # PaLM (540B)
 ]
 
 
@@ -113,13 +126,14 @@ def generate_input_sizes(dims: Union[int, List] = 2) -> List[Tuple]:
     return inputs
 
 
-# Datatypes to benchmark
-FLOAT_DTYPES = [torch.float32]
-# Run only one of float16 / bfloat16.
-if DEVICE_PROPERTIES["gpu_compute_capability_major"] >= 8:
-    FLOAT_DTYPES.append(torch.bfloat16)
-else:
-    FLOAT_DTYPES.append(torch.float16)
-
-# Datatypes that will be promoted to Datatype.Float in Fusion Definitions
-PROMOTE_DTYPES = [DataType.BFloat16, DataType.Half]
+# Utility function to generate input sizes for attention benchmarks.
+def generate_attn_inputs():
+    batch_range = [16, 32]
+    seq_lengths = [2**i for i in range(2, 10)]  # {4, 512}
+    inputs = [
+        (batch_size, seq_len, nh, n_embd)
+        for (batch_size, seq_len, (nh, n_embd)) in itertools.product(
+            batch_range, seq_lengths, LLM_CONFIGS
+        )
+    ]
+    return inputs
