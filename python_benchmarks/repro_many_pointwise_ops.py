a4a304f29fabe5d34bd51b5d2609011a3a388068
root <26priya11@gmail.com>
2024-01-12 15:24:15 -0800

    repro script for comparison

--- python_benchmarks/repro_many_pointwise_ops.py
+++ python_benchmarks/repro_many_pointwise_ops.py
@@ -1,7 +1,7 @@
 import pytest
 from nvfuser import FusionDefinition, DataType
 from nvfuser.pytorch_utils import torch_dtype_to_nvfuser_dtype
-from .core import run_benchmark, clear_cuda_cache
+from .core import clear_cuda_cache
 import torch
 from .global_params import PROMOTE_DTYPES
 
@@ -27,28 +27,16 @@ def pointwise_ops_fusion(
         a = fd.ops.cast(a, dtype=dtype)
     fd.add_output(a)
 
-#NOTE: num_iters restricted due to issue #1234.
-@pytest.mark.parametrize("num_iters", [2, 4, 8, 16])
 def test_pointwise_ops_benchmark(
-    benchmark,
-    num_iters: int,
-    disable_validation: bool,
-    disable_benchmarking: bool,
+    num_iters: int
 ):  
     clear_cuda_cache()
     inputs = [torch.randn(13, device="cuda", dtype=torch.float16) for _ in range(2)]
     with FusionDefinition() as fd:
         pointwise_ops_fusion(fd, torch_dtype_to_nvfuser_dtype(torch.float16), num_iters)
-    
-    if not disable_validation:
-        eager_output = inputs[0] + inputs[1]
-        for _ in range(num_iters):
-            x = torch.cos(eager_output)
-            y = torch.sin(eager_output)
-            eager_output = x + y
 
-        fd.validate(inputs, [eager_output])
+    print(f'num_iters: {num_iters}')
+    fd.execute(inputs)
 
-    if not disable_benchmarking:
-        run_benchmark(benchmark, fd.execute, inputs, device="cpu")
-   
+for num_iters in [2, 4, 8, 16]:
+    test_pointwise_ops_benchmark(num_iters)
