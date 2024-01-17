b82051116a053ebc12d89f80dca76c60a4473f12
Xiang Gao <qasdfgtyuiop@gmail.com>
2024-01-17 07:35:03 -0800

    fix

--- csrc/device_lower/pass/index.cpp
+++ csrc/device_lower/pass/index.cpp
@@ -1342,7 +1342,8 @@ void IndexLowering::handleCpAsyncBulkLoad(const LoadStoreOp* ldst) {
 
 void IndexLowering::handleCpAsyncBulkStore(const LoadStoreOp* ldst) {
   auto in = IrBuilder::create<kir::TensorIndex>(
-      ldst->in(), IrBuilder::baseAddressExpr(ldst->in()));
+      ldst->in()->as<TensorView>(),
+      IrBuilder::baseAddressExpr(ldst->in()->as<TensorView>()));
   auto out_tv = ldst->out()->as<TensorView>();
   auto out = Index::cpAsyncBulkIndex(out_tv, out_tv, nullptr, for_loops_);
   auto new_ldst =
