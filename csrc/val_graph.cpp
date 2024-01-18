64b409ef096d583fc298bd6afa4eaa23908435c9
Naoya Maruyama <nmaruyama@nvidia.com>
2024-01-17 17:22:02 -0800

    fix memory usage

--- csrc/val_graph.cpp
+++ csrc/val_graph.cpp
@@ -649,10 +649,13 @@ void ValGraph::mapVals(Val* val0, Val* val1) {
   const ValGroup orig_val_group0 = toGroup(val0);
   const ValGroup orig_val_group1 = toGroup(val1);
 
-  const ExprGroups& orig_defs0 = getDefinitions(orig_val_group0);
-  const ExprGroups& orig_defs1 = getDefinitions(orig_val_group1);
-  const ExprGroups& orig_uses0 = getUses(orig_val_group0);
-  const ExprGroups& orig_uses1 = getUses(orig_val_group1);
+  // Note that getDefinitions and getUses return references, which
+  // will be invalidated once unique_definitions_ and unique_uses_ are
+  // updated
+  const ExprGroups orig_defs0 = getDefinitions(orig_val_group0);
+  const ExprGroups orig_defs1 = getDefinitions(orig_val_group1);
+  const ExprGroups orig_uses0 = getUses(orig_val_group0);
+  const ExprGroups orig_uses1 = getUses(orig_val_group1);
 
   // Map the iter domains together before we traverse across definitions and
   // uses. Traversing definitions and uses could use the new property of id0 and
@@ -666,7 +669,11 @@ void ValGraph::mapVals(Val* val0, Val* val1) {
   // Propagate on uses
   if (!orig_uses0.empty() && !orig_uses1.empty()) {
     for (const ExprGroup& use_group_1 : orig_uses1) {
+      NVF_ERROR(use_group_1.get() != nullptr);
+      NVF_ERROR(!use_group_1->empty());
       for (const ExprGroup& use_group_0 : orig_uses0) {
+        NVF_ERROR(use_group_0.get() != nullptr);
+        NVF_ERROR(!use_group_0->empty());
         if (use_group_0 == use_group_1) {
           continue;
         }
@@ -680,7 +687,11 @@ void ValGraph::mapVals(Val* val0, Val* val1) {
   // Propagate on definitions
   if (!orig_defs0.empty() && !orig_defs1.empty()) {
     for (const ExprGroup& def_group_1 : orig_defs1) {
+      NVF_ERROR(def_group_1.get() != nullptr);
+      NVF_ERROR(!def_group_1->empty());
       for (const ExprGroup& def_group_0 : orig_defs0) {
+        NVF_ERROR(def_group_0.get() != nullptr);
+        NVF_ERROR(!def_group_0->empty());
         if (def_group_0 == def_group_1) {
           continue;
         }
