NVIDIA/Fuser

commit e3430a92173841216ee6d3ed696dba4cf46769e0

Set allocation domains of intermediate tensors for aliasing. (#1579)

csrc/optimization/mark_aliases_prepare.cpp
@@ -28,37 +28,37 @@ void MarkAliasesPreparePass::runPass(Fusion* fusion) {
   // an input). Code will later add `segment_set` before them so aliases are
   // separated from non-aliases and more likely to be accepted by the no-op
   // scheduler.
-  std::vector<TensorView*> aliased_outs;
-  aliased_outs.reserve(fusion->outputs().size());
+  std::unordered_set<TensorView*> aliased_outs;
 
-  for (TensorView* out :
-       ir_utils::filterByType<TensorView>(fusion->outputs())) {
-    TensorView* aliased_io = analysis.getNearestAliasedIo(out);
+  for (TensorView* tv : ir_utils::allTvs(fusion)) {
+    TensorView* aliased_io = analysis.getNearestAliasedIo(tv);
     if (aliased_io == nullptr) {
       continue;
     }
 
     if (aliased_io->isFusionOutput() && !aliased_io->isFusionInput() &&
         analysis.getNearestAliasedIo(aliased_io) == nullptr) {
-      aliased_outs.push_back(aliased_io);
+      aliased_outs.insert(aliased_io);
     }
 
-    // We already checked it's compatible; no need to change.
-    if (out->hasAllocation()) {
+    // `AliasAnalysisResult::finalize` already checked the alias-enabling layout
+    // is compliant with `tv`'s existing layout before adding `tv` to
+    // `alias_to_root_`. So the existing layout can remain unchanged.
+    if (tv->hasAllocation()) {
       continue;
     }
 
-    // A scalar `out` triggers a corner case that crashes
+    // A scalar `tv` triggers a corner case that crashes
     // `validateDomainEquivalence`.
-    if (out->isZeroDim()) {
+    if (tv->isZeroDim()) {
       continue;
     }
 
-    const Layout preferred_layout = analysis.preferredLayout(out);
-    out->setAllocationDomain(
+    const Layout preferred_layout = analysis.preferredLayout(tv);
+    tv->setAllocationDomain(
         preferred_layout.allocation_domain, preferred_layout.contiguity);
     if (isDebugDumpEnabled(DebugDumpOption::PreSegmenterLogging)) {
-      debug() << "Set the layout of " << ir_utils::varName(out) << " to "
+      debug() << "Set the layout of " << ir_utils::varName(tv) << " to "
               << preferred_layout.toString() << std::endl;
     }
   }

test/test_alias.cpp
@@ -30,6 +30,7 @@ using testing::IsTrue;
 using testing::Not;
 using testing::Optional;
 using testing::Pair;
+using testing::UnorderedElementsAre;
 
 using AliasAnalysisTest = NVFuserTest;
 
@@ -529,7 +530,6 @@ TEST_F(AliasTest, SliceViewPermute) {
 }
 
 TEST_F(AliasTest, DuplicateOutputsSegmentedFusion) {
-  // testing duplicated output in segmented fusion
   auto fusion = std::make_unique<Fusion>();
   FusionGuard fg(fusion.get());
 
@@ -537,37 +537,29 @@ TEST_F(AliasTest, DuplicateOutputsSegmentedFusion) {
 
   TensorView* in = makeContigConcreteTensor(in_shape);
   fusion->addInput(in);
-  TensorView* intermediate_tv = add(in, IrBuilder::create<Val>(3.141));
-  TensorView* segment_tv = segment_set(intermediate_tv);
-  TensorView* out = mul(segment_tv, IrBuilder::create<Val>(2.0));
+  TensorView* mid = add(in, IrBuilder::create<Val>(3.141));
+  mid = segment_set(mid);
+  TensorView* out = mul(mid, IrBuilder::create<Val>(2.0));
 
-  fusion->addOutput(intermediate_tv);
-  fusion->addOutput(intermediate_tv);
+  fusion->addOutput(mid);
+  fusion->addOutput(mid);
   fusion->addOutput(out);
-  fusion->addOutput(out); // duplicated outputs
+  fusion->addOutput(out);
 
   FusionExecutorCache fec(std::move(fusion));
   at::Tensor in_tensor =
       at::randn(in_shape, at::dtype(at::kFloat).device(at::kCUDA, 0));
   std::vector<at::Tensor> out_tensors = fec.runFusionWithInputs({in_tensor});
-  ASSERT_EQ(out_tensors.size(), 4);
-  at::Tensor out_tensor_0 = out_tensors[0];
-  at::Tensor out_tensor_1 = out_tensors[1];
-  at::Tensor out_tensor_2 = out_tensors[2];
-  at::Tensor out_tensor_3 = out_tensors[3];
+  testValidate(fec.fusion(), out_tensors, {in_tensor}, __LINE__, __FILE__);
 
   // Verify aliasing among duplicated outputs
-  EXPECT_TRUE(out_tensor_0.is_alias_of(out_tensor_1));
-  EXPECT_TRUE(out_tensor_2.is_alias_of(out_tensor_3));
-  // Verify segmentation
-  NVF_CHECK(
-      fec.getMostRecentKernelRuntime()->fusionSegments()->groups().size() == 2,
-      "segmentation didn't happen as expected");
+  EXPECT_TRUE(out_tensors[0].is_alias_of(out_tensors[1]));
+  EXPECT_TRUE(out_tensors[2].is_alias_of(out_tensors[3]));
 
-  at::Tensor intermediate_tensor = in_tensor.add(3.141);
-  at::Tensor out_tensor = intermediate_tensor.mul(2.0);
-  // Verify output values.
-  testValidate(fec.fusion(), out_tensors, {in_tensor}, __LINE__, __FILE__);
+  // Verify segmentation
+  EXPECT_EQ(
+      fec.getMostRecentKernelRuntime()->fusionSegments()->groups().size(), 2)
+      << "segmentation didn't happen as expected";
 }
 
 namespace {
@@ -920,6 +912,37 @@ TEST_F(AliasTest, SourceIsBothInputAndOutput) {
 
   EXPECT_EQ(in_tensor.data_ptr(), out_tensors[0].data_ptr());
   EXPECT_EQ(in_tensor.data_ptr(), out_tensors[1].data_ptr());
+}
+
+MATCHER_P(HeuristicIs, heuristic, "") {
+  return arg->heuristic() == heuristic;
+}
+
+TEST_F(AliasTest, SegmentBoundary) {
+  auto fusion = std::make_unique<Fusion>();
+  FusionGuard fg(fusion.get());
+
+  TensorView* in = makeContigConcreteTensor({2, 3});
+  TensorView* out = permute(in, {1, 0});
+  // With the current segmentation algorithm, `slice` has to be the start of a
+  // fusion. So we expect `permute` to form a meta-op-only segment and the rest
+  // a pointwise segment.
+  out = slice(out, {0, 0}, {2, 2});
+  out = add(out, out);
+  fusion->addInput(in);
+  fusion->addOutput(out);
+
+  FusionExecutorCache fec(std::move(fusion));
+  at::Tensor in_tensor = at::randn({2, 3}).cuda();
+  at::Tensor out_tensor = fec.runFusionWithInputs({in_tensor})[0];
+  testValidate(fec.fusion(), {out_tensor}, {in_tensor}, __LINE__, __FILE__);
+
+  FusionKernelRuntime* runtime = fec.getMostRecentKernelRuntime();
+  EXPECT_THAT(
+      runtime->fusionSegments()->groups(),
+      UnorderedElementsAre(
+          HeuristicIs(ScheduleHeuristic::NoOp),
+          HeuristicIs(ScheduleHeuristic::PointWise)));
 }
 
 } // namespace nvfuser

csrc/alias_analysis.cpp
@@ -395,52 +395,44 @@ void AliasAnalysisResult::add(
       i->second.first);
 }
 
-TensorView* AliasAnalysisResult::findNearestAliasedIo(
-    TensorView* fusion_out) const {
-  TensorView* root = fusion_out;
-  do {
-    const auto i = alias_to_source_.find(root);
-    root = (i == alias_to_source_.end() ? nullptr : i->second.first);
-  } while (root != nullptr && !root->isFusionInput() &&
-           !root->isFusionOutput());
-  return root;
-}
-
 TensorView* AliasAnalysisResult::getNearestAliasedIo(
-    const TensorView* fusion_out) const {
-  const auto i = out_to_root_.find(fusion_out);
-  return i == out_to_root_.end() ? nullptr : i->second;
+    const TensorView* alias) const {
+  const auto i = alias_to_root_.find(alias);
+  return i == alias_to_root_.end() ? nullptr : i->second;
 }
 
 namespace {
 bool okToRelayout(
-    const TensorView* out,
+    const TensorView* tv,
     const Layout& new_layout,
     const bool can_override_empty_allocation_domain) {
-  const std::vector<IterDomain*> out_allocation =
-      (can_override_empty_allocation_domain ? out->getAllocationDomain()
-                                            : out->getMaybeAllocationDomain());
-  return new_layout.isCompliantWith({out_allocation, out->getContiguity()});
+  const std::vector<IterDomain*> allocation =
+      (can_override_empty_allocation_domain ? tv->getAllocationDomain()
+                                            : tv->getMaybeAllocationDomain());
+  return new_layout.isCompliantWith({allocation, tv->getContiguity()});
 }
 } // namespace
 
 void AliasAnalysisResult::finalize(
-    Fusion* fusion,
     const bool can_override_empty_allocation_domain) {
-  for (TensorView* out :
-       ir_utils::filterByType<TensorView>(fusion->outputs())) {
-    TensorView* root = findNearestAliasedIo(out);
+  for (auto [alias, root_and_layout] : alias_to_source_) {
+    auto [root, preferred_layout] = root_and_layout;
+    // Walks up the `alias_to_source_` chain.
+    while (root != nullptr && !root->isFusionInput() &&
+           !root->isFusionOutput()) {
+      const auto i = alias_to_source_.find(root);
+      root = (i == alias_to_source_.end() ? nullptr : i->second.first);
+    }
     if (root == nullptr) {
       continue;
     }
 
-    const Layout preferred_layout = preferredLayout(out);
     if (!okToRelayout(
-            out, preferred_layout, can_override_empty_allocation_domain)) {
+            alias, preferred_layout, can_override_empty_allocation_domain)) {
       continue;
     }
 
-    out_to_root_[out] = root;
+    alias_to_root_[alias] = root;
   }
 }
 
@@ -456,7 +448,7 @@ Layout AliasAnalysisResult::preferredLayout(const Val* v) const {
 
 std::string AliasAnalysisResult::toString(const int indent_size) const {
   std::stringstream ss;
-  indent(ss, indent_size) << "All aliases:"
+  indent(ss, indent_size) << "Potential aliases:"
                           << (alias_to_source_.empty() ? " <empty>" : "")
                           << std::endl;
   for (const auto& [alias, source_and_layout] : alias_to_source_) {
@@ -466,13 +458,15 @@ std::string AliasAnalysisResult::toString(const int indent_size) const {
         << ir_utils::varName(source) << " if its layout is "
         << layout.toString() << std::endl;
   }
-  indent(ss, indent_size) << "Output aliases only:"
-                          << (out_to_root_.empty() ? " <empty>" : "")
-                          << std::endl;
-  for (const auto& [out, root] : out_to_root_) {
+  indent(ss, indent_size) << "Finalized aliases:" << std::endl;
+  for (const auto& [alias, root] : alias_to_root_) {
     indent(ss, indent_size + 1)
-        << ir_utils::varName(out) << " is a transitive alias of "
-        << ir_utils::varName(root) << std::endl;
+        << ir_utils::varName(alias) << " of allocation domain ["
+        << toDelimitedString(alias->getAllocationDomain())
+        << "] and rfactor domain ["
+        << toDelimitedString(alias->getMaybeRFactorDomain())
+        << "] is a transitive alias of " << ir_utils::varName(root)
+        << std::endl;
   }
   return ss.str();
 }
@@ -491,7 +485,7 @@ AliasAnalysisResult findAliases(
     // results).
     finder.dispatch(expr);
   }
-  analysis.finalize(fusion, can_override_empty_allocation_domain);
+  analysis.finalize(can_override_empty_allocation_domain);
   return analysis;
 }
 

csrc/alias_analysis.h
@@ -38,7 +38,7 @@ struct Layout {
 // analysis.add(...);
 // ...
 // analysis.add(...);
-// analysis.finalize(fusion, ...);
+// analysis.finalize(...);
 //
 // // The user can now call const methods to retrieve information.
 // ```
@@ -54,9 +54,10 @@ class AliasAnalysisResult {
   // preferred layout.
   void add(const TensorView* alias, TensorView* source, Layout&& layout);
 
+  // Computes transitive aliases and caches them in `alias_to_root_`.
   // See `findAliases` for the meaning of
   // `can_override_empty_allocation_domain`.
-  void finalize(Fusion* fusion, bool can_override_empty_allocation_domain);
+  void finalize(bool can_override_empty_allocation_domain);
 
   // Returns the preferred layout. If `alias` is not in `preferred_layout_`,
   // returns the `TensorView`'s initial layout.
@@ -64,25 +65,23 @@ class AliasAnalysisResult {
 
   std::string toString(int indent_size) const;
 
-  // Gets the nearest aliased fusion input/output of a `fusion_out` other than
-  // `fusion_out` itself. Returns null if that doesn't exist.
-  TensorView* getNearestAliasedIo(const TensorView* fusion_out) const;
+  // Returns the mapped value in `alias_to_root_` or null.
+  TensorView* getNearestAliasedIo(const TensorView* alias) const;
 
  private:
-  // Same as `getNearestAliasedIo` except that the `get` method returns the
-  // cached result.
-  TensorView* findNearestAliasedIo(TensorView* fusion_out) const;
-
-  // Maps aliases (e.g. the output of a View) to their direct sources (e.g. the
-  // input of the same View). Also stores the preferred output layout for the
-  // alias. Consider path compression, a common optimization used in
+  // Maps an alias (e.g. the output of a `ViewOp`) to its direct source (e.g.
+  // the input of the same `ViewOp`). Also stores the preferred output layout
+  // for the alias. Consider path compression, a common optimization used in
   // disjoint-set data structure, so it's easy to figure out the root of an
   // alias.
   std::unordered_map<const TensorView*, std::pair<TensorView*, Layout>>
       alias_to_source_;
 
-  // Maps a fusion output to its nearest aliased fusion input/output.
-  std::unordered_map<const TensorView*, TensorView*> out_to_root_;
+  // Maps an alias to its nearest, transitively aliased fusion input/output, if
+  // its preferred layout is compliant with its actual layout.
+  //
+  // TODO(wujingyue): consider to merge `alias_to_source_` and `alias_to_root_`.
+  std::unordered_map<const TensorView*, TensorView*> alias_to_root_;
 };
 
 // Finds aliases of the fusion inputs. The analysis should be conservative --

commit 57daf5151d8091c4cad2412c6ecd193505cb5be3

Fix #1646, a bug introduced by #1579.

csrc/optimization/mark_aliases_prepare.cpp
@@ -23,10 +23,10 @@ void MarkAliasesPreparePass::runPass(Fusion* fusion) {
     debug() << analysis.toString(/*indent_size=*/1) << std::endl;
   }
 
-  // Fusion outputs that are (1) aliased by others, (2) not aliases
-  // themselves, and (3) not fusion inputs (yes, a fusion may trivially forward
-  // an input). Code will later add `segment_set` before them so aliases are
-  // separated from non-aliases and more likely to be accepted by the no-op
+  // Fusion outputs that are (1) aliased by another fusion output, (2) not
+  // aliases themselves, and (3) not fusion inputs (yes, a fusion may trivially
+  // forward an input). Code will later add `segment_set` before them so aliases
+  // are separated from non-aliases and more likely to be accepted by the no-op
   // scheduler.
   std::unordered_set<TensorView*> aliased_outs;
 
@@ -36,7 +36,8 @@ void MarkAliasesPreparePass::runPass(Fusion* fusion) {
       continue;
     }
 
-    if (aliased_io->isFusionOutput() && !aliased_io->isFusionInput() &&
+    if (tv->isFusionOutput() && aliased_io->isFusionOutput() &&
+        !aliased_io->isFusionInput() &&
         analysis.getNearestAliasedIo(aliased_io) == nullptr) {
       aliased_outs.insert(aliased_io);
     }

test/test_alias.cpp
@@ -853,6 +853,30 @@ TEST_F(AliasTest, OutputAliasesAnotherOutput) {
   EXPECT_TRUE(permute_out_tensor.is_alias_of(reshape_out_tensor));
 }
 
+TEST_F(AliasTest, OutputNotAliasedByAnotherOutputShouldNotBeSegmented) {
+  // Reproduces #1646.
+  auto fusion = std::make_unique<Fusion>();
+  FusionGuard fg(fusion.get());
+
+  TensorView* in = makeContigConcreteTensor({2, 3, 5});
+  TensorView* add_out = add(in, in);
+  TensorView* reshape_out = reshape(add_out, {2, 3, 5}, {6, 5});
+  TensorView* permute_out = permute(reshape_out, {1, 0});
+  TensorView* mul_out = mul(permute_out, permute_out);
+
+  fusion->addInput(in);
+  fusion->addOutput(reshape_out);
+  fusion->addOutput(mul_out);
+
+  FusionExecutorCache fec(std::move(fusion));
+  at::Tensor in_tensor = at::randn({2, 3, 5}).cuda();
+  std::vector<at::Tensor> out_tensors = fec.runFusionWithInputs({in_tensor});
+  testValidate(fec.fusion(), out_tensors, {in_tensor}, __LINE__, __FILE__);
+
+  FusionKernelRuntime* runtime = fec.getMostRecentKernelRuntime();
+  EXPECT_FALSE(runtime->isSegmented());
+}
+
 TEST_F(AliasTest, ManyAliasesBetweenOutputs) {
   auto fusion = std::make_unique<Fusion>();
   FusionGuard fg(fusion.get());