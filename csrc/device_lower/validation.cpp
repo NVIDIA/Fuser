c9e27a72399d4fba0bde72fbece940ae09059556
Jacob Hinkle <jhinkle@nvidia.com>
2024-01-19 13:39:24 +0000

    Restore changes from branch

--- csrc/device_lower/validation.cpp
+++ csrc/device_lower/validation.cpp
@@ -579,9 +579,10 @@ void validateAndCollectVectorizeInfo(Fusion* fusion) {
       }
     }
     if (has_vectorize_dim) {
+      Expr* def = tv->definition();
       NVF_ERROR(
-          tv->definition() == nullptr || tv->definition()->isA<LoadStoreOp>() ||
-              tv->definition()->isA<SliceOp>(),
+          def == nullptr || def->isA<LoadStoreOp>() ||
+              def->isA<SliceOp>() || (def->isA<ReductionOp>()), // && def->as<ReductionOp>()->SerialGridReductionRequested(),
           "Vectorized accesses cannot be inline with computation, they are only supported with a Set operation.",
           "TensorView: ",
           tv);
