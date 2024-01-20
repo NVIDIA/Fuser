99cc724bfd0076d50303283038148b230f65326b
Jingyue Wu <wujingyue@gmail.com>
2024-01-20 00:46:21 +0000

    The hack in tensor_metadata.cpp seems unnecessary.

--- csrc/tensor_metadata.cpp
+++ csrc/tensor_metadata.cpp
@@ -217,7 +217,7 @@ void validateAllocationSizesAndStrides(
   // Validate contiguity
   int64_t contiguous_stride = 1;
   auto contiguity_rev = contiguity.crbegin();
-  for (auto i = (int64_t)sizes.size() - 1; i >= 0; i--) {
+  for (int64_t i = (int64_t)sizes.size() - 1; i >= 0; i--) {
     if (alloc_dom_no_reductions.at(i)->isBroadcast()) {
       continue;
     }
@@ -228,21 +228,10 @@ void validateAllocationSizesAndStrides(
     auto stride = strides.at(i);
     NVF_ERROR(!contiguity.empty());
     auto last_contiguity = *contiguity_rev;
-    contiguity_rev++;
     NVF_ERROR(
         last_contiguity.has_value(),
         "I don't think this check makes sense, but unfortunately ",
         "clang-tidy is not smart enough to infer from the context that this is always true.");
-
-    // TODO(#1126): Due to #1126, alias analysis sometimes is more accurate than
-    // IterType in telling whether an IterDomain is broadcast. So, some
-    // IterDommains in `alloc_dom_no_reductions` may be at runtime size-1 or
-    // stride-0 even though their `isBroadcast` returns false. When that
-    // happens, skip the stride check and don't mess up `contiguous_stride`.
-    if (size == 1 || stride == 0) {
-      continue;
-    }
-
     if (*last_contiguity) {
       NVF_CHECK(
           stride == contiguous_stride,
@@ -257,8 +246,8 @@ void validateAllocationSizesAndStrides(
           stride);
     }
     contiguous_stride = stride * size;
+    contiguity_rev++;
   }
-
   NVF_ERROR(
       std::none_of(
           contiguity_rev,
