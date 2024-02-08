34a99713271014bbde78cb60c570a85eb1f807a4
root <26priya11@gmail.com>
2024-02-08 22:39:03 +0000

    clean unnecessary checks; use fusion output instead of mma->out

--- csrc/fusion.cpp
+++ csrc/fusion.cpp
@@ -829,6 +829,7 @@ bool Fusion::hasDynamicTransform() {
 }
 
 void Fusion::markOutputForEvaluation(Val* output){
+  NVF_CHECK(output->isFusionOutput(), "Only fusion outputs can be expression evaluaed.");
   io_alias_[output] = AliasInfo{
     .type = AllocationType::Evaluate,
     .aliased_io = nullptr,
