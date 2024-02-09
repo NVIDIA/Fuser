e521180d02a029f2311b6d3c636dcf823edd94d9
root <26priya11@gmail.com>
2024-02-09 00:19:28 +0000

    add tests

--- test/test_matmul_default.cpp
+++ test/test_matmul_default.cpp
@@ -20,7 +20,7 @@ namespace nvfuser {
 
 class MatmulDefaultTest : public NVFuserTest {};
 
-TEST (MatmulDefaultTest, ComputeMmaThroughEE){
+TEST (MatmulDefaultTest, SingleMmaOp){
     auto fusion = std::make_unique<Fusion>();
     FusionGuard fg(fusion.get());
     EnableOptionsGuard::getCurOptions().set(EnableOption::MatmulExprEval);
@@ -44,6 +44,69 @@ TEST (MatmulDefaultTest, ComputeMmaThroughEE){
 
     FusionExecutorCache fec(std::move(fusion));
     auto out = fec.runFusionWithInputs({t0, t1});
+
+    EXPECT_TRUE(at::allclose(out[0], out_ref));
+}
+
+TEST (MatmulDefaultTest, MmaOpAndCast){
+    auto fusion = std::make_unique<Fusion>();
+    FusionGuard fg(fusion.get());
+    EnableOptionsGuard::getCurOptions().set(EnableOption::MatmulExprEval);
+
+    int64_t m = 2, k = 3, n = 4;
+    std::vector<int64_t> a_shape{m, k}, b_shape{k, n}, out_shape{m, n};
+
+    auto tv0 = makeConcreteTensor(a_shape, DataType::Half);
+    auto tv1 = makeConcreteTensor(b_shape, DataType::Half);
+    auto tv0b = broadcast(tv0, {false, false, true}); // [M, K, 1]
+    auto tv1b = broadcast(tv1, {true, false, false}); // [1, K, N]
+    auto tv2 = fusedMultiplySum(tv0b, tv1b, {1});
+    auto tv3 = castOp(DataType::Half, tv2);
+
+    fusion->addInput(tv0);
+    fusion->addInput(tv1);
+    fusion->addOutput(tv3);
+
+    at::Tensor t0 = at::ones(a_shape, at::kHalf).cuda();
+    at::Tensor t1 = at::ones(b_shape, at::kHalf).cuda();
+    at::Tensor out_ref = at::full(out_shape, k, at::kHalf).cuda();
+
+    FusionExecutorCache fec(std::move(fusion));
+    auto out = fec.runFusionWithInputs({t0, t1});
+
+    EXPECT_TRUE(at::allclose(out[0], out_ref));
+}
+
+TEST (MatmulDefaultTest, MatmulWithBias){
+    auto fusion = std::make_unique<Fusion>();
+    FusionGuard fg(fusion.get());
+    EnableOptionsGuard::getCurOptions().set(EnableOption::MatmulExprEval);
+
+    int64_t m = 2, k = 3, n = 4;
+    std::vector<int64_t> a_shape{m, k}, b_shape{k, n}, out_shape{m, n};
+
+    auto tv0 = makeConcreteTensor(a_shape, DataType::Half);
+    auto tv1 = makeConcreteTensor(b_shape, DataType::Half);
+    auto tv0b = broadcast(tv0, {false, false, true}); // [M, K, 1]
+    auto tv1b = broadcast(tv1, {true, false, false}); // [1, K, N]
+    auto tv2 = fusedMultiplySum(tv0b, tv1b, {1});
+    auto tv3 = castOp(DataType::Half, tv2);
+    auto tv4 = makeConcreteTensor({m}, DataType::Half);
+    auto tv5 = biasEpilogue(tv3, tv4);
+    auto tv6 = castOp(DataType::Half, tv5);
+
+    fusion->addInput(tv0);
+    fusion->addInput(tv1);
+    fusion->addInput(tv4);
+    fusion->addOutput(tv6);
+
+    at::Tensor t0 = at::ones(a_shape, at::kHalf).cuda();
+    at::Tensor t1 = at::ones(b_shape, at::kHalf).cuda();
+    at::Tensor t2 = at::randn({m}, at::kHalf).cuda();
+    at::Tensor out_ref = at::full(out_shape, k, at::kHalf).cuda() + t2.unsqueeze(-1);
+
+    FusionExecutorCache fec(std::move(fusion));
+    auto out = fec.runFusionWithInputs({t0, t1, t2});
 
     EXPECT_TRUE(at::allclose(out[0], out_ref));
 }
