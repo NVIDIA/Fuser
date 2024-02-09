2144a1edf21128f6ab213b7efcdc582bd15ad33c
snordmann <snordmann@nvidia.com>
2024-02-09 14:15:09 +0000

    add mesh_ in TensorView copy operator

--- csrc/tensor_view.cpp
+++ csrc/tensor_view.cpp
@@ -249,7 +249,8 @@ TensorView::TensorView(const TensorView* src, IrCloner* ir_cloner)
       has_swizzle_op_(src->has_swizzle_op_),
       compute_with_consumers_(ir_cloner->clone(src->compute_with_consumers_)),
       compute_with_pos_(src->compute_with_pos_),
-      promote_reuse_(src->promote_reuse_) {}
+      promote_reuse_(src->promote_reuse_),
+      mesh_(src->mesh_) {}
 
 void TensorView::printTransforms() const {
   IrTransformPrinter(std::cout).printTransforms(this);
