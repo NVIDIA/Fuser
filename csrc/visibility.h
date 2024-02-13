4fac494816e4a84ce98e5be9270ae2e33414ef7b
Tom Fogal <60981+tfogal@users.noreply.github.com>
2024-02-13 00:47:59 +0000

    Add reference to visibility.md in visibility.h.

--- csrc/visibility.h
+++ csrc/visibility.h
@@ -8,7 +8,7 @@
 #pragma once
 
 /// Defines the NVF_API macro, which should be added on methods or classes
-/// that are used outside of nvFuser.
+/// that are used outside of nvFuser. See doc/dev/visibility.md for details.
 
 #if defined _WIN32 || defined __CYGWIN__
 #ifdef BUILDING_DLL
