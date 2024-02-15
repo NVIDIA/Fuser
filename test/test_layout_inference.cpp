cfe526784f3d246214035b8695e082ebce701e92
jjsjann123 <alex.jann2012@gmail.com>
2024-02-14 16:14:12 -0800

    adding cpp tests

--- test/test_layout_inference.cpp
+++ test/test_layout_inference.cpp
@@ -173,4 +173,30 @@ TEST_F(LayoutInferenceTest, BinaryOpPropagation) {
   }
 }
 
+TEST_F(LayoutInferenceTest, MemoryFormatOptimizationTest) {
+  auto fusion = std::make_unique<Fusion>();
+  FusionGuard fg(fusion.get());
+
+  auto tv0 = makeContigTensor(4);
+  fusion->addInput(tv0);
+  auto tv1 = relu(tv0);
+  fusion->addOutput(tv1);
+
+  std::vector<IterDomain*> tv0_nhwc = {
+      tv0->axis(0), tv0->axis(2), tv0->axis(3), tv0->axis(1)};
+  tv0->setAllocationDomain(tv0_nhwc, true);
+
+  auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
+
+  at::Tensor in_tensor = at::randn({2, 4, 8, 8}, options);
+  at::Tensor in_nhwc = in_tensor.as_strided({2, 4, 8, 8}, {4 * 8 * 8, 1, 4 * 8, 4});
+  FusionExecutorCache fec(std::move(fusion));
+
+  auto cg_outputs = fec.runFusionWithInputs({in_nhwc});
+
+  ASSERT_TRUE(cg_outputs[0].is_contiguous(at::MemoryFormat::ChannelsLast))
+
+  testValidate(&fusion, cg_outputs, {in_nhwc}, __LINE__, __FILE__);
+}
+
 } // namespace nvfuser
