ee0f29e32e3116055341fc22c02acbe4e484d757
jjsjann123 <alex.jann2012@gmail.com>
2024-02-19 11:27:36 -0800

    more tests added for binary ops

--- test/test_allocation_order_inference.cpp
+++ test/test_allocation_order_inference.cpp
@@ -50,14 +50,22 @@ TEST_F(AllocationOrderInferenceTest, BinaryOpPropagation) {
     Fusion& fusion = *fusion_ptr.get();
     FusionGuard fg(&fusion);
 
-    // Testing propagation between tensor and a scalar
     auto tv0 = makeSymbolicTensor({-1, -1, -1, -1});
     fusion.addInput(tv0);
-    auto s1 = IrBuilder::create<Val>(0L);
+    auto s1 = IrBuilder::create<Val>(1L);
+    // Testing propagation between tensor and a scalar
     auto tv2 = add(tv0, s1);
     fusion.addOutput(tv2);
+    // Testing propagation between tensor and a scalar
     auto tv3 = add(s1, tv0);
     fusion.addOutput(tv3);
+    auto s4 = IrBuilder::create<Val>(3L);
+    // binary op between scalars
+    auto s5 = add(s1, s4);
+    auto tv6 = add(tv0, s5);
+    fusion.addOutput(tv6);
+    auto tv7 = add(s5, tv0);
+    fusion.addOutput(tv7);
 
     std::vector<IterDomain*> tv0_nhwc = {
         tv0->axis(0), tv0->axis(2), tv0->axis(3), tv0->axis(1)};
@@ -66,6 +74,8 @@ TEST_F(AllocationOrderInferenceTest, BinaryOpPropagation) {
     auto updated_layout = inferenceAllocationOrder(&fusion);
     EXPECT_THAT(updated_layout[tv2], ElementsAre(0, 2, 3, 1));
     EXPECT_THAT(updated_layout[tv3], ElementsAre(0, 2, 3, 1));
+    EXPECT_THAT(updated_layout[tv6], ElementsAre(0, 2, 3, 1));
+    EXPECT_THAT(updated_layout[tv7], ElementsAre(0, 2, 3, 1));
   }
   {
     auto fusion_ptr = std::make_unique<Fusion>();
@@ -124,6 +134,37 @@ TEST_F(AllocationOrderInferenceTest, BinaryOpPropagation) {
     EXPECT_THAT(updated_layout[tv2], ElementsAre(0, 2, 1, 3));
     EXPECT_THAT(updated_layout[tv3], ElementsAre(1, 0, 2, 3));
   }
+}
+
+TEST_F(AllocationOrderInferenceTest, TensorFactoryBinaryOpPropagation) {
+  auto fusion_ptr = std::make_unique<Fusion>();
+  Fusion& fusion = *fusion_ptr.get();
+  FusionGuard fg(&fusion);
+
+  auto tv0 = makeSymbolicTensor({-1, 1});
+  fusion.addInput(tv0);
+  auto s1 = IrBuilder::create<Val>(16L);
+  auto s2 = IrBuilder::create<Val>(32L);
+  auto fill_value = IrBuilder::create<Val>(1.0);
+  // factory method
+  auto tv1 = full({s1, s2}, fill_value, DataType::Float);
+  auto tv2 = add(tv0, tv1);
+  fusion.addOutput(tv2);
+  auto tv3 = add(tv1, tv0);
+  fusion.addOutput(tv3);
+
+  std::vector<IterDomain*> tv0_c_last = {
+      tv0->axis(1), tv0->axis(0)};
+  tv0->setAllocationDomain(tv0_c_last, true);
+
+  // tv1 is tensor created by factory method, its layout shouldn't be propagated to output
+  std::vector<IterDomain*> tv1_c_last = {
+      tv1->axis(0), tv1->axis(1)};
+  tv1->setAllocationDomain(tv1_c_last, true);
+
+  auto updated_layout = inferenceAllocationOrder(&fusion);
+  EXPECT_THAT(updated_layout[tv2], ElementsAre(1, 0));
+  EXPECT_THAT(updated_layout[tv3], ElementsAre(1, 0));
 }
 
 } // namespace nvfuser
