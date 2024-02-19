49fc6715bd216ccefb446db6c9517d2afe372fe8
jjsjann123 <alex.jann2012@gmail.com>
2024-02-18 19:48:04 -0800

    include in tests

--- test/test_layout_inference.cpp
+++ test/test_layout_inference.cpp
@@ -12,7 +12,7 @@
 #include <ir/all_nodes.h>
 #include <ir/builder.h>
 #include <ops/all_ops.h>
-#include <optimization/layout_inference.h>
+#include <preseg_passes/layout_inference.h>
 
 #include <test/utils.h>
 #include <test/validator.h>
