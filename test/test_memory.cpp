5a3d1d39f17d0149e72c222999bb43d6de435924
Xiang Gao <qasdfgtyuiop@gmail.com>
2024-02-19 22:56:30 -0800

    save

--- test/test_memory.cpp
+++ test/test_memory.cpp
@@ -239,13 +239,13 @@ TEST_P(TMALdstTest, LoadCompleteTensor2D) {
   auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
   auto inner_dim_size =
       getBytesFromSwizzle(swizzle) / dataTypeSize(tv0->dtype());
-  // auto t0 = at::arange(32 * inner_dim_size, options).view({32,
-  // inner_dim_size});
-  auto t0 = at::arange(32 * 2, options)
-                .view({32 * 2, 1})
-                .expand({32 * 2, inner_dim_size / 2})
-                .contiguous()
-                .view({32, inner_dim_size});
+  auto t0 = at::arange(32 * inner_dim_size, options).view({32,
+  inner_dim_size});
+  // auto t0 = at::arange(32 * 2, options)
+  //               .view({32 * 2, 1})
+  //               .expand({32 * 2, inner_dim_size / 2})
+  //               .contiguous()
+  //               .view({32, inner_dim_size});
   FusionExecutor fe;
   fe.compileFusion(&fusion, {t0}, {}, {DataType::Int32, 255, false});
   auto cg_outputs = fe.runFusion({t0});
