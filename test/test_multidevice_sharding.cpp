24484259576d01add73b8301a247ac89c0408e96
mcowan <mcowan@nvidia.com>
2024-02-23 17:44:03 +0000

    test sharded sizes and comments

--- test/test_multidevice_sharding.cpp
+++ test/test_multidevice_sharding.cpp
@@ -20,8 +20,9 @@ namespace nvfuser {
 
 class ShardingTest : public MultiDeviceTest {};
 
+// Test memory allocation of multidevice fusion with unsharded inputs
+// and sharded intermediates, outputs.
 TEST_F(ShardingTest, UnshardedGlobalInput) {
-  int sharded_dim = 0;
   std::unique_ptr<Fusion> fusion = std::make_unique<Fusion>();
   FusionGuard fg(fusion.get());
   int num_devices = communicator->size();
@@ -30,38 +31,44 @@ TEST_F(ShardingTest, UnshardedGlobalInput) {
   DeviceMesh mesh(devices);
   std::vector<int64_t> input_size = {num_devices, 3};
 
-  TensorView* tv0 = makeConcreteTensor(input_size);
-  TensorView* tv1 = add(tv0, tv0);
-  TensorView* tv2 = set(tv1);
-  TensorView* tv3 = add(tv2, tv2);
-  TensorView* tv4 = set(tv3);
-  TensorView* tv5 = sum(tv4, {0});
+  TensorView* tv0 = makeContigConcreteTensor(input_size);
+  TensorView* tv1 = set(tv0);
+  TensorView* tv2 = add(tv1, tv1);
+  TensorView* tv3 = sum(tv2, {1});
   fusion->addInput(tv0);
-  fusion->addOutput(tv5);
+  fusion->addOutput(tv1);
+  fusion->addOutput(tv2);
+  fusion->addOutput(tv3);
 
+  tv1->axis(0)->parallelize(ParallelType::DIDx);
   tv2->axis(0)->parallelize(ParallelType::DIDx);
-  tv3->axis(sharded_dim)->parallelize(ParallelType::DIDx);
+  tv3->axis(0)->parallelize(ParallelType::DIDx);
 
-  std::vector<TensorView*> tvs = {tv0, tv1, tv2, tv3, tv4, tv5};
+  std::vector<TensorView*> tvs = {tv0, tv1, tv2, tv3};
   for (auto tv : tvs) {
     tv->setDeviceMesh(mesh);
   }
 
-  auto x = at::randn(input_size, tensor_options);
-  std::vector<c10::IValue> inputs = {x};
-  auto ref_outputs = at::sum(x * 4, {0});
+  auto x0 = at::randn(input_size, tensor_options);
+  std::vector<c10::IValue> inputs = {x0};
+  auto x1 = shardTensor(x0, mesh, communicator->deviceId());
+  auto x2 = x1 + x1;
+  auto x3 = at::sum(x2, {1});
 
   MultiDeviceExecutor runtime(std::move(fusion), *communicator);
   auto outputs = runtime.runWithInput(inputs);
+
   testValidate(
       runtime.completeFusion(),
       outputs,
       inputs,
-      {ref_outputs},
+      {x1, x2, x3},
       __LINE__,
       __FILE__);
 }
 
+// Test memory allocation of multidevice fusion with sharded input
+// and replicated intermediates and output.
 TEST_F(ShardingTest, ShardGlobalInput) {
   int sharded_dim = 0;
   std::unique_ptr<Fusion> fusion = std::make_unique<Fusion>();
@@ -76,6 +83,7 @@ TEST_F(ShardingTest, ShardGlobalInput) {
   TensorView* tv1 = set(tv0);
   TensorView* tv2 = add(tv1, tv1);
   fusion->addInput(tv0);
+  fusion->addOutput(tv1);
   fusion->addOutput(tv2);
 
   tv0->axis(sharded_dim)->parallelize(ParallelType::DIDx);
@@ -85,20 +93,15 @@ TEST_F(ShardingTest, ShardGlobalInput) {
     tv->setDeviceMesh(mesh);
   }
 
-  auto x = at::randn(unsharded_input_size, tensor_options);
+  auto x1 = at::randn(unsharded_input_size, tensor_options);
   std::vector<c10::IValue> inputs = {
-      shardTensor(x, mesh, communicator->deviceId())};
-  auto ref_outputs = x * 2;
+      shardTensor(x1, mesh, communicator->deviceId())};
+  auto x2 = x1 * 2;
 
   MultiDeviceExecutor runtime(std::move(fusion), *communicator);
   auto outputs = runtime.runWithInput(inputs);
   testValidate(
-      runtime.completeFusion(),
-      outputs,
-      inputs,
-      {ref_outputs},
-      __LINE__,
-      __FILE__);
+      runtime.completeFusion(), outputs, inputs, {x1, x2}, __LINE__, __FILE__);
 }
 
 } // namespace nvfuser
