0ea93ab2221da61e3c6d125ec6c4d439218379b2
root <26priya11@gmail.com>
2024-02-23 22:48:59 +0000

    rebase

--- test/test_matmul_aten_evaluation.cpp
+++ test/test_matmul_aten_evaluation.cpp
@@ -15,10 +15,24 @@
 #include <scheduler/mma_utils.h>
 #include <test/utils.h>
 #include <test/validator.h>
+#include <preseg_passes/allocation_order_inference.h>
+#include <preseg_passes/optimization_pass.h>
 
 namespace nvfuser {
 
-using MatmulATenEvaluationTest = NVFuserTest;
+class MatmulATenEvaluationTest : public NVFuserTest {
+ protected:
+  void SetUp() override {
+    // allocation order set by the pass breaks matmul tests
+    // see issue https://github.com/NVIDIA/Fuser/issues/1810
+    guard_ = std::make_unique<nvfuser::preseg_passes::OptimizationPassGuard<
+        nvfuser::preseg_passes::AllocationDomainPass>>(false);
+  }
+  std::unique_ptr<nvfuser::preseg_passes::OptimizationPassGuard<
+      nvfuser::preseg_passes::AllocationDomainPass>>
+      guard_;
+};
+
 
 TEST(MatmulATenEvaluationTest, SingleMmaOp) {
   auto fusion = std::make_unique<Fusion>();
