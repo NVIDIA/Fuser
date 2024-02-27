6d63ccbc55d5c78035096164ca28c3e69a751965
jjsjann123 <jiej@nvidia.com>
2024-02-26 18:13:43 -0800

    remove noises

--- runtime/fp8_support.cu
+++ runtime/fp8_support.cu
@@ -10,14 +10,14 @@
 #define __NVFUSER_HALF_TO_CUS(var) \
   *(reinterpret_cast<const unsigned short*>(&(var)))
 
-struct __fp8;
-__device__ __inline__ __fp8 __float2fp8(const float);
+struct __e4m3;
+__device__ __inline__ __e4m3 __float2e4m3(const float);
 
-struct __align__(1) __fp8 {
-  __fp8() = default;
+struct __align__(1) __e4m3 {
+  __e4m3() = default;
 
-  __device__ __fp8(const float f) {
-    __x = __float2fp8(f).__x;
+  __device__ __e4m3(const float f) {
+    __x = __float2e4m3(f).__x;
   }
 
   __device__ uint8_t raw() const {
@@ -28,132 +28,21 @@ struct __align__(1) __fp8 {
   uint8_t __x;
 };
 
-__device__ __inline__ __fp8 __float2fp8(const float f) {
-  __fp8 val;
-  asm("{  cvt.rn.f16.f32 %0, %1;}\n"
-      : "=h"(__NVFUSER_HALF_TO_US(val))
-      : "f"(f));
+__device__ __inline__ __e4m3 __float2e4m3(const float f) {
+  constexpr float f_const_zero = 0.f;
+  unsigned short _tmp_buffer;
+  __e4m3 val;
+  asm("{cvt.rn.satfinite.e4m3x2.f32 %0, %1, %2;}"
+      : "=h"(_tmp_buffer)
+      : "f"(f_const_zero), "f"(f));
+  std::memcpy(&val, &_tmp_buffer, sizeof(uint8_t));
+
   return val;
 }
 
-__device__ __inline__ __fp8 __double2fp8(const double d) {
-  __fp8 val;
-  asm("{  cvt.rn.f16.f64 %0, %1;}\n"
-      : "=h"(__NVFUSER_HALF_TO_US(val))
-      : "d"(d));
-  return val;
+__device__ __inline__ float __e4m32float(const __e4m3 h) {
 }
 
-__device__ __inline__ __fp8 __int2fp8(const int i) {
-  __fp8 val;
-  asm("{  cvt.rn.f16.s32 %0, %1;}\n"
-      : "=h"(__NVFUSER_HALF_TO_US(val))
-      : "r"(i));
-  return val;
-}
-
-__device__ __inline__ __fp8 __int2fp8(const int64_t i64) {
-  __fp8 val;
-  asm("{  cvt.rn.f16.s64 %0, %1;}\n"
-      : "=h"(__NVFUSER_HALF_TO_US(val))
-      : "l"(i64));
-  return val;
-}
-
-__device__ __inline__ __fp8 __int2fp8(const uint32_t i) {
-  __fp8 val;
-  asm("{  cvt.rn.f16.u32 %0, %1;}\n"
-      : "=h"(__NVFUSER_HALF_TO_US(val))
-      : "r"(i));
-  return val;
-}
-
-__device__ __inline__ __fp8 __int2fp8(const uint64_t i64) {
-  __fp8 val;
-  asm("{  cvt.rn.f16.u64 %0, %1;}\n"
-      : "=h"(__NVFUSER_HALF_TO_US(val))
-      : "l"(i64));
-  return val;
-}
-
-__device__ __inline__ __fp8 __bool2fp8(const bool b) {
-  return __int2fp8((int)b);
-}
-
-__device__ __inline__ float __fp82float(const __fp8 h) {
-  float val;
-  asm("{  cvt.f32.f16 %0, %1;}\n" : "=f"(val) : "h"(__NVFUSER_HALF_TO_CUS(h)));
-  return val;
-}
-
-__device__ __inline__ double __fp82double(const __fp8 h) {
-  double val;
-  asm("{  cvt.f64.f16 %0, %1;}\n" : "=d"(val) : "h"(__NVFUSER_HALF_TO_CUS(h)));
-  return val;
-}
-
-__device__ int __fp82int32(const __fp8 h) {
-  int val;
-  asm("{  cvt.rzi.s32.f16 %0, %1;}\n"
-      : "=r"(val)
-      : "h"(__NVFUSER_HALF_TO_CUS(h)));
-  return val;
-}
-
-__device__ __inline__ int64_t __fp82int(const __fp8 h) {
-  int64_t val;
-  asm("{  cvt.rzi.s64.f16 %0, %1;}\n"
-      : "=l"(val)
-      : "h"(__NVFUSER_HALF_TO_CUS(h)));
-  return val;
-}
-
-__device__ int __fp82uint32(const __fp8 h) {
-  int val;
-  asm("{  cvt.rzi.u32.f16 %0, %1;}\n"
-      : "=r"(val)
-      : "h"(__NVFUSER_HALF_TO_CUS(h)));
-  return val;
-}
-
-__device__ __inline__ int64_t __fp82uint(const __fp8 h) {
-  int64_t val;
-  asm("{  cvt.rzi.u64.f16 %0, %1;}\n"
-      : "=l"(val)
-      : "h"(__NVFUSER_HALF_TO_CUS(h)));
-  return val;
-}
-
-__device__ __inline__ void __fp82int(const __fp8 h, int& output) {
-  output = __fp82int32(h);
-}
-
-__device__ __inline__ void __fp82int(const __fp8 h, int64_t& output) {
-  output = __fp82int(h);
-}
-
-__device__ __inline__ void __fp82int(const __fp8 h, uint32_t& output) {
-  output = __fp82uint32(h);
-}
-
-__device__ __inline__ void __fp82int(const __fp8 h, uint64_t& output) {
-  output = __fp82uint(h);
-}
-
-__device__ __inline__ nvfuser_index_t __fp82index(const __fp8 h) {
-  nvfuser_index_t result;
-  __fp82int(h, result);
-  return result;
-}
-
-__device__ __inline__ bool __fp82bool(const __fp8 h) {
-  return (bool)__fp82float(h) != 0;
-}
-
-__device__ __inline__ __fp8 __real_then_2fp8(const std::complex<float> c) {
-  return __float2fp8(std::real(c));
-}
-
-__device__ __inline__ __fp8 __real_then_2fp8(const std::complex<double> c) {
-  return __double2fp8(std::real(c));
+__device__ __inline__ __e4m3 __real_then_2e4m3(const std::complex<float> c) {
+  return __float2e4m3(std::real(c));
 }
