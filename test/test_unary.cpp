32731460a065e344bbf8c02c8b2d5c8af2a31394
Jingyue Wu <wujingyue@gmail.com>
2024-03-08 22:15:55 +0000

    Fix.

--- test/test_unary.cpp
+++ test/test_unary.cpp
@@ -15,23 +15,42 @@
 
 namespace nvfuser {
 
-using UnaryTest = NVFuserTest;
+using UnaryTest = NVFuserFixtureParamTest<DataType>;
 
-TEST_F(UnaryTest, BFloatNeg) {
+TEST_P(UnaryTest, Neg) {
+  DataType dtype = GetParam();
+
   auto fusion = std::make_unique<Fusion>();
   FusionGuard fg(fusion.get());
 
-  TensorView* in = makeContigConcreteTensor({3}, DataType::BFloat16);
+  std::vector<int64_t> shape({640, 64});
+
+  TensorView* in = makeContigConcreteTensor(shape, dtype);
   TensorView* out = neg(in);
   fusion->addInput(in);
   fusion->addOutput(out);
 
-  auto options = at::TensorOptions().dtype(at::kBFloat16).device(at::kCUDA, 0);
-  at::Tensor in_tensor = at::randn({3}, options);
+  auto options =
+      at::TensorOptions().dtype(data_type_to_aten(dtype)).device(at::kCUDA, 0);
+  at::Tensor in_tensor = at::randn(shape, options);
 
   FusionExecutorCache fec(std::move(fusion));
   auto out_tensors = fec.runFusionWithInputs({in_tensor});
   testValidate(fec.fusion(), out_tensors, {in_tensor}, __LINE__, __FILE__);
 }
+
+INSTANTIATE_TEST_SUITE_P(
+    UnaryTests,
+    UnaryTest,
+    testing::Values(
+        DataType::Half,
+        DataType::BFloat16,
+        DataType::Float,
+        DataType::Double),
+    [](const testing::TestParamInfo<DataType>& info) {
+      std::ostringstream os;
+      os << info.param;
+      return os.str();
+    });
 
 } // namespace nvfuser
