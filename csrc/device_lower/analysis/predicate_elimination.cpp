fcf9a8594df708d4399479f961c423c283b4ed8a
Xiang Gao <qasdfgtyuiop@gmail.com>
2024-03-12 14:03:27 -0700

    format

--- csrc/device_lower/analysis/predicate_elimination.cpp
+++ csrc/device_lower/analysis/predicate_elimination.cpp
@@ -1070,7 +1070,7 @@ std::string PredicateElimination::toString() const {
   ss << "\n";
   ss << "Non-predicated expressions:";
   for (auto expr : non_predicated_exprs_) {
-    ss << " " << expr->toString();
+    ss << " " << expr;
   }
   return ss.str();
 }
