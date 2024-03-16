26c4ca69d584559231467359188915f86c2556ac
Meghan Cowan <mcowan@nvidia.com>
2024-03-15 17:24:28 -0700

    Sharded tensor metadata (#1932)
    Redo of: https://github.com/NVIDIA/Fuser/pull/1897
    
    Fixes an inconsistency in tensor metadata. We previously saved the
    unsharded sizes in the logical size, but saved the sharded strides in
    the logical strides. This is made consistent: the sharded sizes and
    sharded strides are stored in tensor metadata.
    Note, the TensorView's IterDomains still refer to the unsharded extents.
    In index computation, we ensure sharded axes are treated as having an
    extent of 1.
    
    Tests that computation with non-outermost sharded indices is lowered and
    executes properly.

--- tests/cpp/test_multidevice_sharding.cpp
+++ tests/cpp/test_multidevice_sharding.cpp
@@ -18,6 +18,80 @@
 
 namespace nvfuser {
 
+// TODO: This test checks that isSharded generates an error when a split/merged
+// axis is parallelized with DIDx. Update when this restriction is lifted.
+TEST(NVFuserTest, TestIsSharded) {
+  std::unique_ptr<Fusion> fusion = std::make_unique<Fusion>();
+  FusionGuard fg(fusion.get());
+
+  TensorView* a = makeSymbolicTensor(3);
+  a->axis(2)->parallelize(ParallelType::DIDx);
+  a->split(0, 4);
+  ASSERT_TRUE(isSharded(a));
+
+  TensorView* b = makeSymbolicTensor(3);
+  b->split(1, 4);
+  b->axis(1)->parallelize(ParallelType::DIDx);
+  ASSERT_ANY_THROW(isSharded(b));
+
+  TensorView* c = makeSymbolicTensor(3);
+  c->axis(0)->parallelize(ParallelType::DIDx);
+  c->axis(1)->parallelize(ParallelType::DIDx);
+  ASSERT_ANY_THROW(isSharded(c));
+}
+
+class ShardedComputeTest : public NVFuserTest,
+                           public ::testing::WithParamInterface<bool> {};
+
+TEST_P(ShardedComputeTest, ComputeIndex) {
+  auto concreteTv = GetParam();
+  std::unique_ptr<Fusion> fusion = std::make_unique<Fusion>();
+  FusionGuard fg(fusion.get());
+  DeviceMesh mesh({0, 1, 2});
+
+  TensorView* a =
+      concreteTv ? makeConcreteTensor({4, 2, 3, 5}) : makeSymbolicTensor(4);
+  TensorView* b = sum(a, {0});
+  TensorView* c = add(a, a);
+  TensorView* d = permute(a, {{2, 0}});
+
+  fusion->addInput(a);
+  fusion->addOutput(b);
+  fusion->addOutput(c);
+  fusion->addOutput(d);
+
+  a->setDeviceMesh(mesh);
+  b->setDeviceMesh(mesh);
+  c->setDeviceMesh(mesh);
+  d->setDeviceMesh(mesh);
+  a->axis(2)->parallelize(ParallelType::DIDx);
+  b->axis(2)->parallelize(ParallelType::DIDx);
+  c->axis(2)->parallelize(ParallelType::DIDx);
+  d->axis(0)->parallelize(ParallelType::DIDx);
+
+  auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
+  auto a_ = at::randn({4, 2, 1, 5}, options);
+  auto b_ = at::sum(a_, {0});
+  auto c_ = a_ + a_;
+  auto d_ = at::permute(a_, {2, 0, 1, 3});
+  std::vector<at::Tensor> outputs_ = {b_, c_, d_};
+
+  FusionExecutor fe;
+  fe.compileFusion(fusion.get(), {a_});
+  auto outputs = fe.runFusion({a_});
+  testValidate(fusion.get(), outputs, {a_}, outputs_, __LINE__, __FILE__);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    ConcreteInput,
+    ShardedComputeTest,
+    ::testing::Values(true));
+
+INSTANTIATE_TEST_SUITE_P(
+    SymbolicInput,
+    ShardedComputeTest,
+    ::testing::Values(false));
+
 class ShardingTest : public MultiDeviceTest,
                      public ::testing::WithParamInterface<bool> {};
 
