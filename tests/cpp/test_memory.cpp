e22066932ae7b650b65138307b478f9ac878e91f
Xiang Gao <qasdfgtyuiop@gmail.com>
2024-03-18 18:52:27 -0700

    no print

--- tests/cpp/test_memory.cpp
+++ tests/cpp/test_memory.cpp
@@ -414,8 +414,8 @@ TEST_P(TMALdstTest, SimpleStore) {
       XorFinder::findXor(fe.kernel()), (swizzle != MmaInputSmemSwizzle::None));
 
   auto cg_outputs = fe.runFusion({t0});
-  std::cout << "t0:\n" << t0 << std::endl;
-  std::cout << "cg_outputs[0]:\n" << cg_outputs[0] << std::endl;
+  // std::cout << "t0:\n" << t0 << std::endl;
+  // std::cout << "cg_outputs[0]:\n" << cg_outputs[0] << std::endl;
   testValidate(&fusion, cg_outputs, {t0}, {t0}, __LINE__, __FILE__);
 }
 
