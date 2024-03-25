e7bc05a053eb8ae85997fed76e0047b3b5a36ccb
Xiang Gao <qasdfgtyuiop@gmail.com>
2024-03-25 10:31:24 -0700

    thread parallelization

--- tests/cpp/test_memory.cpp
+++ tests/cpp/test_memory.cpp
@@ -840,6 +840,8 @@ TEST_F(TMARuntimeInvalidTest, MisalignedGlobalStride) {
   }
   tv1->axis(1)->parallelize(ParallelType::Bulk);
   tv1->axis(3)->parallelize(ParallelType::Bulk);
+  // tv2->axis(1)->parallelize(ParallelType::TIDy);
+  // tv2->axis(3)->parallelize(ParallelType::TIDx);
 
   auto options =
       at::TensorOptions().dtype(data_type_to_aten(dtype)).device(at::kCUDA, 0);
