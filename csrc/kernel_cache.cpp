eb456c7b999c3e6ae21ddc6898e881034cbf6cbb
snordmann <snordmann@nvidia.com>
2024-04-03 13:13:11 -0700

    revert change in prepareRuntimeOrder

--- csrc/kernel_cache.cpp
+++ csrc/kernel_cache.cpp
@@ -376,8 +376,9 @@ void prepareRuntimeOrder(
     available_input.insert(input_val);
 
     if (auto input_tv = dynamic_cast<TensorView*>(input_val)) {
-      for (auto id : TensorDomain::noReductions(input_tv->getLeafDomain())) {
-        const auto extent = id->getMaybeExpandedExtent();
+      auto root_dom = TensorDomain::noReductions(input_tv->getRootDomain());
+      for (const size_t dim : c10::irange(root_dom.size())) {
+        const auto extent = root_dom[dim]->getMaybeExpandedExtent();
         available_input.insert(extent);
         runtime_workspace.group_extent_binding_order.push_back(extent);
       }
