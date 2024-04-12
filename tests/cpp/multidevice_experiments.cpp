6669935b9d5b59e1041a6cc955461875fcb18c1e
snordmann <snordmann@nvidia.com>
2024-04-12 03:57:41 -0700

    remove one input

--- tests/cpp/multidevice_experiments.cpp
+++ tests/cpp/multidevice_experiments.cpp
@@ -28,19 +28,18 @@ auto global_context = static_cast<GlobalContext*>(
 
 /*
 The tensor program we target is the following:
-    | tv0[A,B,C], sharded accross first dimension (locally [1,B,C])
-    | tv1[A,B,C], sharded accross first dimension (locally [1,B,C])
-    | tv2[A,B,C] = tv0 + tv1 (locally [1,B,C])
-    | tv3[A,B,C] = allgather(tv2) (locally [A,B,C])
+    | input: tv0[A,B,C], sharded accross first dimension (locally [1,B,C])
+    | tv1[A,B,C] = tv0 * 2 (locally [1,B,C])
+    | output: tv2[A,B,C] = allgather(tv1) (locally [A,B,C])
 
 From the host perspective, this program could be schematized as:
-    | tv2 = Compute(tv0,tv1)
-    | tv3 = Comm(tv2)
+    | tv1 = Compute(tv0)
+    | tv2 = Comm(tv1)
 
 We want to compare this baseline program with the one where we interleave and pipeline the Comm and Compute kernels:
     | for (j=0; j<number_of_tiles; j++) {
-    |     tv2 = Compute(tv0[j],tv1[j])
-    |     tv3 = Comm(tv2[j])
+    |     tv1 = Compute(tv0[j])
+    |     tv2 = Comm(tv1[j])
     | }
 where "[j]" referes to taking a slice onto the "B" dimension
 */
@@ -71,51 +70,48 @@ TEST_F(MultiDeviceTest, OverlapExperiment) {
     auto options =
         at::TensorOptions().dtype(at::kFloat).device(communicator->device());
     auto tv0_unsharded = at::randn(unsharded_sizes, options);
-    auto tv1_unsharded = at::randn(unsharded_sizes, options);
-    // Index into the unsharded inputs to get the local inputs tv0 and tv1
+    // Index into the unsharded inputs to get the local input tv0
     // We prepare the inputs slices, because profiling shows that slicing sometimes result in a copy instead of a view
     const auto my_device_index = communicator->deviceId();
     std::vector<at::Tensor> tv0_slices;
-    std::vector<at::Tensor> tv1_slices;
     for (auto j: c10::irange(number_of_tiles)) {
         tv0_slices.push_back(get_slice(tv0_unsharded, my_device_index, j));
-        tv1_slices.push_back(get_slice(tv1_unsharded, my_device_index, j));
     }
 
-    // Allocate ouput global buffer for tv3 for the data to be allgathered
-    auto tv3_buf = at::empty(unsharded_sizes, options);
+    // Allocate ouput global buffer for tv2 for the data to be allgathered
+    auto tv2_buf = at::empty(unsharded_sizes, options);
     // Setup the recv buffer slices. c10d needs the destinations buffers to be in a certain format
-    std::vector<std::vector<std::vector<at::Tensor>>> tv3_slices;
+    std::vector<std::vector<std::vector<at::Tensor>>> tv2_slices;
     for (auto j: c10::irange(number_of_tiles)) {
-        std::vector<at::Tensor> tv3_j_slices;
+        std::vector<at::Tensor> tv2_j_slices;
         for (auto i: c10::irange(num_devices)) {
-            tv3_j_slices.push_back(get_slice(tv3_buf, i, j));
+            tv2_j_slices.push_back(get_slice(tv2_buf, i, j));
         }
-        tv3_slices.push_back({std::move(tv3_j_slices)});
+        tv2_slices.push_back({std::move(tv2_j_slices)});
     }
 
     // Iterate over the number of tiles and pipeline the comms and compute
     for (auto j: c10::irange(number_of_tiles)) {
         setCurrentCUDAStream(c10::cuda::getStreamFromPool(/* high priority */false, my_device_index));
         // local compute
-        auto tv2_j = tv0_slices.at(j) + tv1_slices.at(j);
+        auto tv1_j = tv0_slices.at(j) * 2;
 
         // communication
-        std::vector<at::Tensor> src_buf = {tv2_j};
-        std::vector<std::vector<at::Tensor>>& dst_bufs = tv3_slices.at(j);
+        std::vector<at::Tensor> src_buf = {tv1_j};
+        std::vector<std::vector<at::Tensor>>& dst_bufs = tv2_slices.at(j);
         auto req_handle = world_comm->allgather(dst_bufs, src_buf);
         req_handle->wait();
     }
 
     // validation
     // compute the expected output
-    auto tv2_ref = tv0_unsharded + tv1_unsharded;
-    auto tv3_ref = tv2_ref;
+    auto tv1_ref = tv0_unsharded * 2;
+    auto tv2_ref = tv1_ref;
     // compare obtained and expected outputs
     for (auto i: c10::irange(num_devices)) {
         for (auto j: c10::irange(number_of_tiles)) {
-            auto expected = get_slice(tv3_ref, i, j);
-            auto obtained = tv3_slices.at(j).at(0).at(i);
+            auto expected = get_slice(tv2_ref, i, j);
+            auto obtained = tv2_slices.at(j).at(0).at(i);
             EXPECT_TRUE(torch::allclose(expected, obtained))
                 << "On device " << my_device_index << "\n"
                 << "i=" << i << "\n"
