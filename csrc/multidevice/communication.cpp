9c311022456dd5067a0905ff6e69ee7e57c88319
Jingyue Wu <wujingyue@gmail.com>
2024-04-12 22:48:27 +0000

    Fix

--- csrc/multidevice/communication.cpp
+++ csrc/multidevice/communication.cpp
@@ -234,7 +234,7 @@ c10::intrusive_ptr<c10d::Work> Reduce::post(
   c10d::ReduceOptions options = {
       .reduceOp = params_.redOp, .rootRank = root_relative_index_};
   auto team_backend = comm.getBackendForTeam(params_.team, backend);
-#ifdef USE_C10D_NCCL
+#if defined(NVFUSER_DISTRIBUTED) && defined(USE_C10D_NCCL)
   auto nccl_backend = dynamic_cast<c10d::ProcessGroupNCCL*>(team_backend.get());
   if (nccl_backend) {
 #if NVF_TORCH_VERSION_NO_LESS(2, 3, 0)
