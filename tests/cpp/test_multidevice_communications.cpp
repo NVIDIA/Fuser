b003ff45c819cd25b7eb952265e83e1f8b3c000f
Jingyue Wu <wujingyue@gmail.com>
2024-04-16 22:43:23 +0000

    Print better test names.

--- tests/cpp/test_multidevice_communications.cpp
+++ tests/cpp/test_multidevice_communications.cpp
@@ -270,9 +270,8 @@ TEST_P(CommunicationTest, Communication_ReduceScatter) {
 INSTANTIATE_TEST_SUITE_P(
     CommunicatorBackend,
     CommunicationTest,
-    testing::Values(CommunicatorBackend::nccl, CommunicatorBackend::ucc)
-
-);
+    testing::Values(CommunicatorBackend::nccl, CommunicatorBackend::ucc),
+    testing::PrintToStringParamName());
 
 } // namespace nvfuser
 
