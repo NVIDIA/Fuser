f4bd69d6d999f8e2720c0922fc4edb9c0c594dee
Jacob Hinkle <jhinkle@nvidia.com>
2024-04-17 12:58:24 +0000

    [REVERT ME] Test CI job by removing license

--- tests/cpp/test_shift.cpp
+++ tests/cpp/test_shift.cpp
@@ -1,10 +1,3 @@
-// clang-format off
-/*
- * SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
- * All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause
- */
-// clang-format on
 #include <csrc/exceptions.h>
 #include <gtest/gtest.h>
 
