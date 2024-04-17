445bf4daedc512b220cd814c5039c083a868449f
Jacob Hinkle <jhinkle@nvidia.com>
2024-04-17 09:33:21 -0400

    Load CSV to find benchmark problems

--- benchmarks/python/test_matmul.py
+++ benchmarks/python/test_matmul.py
@@ -6,7 +6,10 @@ from nvfuser import FusionDefinition, DataType
 from nvfuser.pytorch_utils import torch_dtype_to_nvfuser_dtype
 from .core import run_benchmark, clear_cuda_cache
 import torch
-from .global_params import generate_input_sizes, FLOAT_DTYPES, PROMOTE_DTYPES
+
+import csv
+import os
+
 
 def matmul_fusion(fd: FusionDefinition, dtype: DataType) -> None:
     # Decide contiguity based on layout
@@ -20,13 +23,17 @@ def matmul_fusion(fd: FusionDefinition, dtype: DataType) -> None:
     fd.add_output(out)
 
 
-precision_types = {
-    "H": torch.float16, 
-    "S": torch.float32, 
-    "T": torch.bfloat16
-}
+precision_types = {"H": torch.float16, "S": torch.float32, "T": torch.bfloat16}
 
-@pytest.mark.parametrize("config", [(272, 9104, 3200, "TT")]) # load from file
+
+def load_matmul_problems():
+    with open(os.path.join(os.path.dirname(__file__), "matmul_problems.csv"), "r") as f:
+        reader = csv.reader(f)
+        next(reader, None)  # skip header row
+        return list((int(m), int(n), int(k), layout) for m, n, k, layout in reader)
+
+
+@pytest.mark.parametrize("config", load_matmul_problems())
 @pytest.mark.parametrize("dtype", [torch.float16, torch.bfloat16])
 def test_reduction_nvf_benchmark(
     benchmark,
@@ -39,7 +46,7 @@ def test_reduction_nvf_benchmark(
     m, n, k, layout = config
 
     # a_dtype, b_dtype, out_dtype = [precision_types[prec] for prec in precision]
-    a = torch.randn(m, k, device="cuda", dtype=dtype) 
+    a = torch.randn(m, k, device="cuda", dtype=dtype)
     b = torch.randn(k, n, device="cuda", dtype=dtype)
 
     if layout == "NT" or layout == "NN":
