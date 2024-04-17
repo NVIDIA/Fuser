727c56979b2932a6abe7957fd9778663af1621df
Liqiang Lu <liqiangxl@gmail.com>
2024-04-17 01:31:30 +0000

    python bm

---
+++ benchmarks/python/test_reduction_epilogue.py
@@ -0,0 +1,81 @@
+import pytest
+from nvfuser import FusionDefinition, DataType
+from nvfuser.pytorch_utils import torch_dtype_to_nvfuser_dtype
+from .core import run_benchmark, clear_cuda_cache
+import torch
+from .global_params import generate_input_sizes, FLOAT_DTYPES, PROMOTE_DTYPES
+
+
+def reduction_fusion(
+    fd: FusionDefinition,
+    dtype: DataType,
+    reduction_axis: int,
+) -> None:
+    T0 = fd.define_tensor(
+        shape=[-1, -1], contiguity=[True, True], dtype=dtype, is_cpu=False
+    )
+    T1 = fd.define_tensor(
+        shape=[-1], contiguity=[True], dtype=dtype, is_cpu=False
+    )
+    if dtype in PROMOTE_DTYPES:
+        T0 = fd.ops.cast(T0, dtype=DataType.Float)
+        T1 = fd.ops.cast(T1, dtype=DataType.Float)
+    T2 = fd.ops.sum(T0, dims=[reduction_axis], keepdim=False)
+    T3 = fd.ops.add(T2, T1)
+    if dtype in PROMOTE_DTYPES:
+        T3 = fd.ops.cast(T3, dtype=dtype)
+    fd.add_output(T3)
+
+
+def reduction_fwd_fn(inputs: list):  # in_tensor, epilogue_tensor, reduction_axis
+    return torch.sum(inputs[0], dim=inputs[2]) + inputs[1]
+
+
+@pytest.mark.parametrize("size", generate_input_sizes(dims=2))
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
+@pytest.mark.parametrize("reduction_axis", [0, 1])
+def test_reduction_nvf_benchmark(
+    benchmark,
+    size: tuple,
+    dtype: torch.dtype,
+    reduction_axis: int,
+    disable_validation: bool,
+    disable_benchmarking: bool,
+):
+    clear_cuda_cache()
+
+    input = torch.randn(*size, device="cuda", dtype=dtype)
+    epilogue = torch.randn(size[-1], device="cuda", dtype=dtype)
+    inputs = [input, epilogue, reduction_axis]
+    with FusionDefinition() as fd:
+        reduction_fusion(fd, torch_dtype_to_nvfuser_dtype(dtype), reduction_axis)
+
+    if not disable_validation:
+        eager_output = reduction_fwd_fn(inputs)
+        fd.validate(inputs, [eager_output.to(dtype)])
+
+    if not disable_benchmarking:
+        run_benchmark(benchmark, fd.execute, inputs)
+
+
+@pytest.mark.parametrize("compile", [False, True], ids=["eager", "compile"])
+@pytest.mark.parametrize("size", generate_input_sizes(dims=2))
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
+@pytest.mark.parametrize("reduction_axis", [0, 1])
+def test_reduction_baseline_benchmark(
+    benchmark,
+    size: tuple,
+    dtype: torch.dtype,
+    reduction_axis: int,
+    compile: bool,
+):
+    clear_cuda_cache()
+
+    input = torch.randn(*size, device="cuda", dtype=dtype)
+    epilogue = torch.randn(size[-1], device="cuda", dtype=dtype)
+    # Inputs and outputs are same as nvFuser, no need for manual IOByte computation
+    run_benchmark(
+        benchmark,
+        torch.compile(reduction_fwd_fn) if compile else reduction_fwd_fn,
+        [input, epilogue, reduction_axis],
+    )
