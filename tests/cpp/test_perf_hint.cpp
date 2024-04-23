a08f88aa535fc52394183a11d9bc1003ef867148
Jacob Hinkle <jhinkle@nvidia.com>
2024-04-23 17:14:10 +0000

    Test various options

--- tests/cpp/test_perf_hint.cpp
+++ tests/cpp/test_perf_hint.cpp
@@ -22,9 +22,52 @@ TEST_F(PerfHintTest, Basic) {
   std::stringstream ss;
   DebugStreamGuard dsg(ss);
 
-  NVF_PERF_HINT("test_perf_bug", "Your kernel will be slow because ", 42);
+  {
+    ss.str("");
+    DisableOptionsGuard dog;
+    // NVFUSER_DISABLE=
+    DisableOptionsGuard::getCurOptions().unset(DisableOption::PerfHints);
+    NVF_PERF_HINT("test_perf_bug", "Your kernel will be slow because ", 42);
+    EXPECT_FALSE(ss.str().empty());
+  }
 
-  std::cout << "Debug stream is \"" << ss.str() << "\"" << std::endl;
+  {
+    ss.str("");
+    DisableOptionsGuard dog;
+    // NVFUSER_DISABLE=perf_hints(foo)
+    DisableOptionsGuard::getCurOptions().set(DisableOption::PerfHints, {"foo"});
+    NVF_PERF_HINT("test_perf_bug", "Your kernel will be slow because ", 42);
+    EXPECT_FALSE(ss.str().empty());
+  }
+
+  {
+    ss.str("");
+    DisableOptionsGuard dog;
+    // NVFUSER_DISABLE=perf_hints(-test_perf_bug)
+    DisableOptionsGuard::getCurOptions().set(
+        DisableOption::PerfHints, {"-test_perf_bug"});
+    NVF_PERF_HINT("test_perf_bug", "Your kernel will be slow because ", 42);
+    EXPECT_FALSE(ss.str().empty());
+  }
+
+  {
+    ss.str("");
+    DisableOptionsGuard dog;
+    // NVFUSER_DISABLE=perf_hints(test_perf_bug)
+    DisableOptionsGuard::getCurOptions().set(
+        DisableOption::PerfHints, {"test_perf_bug"});
+    NVF_PERF_HINT("test_perf_bug", "Your kernel will be slow because ", 42);
+    EXPECT_TRUE(ss.str().empty());
+  }
+
+  {
+    ss.str("");
+    DisableOptionsGuard dog;
+    // NVFUSER_DISABLE=perf_hints
+    DisableOptionsGuard::getCurOptions().set(DisableOption::PerfHints);
+    NVF_PERF_HINT("test_perf_bug", "Your kernel will be slow because ", 42);
+    EXPECT_TRUE(ss.str().empty());
+  }
 }
 
 } // namespace nvfuser
