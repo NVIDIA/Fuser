3154ea4e60fd620a5f01fef1493d85cf941c4570
root <26priya11@gmail.com>
2024-04-29 19:46:27 +0000

    root map override

--- csrc/root_domain_map.h
+++ csrc/root_domain_map.h
@@ -245,6 +245,7 @@ class UnmappableReductionDomains : private IterVisitor {
   void handle(GroupedReductionOp* op) override;
   void handle(WelfordOp* op) override;
   void handle(MmaOp* op) override;
+  // void handle(MatmulOp* op) override;
 
   void handleReductionOutput(TensorView* out_tv);
 
@@ -489,6 +490,10 @@ class ComputeAtRootDomainMapBuilder : private BackwardVisitor {
   }
 
   void handle(MmaOp* wop) override {
+    mapPointwiseLikeOp(wop);
+  }
+
+  void handle(MatmulOp* wop) override {
     mapPointwiseLikeOp(wop);
   }
 
