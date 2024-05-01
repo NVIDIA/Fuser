05b7e1b9e571e18572269a73f3917bc23808bcb6
Xiang Gao <qasdfgtyuiop@gmail.com>
2024-05-01 15:44:27 -0700

    save

--- csrc/device_lower/pass/index.cpp
+++ csrc/device_lower/pass/index.cpp
@@ -1627,9 +1627,7 @@ void IndexLowering::handle(const MmaOp* mma) {
         /*number of core matrices, rounded up to handle padding */
         roundUpToMultiple(inner_size * /*bytes per item*/ 2L,
                           getBytesFromSwizzle(swizzle));
-    if (swizzle != MmaInputSmemSwizzle::None &&
-        (mma->layout() == MmaLayout::TT || mma->layout() == MmaLayout::TN)) {
-      // TODO: why???!!!
+    if (swizzle != MmaInputSmemSwizzle::None) {
       std::swap(leading_bytes, stride_bytes);
     }
     auto matrix_desc = constructMatrixDescriptor(
