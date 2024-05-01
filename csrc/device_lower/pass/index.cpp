2054963081e97c8f16195608b130d269a9e77546
Xiang Gao <qasdfgtyuiop@gmail.com>
2024-05-01 16:58:43 -0700

    save

--- csrc/device_lower/pass/index.cpp
+++ csrc/device_lower/pass/index.cpp
@@ -1620,11 +1620,12 @@ void IndexLowering::handle(const MmaOp* mma) {
     auto swizzle = getSwizzleMode(tv);
     auto base_addr = IrBuilder::baseAddressExpr(tv);
     bool transpose = (layout == MmaLayout::TN || layout == MmaLayout::NN);
+    bool inner_size = transpose ? getK(mma->macro()) : getN(mma->macro());
     int64_t leading_bytes = core_matrix_outer_size *
         getBytesFromSwizzle(swizzle); // swizzle period in bytes
     int64_t stride_bytes = core_matrix_outer_size *
         /*number of core matrices, rounded up to handle padding */
-        roundUpToMultiple(getN(mma->macro()) * /*bytes per item*/ 2L,
+        roundUpToMultiple(inner_size * /*bytes per item*/ 2L,
                           getBytesFromSwizzle(swizzle));
     if (transpose) {
       std::swap(leading_bytes, stride_bytes);
