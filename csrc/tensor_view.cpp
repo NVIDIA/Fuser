5af71046e8993adcb0a36eb35aaa74d864d10df8
Protonu <pbasu@nvidia.com>
2024-05-07 18:48:20 -0400

    Unit test for scheduling a Mma op where input B has an allocation domain. (#2208)
    As a first step to supporting allocation domain in our matmul scheduler,
    this unit test demonstrates how we can use reordering to extend
    scheduling a Mma op when the input Tensorview (B) has an allocation
    domain.
    
    The next PR on top this will extend the scheduler to support a strided
    input B in a unit test that schedules a matmul.

--- csrc/tensor_view.cpp
+++ csrc/tensor_view.cpp
@@ -613,6 +613,29 @@ TensorView* TensorView::reorder(
   return this;
 }
 
+TensorView* TensorView::reorder(
+    const std::initializer_list<std::pair<const int64_t, int64_t>>& old2new) {
+  return reorder(std::unordered_map<int64_t, int64_t>(old2new));
+}
+
+// We have to convert the above permutation to a map of old2new.
+TensorView* TensorView::reorder(const std::vector<int64_t>& permutation) {
+  std::unordered_map<int64_t, int64_t> reorder_map;
+  int64_t idx = 0;
+  std::transform(
+      permutation.begin(),
+      permutation.end(),
+      std::inserter(reorder_map, reorder_map.end()),
+      [&idx](const int64_t v) { return std::make_pair(idx++, v); });
+
+  return reorder(reorder_map);
+}
+
+TensorView* TensorView::reorder(
+    const std::initializer_list<int64_t>& permutation) {
+  return reorder(std::vector<int64_t>(permutation));
+}
+
 TensorView* TensorView::swizzle(
     SwizzleType swizzle_type,
     int64_t x,
