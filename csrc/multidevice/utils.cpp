d065d75a47d879d93a470dced168b094f47ff145
snordmann <snordmann@nvidia.com>
2024-05-15 04:04:29 -0700

    minor comments

--- csrc/multidevice/utils.cpp
+++ csrc/multidevice/utils.cpp
@@ -141,46 +141,42 @@ int64_t numDeviceDims(TensorView* tv) {
       [](IterDomain* id) { return id->isDeviceDim(); });
 }
 
+bool haveDifferentShardings(TensorView* producer, TensorView* consumer) {
+  // exit early in the unsharded case for performance
+  if (!producer->hasDeviceMesh() && !consumer->hasDeviceMesh()) {
+    return false;
+  }
+  // If device mesh are different, the Expr is resharding
+  if (!(producer->getDeviceMesh() == consumer->getDeviceMesh())) {
+    return true;
+  }
+  // Create a map between producer's and consumer's IterDomains. We iterate
+  // over producer's iterdomain and compare sharding type with consumer's
+  // iterdomain
+  const auto p2c_map =
+      PairwiseRootDomainMap(producer, consumer).mapProducerToConsumer();
+  for (auto p_id :
+        TensorDomain::noReductions(producer->getMaybeRFactorDomain())) {
+    auto p2c_map_it = p2c_map.find(p_id);
+    NVF_ERROR( p2c_map_it != p2c_map.end(), "the producer ", producer, " has a dimension ", p_id, " that is not mapped to its consumer ", consumer);
+    auto c_id = p2c_map_it->second;
+    if (p_id->getParallelType() != c_id->getParallelType() &&
+        (p_id->isDeviceDim() || c_id->isDeviceDim())) {
+      // Mismatch found
+      return true;
+    }
+  }
+  return false;
+}
+
 bool isResharding(Expr* expr) {
   // we don't use getTvsWithDifferentSharding because it creates a computeAtMap,
   // which is too costly
-  for (auto producer : ir_utils::filterByType<TensorView>(expr->inputs())) {
-    for (auto consumer : ir_utils::filterByType<TensorView>(expr->outputs())) {
+  for (auto input : ir_utils::filterByType<TensorView>(expr->inputs())) {
+    for (auto output : ir_utils::filterByType<TensorView>(expr->outputs())) {
       // exit early in the unsharded case for performance
-      if (!producer->hasDeviceMesh() && !consumer->hasDeviceMesh()) {
-        continue;
-      }
-      // If device mesh are different, the Expr is resharding
-      if (producer->getDeviceMesh().vector() !=
-          consumer->getDeviceMesh().vector()) {
+      if (haveDifferentShardings(input, output)) {
         return true;
-      }
-
-      // Create a map between producer's and consumer's IterDomains. We iterate
-      // over producer's iterdomain and compare sharding type with consumer's
-      // iterdomain
-      const auto p2c_map =
-          PairwiseRootDomainMap(producer, consumer).mapProducerToConsumer();
-      for (auto p_id :
-           TensorDomain::noReductions(producer->getMaybeRFactorDomain())) {
-        auto p2c_map_it = p2c_map.find(p_id);
-        if (p2c_map_it == p2c_map.end()) {
-          NVF_ERROR(
-              !p_id->isDeviceDim(),
-              "the producer ",
-              producer,
-              " has a sharded dimension ",
-              p_id,
-              " that is not mapped to its consumer ",
-              consumer);
-          continue;
-        }
-        auto c_id = p2c_map_it->second;
-        if (p_id->getParallelType() != c_id->getParallelType() &&
-            (p_id->isDeviceDim() || c_id->isDeviceDim())) {
-          // Mismatch found
-          return true;
-        }
       }
     }
   }
@@ -249,8 +245,12 @@ void insertReshardingBefore(Fusion* fusion) {
         "multi-output expressions are not supported");
 
     auto output = expr->outputs().at(0)->as<TensorView>();
-    auto inputs = getTvsWithDifferentSharding(
-        output, ir_utils::filterByType<TensorView>(expr->inputs()));
+    std::unordered_set<TensorView*> inputs;
+    for (auto input: ir_utils::filterByType<TensorView>(expr->inputs())) {
+      if (haveDifferentShardings(input, output)) {
+        inputs.insert(input);
+      }
+    }
 
     // Reshard each input of expr to match output if necessary
     std::vector<TensorView*> new_inputs;
@@ -288,8 +288,12 @@ void insertReshardingsAfter(Fusion* fusion) {
         "multi-output expressions are not supported");
 
     auto output = expr->outputs().at(0)->as<TensorView>();
-    auto inputs = getTvsWithDifferentSharding(
-        output, ir_utils::filterByType<TensorView>(expr->inputs()));
+    std::unordered_set<TensorView*> inputs;
+    for (auto input: ir_utils::filterByType<TensorView>(expr->inputs())) {
+      if (haveDifferentShardings(input, output)) {
+        inputs.insert(input);
+      }
+    }
 
     // Insert resharding set after the expr and update
     // output of expr to match input's sharding.
--- csrc/multidevice/utils.h
+++ csrc/multidevice/utils.h
@@ -66,6 +66,9 @@ std::unordered_set<TensorView*> getTvsWithDifferentSharding(
 // Returns whether an Expr embeds multi-device resharding
 bool isResharding(Expr* expr);
 
+// Returns whether two tensors have different shardings. Expect a producer/consumer relationship between the arguments.
+bool haveDifferentShardings(TensorView* producer, TensorView* consumer);
+
 // Returns whether a resharding expr reshards an inner axis
 bool isInnerResharding(Expr* expr);
 
