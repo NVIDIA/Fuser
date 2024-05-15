efe406904cfbd11e88f5a4b9b225dc35f73cf1e4
Jingyue Wu <wujingyue@gmail.com>
2024-05-15 21:37:11 +0000

    Fix comments.

--- csrc/multidevice/communication.h
+++ csrc/multidevice/communication.h
@@ -53,21 +53,54 @@ struct CommParams {
 // Communication should not be used directly but through its derived classes:
 // Broadcast, Gather, Scatter, Allgather, and SendRecv. Other collectives will
 // be added later.
+class Communication : public Expr {
+ public:
+  using Expr::Expr;
+  Communication(IrBuilderPasskey passkey, CommParams params);
+  Communication(const Communication* src, IrCloner* ir_cloner);
 
-// CommParams contains the arguments for the communication constructors.
-// Note that each process (associated with a device index given by
-// communicator.deviceId()) will fill CommParams with different arguments,
-// depending on the role they play in this communication. For example, the root
-// of a Gather communication will have <team_size> destination buffers, whereas
-// non-root will have no destination buffers. Also, the ranks not participating
-// in the communication should not instantiate it.
+  Communication(const Communication& other) = delete;
+  Communication& operator=(const Communication& other) = delete;
+  Communication(Communication&& other) = delete;
+  Communication& operator=(Communication&& other) = delete;
+
+  NVFUSER_DECLARE_CLONE_AND_CREATE
+
+  std::string toString(int indent_size = 0) const override;
+  std::string toInlineString(int indent_size = 0) const override;
+  const char* getOpString() const override {
+    return "Communication";
+  }
+
+  // TDOO: add params_ (or flattened parameters) as data attributes so this and
+  // the constructor that takes IrCloner aren't needed.
+  bool sameAs(const Statement* other) const override;
+
+  const CommParams& params() const {
+    return params_;
+  }
+
+  bool isRootInMesh() const {
+    return params_.mesh.has(params_.root);
+  }
+
+  const Team& team();
+
+ private:
+  // Stores the arguments used to construct the communication.
+  CommParams params_;
+  // This can be computed from params_, but given how frequently this is used
+  // in the hot path, I'm currently storing it as a field that'll be computed by
+  // Communication::team().
+  Team team_;
+};
 
 // The method "post" triggers the execution of the communication. This call is
 // non-blocking. The communication can be posted multiple times.
 // It is assumed that the current device_index (given by
 // communicator.deviceId()) belongs to the team of the communication,
 // otherwise an error is thrown.
-
+//
 // NOTE: pytorch's NCCL process group API needs <team_size> buffers on root for
 // scatter/gather operation.
 // (*) Broadcast
@@ -121,48 +154,7 @@ struct CommParams {
 // (*) SendRecv
 // Copies the sender's src buffers to the receiver's dst buffer
 // It is equivalent to a Broadcast with a team of size == 2
-class Communication : public Expr {
- public:
-  using Expr::Expr;
-  Communication(IrBuilderPasskey passkey, CommParams params);
-  Communication(const Communication* src, IrCloner* ir_cloner);
-
-  Communication(const Communication& other) = delete;
-  Communication& operator=(const Communication& other) = delete;
-  Communication(Communication&& other) = delete;
-  Communication& operator=(Communication&& other) = delete;
-
-  NVFUSER_DECLARE_CLONE_AND_CREATE
-
-  std::string toString(int indent_size = 0) const override;
-  std::string toInlineString(int indent_size = 0) const override;
-  const char* getOpString() const override {
-    return "Communication";
-  }
-
-  // TDOO: add params_ (or flattened parameters) as data attributes so this and
-  // the constructor that takes IrCloner aren't needed.
-  bool sameAs(const Statement* other) const override;
-
-  const CommParams& params() const {
-    return params_;
-  }
-
-  bool isRootInMesh() const {
-    return params_.mesh.has(params_.root);
-  }
-
-  const Team& team() const {
-    return params_.team;
-  }
-
- private:
-  // Stores the arguments used to construct the communication.
-  CommParams params_;
-};
-
-// Triggers the execution of the communication. This is a non-blocking call.
-// The communication can be posted multiple times
+//
 // TODO: c10d::Backend* should be sufficient.
 c10::intrusive_ptr<c10d::Work> postSingleCommunication(
     Communication* communication,
