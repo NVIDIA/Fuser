0403ebd73ca37349cfc8ff14ac2d41af6507f7cd
root <26priya11@gmail.com>
2024-05-23 21:02:15 +0000

    id mapping checks

--- tests/cpp/test_sdpa_node.cpp
+++ tests/cpp/test_sdpa_node.cpp
@@ -23,7 +23,8 @@ namespace nvfuser {
 
 using Sizes = std::vector<int64_t>;
 using SDPAParamType = std::tuple<std::optional<Sizes>, DataType>;
-using SDPATest = NVFuserFixtureParamTest<SDPAParamType>;
+using SDPAParametrizedTest = NVFuserFixtureParamTest<SDPAParamType>;
+using SDPATest = NVFuserTest;
 
 constexpr int64_t n = 16, h = 32, l = 64, s = 128, e = 64;
 
