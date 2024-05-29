d3f44a078f37047ebb95597b024be3e30ba294b0
Ryan Spring <rspring@nvidia.com>
2024-05-29 11:45:14 -0700

    save

--- csrc/preseg_passes/factor_reduction.cpp
+++ csrc/preseg_passes/factor_reduction.cpp
@@ -16,6 +16,135 @@
 
 namespace nvfuser::preseg_passes {
 
+// Transform the provided tensor domain to two domains, a producer and
+// consumer domain. These domains are created by taking axes and reducing them
+// in the producer domain, and taking the remaining reduction axes and
+// reducing them in the consumer domain.
+std::pair<TensorDomain*, TensorDomain*> factorReductionDomain(
+    TensorDomain* original_td,
+    const std::vector<int64_t>& axes) {
+  NVF_CHECK(!axes.empty(), "No axes provided to rfactor replay.");
+
+  const int64_t kNumDims = original_td->nDims();
+
+  NVF_CHECK(axes.size() < kNumDims);
+
+  // Check that axes are valid
+  std::for_each(axes.begin(), axes.end(), [kNumDims](int64_t i) {
+    NVF_CHECK(
+        i >= -kNumDims && i < kNumDims,
+        "Rfactor replay received an axis outside the number of dims in the tensor, acceptable inclusive range is ",
+        -kNumDims,
+        " to ",
+        kNumDims - 1);
+  });
+
+  NVF_CHECK(
+      std::all_of(
+          axes.begin(),
+          axes.end(),
+          [original_td](int64_t i) {
+            return original_td->axis(i)->isReduction();
+          }),
+      "Cannot rfactor axes that are not reduction axes.");
+
+  std::vector<IterDomain*> original_td_root = original_td->root();
+
+  // Put in a set to make searching easy
+  std::unordered_set<IterDomain*> rfactor_root_axes;
+  std::copy_if(axes.begin(), axes.end(), [&original_td_root](int64_t pos) {
+    return original_td_root.at(pos);
+  });
+
+  // Generate a new TensorDomain and set up map from one root to this one.
+  std::vector<IterDomain*> new_producer_root;
+  new_producer_root.reserve(original_td_root.size());
+
+  std::transform(
+      original_td_root.begin(),
+      original_td_root.end(),
+      std::back_inserter(new_producer_root),
+      [&](IterDomain* id) {
+        // If this is an rfactor root, it will be a reduction in this stage
+        if (rfactor_root_axes.find(id) != rfactor_root_axes.end()) {
+          return IterDomainBuilder(id->start(), id->extent())
+              .stop_offset(id->stopOffset())
+              .iter_type(IterType::Reduction)
+              .build();
+          // If this is not an rfactor root, but a reduction root, it should be
+          // turned into an iteration domain
+        } else if (id->isReduction()) {
+          return IterDomainBuilder(id->start(), id->extent())
+              .stop_offset(id->stopOffset())
+              .build();
+        } else {
+          return id->cloneWithoutRFactor();
+        }
+      });
+
+  TensorDomain* producer_domain = IrBuilder::create<TensorDomain>(
+      original_td->container(),
+      new_producer_root,
+      TensorDomain::getContiguityFilledWith(new_producer_root, true));
+
+  std::vector<IterDomain*> new_consumer_root_domain;
+  new_consumer_root_domain.reserve(original_td_root.size() - axes.size());
+  for (IterDomain* id : original_td_root) {
+    // If this is an rfactor root, skip it at this stage
+    if (rfactor_root_axes.find(id) != rfactor_root_axes.end()) {
+      continue;
+    }
+    IterDomain* new_consumer_root = id->cloneWithoutRFactor();
+    new_consumer_root_domain.push_back(new_consumer_root);
+  }
+
+  TensorDomain* consumer_domain = IrBuilder::create<TensorDomain>(
+      original_td->container(),
+      new_consumer_root,
+      TensorDomain::getContiguityFilledWith(new_consumer_root, true));
+
+  return std::make_pair(producer_domain, consumer_domain);
+}
+
+void applyReductionFactor(
+    TensorView* consumer,
+    const std::vector<int64_t>& axes) {
+  FusionGuard fg(consumer->fusion());
+  NVF_ERROR(consumer->nDims() > 0, "Tried reduction factor a 0-dim TensorView");
+  NVF_CHECK(
+      consumer->definition() != nullptr &&
+          (consumer->definition()->isStrictlyOneOf<ReductionOp>()),
+      "Error factoring out reduction axes from",
+      consumer->toString(),
+      " its definition is either a nullptr or not a reduction.");
+
+  // Split tensor view into 2 parts
+  auto&& [producer_domain, consumer_domain] = factorReductionDomain(axes);
+
+  // Create the new producer
+  TensorView* producer =
+      IrBuilder::create<TensorView>(producer_domain, tv->getDataType().value());
+
+  // This TensorView is the consumer; Update its domain
+  consumer->setDomain(consumer_domain);
+
+  ReductionOp* this_reduction = dynamic_cast<ReductionOp*>(definition());
+  // Setup dependency chain, inserting producer before this op.
+  // Expr* producer_definition =
+  IrBuilder::create<ReductionOp>(
+      this_reduction->getReductionOpType(),
+      this_reduction->init(),
+      producer,
+      this_reduction->in());
+
+  // Expr* consumer_definition =
+  IrBuilder::create<ReductionOp>(
+      this_reduction->getReductionOpType(),
+      this_reduction->init(),
+      consumer,
+      producer);
+}
+
 void FactorReductionPass::runPass(Fusion* fusion) {
   // Persistent schedule expects all reductions to have same axes.
   // Factor common reduction axes into separate reduction operations
@@ -120,8 +249,7 @@ void FactorReductionPass::runPass(Fusion* fusion) {
     // Skip if all ids are used for this TensorView
     if (rfactor_indices.size() < num_reduction_ids) {
       // Separate common reduction axes
-      // TODO replace pseudo-like rfactor operation
-      // tv->rFactor(rfactor_indices);
+      applyReductionFactor(tv, rfactor_indices);
     }
   }
 }
