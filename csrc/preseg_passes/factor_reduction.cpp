917374b935e3ecd79b99a4c0b95eff5b8d19211f
Ryan Spring <rspring@nvidia.com>
2024-05-28 21:45:21 -0700

    save

--- csrc/preseg_passes/factor_reduction.cpp
+++ csrc/preseg_passes/factor_reduction.cpp
@@ -7,6 +7,8 @@
 // clang-format on
 #include <preseg_passes/factor_reduction.h>
 
+#include <id_model/id_model.h>
+#include <scheduler/utils.h>
 #include <unordered_set>
 #include <vector>
 
@@ -15,64 +17,95 @@
 namespace nvfuser::preseg_passes {
 
 void FactorReductionPass::runPass(Fusion* fusion) {
-  // RFactor greatest common subset
-  //
-  // std::unordered_set<IterDomain*> id_subset;
-  // std::unordered_set<TensorView*> tv_subset;
-  // TensorView* reference_tv;
-  //
-  // std::vector<TensorView*> reduction_tvs =
-  // scheduler_utils::getReductionTvs(fusion); 
-  // if (reduction_tvs.size() < 1) {
-  //   return;
-  // }
-  //
-  // for (TensorView* tv : reduction_tvs) {
-  //   const std::vector<IterDomain*>& tv_root_domain =
-  //   reference_tv->getRootDomain(); if (reference_tv == nullptr) {
-  //     reference_tv = tv;
-  //     id_subset.insert(tv_root_domain.begin(), tv_root_domain.end());
-  //     tv_subset.insert(tv);
-  //     continue;
-  //   }
-  //
-  //   std::unordered_set<IterDomain*> common_ids;
-  //   size_t mismatch_ids = 0;
-  //   for (IterDomain* id : id_subset) {
-  //     map reference_id to this_tv_id
-  //     if this_tv_id exists {
-  //       common_ids.insert(id);
-  //     } else {
-  //       ++mismatch_ids;
-  //     }
-  //   }
-  //
-  //   if (mismatch_ids == id_subset.size()) {
-  //     // rfactor greatest common subset
-  //     if (tv_subset.size() == 1) {
-  //       continue;
-  //     }
-  //
-  //     // map common reduction axes to integer
-  //     for (TensorView* tv : tv_subset) {
-  //       for (IterDomain* id : id_subset) {
-  //         map reduction id to this_tv_id
-  //         get integer for this_tv_id
-  //       }
-  //       rfactor common reduction axes
-  //     }
-  //
-  //     // reset state
-  //     id_subset.clear();
-  //     tv_subset.clear();
-  //     reference_tv == nullptr;
-  //
-  //     continue;
-  //   }
-  //
-  //   swap(id_subset, common_ids);
-  //   tv_subset.append(tv);
-  // }
+  // Persistent schedule expects all reductions to have same axes.
+  // Factor common reduction axes into separate reduction operations
+  // to create better fusions.
+
+  // Common reduction iterDomains for reference TensorView
+  std::unordered_set<IterDomain*> id_subset;
+  // All TensorViews with common reduction axes
+  std::vector<TensorView*> tv_subset;
+
+  std::vector<TensorView*> reduction_tvs =
+      scheduler_utils::getReductionTvs(fusion);
+  if (reduction_tvs.size() <= 1) {
+    return;
+  }
+
+  FusionGuard fg(fusion);
+  IdModel id_model(fusion, /*build_graphs=*/false, /*allow_self_mapping=*/true);
+  id_model.buildExactGraph();
+  ValGraph exact_graph = id_model.idGraph(IdMappingMode::EXACT);
+  const DisjointSets<Val*>& val_sets = exact_graph.disjointValSets();
+
+  for (TensorView* tv : reduction_tvs) {
+    const std::vector<IterDomain*>& tv_root_domain = tv->getRootDomain();
+
+    if (tv_subset.empty()) {
+      std::copy_if(
+          tv_root_domain.begin(),
+          tv_root_domain.end(),
+          std::inserter(id_subset, id_subset.begin()),
+          [](IterDomain* id) { return id->isReduction(); });
+      tv_subset.push_back(tv);
+      continue;
+    }
+
+    std::vector<IterDomain*> reduction_ids;
+    std::copy_if(
+        tv_root_domain.begin(),
+        tv_root_domain.end(),
+        std::back_inserter(reduction_ids),
+        [](IterDomain* id) { return id->isReduction(); });
+
+    std::unordered_set<IterDomain*> intersection;
+    std::copy_if(
+        id_subset.begin(),
+        id_subset.end(),
+        std::inserter(intersection, intersection.begin()),
+        [&](IterDomain* subset_id) {
+          return std::any_of(
+              reduction_ids.begin(), reduction_ids.end(), [&](IterDomain* id) {
+                return val_sets.permissiveAreMapped(subset_id, id);
+              });
+        });
+
+    if (!intersection.empty()) {
+      id_subset.swap(intersection);
+      tv_subset.push_back(tv);
+    }
+  }
+
+  // All reduction iterDomains in last TensorView do not match any in id_subset
+  // rfactor common subset of reduction axes for TensorView's in subset
+  if (tv_subset.size() == 1) {
+    return;
+  }
+
+  // Map common reduction iterDomains to integer axes
+  for (TensorView* tv : tv_subset) {
+    const std::vector<IterDomain*>& tv_root_domain = tv->getRootDomain();
+
+    std::vector<int64_t> rfactor_indices;
+    rfactor_indices.reserve(id_subset.size());
+
+    std::transform(
+        id_subset.begin(),
+        id_subset.end(),
+        std::back_inserter(rfactor_indices),
+        [&](IterDomain* subset_id) {
+          auto iter = std::find_if(
+              tv_root_domain.begin(),
+              tv_root_domain.end(),
+              [&](IterDomain* id) {
+                return val_sets.permissiveAreMapped(subset_id, id);
+              });
+          return std::distance(iter, tv_root_domain.begin());
+        });
+
+    // rfactor common reduction axes
+    tv->rFactor(rfactor_indices);
+  }
 }
 
 } // namespace nvfuser::preseg_passes
