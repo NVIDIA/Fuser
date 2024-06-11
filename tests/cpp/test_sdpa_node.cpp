aa1f4ebb2a2d9693fd06ae461198769d54464ff7
Priya Mishra <52657555+Priya2698@users.noreply.github.com>
2024-06-10 20:25:34 -0700

    Add scheduling support for `SDPAOp` (#2361)
    Stacked on #2294.
    
    1. Adds the producer-consumer mapping to root domain map.
    2. Adds `SDPAOp` to `ExprEvalScheduler`.
    3. Modifies `ExprEvalSched::canSchedule` to skip computeAt checks and
    only use the compile time check since expression evaluator scheduler
    will only accept segments with a single expression of type MatmulOp /
    LinearOp / SdpaOp.,
    
    ---------
    
    Co-authored-by: Jacob Hinkle <1454944+jacobhinkle@users.noreply.github.com>

--- tests/cpp/test_sdpa_node.cpp
+++ tests/cpp/test_sdpa_node.cpp
@@ -9,25 +9,36 @@
 #include <gtest/gtest.h>
 
 #include <fusion.h>
-#include <id_model/id_model.h>
-#include <mma_type.h>
 #include <ops/all_ops.h>
+#include <ops/utils.h>
 #include <preseg_passes/allocation_order_inference.h>
+#include <preseg_passes/move_split_cat.h>
 #include <preseg_passes/optimization_pass.h>
-#include <scheduler/all_schedulers.h>
-#include <scheduler/mma_utils.h>
 #include <tests/cpp/utils.h>
 #include <tests/cpp/validator.h>
 
 namespace nvfuser {
 
-using SDPATest = NVFuserTest;
+class SDPATest : public NVFuserTest {
+ protected:
+  SDPATest() : optimization_guard_(false), allocation_order_guard_(false) {}
+
+ private:
+  // Note: `MoveSplitCat` and `AllocationDomain` preseg passes use ID model.
+  // `SdpaFwdOp` currently does not work with ID model since it requires all
+  // sibling outputs to have the same root domain.
+  //  This will be modified in a future PR.
+  preseg_passes::OptimizationPassGuard<preseg_passes::MoveSplitCatPass>
+      optimization_guard_;
+  preseg_passes::OptimizationPassGuard<preseg_passes::AllocationDomainPass>
+      allocation_order_guard_;
+};
 
 constexpr int64_t n = 16, h = 32, l = 64, s = 128, e = 64;
 
 // Note: Flash Attention is only supported on Ampere and above.
 
-TEST(SDPATest, NonCausalAttnConcrete) {
+TEST_F(SDPATest, NonCausalAttnConcrete) {
   NVFUSER_TEST_CUDA_ARCH_GUARD(8, 0);
   auto fusion = std::make_unique<Fusion>();
   FusionGuard fg(fusion.get());
@@ -68,22 +79,12 @@ TEST(SDPATest, NonCausalAttnConcrete) {
       scale);
   ;
 
-  FusionExecutor fe;
-  fusion->aliasOutputToInput(
-      fusion->outputs()[0], /*input=*/nullptr, AllocationType::Evaluate);
-  fe.compileFusion(fusion.get(), {q, k, v});
-  auto out = fe.runFusion({q, k, v});
-
-  testValidate(
-      fusion.get(),
-      out,
-      {q, k, v},
-      {std::get<0>(aten_outputs)},
-      __LINE__,
-      __FILE__);
+  FusionExecutorCache fec(std::move(fusion));
+  auto out = fec.runFusionWithInputs({q, k, v});
+  EXPECT_TRUE(at::allclose(out[0], std::get<0>(aten_outputs)));
 }
 
-TEST(SDPATest, NonCausalAttnSymbolic) {
+TEST_F(SDPATest, NonCausalAttnSymbolic) {
   NVFUSER_TEST_CUDA_ARCH_GUARD(8, 0);
   auto fusion = std::make_unique<Fusion>();
   FusionGuard fg(fusion.get());
@@ -124,22 +125,12 @@ TEST(SDPATest, NonCausalAttnSymbolic) {
       scale);
   ;
 
-  FusionExecutor fe;
-  fusion->aliasOutputToInput(
-      fusion->outputs()[0], /*input=*/nullptr, AllocationType::Evaluate);
-  fe.compileFusion(fusion.get(), {q, k, v});
-  auto out = fe.runFusion({q, k, v});
-
-  testValidate(
-      fusion.get(),
-      out,
-      {q, k, v},
-      {std::get<0>(aten_outputs)},
-      __LINE__,
-      __FILE__);
+  FusionExecutorCache fec(std::move(fusion));
+  auto out = fec.runFusionWithInputs({q, k, v});
+  EXPECT_TRUE(at::allclose(out[0], std::get<0>(aten_outputs)));
 }
 
-TEST(SDPATest, CausalAttn) {
+TEST_F(SDPATest, CausalAttn) {
   NVFUSER_TEST_CUDA_ARCH_GUARD(8, 0);
   auto fusion = std::make_unique<Fusion>();
   FusionGuard fg(fusion.get());
@@ -179,18 +170,66 @@ TEST(SDPATest, CausalAttn) {
       /*scale=*/1e-3);
   ;
 
-  FusionExecutor fe;
-  fusion->aliasOutputToInput(
-      fusion->outputs()[0], /*input=*/nullptr, AllocationType::Evaluate);
-  fe.compileFusion(fusion.get(), {q, k, v});
-  auto out = fe.runFusion({q, k, v});
+  FusionExecutorCache fec(std::move(fusion));
+  auto out = fec.runFusionWithInputs({q, k, v});
+  EXPECT_TRUE(at::allclose(out[0], std::get<0>(aten_outputs)));
+}
 
-  testValidate(
-      fusion.get(),
-      out,
-      {q, k, v},
-      {std::get<0>(aten_outputs)},
-      __LINE__,
-      __FILE__);
+TEST_F(SDPATest, PairwiseRootDomainMap) {
+  NVFUSER_TEST_CUDA_ARCH_GUARD(8, 0);
+  auto fusion = std::make_unique<Fusion>();
+  FusionGuard fg(fusion.get());
+  std::vector<int64_t> q_shape({n, h, l, e});
+  std::vector<int64_t> k_shape({n, h, s, e});
+  std::vector<int64_t> v_shape({n, h, s, e});
+
+  auto tvq = makeSymbolicTensor(q_shape, DataType::Half);
+  auto tvk = makeSymbolicTensor(k_shape, DataType::Half);
+  auto tvv = makeSymbolicTensor(k_shape, DataType::Half);
+
+  fusion->addInput(tvq);
+  fusion->addInput(tvk);
+  fusion->addInput(tvv);
+
+  auto tvattn = sdpfa_fwd(
+      tvq,
+      tvk,
+      tvv,
+      /*dropout_p=*/IrBuilder::create<Val>(0.0),
+      /*is_causal=*/IrBuilder::create<Val>(true),
+      /*scale=*/IrBuilder::create<Val>(1e-3));
+  fusion->addOutput(tvattn.output);
+
+  // Verify mapping between Q,K,V and attention output
+  std::vector<TensorView*> producer_tvs{tvq, tvk, tvv};
+  for (auto role : {AttnRole::Q, AttnRole::K, AttnRole::V}) {
+    auto producer_tv = producer_tvs[(int)role];
+    auto pairwise_map = PairwiseRootDomainMap(producer_tv, tvattn.output)
+                            .mapProducerToConsumer();
+
+    auto mappingExists = [&pairwise_map](
+                             IterDomain* p_id, IterDomain* c_id) -> bool {
+      return pairwise_map.find(p_id) != pairwise_map.end() &&
+          pairwise_map[p_id] == c_id;
+    };
+
+    // Mapping for N, H exists from Q/K/V to output.
+    for (auto idx : c10::irange(2)) {
+      EXPECT_TRUE(
+          mappingExists(producer_tv->axis(idx), tvattn.output->axis(idx)));
+    }
+    // Mapping for L exists between Q and output.
+    if (role == AttnRole::Q) {
+      EXPECT_TRUE(mappingExists(producer_tv->axis(2), tvattn.output->axis(2)));
+    } else {
+      EXPECT_FALSE(mappingExists(producer_tv->axis(2), tvattn.output->axis(2)));
+    }
+    // Mapping for Ev exists between V and output.
+    if (role == AttnRole::V) {
+      EXPECT_TRUE(mappingExists(producer_tv->axis(3), tvattn.output->axis(3)));
+    } else {
+      EXPECT_FALSE(mappingExists(producer_tv->axis(3), tvattn.output->axis(3)));
+    }
+  }
 }
 } // namespace nvfuser
