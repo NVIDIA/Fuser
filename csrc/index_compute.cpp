1dcd708d98d6d10758b2922590ef6481d43d102b
Xiang Gao <qasdfgtyuiop@gmail.com>
2024-06-13 13:04:54 -0700

    fix

--- csrc/index_compute.cpp
+++ csrc/index_compute.cpp
@@ -2091,9 +2091,8 @@ kir::TensorIndex* Index::getProducerIndex(
     DataType as_type) {
   Val* index = nullptr;
 
-  if (GpuLower::current()->requiresIdModel() ||
-      hasEnableOptionArgument(EnableOption::IdModel, "producer_index") &&
-          GpuLower::current()->isTensorIndexerEnabled()) {
+  if (hasEnableOptionArgument(EnableOption::IdModel, "producer_index") &&
+      GpuLower::current()->isTensorIndexerEnabled()) {
     index = GpuLower::current()->tensorIndexer().getLinearIndex(
         producer, consumer->definition());
   } else {
@@ -2181,9 +2180,8 @@ kir::TensorIndex* Index::getConsumerIndex(
     bool generate_pointer,
     DataType as_type) {
   Val* index = nullptr;
-  if (GpuLower::current()->requiresIdModel() ||
-      hasEnableOptionArgument(EnableOption::IdModel, "consumer_index") &&
-          GpuLower::current()->isTensorIndexerEnabled()) {
+  if (hasEnableOptionArgument(EnableOption::IdModel, "consumer_index") &&
+      GpuLower::current()->isTensorIndexerEnabled()) {
     index = GpuLower::current()->tensorIndexer().getLinearIndex(
         consumer, consumer->definition());
   } else {
