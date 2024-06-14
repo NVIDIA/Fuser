fe927684bd8e3b536adb0d4a18737b793878e04c
jjsjann123 <jiej@nvidia.com>
2024-06-14 15:19:20 -0700

    lintrunner python test

--- tests/python/test_python_frontend.py
+++ tests/python/test_python_frontend.py
@@ -4221,8 +4221,20 @@ class TestNvFuserFrontend(TestCase):
 
     def test_matmul_issue_2354(self):
         def fusion_func(fd: FusionDefinition):
-            T0 = fd.define_tensor(shape=[-1, -1], contiguity=[True, True], dtype=DataType.Float, is_cpu=False, stride_order=[1, 0])
-            T1 = fd.define_tensor(shape=[-1, -1, -1], contiguity=[True, True, True], dtype=DataType.Float, is_cpu=False, stride_order=[2, 1, 0])
+            T0 = fd.define_tensor(
+                shape=[-1, -1],
+                contiguity=[True, True],
+                dtype=DataType.Float,
+                is_cpu=False,
+                stride_order=[1, 0],
+            )
+            T1 = fd.define_tensor(
+                shape=[-1, -1, -1],
+                contiguity=[True, True, True],
+                dtype=DataType.Float,
+                is_cpu=False,
+                stride_order=[2, 1, 0],
+            )
             T2 = fd.ops.linear(T1, T0)
             S3 = fd.define_scalar(1.41421, dtype=DataType.Double)
             T4 = fd.ops.mul(T2, S3)
@@ -4233,8 +4245,16 @@ class TestNvFuserFrontend(TestCase):
             fusion_func(fd)
 
         inputs = [
-            torch.randn((8, 4), dtype=torch.float32, device='cuda:0'),
-            torch.randn((6, 2, 4,), dtype=torch.float32, device='cuda:0')
+            torch.randn((8, 4), dtype=torch.float32, device="cuda:0"),
+            torch.randn(
+                (
+                    6,
+                    2,
+                    4,
+                ),
+                dtype=torch.float32,
+                device="cuda:0",
+            ),
         ]
 
         nvf_out, _ = self.exec_nvfuser(fusion_func, inputs)
