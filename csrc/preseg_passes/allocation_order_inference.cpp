8d33abe277458d81ee1a582beb66f552a865a03b
jjsjann123 <jiej@nvidia.com>
2024-06-14 15:28:28 -0700

    comment

--- csrc/preseg_passes/allocation_order_inference.cpp
+++ csrc/preseg_passes/allocation_order_inference.cpp
@@ -302,6 +302,12 @@ void AllocationDomainPass::runPass(Fusion* fusion) {
   auto output_tvs = ir_utils::filterByType<TensorView>(fusion->outputs());
   std::vector<TensorView*> dsts;
   dsts.reserve(output_tvs.size());
+  // TODO: instead of exclusion to propagation, this pass should mark it clear
+  // that the propagated allocation order is strictly an optimization hint,
+  // rather than a semantic requirement coming from computation definition.
+  // Scheduler/segmenter would be able to coordinate and discard optimization
+  // hint, but they should respect semantic requirement.
+  // see issue: https://github.com/NVIDIA/Fuser/pull/2426
   for (TensorView* output : output_tvs) {
     if (output->isDefinitionType<LinearOp>() ||
         output->isDefinitionType<MatmulOp>() ||
