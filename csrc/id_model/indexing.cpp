4cade6dea9e1c0942dcd95d2e30cdb25161ffbb3
Naoya Maruyama <nmaruyama@nvidia.com>
2024-07-03 12:04:09 -0700

    Reorder allocation domains to align with logical domains
    Not strictly necessary unless vectorized domains are not placed
    innermost

--- csrc/id_model/indexing.cpp
+++ csrc/id_model/indexing.cpp
@@ -219,6 +219,15 @@ class AllocationDomainSetup : private kir::IrVisitor {
       }
     }
 
+    if (auto reordered_domains =
+            reorderAllocationDomains(tv, allocation_domains);
+        reordered_domains.has_value()) {
+      allocation_domains = reordered_domains.value();
+      NVF_ERROR(std::all_of(contiguity.begin(), contiguity.end(), [](auto b) {
+        return b.has_value() && b.value();
+      }));
+    }
+
     return {allocation_domains, contiguity};
   }
 
@@ -301,6 +310,103 @@ class AllocationDomainSetup : private kir::IrVisitor {
     }
 
     return IndexingAllocationInfo{actual_allocation_domains, actual_strides};
+  }
+
+  std::optional<std::vector<IterDomain*>> reorderAllocationDomains(
+      const TensorView* tv,
+      const std::vector<IterDomain*>& allocation_domains) const {
+    // Don't change the set allocation domain. Ignore allocation
+    // domains of non-global tensors. For example,
+    // AliasTest.NotAllOutputAlias_Reduction has a tensor, tv6, that
+    // is a Local tensor with CA position of 4 but has an allocation
+    // domain that's just a permutation of its logical domain. Such
+    // invalid allocations need to be ignored.
+    if (tv->hasAllocation() && tv->getMemoryType() == MemoryType::Global) {
+      return std::nullopt;
+    }
+
+    auto exprs = DependencyCheck::getAllExprsBetween(
+        {tv->getLogicalDomain().begin(), tv->getLogicalDomain().end()},
+        {allocation_domains.begin(), allocation_domains.end()});
+
+    if (exprs.empty()) {
+      return std::nullopt;
+    }
+
+    // Replay exprs from the logical domain to get the non-reordered
+    // domains
+    auto ordered_domains = tv->getLogicalDomain();
+    for (auto expr : exprs) {
+      // Find the position to insert the outputs.
+      int64_t insertion_pos = -1;
+      for (auto inp : expr->inputs()) {
+        auto it =
+            std::find(ordered_domains.begin(), ordered_domains.end(), inp);
+        if (it == ordered_domains.end()) {
+          continue;
+        }
+        // Insert right after the input
+        int64_t pos = std::distance(ordered_domains.begin(), it) + 1;
+        if (insertion_pos == -1 || pos > insertion_pos) {
+          insertion_pos = pos;
+        }
+      }
+      NVF_ERROR(
+          insertion_pos >= 0,
+          "Failed to replay: ",
+          expr->toString(),
+          " in ",
+          tv->toString());
+      // Insert the outputs
+      for (auto out : expr->outputs()) {
+        ordered_domains.insert(
+            ordered_domains.begin() + insertion_pos, out->as<IterDomain>());
+        ++insertion_pos;
+      }
+      // Delete the inputs
+      for (auto inp : expr->inputs()) {
+        auto it =
+            std::find(ordered_domains.begin(), ordered_domains.end(), inp);
+        if (it == ordered_domains.end()) {
+          continue;
+        }
+        ordered_domains.erase(it);
+      }
+    }
+
+    // At this point, all domains of allocation_domains must exist in
+    // domains.
+    for (auto alloc_dom : allocation_domains) {
+      auto it =
+          std::find(ordered_domains.begin(), ordered_domains.end(), alloc_dom);
+      NVF_ERROR(
+          it != ordered_domains.end(),
+          "Missing allocation domain: ",
+          alloc_dom->toString(),
+          ", domains: ",
+          toDelimitedString(ordered_domains));
+    }
+
+    // Pick only the allocation domains from the ordered domains
+    std::vector<IterDomain*> reordered_allocation_domains;
+    reordered_allocation_domains.reserve(allocation_domains.size());
+
+    for (auto dom : ordered_domains) {
+      auto it =
+          std::find(allocation_domains.begin(), allocation_domains.end(), dom);
+      if (it == allocation_domains.end()) {
+        continue;
+      }
+      reordered_allocation_domains.push_back(dom);
+    }
+
+    // If it's the same order, just return nullopt to tell nothing
+    // needs to be reordered
+    if (reordered_allocation_domains == allocation_domains) {
+      return std::nullopt;
+    }
+
+    return reordered_allocation_domains;
   }
 
   std::unordered_map<TensorView*, IndexingAllocationInfo> tv_alloc_info_map;
