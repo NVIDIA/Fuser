3793f5f8c62ed6c1ab75a2dcb46352598086c3fe
Jingyue Wu <wujingyue@gmail.com>
2024-07-03 23:42:33 +0000

    Add license.

--- tests/python/test_pointwise.py
+++ tests/python/test_pointwise.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024-present NVIDIA CORPORATION & AFFILIATES.
+# All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
 import pytest
 import torch
 from nvfuser import FusionDefinition, DataType
