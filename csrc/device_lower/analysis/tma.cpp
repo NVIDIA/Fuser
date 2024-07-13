54e7a3b03bd0bd787e191395112c8dd0d00951ac
Gao, Xiang <qasdfgtyuiop@gmail.com>
2024-07-12 19:23:53 -0700

    format

--- csrc/device_lower/analysis/tma.cpp
+++ csrc/device_lower/analysis/tma.cpp
@@ -440,8 +440,7 @@ class MovePartitionedGroupUpByRotation : public Pass {
   MovePartitionedGroupUpByRotation(
       const std::unordered_set<ValGroup>& non_bulk_groups,
       std::list<TMADim>& inferred_dims)
-      : non_bulk_groups_(non_bulk_groups),
-        inferred_dims_(inferred_dims) {}
+      : non_bulk_groups_(non_bulk_groups), inferred_dims_(inferred_dims) {}
 };
 
 // Find the following pattern:
@@ -555,8 +554,7 @@ class MergeTileGroupsByRotation : public Pass {
   MergeTileGroupsByRotation(
       std::unordered_set<ValGroup>& bulk_groups,
       std::list<TMADim>& inferred_dims)
-      : bulk_groups_(bulk_groups),
-        inferred_dims_(inferred_dims) {}
+      : bulk_groups_(bulk_groups), inferred_dims_(inferred_dims) {}
 };
 
 std::tuple<
