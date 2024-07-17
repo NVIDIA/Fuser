57761a1ef345a37f1e75e09198d88f3636cf9d7d
Ryan Spring <rspring@nvidia.com>
2024-07-17 15:59:13 +0000

    per-sample

--- tests/python/pytest_ops.py
+++ tests/python/pytest_ops.py
@@ -190,9 +190,10 @@ def definition_op_in_schedule_error_test_fn(opinfo: OpInfo, sample: SampleInput)
 # TODO Maybe only test a single dtype
 @create_op_test(tuple(op for op in opinfos if op.sample_input_generator is not None))
 def test_definition_op_in_schedule_error(op: OpInfo, dtype: torch.dtype):
-    FusionCache.reset()
-    clear_cuda_cache()
     for sample in op.sample_input_generator(op, dtype):
+        # clear cache for each sample
+        FusionCache.reset()
+        clear_cuda_cache()
         with pytest.raises(
             RuntimeError, match=r"Attempting to add to a completed definition"
         ):
