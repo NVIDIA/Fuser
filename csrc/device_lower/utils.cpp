e0a937464e2fe4fba91f5f6a635883d7a18763fa
Jacob Hinkle <jhinkle@nvidia.com>
2024-07-18 20:27:54 +0000

    Add missing cast in allocGlobalBufferForGridComm
    Fixes #2634
    
    I'm working on creating a smaller repro.

--- csrc/device_lower/utils.cpp
+++ csrc/device_lower/utils.cpp
@@ -695,7 +695,8 @@ kir::Allocate* allocGlobalBufferForGridComm(
     bool resets_to_zero) {
   const std::vector<IterDomain*> new_buffer_ids = {
       IrBuilder::create<IterDomain>(IterDomainBuilder(
-          GpuLower::current()->kernel()->zeroVal(), buffer_size))};
+          GpuLower::current()->kernel()->zeroVal(DataType::Index),
+          SimplifyingIrBuilder::maybeCastExpr(DataType::Index, buffer_size)))};
   const auto buffer_domain = IrBuilder::create<TensorDomain>(new_buffer_ids);
   const auto buffer_tv =
       IrBuilder::create<TensorView>(buffer_domain, dtype, MemoryType::Global);
