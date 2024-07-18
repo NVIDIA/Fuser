c9a7c58724f183637f47c9b79e050542c958cb8d
Liqiang Lu <liqiangxl@gmail.com>
2024-07-18 13:45:31 +0000

    clang tidy

--- csrc/scheduler/no_op.cpp
+++ csrc/scheduler/no_op.cpp
@@ -33,7 +33,7 @@ bool allOutputsArePointerArithmetics(Fusion* fusion) {
   const AliasAnalysisResult analysis = findAliases(
       fusion,
       /*can_override_empty_allocation_domain=*/false,
-      /*allow_output_alias_intermediate=*/false);
+      /*may_alias_intermediate=*/false);
   auto out_tvs = ir_utils::filterByType<TensorView>(fusion->outputs());
   return std::all_of(
       out_tvs.begin(), out_tvs.end(), [&analysis](TensorView* out) {
