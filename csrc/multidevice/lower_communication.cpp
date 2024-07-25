d863619b4a4e37fc2d0b6650ce3fc44dc86ef6ae
mcowan <mcowan@nvidia.com>
2024-07-25 16:19:48 +0000

    lint

--- csrc/multidevice/lower_communication.cpp
+++ csrc/multidevice/lower_communication.cpp
@@ -295,7 +295,7 @@ std::vector<Communication*> lowerCommunication(Expr* c) {
 bool isLowerableToCommunication(Expr* expr) {
   if (!ir_utils::isTvOp(expr)) {
     return false;
-  } 
+  }
   if (expr->isA<ReductionOp>()) {
     auto in = expr->as<ReductionOp>()->in()->as<TensorView>();
     auto out = expr->as<ReductionOp>()->out()->as<TensorView>();
