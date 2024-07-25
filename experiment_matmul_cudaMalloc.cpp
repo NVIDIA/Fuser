94e9e3dca722b72d7563bbe77357967309eea63b
snordmann <snordmann@nvidia.com>
2024-07-25 09:24:33 -0700

    add option to disable torch cuda memory cache

--- experiment_matmul_cudaMalloc.cpp
+++ experiment_matmul_cudaMalloc.cpp
@@ -1,5 +1,13 @@
-#include <torch/torch.h>
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+
 #include <c10/cuda/CUDAStream.h>
+#include <torch/torch.h>
 #include <iostream>
 #include <vector>
 
@@ -7,76 +15,78 @@ using namespace c10::cuda;
 
 // Perform matrix multiplication I times
 int main(int argc, char* argv[]) {
+  // Parse command-line arguments
+  if (argc != 6) {
+    std::cerr << "Usage: " << argv[0]
+              << " LOG2_M LOG2_N LOG2_K use_stream use_matmul_out" << std::endl;
+    std::cerr << "use_stream: 0 for default stream, 1 to use multiple streams"
+              << std::endl;
+    std::cerr
+        << "use_matmul_out: 0 for using at::matmul, 1 for using at::matmul_out with preallocated output"
+        << std::endl;
+    return 1;
+  }
+  int64_t M = std::pow(2, std::stoi(argv[1]));
+  int64_t N = std::pow(2, std::stoi(argv[2]));
+  int64_t K = std::pow(2, std::stoi(argv[3]));
+  bool use_stream = std::stoi(argv[4]) != 0;
+  bool use_matmul_out = std::stoi(argv[5]) != 0;
 
-    // Parse command-line arguments
-    if (argc != 6) {
-        std::cerr << "Usage: " << argv[0] << " LOG2_M LOG2_N LOG2_K use_stream use_matmul_out" << std::endl;
-        std::cerr << "use_stream: 0 for default stream, 1 to use multiple streams" << std::endl;
-        std::cerr << "use_matmul_out: 0 for using at::matmul, 1 for using at::matmul_out with preallocated output" << std::endl;
-        return 1;
+  constexpr int I = 8; // number of iterations
+
+  std::cout << "M=" << M << std::endl;
+  std::cout << "N=" << N << std::endl;
+  std::cout << "K=" << K << std::endl;
+  std::cout << "use_stream=" << use_stream << std::endl;
+  std::cout << "use_matmul_out=" << use_matmul_out << std::endl;
+
+  // input tensors
+  torch::Device device(torch::kCUDA);
+  torch::Tensor mat1 = torch::rand({M, K}, device);
+  torch::Tensor mat2 = torch::rand({K, N}, device);
+
+  // output tensors
+  std::vector<at::Tensor> results;
+  if (use_matmul_out) {
+    for (int i = 0; i < I; ++i) {
+      results.push_back(torch::empty({M, N}, device));
     }
-    int64_t M = std::pow(2, std::stoi(argv[1]));
-    int64_t N = std::pow(2, std::stoi(argv[2]));
-    int64_t K = std::pow(2, std::stoi(argv[3]));
-    bool use_stream = std::stoi(argv[4]) != 0;
-    bool use_matmul_out = std::stoi(argv[5]) != 0;
+  }
 
-    constexpr int I = 8; // number of iterations
+  // Streams init
+  std::vector<CUDAStream> streams;
+  if (use_stream) {
+    // Create I CUDA streams
+    for (int i = 0; i < I; ++i) {
+      streams.push_back(getStreamFromPool(/*isHighPriority=*/false));
+    }
+  }
 
-    std::cout << "M=" << M << std::endl;
-    std::cout << "N=" << N << std::endl;
-    std::cout << "K=" << K << std::endl;
-    std::cout << "use_stream=" << use_stream << std::endl;
-    std::cout << "use_matmul_out=" << use_matmul_out << std::endl;
-
-    // input tensors
-    torch::Device device(torch::kCUDA);
-    torch::Tensor mat1 = torch::rand({M, K}, device);
-    torch::Tensor mat2 = torch::rand({K, N}, device);
-
-    // output tensors
-    std::vector<at::Tensor> results;
+  // Matmul execution
+  for (int i = 0; i < I; ++i) {
+    if (use_stream) {
+      setCurrentCUDAStream(streams[i]);
+    }
     if (use_matmul_out) {
-        for (int i = 0; i < I; ++i) {
-            results.push_back(torch::empty({M, N}, device));
-        }
-    }
-
-    // Streams init
-    std::vector<CUDAStream> streams;
-    if (use_stream) {
-        // Create I CUDA streams
-        for (int i = 0; i < I; ++i) {
-            streams.push_back(getStreamFromPool(/*isHighPriority=*/false));
-        }
-    }
-
-    // Matmul execution
-    for (int i = 0; i < I; ++i) {
-        if (use_stream) {
-            setCurrentCUDAStream(streams[i]);
-        }
-        if (use_matmul_out) {
-            torch::matmul_out(results[i], mat1, mat2);
-        } else {
-            results.push_back(torch::matmul(mat1, mat2));
-        }
-
-    }
-
-    // Stream sync
-    if (use_stream) {
-        for (int i = 0; i < I; ++i) {
-            streams[i].synchronize();
-        }
+      torch::matmul_out(results[i], mat1, mat2);
     } else {
-        torch::cuda::synchronize();
+      results.push_back(torch::matmul(mat1, mat2));
     }
+  }
 
-    // "Validation"
+  // Stream sync
+  if (use_stream) {
     for (int i = 0; i < I; ++i) {
-        assert(results.at(0).equal(results.at(i)));
+      streams[i].synchronize();
     }
+  } else {
+    torch::cuda::synchronize();
+  }
 
-    return 0;
+  // "Validation"
+  for (int i = 0; i < I; ++i) {
+    assert(results.at(0).equal(results.at(i)));
+  }
+
+  return 0;
 }
--- run_matmul_experiment.sh
+++ run_matmul_experiment.sh
@@ -1,14 +1,19 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
+# All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+
 #!/bin/bash
 
 BINARY="${BUILD_DIRECTORY}/experiment_matmul_cudaMalloc"
 
-LOG2_M_VALUES=(2 6 10)
-LOG2_N_VALUES=(2 6 10)
-LOG2_K_VALUES=(2 6 10)
+LOG2_M_VALUES=(15)
+LOG2_N_VALUES=(10)
+LOG2_K_VALUES=(10)
 USE_STREAM_VALUES=(0 1) # Bool
 USE_MATMUL_OUT_VALUES=(0 1) # Bool
+DISABLE_MEMORY_CACHE_VALUES=(0 1) # Bool
 
-./install-nsys.sh
+# ./install-nsys.sh
 NSYS=$(sudo which nsys)
 NSYS_CMD="${NSYS} profile --stats=false -w true -t cublas,cuda,nvtx,osrt"
 
@@ -20,12 +25,15 @@ for LOG2_M in "${LOG2_M_VALUES[@]}"; do
         for LOG2_K in "${LOG2_K_VALUES[@]}"; do
             for USE_STREAM in "${USE_STREAM_VALUES[@]}"; do
                 for USE_MATMUL_OUT in "${USE_MATMUL_OUT_VALUES[@]}"; do
-                    FILE_NAME="LOG2_M=${LOG2_M}__LOG2_N=${LOG2_N}__LOG2_K=${LOG2_K}__USE_STREAM=${USE_STREAM}__USE_MATMUL_OUT=${USE_MATMUL_OUT}"
-                    CMD="${NSYS_CMD}\
-                          -o ${OUTPUT_DIRECTORY}/${FILE_NAME}\
-                          ${BINARY} ${LOG2_M} ${LOG2_N} ${LOG2_K} ${USE_STREAM} ${USE_MATMUL_OUT}"
-                    echo $CMD
-                    $CMD
+                    for DISABLE_MEMORY_CACHE in "${DISABLE_MEMORY_CACHE_VALUES[@]}"; do
+                        FILE_NAME="LOG2_M=${LOG2_M}__LOG2_N=${LOG2_N}__LOG2_K=${LOG2_K}__USE_STREAM=${USE_STREAM}__USE_MATMUL_OUT=${USE_MATMUL_OUT}__DISABLE_MEMORY_CACHE=${DISABLE_MEMORY_CACHE}"
+                        export PYTORCH_NO_CUDA_MEMORY_CACHING=${DISABLE_MEMORY_CACHE}
+                        CMD="${NSYS_CMD}\
+                              -o ${OUTPUT_DIRECTORY}/${FILE_NAME}\
+                              ${BINARY} ${LOG2_M} ${LOG2_N} ${LOG2_K} ${USE_STREAM} ${USE_MATMUL_OUT}"
+                        echo $CMD
+                        $CMD
+                    done
                 done
             done
         done
