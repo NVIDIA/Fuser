158ad6ef220d16ee3a223513b36c329a715ac11d
Ryan Spring <rspring@nvidia.com>
2024-07-27 18:21:41 -0700

    save

--- tests/cpp/test_circular_buffering.cpp
+++ tests/cpp/test_circular_buffering.cpp
@@ -26,6 +26,10 @@ TEST_P(CircularBufferingTest, SingleDim1) {
   Fusion fusion;
   FusionGuard fg(&fusion);
 
+  constexpr int64_t tensor_size = 1000;
+  constexpr int64_t split_size = 128;
+  constexpr int64_t max_buffers = ceilDiv(tensor_size, split_size);
+
   auto tv0 = makeContigTensor(1);
   fusion.addInput(tv0);
 
@@ -37,7 +41,6 @@ TEST_P(CircularBufferingTest, SingleDim1) {
   tv1->setMemoryType(MemoryType::Shared);
 
   // I0
-  constexpr int64_t split_size = 128;
   tv3->split(-1, split_size);
   // I0/128, 128
   tv3->split(-1, 32);
@@ -55,22 +58,14 @@ TEST_P(CircularBufferingTest, SingleDim1) {
 
   tv1->circularBuffer(number_of_stages);
 
-  constexpr int64_t tensor_size = 1000;
   auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
   auto t0 = at::randn({tensor_size}, options);
 
   FusionExecutor fe;
   fe.compileFusion(&fusion, {t0});
 
-  constexpr int64_t max_buffers = ceilDiv(tensor_size, split_size);
   if (number_of_stages > max_buffers) {
-    try {
-      fe.runFusion({t0});
-    } catch (const std::exception& e) {
-      SUCCEED();
-      return;
-    }
-    FAIL() << "Failed to detect circular buffer underfill exception";
+    ASSERT_ANY_THROW(fe.runFusion({t0}));
   } else {
     auto cg_outputs = fe.runFusion({t0});
     auto ref = t0 + 1;
@@ -82,6 +77,10 @@ TEST_P(CircularBufferingTest, SingleDim2) {
   Fusion fusion;
   FusionGuard fg(&fusion);
 
+  constexpr int64_t tensor_size = 1000;
+  constexpr int64_t split_size = 128;
+  constexpr int64_t max_buffers = ceilDiv(tensor_size, split_size);
+
   auto tv0 = makeContigTensor(1);
   fusion.addInput(tv0);
 
@@ -91,7 +90,7 @@ TEST_P(CircularBufferingTest, SingleDim2) {
   fusion.addOutput(tv3);
 
   // I0
-  tv3->split(-1, 128);
+  tv3->split(-1, split_size);
   // I0/128, 128
   tv3->split(-1, 32);
   // I0/128, 4, 32
@@ -109,15 +108,18 @@ TEST_P(CircularBufferingTest, SingleDim2) {
   tv1->circularBuffer(number_of_stages);
 
   auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
-  auto t0 = at::randn({1000}, options);
+  auto t0 = at::randn({tensor_size}, options);
 
   FusionExecutor fe;
   fe.compileFusion(&fusion, {t0});
-  auto cg_outputs = fe.runFusion({t0});
 
-  auto ref = t0 + 1;
-
-  testValidate(&fusion, cg_outputs, {t0}, {ref}, __LINE__, __FILE__);
+  if (number_of_stages > max_buffers) {
+    ASSERT_ANY_THROW(fe.runFusion({t0}));
+  } else {
+    auto cg_outputs = fe.runFusion({t0});
+    auto ref = t0 + 1;
+    testValidate(&fusion, cg_outputs, {t0}, {ref}, __LINE__, __FILE__);
+  }
 }
 
 TEST_P(CircularBufferingTest, SingleDim3) {
