b0e9351a950fd6c09b169b289a176da0f1a950e1
snordmann <snordmann@nvidia.com>
2024-07-30 04:36:27 -0700

    Revert "stop using std::variant for caching streams"
    This reverts commit 3f84fa6fafa8d074931dbe99f6e819772bff462b.

--- csrc/host_ir/executor.h
+++ csrc/host_ir/executor.h
@@ -87,8 +87,8 @@ class HostIrExecutor final : public OptInDispatch {
   // Cache Fusions, FusionExecutors
   std::unordered_map<HostUnit*, FusionExecutor> fe_;
   std::unordered_map<HostUnit*, FusionExecutorCache> fec_;
-  std::unordered_map<Stream*, c10::cuda::CUDAStream> streams_by_pointer_;
-  std::unordered_map<int64_t, c10::cuda::CUDAStream> streams_by_index_;
+  using StreamKey = std::variant<int64_t, Stream*>;
+  std::unordered_map<StreamKey, c10::cuda::CUDAStream> streams_;
   std::unordered_map<Communication*, c10::intrusive_ptr<c10d::Work>> works_;
 };
 
