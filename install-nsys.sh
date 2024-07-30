02cd7448fec7c62e3a944114ea2f28f564574f5a
snordmann <snordmann@nvidia.com>
2024-07-30 04:34:10 -0700

    fix nsys version

--- install-nsys.sh
+++ install-nsys.sh
@@ -6,7 +6,7 @@ run_with_sudo() {
   echo "deb http://developer.download.nvidia.com/devtools/repos/ubuntu$(source /etc/lsb-release; echo "$DISTRIB_RELEASE" | tr -d .)/$(dpkg --print-architecture) /" | tee /etc/apt/sources.list.d/nvidia-devtools.list
   apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
   apt update
-  apt install -y  nsight-systems-cli
+  apt install -y  nsight-systems-cli-2024.4.1
 }
 
 sudo bash -c "$(declare -f run_with_sudo); run_with_sudo"
