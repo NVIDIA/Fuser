3c8a3c4798d6c168763e6261f81263bf22c218c9
Liqiang Lu <116412316+liqiangxl@users.noreply.github.com>
2024-08-02 14:59:52 -0400

    clear_dynamo_cache (#2736)
    following #2722, adding `clear_dynamo_cache()` to other torch.compile
    benchmarks.

--- benchmarks/python/test_scale_bias_relu_bwd.py
+++ benchmarks/python/test_scale_bias_relu_bwd.py
@@ -4,7 +4,7 @@
 import pytest
 from nvfuser import FusionDefinition, DataType
 from nvfuser.pytorch_utils import torch_dtype_to_nvfuser_dtype
-from .core import run_benchmark, clear_cuda_cache, unary_bwd_torch
+from .core import run_benchmark, clear_cuda_cache, clear_dynamo_cache, unary_bwd_torch
 import torch
 from .global_params import generate_input_sizes, FLOAT_DTYPES, PROMOTE_DTYPES
 import numpy as np
@@ -91,7 +91,8 @@ def test_sbr_bwd_baseline_benchmark(
     compile: bool,
 ):
     clear_cuda_cache()
-
+    if compile:
+        clear_dynamo_cache()
     inputs = torch.randn(*size, device="cuda", dtype=dtype, requires_grad=True)
     grads = torch.randn(*size, device="cuda", dtype=dtype)
     scale = torch.ones(size[-1], device="cuda", dtype=dtype)
