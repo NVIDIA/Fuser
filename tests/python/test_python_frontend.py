37289f4ed0269eb2cbb34e536144a65bcfccb6fb
Jacob Hinkle <1454944+jacobhinkle@users.noreply.github.com>
2024-08-06 08:26:45 -0400

    Improve error message with mismatched valtype/dtype (#2739)
    We already check types when binding arguments, but now we explicitly
    state which arguments mismatch, and in which positions.
    
    Fixes #2670 by improving the error message. Before this change the error
    message was
    ```
    Expected T0_g[ iS0{i1} ] to be bound to an at::Tensor, but got l
    ```
    After this PR, the error is
    ```
    Expected input 0, T0_g[ iS0{i0} ], to be an at::Tensor but got scalar 2
    ```

--- tests/python/test_python_frontend.py
+++ tests/python/test_python_frontend.py
@@ -4512,6 +4512,65 @@ class TestNvFuserFrontend(TestCase):
         ]
         nvf_out, _ = self.exec_nvfuser(fusion_func, inputs)
 
+    # Test that we properly raise an error when passing inputs with the wrong types
+    def test_mismatched_input_types(self):
+        scalar_inp = 2.0
+        tensor_inp = torch.rand((15,), dtype=torch.float32, device="cuda:0")
+
+        def fusion_func(fd: FusionDefinition):
+            T0 = fd.define_tensor(
+                shape=[-1],
+                contiguity=[True],
+                dtype=DataType.Float,
+                is_cpu=False,
+                stride_order=[0],
+            )
+            s0 = fd.define_scalar()
+            T1 = fd.ops.mul(T0, s0)
+            fd.add_output(T1)
+
+        with FusionDefinition() as fd:
+            fusion_func(fd)
+
+        try:
+            import pytest
+        except ImportError:
+            self.skipTest("Could not import pytest")
+
+        with pytest.raises(
+            Exception,
+            match=re.escape(
+                "Expected input 0, T0_g[ iS0{i0} ], to be an at::Tensor but got scalar 2"
+            ),
+        ):
+            nvf_out = fd.execute([scalar_inp, scalar_inp])
+
+        with pytest.raises(
+            Exception,
+            match=re.escape(
+                "Expected input 1, d2, to be a scalar but got float tensor of rank 1"
+            ),
+        ):
+            nvf_out = fd.execute([tensor_inp, tensor_inp])
+
+        with pytest.raises(
+            Exception,
+            match=re.escape(
+                "Expected input 0, T0_g[ iS0{i0} ], to be bound to a tensor of dtype float,"
+                " but got a tensor of dtype __half"
+            ),
+        ):
+            wrong_tensor_inp = torch.rand((15,), dtype=torch.float16, device="cuda:0")
+            nvf_out = fd.execute([wrong_tensor_inp, 2.0])
+
+        with pytest.raises(
+            Exception,
+            match=re.escape(
+                "Scalar value (2,1) is not compatible with the expected data type: double."
+            ),
+        ):
+            nvf_out = fd.execute([tensor_inp, 2.0 + 1.0j])
+
 
 if __name__ == "__main__":
     run_tests()
