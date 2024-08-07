12102b314bf4ab2068b012a9f125f2c4f05b7041
Liqiang Lu <liqiangxl@gmail.com>
2024-08-07 12:52:26 +0000

    wip

--- torch_compile_debug/nhwc_redu/torchinductor/model__0_inference_0.0/fx_graph_transformed.py
+++
@@ -1,6 +0,0 @@
-class <lambda>(torch.nn.Module):
-    def forward(self, arg0_1: "f16[1024, 256, 16, 16]"):
-         # File: /opt/pytorch/nvfuser/nhwc.py:5 in torch_group_norm, code: return x.sum(1)
-        sum_1: "f16[1024, 16, 16]" = torch.ops.aten.sum.dim_IntList(arg0_1, [1]);  arg0_1 = None
-        return (sum_1,)
-        
--- torch_compile_debug/run_2024_08_02_18_10_06_865011-pid_63084/torchinductor/model__0_inference_0.0/fx_graph_readable.py
+++
@@ -1,6 +0,0 @@
-class <lambda>(torch.nn.Module):
-    def forward(self, arg0_1: "f16[1024, 16, 16, 256]"):
-         # File: /opt/pytorch/nvfuser/nhwc.py:5 in torch_group_norm, code: return x.sum(r_axis)
-        sum_1: "f16[1024, 16, 16]" = torch.ops.aten.sum.dim_IntList(arg0_1, [-1]);  arg0_1 = None
-        return (sum_1,)
-        
