d587d4203ddc3361e3527d657c51096dc3ee17d6
Ryan Spring <rspring@nvidia.com>
2024-08-22 19:23:47 -0700

    move

--- csrc/python_frontend/translation.cpp
+++ csrc/python_frontend/translation.cpp
@@ -14,6 +14,167 @@ namespace nvfuser::python_frontend {
 
 namespace {
 
+template <typename ResultType, typename... ArgTypes>
+std::function<ResultType(ArgTypes...)> getFunction(const BinaryOp* bop) {
+  auto get_std_function = [](ResultType (*fn)(ArgTypes...)) {
+    return static_cast<ResultType (*)(ArgTypes...)>(fn);
+  };
+
+  switch (bop->getBinaryOpType()) {
+    case BinaryOpType::Add:
+      return get_std_function(add);
+      break;
+    case BinaryOpType::Atan2:
+      return get_std_function(atan2);
+      break;
+    case BinaryOpType::Div:
+      return get_std_function(div);
+      break;
+    case BinaryOpType::Fmod:
+      return get_std_function(fmod);
+      break;
+    case BinaryOpType::Mul:
+      return get_std_function(mul);
+      break;
+    case BinaryOpType::Nextafter:
+      return get_std_function(nextafter);
+      break;
+    case BinaryOpType::Pow:
+      return get_std_function(pow);
+      break;
+    case BinaryOpType::Remainder:
+      return get_std_function(remainder);
+      break;
+    case BinaryOpType::Sub:
+      return get_std_function(sub);
+      break;
+    case BinaryOpType::Mod:
+      return get_std_function(mod);
+      break;
+    case BinaryOpType::Eq:
+      return get_std_function(eq);
+      break;
+    case BinaryOpType::NE:
+      return get_std_function(ne);
+      break;
+    case BinaryOpType::GT:
+      return get_std_function(gt);
+      break;
+    case BinaryOpType::GE:
+      return get_std_function(ge);
+      break;
+    case BinaryOpType::LT:
+      return get_std_function(lt);
+      break;
+    case BinaryOpType::LE:
+      return get_std_function(le);
+      break;
+    case BinaryOpType::BitwiseAnd:
+      return get_std_function(bitwise_and);
+      break;
+    case BinaryOpType::BitwiseOr:
+      return get_std_function(bitwise_or);
+      break;
+    case BinaryOpType::BitwiseXor:
+      return get_std_function(bitwise_xor);
+      break;
+    case BinaryOpType::Lshift:
+      return get_std_function(bitwise_left_shift);
+      break;
+    case BinaryOpType::Rshift:
+      return get_std_function(bitwise_right_shift);
+      break;
+    case BinaryOpType::Gcd:
+      return get_std_function(gcd);
+      break;
+    default:
+      NVF_CHECK(
+          false,
+          "Unexpected operator type: ",
+          bop->getBinaryOpType(),
+          " in ",
+          bop->toString());
+  }
+}
+
+std::string getString(const BinaryOp* bop) {
+  switch (bop->getBinaryOpType()) {
+    case BinaryOpType::Add:
+      return "add";
+      break;
+    case BinaryOpType::Atan2:
+      return "atan2";
+      break;
+    case BinaryOpType::Div:
+      return "div";
+      break;
+    case BinaryOpType::Fmod:
+      return "fmod";
+      break;
+    case BinaryOpType::Mul:
+      return "mul";
+      break;
+    case BinaryOpType::Nextafter:
+      return "nextafter";
+      break;
+    case BinaryOpType::Pow:
+      return "pow";
+      break;
+    case BinaryOpType::Remainder:
+      return "remainder";
+      break;
+    case BinaryOpType::Sub:
+      return "sub";
+      break;
+    case BinaryOpType::Mod:
+      return "mod";
+      break;
+    case BinaryOpType::Eq:
+      return "eq";
+      break;
+    case BinaryOpType::NE:
+      return "ne";
+      break;
+    case BinaryOpType::GT:
+      return "gt";
+      break;
+    case BinaryOpType::GE:
+      return "ge";
+      break;
+    case BinaryOpType::LT:
+      return "lt";
+      break;
+    case BinaryOpType::LE:
+      return "le";
+      break;
+    case BinaryOpType::BitwiseAnd:
+      return "bitwise_and";
+      break;
+    case BinaryOpType::BitwiseOr:
+      return "bitwise_or";
+      break;
+    case BinaryOpType::BitwiseXor:
+      return "bitwise_xor";
+      break;
+    case BinaryOpType::Lshift:
+      return "bitwise_left_shift";
+      break;
+    case BinaryOpType::Rshift:
+      return "bitwise_right_shift";
+      break;
+    case BinaryOpType::Gcd:
+      return "gcd";
+      break;
+    default:
+      NVF_CHECK(
+          false,
+          "Unexpected operator type: ",
+          bop->getBinaryOpType(),
+          " in ",
+          bop->toString());
+  }
+}
+
 class FusionTranslator : public OptInConstDispatch {
  public:
   static std::unique_ptr<FusionDefinition> clone(const Fusion* fusion) {
@@ -78,6 +239,31 @@ class FusionTranslator : public OptInConstDispatch {
     fd_->finalizeDefinition();
   }
 
+  void handle(const TensorView* tv) final {
+    Tensor output = fd_->defineTensor(tv->nDims());
+
+    std::vector<int64_t> shape;
+    std::transform(
+        tv->domain()->logical().begin(),
+        tv->domain()->logical().end(),
+        std::back_inserter(shape),
+        [](IterDomain* id) {
+          return (id->extent()->isConstScalar())
+              ? id->extent()->evaluate().as<int64_t>()
+              : -1;
+        });
+
+    fd_->defineRecord(new TensorRecord(
+        {fd_->recordingState(output())},
+        shape,
+        tv->domain()->contiguity(),
+        std::get<PrimDataType>(tv->dtype().type),
+        tv->isCpuScalar(),
+        tv->domain()->strideOrder()));
+
+    map_val_to_fd_index_.emplace(tv, output());
+  }
+
   void handleOutput(const TensorView* tv) {
     // Add fusion outputs to FusionDefinition
     size_t output_index = map_val_to_fd_index_.at(tv);
@@ -108,167 +294,6 @@ class FusionTranslator : public OptInConstDispatch {
         "ops." + op_name,
         record_type,
         fn));
-  }
-
-  template <typename ResultType, typename... ArgTypes>
-  std::function<ResultType(ArgTypes...)> getFunction(const BinaryOp* bop) {
-    auto get_std_function = [](ResultType (*fn)(ArgTypes...)) {
-      return static_cast<ResultType (*)(ArgTypes...)>(fn);
-    };
-
-    switch (bop->getBinaryOpType()) {
-      case BinaryOpType::Add:
-        return get_std_function(add);
-        break;
-      case BinaryOpType::Atan2:
-        return get_std_function(atan2);
-        break;
-      case BinaryOpType::Div:
-        return get_std_function(div);
-        break;
-      case BinaryOpType::Fmod:
-        return get_std_function(fmod);
-        break;
-      case BinaryOpType::Mul:
-        return get_std_function(mul);
-        break;
-      case BinaryOpType::Nextafter:
-        return get_std_function(nextafter);
-        break;
-      case BinaryOpType::Pow:
-        return get_std_function(pow);
-        break;
-      case BinaryOpType::Remainder:
-        return get_std_function(remainder);
-        break;
-      case BinaryOpType::Sub:
-        return get_std_function(sub);
-        break;
-      case BinaryOpType::Mod:
-        return get_std_function(mod);
-        break;
-      case BinaryOpType::Eq:
-        return get_std_function(eq);
-        break;
-      case BinaryOpType::NE:
-        return get_std_function(ne);
-        break;
-      case BinaryOpType::GT:
-        return get_std_function(gt);
-        break;
-      case BinaryOpType::GE:
-        return get_std_function(ge);
-        break;
-      case BinaryOpType::LT:
-        return get_std_function(lt);
-        break;
-      case BinaryOpType::LE:
-        return get_std_function(le);
-        break;
-      case BinaryOpType::BitwiseAnd:
-        return get_std_function(bitwise_and);
-        break;
-      case BinaryOpType::BitwiseOr:
-        return get_std_function(bitwise_or);
-        break;
-      case BinaryOpType::BitwiseXor:
-        return get_std_function(bitwise_xor);
-        break;
-      case BinaryOpType::Lshift:
-        return get_std_function(bitwise_left_shift);
-        break;
-      case BinaryOpType::Rshift:
-        return get_std_function(bitwise_right_shift);
-        break;
-      case BinaryOpType::Gcd:
-        return get_std_function(gcd);
-        break;
-      default:
-        NVF_CHECK(
-            false,
-            "Unexpected operator type: ",
-            bop->getBinaryOpType(),
-            " in ",
-            bop->toString());
-    }
-  }
-
-  std::string getString(const BinaryOp* bop) {
-    switch (bop->getBinaryOpType()) {
-      case BinaryOpType::Add:
-        return "add";
-        break;
-      case BinaryOpType::Atan2:
-        return "atan2";
-        break;
-      case BinaryOpType::Div:
-        return "div";
-        break;
-      case BinaryOpType::Fmod:
-        return "fmod";
-        break;
-      case BinaryOpType::Mul:
-        return "mul";
-        break;
-      case BinaryOpType::Nextafter:
-        return "nextafter";
-        break;
-      case BinaryOpType::Pow:
-        return "pow";
-        break;
-      case BinaryOpType::Remainder:
-        return "remainder";
-        break;
-      case BinaryOpType::Sub:
-        return "sub";
-        break;
-      case BinaryOpType::Mod:
-        return "mod";
-        break;
-      case BinaryOpType::Eq:
-        return "eq";
-        break;
-      case BinaryOpType::NE:
-        return "ne";
-        break;
-      case BinaryOpType::GT:
-        return "gt";
-        break;
-      case BinaryOpType::GE:
-        return "ge";
-        break;
-      case BinaryOpType::LT:
-        return "lt";
-        break;
-      case BinaryOpType::LE:
-        return "le";
-        break;
-      case BinaryOpType::BitwiseAnd:
-        return "bitwise_and";
-        break;
-      case BinaryOpType::BitwiseOr:
-        return "bitwise_or";
-        break;
-      case BinaryOpType::BitwiseXor:
-        return "bitwise_xor";
-        break;
-      case BinaryOpType::Lshift:
-        return "bitwise_left_shift";
-        break;
-      case BinaryOpType::Rshift:
-        return "bitwise_right_shift";
-        break;
-      case BinaryOpType::Gcd:
-        return "gcd";
-        break;
-      default:
-        NVF_CHECK(
-            false,
-            "Unexpected operator type: ",
-            bop->getBinaryOpType(),
-            " in ",
-            bop->toString());
-    }
   }
 
   void handle(const BinaryOp* bop) final {
@@ -318,31 +343,6 @@ class FusionTranslator : public OptInConstDispatch {
           bop->lhs(),
           bop->rhs());
     }
-  }
-
-  void handle(const TensorView* tv) final {
-    Tensor output = fd_->defineTensor(tv->nDims());
-
-    std::vector<int64_t> shape;
-    std::transform(
-        tv->domain()->logical().begin(),
-        tv->domain()->logical().end(),
-        std::back_inserter(shape),
-        [](IterDomain* id) {
-          return (id->extent()->isConstScalar())
-              ? id->extent()->evaluate().as<int64_t>()
-              : -1;
-        });
-
-    fd_->defineRecord(new TensorRecord(
-        {fd_->recordingState(output())},
-        shape,
-        tv->domain()->contiguity(),
-        std::get<PrimDataType>(tv->dtype().type),
-        tv->isCpuScalar(),
-        tv->domain()->strideOrder()));
-
-    map_val_to_fd_index_.emplace(tv, output());
   }
 
  private:
