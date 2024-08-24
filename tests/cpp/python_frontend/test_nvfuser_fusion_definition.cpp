fbf3f73e839b16478a70dbf55309304a53ebd922
Ryan Spring <rspring@nvidia.com>
2024-08-24 15:56:03 -0700

    clean-up

--- tests/cpp/python_frontend/test_nvfuser_fusion_definition.cpp
+++ tests/cpp/python_frontend/test_nvfuser_fusion_definition.cpp
@@ -174,22 +174,4 @@ TEST_F(NVFuserTest, FusionDefinition_CUDA) {
   }
 }
 
-TEST_F(NVFuserTest, CreateFusionDefinition) {
-  Fusion fusion;
-  FusionGuard fg(&fusion);
-
-  TensorView* tv0 = makeContigConcreteTensor({128});
-  TensorView* tv1 = makeContigConcreteTensor({128});
-  fusion.addInput(tv0);
-  fusion.addInput(tv1);
-  TensorView* tv2 = add(tv0, tv1);
-  fusion.addOutput(tv2);
-
-  auto fd = std::make_unique<FusionDefinition>(/*id=*/std::nullopt);
-  python_frontend::translate(&fusion, fd.get());
-  std::stringstream ss;
-  fd->print(ss);
-  std::cout << ss.str() << std::endl;
-}
-
 } // namespace nvfuser
