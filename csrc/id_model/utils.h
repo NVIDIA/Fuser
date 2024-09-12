e5cda519482e8a53eb08a675f3dc7ef5bab709b1
Naoya Maruyama <nmaruyama@nvidia.com>
2024-09-12 12:23:32 -0700

    cleanup

--- csrc/id_model/utils.h
+++ csrc/id_model/utils.h
@@ -66,23 +66,4 @@ inline bool isIdModelOptionEnabled(IdModelEnableOption option) {
   return opts.find(option) != opts.end();
 }
 
-// Get the promotion domain of a given loop domain.
-inline IterDomain* getLoopPromotion(
-    IterDomain* loop_id,
-    const IdModel& id_model) {
-  const auto& loop_graph = id_model.idGraph(IdMappingMode::LOOP);
-  const auto& loop_promotion_map = id_model.loopPromotionMap();
-  const auto& loop_group = loop_graph.toGroup(loop_id);
-
-  auto loop_promotion_map_it = loop_promotion_map.find(loop_group);
-  NVF_ERROR(
-      loop_promotion_map_it != loop_promotion_map.end(),
-      "No loop promotion found: ",
-      loop_id->toString(),
-      ". Loop group: ",
-      nvfuser::toString(loop_group));
-
-  return loop_promotion_map_it->second;
-}
-
 } // namespace nvfuser
