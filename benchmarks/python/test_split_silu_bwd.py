d4f5e66b7e7918264da527c2a140daed4849a010
root <26priya11@gmail.com>
2024-09-16 18:05:31 -0700

    split + silu bwd benchmark

--- benchmarks/python/test_split_silu_bwd.py
+++ benchmarks/python/test_split_silu_bwd.py
@@ -11,57 +11,38 @@ import numpy as np
 
 
 def split_silu_bwd_fusion(fd: FusionDefinition, dtype: DataType, size: tuple):
-    T0 = fd.define_tensor(
-        shape=[-1, -1],
-        contiguity=[True, True],
-        dtype=dtype,
-        is_cpu=False,
-        stride_order=[1, 0],
-    )
-    T1 = fd.define_tensor(
-        shape=[-1, -1],
-        contiguity=[True, True],
-        dtype=dtype,
-        is_cpu=False,
-        stride_order=[1, 0],
-    )
+    T0 = fd.define_tensor(shape=[-1, -1], contiguity=[True, True], dtype=dtype, is_cpu=False, stride_order=[1, 0])
+    T1 = fd.define_tensor(shape=[-1, -1], contiguity=[True, True], dtype=dtype, is_cpu=False, stride_order=[1, 0])
+    
     S0, S1 = size
-    T2 = fd.ops.slice(
-        T0, start_indices=[0, 0], end_indices=[S0, S1 // 2], strides=[1, 1]
-    )
-    T3 = fd.ops.slice(
-        T0, start_indices=[0, S1 // 2], end_indices=[S0, S1], strides=[1, 1]
-    )
-
+    T2 = fd.ops.slice(T0, start_indices=[0, 0], end_indices=[S0, S1 // 2], strides=[1, 1])
+    T4 = fd.ops.slice(T0, start_indices=[0, S1 // 2], end_indices=[S0, S1], strides=[1, 1])
+    
     if dtype in PROMOTE_DTYPES:
         T1 = fd.ops.cast(T1, dtype=DataType.Float)
         T2 = fd.ops.cast(T2, dtype=DataType.Float)
-        T3 = fd.ops.cast(T3, dtype=DataType.Float)
-
-    T4 = fd.ops.neg(T2)
-    T5 = fd.ops.exp(T4)
-    S6 = fd.define_scalar(1.00000, dtype=DataType.Double)
-    T7 = fd.ops.add(S6, T5)
-    T8 = fd.ops.reciprocal(T7)
-    T9 = fd.ops.mul(T2, T8)
-    T10 = fd.ops.mul(T3, T1)
-    T11 = fd.ops.mul(T9, T1)
-    T12 = fd.ops.mul(T8, T10)
-    T13 = fd.ops.mul(T2, T10)
-    T14 = fd.ops.neg(T13)
-    T15 = fd.ops.mul(T14, T8)
-    T16 = fd.ops.mul(T15, T8)
-    T17 = fd.ops.mul(T16, T5)
-    T18 = fd.ops.neg(T17)
-    T19 = fd.ops.add(T12, T18)
-    S20 = fd.define_scalar(0.00000, dtype=DataType.Double)
-    T21 = fd.ops.pad(T11, [S1 // 2, 0, 0, 0], S20)
-    S22 = fd.define_scalar(0.00000, dtype=DataType.Double)
-    T23 = fd.ops.pad(T19, [0, S1 // 2, 0, 0], S22)
-    T24 = fd.ops.add(T21, T23)
+        T4 = fd.ops.cast(T4, dtype=DataType.Float)
+    
+    T3 = fd.ops.neg(T2)
+    T5 = fd.ops.exp(T3)
+    T6 = fd.ops.mul(T4, T1)
+    S7 = fd.define_scalar(1.00000, dtype=DataType.Double)
+    T8 = fd.ops.add(S7, T5)
+    T9 = fd.ops.mul(T2, T6)
+    T10 = fd.ops.reciprocal(T8)
+    T11 = fd.ops.neg(T9)
+    T12 = fd.ops.mul(T11, T10)
+    T13 = fd.ops.mul(T12, T10)
+    T14 = fd.ops.mul(T13, T5)
+    T15 = fd.ops.mul(T2, T10)
+    T16 = fd.ops.neg(T14)
+    T17 = fd.ops.mul(T10, T6)
+    T18 = fd.ops.mul(T15, T1)
+    T19 = fd.ops.add(T17, T16)
+    T20 = fd.ops.cat([T19, T18], dim=-1)
     if dtype in PROMOTE_DTYPES:
-        T24 = fd.ops.cast(T24, dtype=dtype)
-    fd.add_output(T24)
+        T20 = fd.ops.cast(T20, dtype=dtype)
+    fd.add_output(T20)
 
 
 def split_silu_bwd_iobytes(size: tuple, dtype: torch.dtype):
@@ -84,7 +65,7 @@ def test_split_silu_bwd_nvf_benchmark(
     with FusionDefinition() as fd:
         split_silu_bwd_fusion(fd, torch_dtype_to_nvfuser_dtype(dtype), size)
     if not disable_validation:
-        x, y = torch.chunk(input, 2, -1)
+        x, y = torch.split(input, size[1] // 2, -1)
         eager_output = torch.nn.functional.silu(x) * y
         eager_output.backward(grads)
         fd.validate([input, grads], [input.grad])
@@ -107,7 +88,7 @@ def test_split_silu_bwd_baseline_benchmark(
         clear_dynamo_cache()
     input = torch.randn(*size, device="cuda", dtype=dtype, requires_grad=True)
     grads = torch.randn(size[0], size[1] // 2, device="cuda", dtype=dtype)
-    x, y = torch.chunk(input, 2, -1)
+    x, y = torch.split(input, size[1] // 2, -1)
     eager_output = torch.nn.functional.silu(x) * y
 
     run_benchmark(
