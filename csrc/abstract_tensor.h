0813c3c797e2b003bdba339bd204982d69b0077c
Jacob Hinkle <jhinkle@nvidia.com>
2024-09-18 10:29:10 -0400

    Revert changes to abstract_tensor.h

--- csrc/abstract_tensor.h
+++ csrc/abstract_tensor.h
@@ -9,7 +9,6 @@
 
 #include <ir/builder.h>
 #include <ir/internal_base_nodes.h>
-#include <ir/utils.h>
 #include <type.h>
 #include <val_graph.h>
 
@@ -178,8 +177,6 @@ struct AbstractTensor {
       : domain(domain.begin(), domain.end()) {}
   AbstractTensor(std::initializer_list<AbstractId> domain) : domain(domain) {}
 
-  virtual ~AbstractTensor() = default;
-
   template <typename T>
   std::vector<T> as() const {
     std::vector<T> result;
@@ -283,43 +280,33 @@ struct AbstractTensor {
 
   AbstractTensor& parallelize(int64_t axis, ParallelType parallel_type);
 
-  virtual AbstractTensor& split(
-      int64_t axis,
-      Val* factor,
-      bool inner_split = true);
+  AbstractTensor& split(int64_t axis, Val* factor, bool inner_split = true);
   AbstractTensor& split(int64_t axis, int64_t factor, bool inner_split = true);
 
-  virtual AbstractTensor& merge(int64_t axis_o, int64_t axis_i);
+  AbstractTensor& merge(int64_t axis_o, int64_t axis_i);
   AbstractTensor& merge(int64_t axis) {
     return merge(axis, axis + 1);
   }
 
-  virtual AbstractTensor& reorder(
-      const std::unordered_map<int64_t, int64_t>& old2new);
+  AbstractTensor& reorder(const std::unordered_map<int64_t, int64_t>& old2new);
   AbstractTensor& reorder(
       const std::initializer_list<std::pair<const int64_t, int64_t>>& old2new) {
     return reorder(std::unordered_map<int64_t, int64_t>(old2new));
   }
   // old2new[index] = permutation[index]
-  virtual AbstractTensor& reorder(const std::vector<int64_t>& permutation);
+  AbstractTensor& reorder(const std::vector<int64_t>& permutation);
   AbstractTensor& reorder(const std::initializer_list<int64_t>& permutation) {
     return reorder(std::vector<int64_t>(permutation));
   }
 
   // Both `from` and `to` are inclusive.
-  virtual AbstractTensor& flatten(int64_t from = 0, int64_t to = -1);
+  AbstractTensor& flatten(int64_t from = 0, int64_t to = -1);
 
-  virtual AbstractTensor& swizzle(
-      SwizzleType swizzle_type,
-      int64_t x,
-      int64_t y);
+  AbstractTensor& swizzle(SwizzleType swizzle_type, int64_t x, int64_t y);
 
   // Temporary helper for legacy swizzle, should be removed eventually.
   // This is a copy-paste of AbstractTensor::swizzle(SwizzleType
-  virtual AbstractTensor& swizzle(
-      Swizzle2DType swizzle_type,
-      int64_t x,
-      int64_t y);
+  AbstractTensor& swizzle(Swizzle2DType swizzle_type, int64_t x, int64_t y);
 
   // Unzip the AbstractTensor to separate tensors. For example, if this
   // AbstractTensor is [dim0={id0, id1}, dim1={id2, id3}], then the return value
@@ -349,122 +336,6 @@ struct AbstractTensor {
 
   // Remove all the null elements.
   AbstractTensor& strip();
-};
-
-//! This is a wrapper around AbstractTensor which propagates a set of tags for
-//! each axis. The tags can be any hashable type with equality; typically Tag
-//! would be an enum. See AbstractMatmulTensor, in which case Tag =
-//! MatmulDimRole.
-template <typename Tag>
-struct TaggedAbstractTensor : AbstractTensor {
-  //! Holds a set of tags for each dimension
-  std::vector<std::unordered_set<Tag>> tags;
-
-  const std::unordered_set<Tag>& getTags(int64_t i) const {
-    i = wrapDim(i, (int64_t)domain.size());
-    return tags[i];
-  }
-
-  bool hasTag(int64_t i, Tag tag) const {
-    return (bool)getTags(i).count(tag);
-  }
-
-  //! Return tag if there is a single tag, otherwise nullopt
-  //!
-  //! This is just a convenience function for the common case where axes with
-  //! different tags have not been merged. In these cases there is a single Tag
-  //! for each axis and it is cumbersome to extract it manually each time it's
-  //! needed.
-  std::optional<Tag> getTag(int64_t i) const {
-    const auto& tags = getTags(i);
-    if (tags.size() == 1) {
-      return *tags.begin();
-    }
-    return std::nullopt;
-  }
-
-  template <typename T>
-  bool operator==(T&& t) const {
-    if constexpr (std::is_same_v<TaggedAbstractTensor<Tag>, std::decay_t<T>>) {
-      return domain == t.domain && tags == t.tags;
-    } else {
-      // Only compare true for other TaggedAbstractTensors
-      return false;
-    }
-  }
-
-  using AbstractTensor::split;
-
-  AbstractTensor& split(int64_t axis, Val* factor, bool inner_split = true)
-      override {
-    axis = wrapDim(axis, (int64_t)domain.size());
-    // copy tags from original axis
-    tags.insert(tags.begin() + axis + 1, tags.at(axis));
-    return AbstractTensor::split(axis, factor, inner_split);
-  }
-
-  using AbstractTensor::merge;
-  AbstractTensor& merge(int64_t axis_o, int64_t axis_i) override {
-    axis_o = wrapDim(axis_o, (int64_t)domain.size());
-    axis_i = wrapDim(axis_i, (int64_t)domain.size());
-    // merge tags from these axes into outer position
-    if (axis_o >= axis_i) {
-      std::swap(axis_o, axis_i);
-    }
-    tags[axis_o].insert(tags[axis_i].begin(), tags[axis_o].end());
-    tags.erase(tags.begin() + axis_i);
-    return AbstractTensor::merge(axis_o, axis_i);
-  }
-
-  using AbstractTensor::reorder;
-
-  AbstractTensor& reorder(
-      const std::unordered_map<int64_t, int64_t>& old2new) override {
-    auto new2old = ir_utils::normalizeOld2New(old2new, (int64_t)domain.size());
-    std::vector<std::unordered_set<Tag>> reordered_tags;
-    std::transform(
-        new2old.begin(),
-        new2old.end(),
-        std::back_inserter(reordered_tags),
-        [this](int64_t i) { return tags[i]; });
-    tags = std::move(reordered_tags);
-    return AbstractTensor::reorder(old2new);
-  }
-
-  using AbstractTensor::flatten;
-
-  // Both `from` and `to` are inclusive.
-  AbstractTensor& flatten(int64_t from = 0, int64_t to = -1) override {
-    from = wrapDim(from, (int64_t)domain.size());
-    to = wrapDim(to, (int64_t)domain.size());
-    for (size_t i : c10::irange(from, to)) {
-      tags[from].insert(tags[i + 1].begin(), tags[i + 1].end());
-    }
-    tags.erase(tags.begin() + from + 1, tags.begin() + to + 1);
-    return AbstractTensor::flatten(from, to);
-  }
-
-  using AbstractTensor::swizzle;
-
-  // swizzle mixes axes, so the tag sets for both x and y become the union of
-  // the input tag sets
-  AbstractTensor& swizzle(SwizzleType swizzle_type, int64_t x, int64_t y)
-      override {
-    x = wrapDim(x, (int64_t)domain.size());
-    y = wrapDim(y, (int64_t)domain.size());
-    tags[x].insert(tags[y].begin(), tags[y].end());
-    return AbstractTensor::swizzle(swizzle_type, x, y);
-  }
-
-  // Temporary helper for legacy swizzle, should be removed eventually.
-  // This is a copy-paste of AbstractTensor::swizzle(SwizzleType
-  AbstractTensor& swizzle(Swizzle2DType swizzle_type, int64_t x, int64_t y)
-      override {
-    x = wrapDim(x, (int64_t)domain.size());
-    y = wrapDim(y, (int64_t)domain.size());
-    tags[x].insert(tags[y].begin(), tags[y].end());
-    return AbstractTensor::swizzle(swizzle_type, x, y);
-  }
 };
 
 } // namespace nvfuser
