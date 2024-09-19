44acd6d408084e022b882c0dbef0ec94bba3be39
mcowan <mcowan@nvidia.com>
2024-09-18 17:49:42 -0700

    with torch compile

--- benchmarks/python/test_pytorch_transformer.py
+++ benchmarks/python/test_pytorch_transformer.py
@@ -41,6 +41,10 @@ def benchmark_loop(model, input):
     forward_time = 0.0
     backward_time = 0.0
 
+    # warm-up
+    output = model(input)
+    output.sum().backward()
+
     for i in range(num_iters):
         start = time.time()
         output = model(input)
@@ -59,23 +63,30 @@ def benchmark_loop(model, input):
     return forward_time, backward_time
 
 
-def benchmark_model():
+def benchmark_model(use_torch_compile=False):
     if rank != 0:
         return
     config = GPTConfig()
     model = Block(config).to(dtype).to("cuda")
+    if use_torch_compile:
+        model = torch.compile(model)
 
     input = torch.rand(
         batch_size, sequence_length, config.n_embd, dtype=dtype, device="cuda"
     )
     forward_time, backward_time = benchmark_loop(model, input)
-    print(f"Average forward time {forward_time}s, backward time {backward_time}s")
+    print(f"torch.compile {not use_torch_compile}, Average forward time {forward_time}s, backward time {backward_time}s")
 
 
-def benchmark_tensor_parallel():
+def benchmark_tensor_parallel(use_torch_compile=False):
     config = GPTConfig()
-    config.n_devices = world_size
+    # TODO: used the world size to scale the sizes in view operations.
+    # this was necessary in eager mode, but not for torch.compile
+    if not use_torch_compile:
+        config.n_devices = world_size
     tp_model = Block(config).to(dtype).to("cuda")
+    if use_torch_compile:
+        tp_model = torch.compile(tp_model)
 
     # Parallelization plan. Tensor parallel
     tp_model = parallelize_module(
@@ -96,10 +107,12 @@ def benchmark_tensor_parallel():
 
     forward_time, backward_time = benchmark_loop(tp_model, input)
     print(
-        f"{rank}: Average tensor parallel forward time {forward_time}s, backward time {backward_time}s"
+        f"{rank}: torch.compile {not use_torch_compile}, Average tensor parallel forward time {forward_time}s, backward time {backward_time}s"
     )
 
 
 benchmark_model()
+benchmark_model(True)
 benchmark_tensor_parallel()
+benchmark_tensor_parallel(True)
 dist.destroy_process_group()
