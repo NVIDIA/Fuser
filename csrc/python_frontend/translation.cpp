a0bc2f2209720d21e1b4eee3a9982468d60844ba
Ryan Spring <rspring@nvidia.com>
2024-09-19 21:37:36 -0700

    use main translate

--- csrc/python_frontend/translation.cpp
+++ csrc/python_frontend/translation.cpp
@@ -43,12 +43,89 @@ class FusionTranslator : public OptInConstDispatch {
   FusionTranslator(Fusion* fusion, FusionDefinition* fd)
       : fusion_(fusion), fd_(fd) {}
 
+  // The new shape for view operation can be dynamic. Check that all dynamic
+  // scalar dependencies are handled before the ViewOp.
+  bool checkViewShapeDependency(const ViewOp* vop) {
+    const std::vector<IterDomain*>& logical_out_domain =
+        vop->out()->as<TensorView>()->domain()->logical();
+    std::vector<Val*> logical_domain_extents;
+    std::transform(
+        logical_out_domain.begin(),
+        logical_out_domain.end(),
+        std::back_inserter(logical_domain_extents),
+        [](IterDomain* id) { return id->getMaybeExpandedExtent(); });
+    return std::all_of(
+        logical_domain_extents.begin(),
+        logical_domain_extents.end(),
+        [&](Val* v) {
+          return v->definition() == nullptr ||
+              map_val_to_fd_index_.count(v) > 0;
+        });
+  }
+
+  // Gather the expressions necessary to create a scalar value.
+  std::vector<Expr*> gatherScalarExpressions(Val* v) {
+    NVF_ERROR(v != nullptr);
+    NVF_ERROR(v->isScalar());
+
+    // short-circuit: v does not have a definition.
+    if (v->definition() == nullptr) {
+      return {};
+    }
+
+    std::vector<Expr*> expression_chain;
+    std::unordered_set<Expr*> visited;
+    std::vector<Expr*> to_visit = {v->definition()};
+    while (!to_visit.empty()) {
+      Expr* e = to_visit.back();
+      to_visit.pop_back();
+
+      expression_chain.push_back(e);
+      visited.insert(e);
+
+      for (Val* input : e->inputs()) {
+        // short-circuit: input does not have a definition.
+        if (input->definition() == nullptr) {
+          continue;
+        }
+
+        // short-circuit: input definition is already visited.
+        if (visited.count(input->definition()) > 0) {
+          continue;
+        }
+
+        to_visit.push_back(input->definition());
+      }
+    }
+    return expression_chain;
+  }
+
+  // Gather the scalar expressions necessary to create the logical domain for a
+  // TensorView.
+  std::vector<Expr*> gatherScalarExpressions(TensorView* tv) {
+    NVF_ERROR(tv != nullptr);
+    std::vector<Expr*> logical_domain_expressions;
+    const std::vector<IterDomain*>& logical_out_domain =
+        tv->domain()->logical();
+    for (IterDomain* id : logical_out_domain) {
+      std::vector<Expr*> extent_definitions =
+          gatherScalarExpressions(id->getMaybeExpandedExtent());
+      logical_domain_expressions.insert(
+          logical_domain_expressions.end(),
+          extent_definitions.begin(),
+          extent_definitions.end());
+    }
+    return logical_domain_expressions;
+  }
+
   // Check that all of the expression's inputs are defined in FusionDefinition.
   bool checkExpressionDependencies(Expr* e) {
-    return std::all_of(
-        e->inputs().begin(), e->inputs().end(), [&](const Val* v) {
-          return map_val_to_fd_index_.count(v) > 0;
-        });
+    bool check_view_dependency =
+        !e->isA<ViewOp>() || checkViewShapeDependency(e->as<ViewOp>());
+    return check_view_dependency &&
+        std::all_of(e->inputs().begin(), e->inputs().end(), [&](const Val* v) {
+             return map_val_to_fd_index_.count(v) > 0;
+           });
   }
 
   void translate() {
@@ -60,15 +137,22 @@ class FusionTranslator : public OptInConstDispatch {
     }
 
     // Gather all expressions in CPP Fusion.
-    std::vector<nvfuser::Val*> used_vals = fusion_->usedMathVals();
-    std::unordered_set<nvfuser::Expr*> expr_set;
-    for (nvfuser::Val* v : used_vals) {
-      std::copy(
-          v->uses().begin(),
-          v->uses().end(),
-          std::inserter(expr_set, expr_set.begin()));
+    const std::vector<nvfuser::Expr*> fusion_exprs = fusion_->exprs();
+    std::deque<nvfuser::Expr*> to_visit(
+        fusion_exprs.begin(), fusion_exprs.end());
+
+    // Scalar expressions are not handled by usedMathVals, so gather them
+    // manually.
+    for (Expr* e : to_visit) {
+      if (e->isA<ViewOp>() || e->isA<ExpandOp>() || e->isA<FullOp>()) {
+        std::vector<Expr*> extent_definitions =
+            gatherScalarExpressions(e->output(0)->as<TensorView>());
+        to_visit.insert(
+            to_visit.end(),
+            extent_definitions.begin(),
+            extent_definitions.end());
+      }
     }
-    std::deque<nvfuser::Expr*> to_visit(expr_set.begin(), expr_set.end());
 
     // Topological search of Fusion expressions
     size_t skip_count = 0;
@@ -83,6 +167,13 @@ class FusionTranslator : public OptInConstDispatch {
 
       // short-circuit: skip if already visited
       if (visited.count(e) > 0) {
+        continue;
+      }
+
+      // short-circuit: skip Split and Merge expressions created by Reshape
+      // short-circuit: skip Resize expressions created by Slice
+      if (e->isA<Split>() || e->isA<Merge>() || e->isA<Resize>()) {
+        visited.insert(e);
         continue;
       }
 
@@ -111,16 +202,39 @@ class FusionTranslator : public OptInConstDispatch {
       skip_count = 0;
     }
 
-    // Outputs and Aliasing
+    // Add tensor outputs and handle aliased outputs
+    std::unordered_set<nvfuser::Val*> visited_alias_output;
     for (nvfuser::Val* v : fusion_->outputs()) {
-      // TODO Add support for aliased outputs
-      // Handle only TensorViews
       NVF_ERROR(v->isA<TensorView>());
-      handleOutput(v->as<TensorView>());
+      const AliasInfo& alias_info = fusion_->getOutputAlias(v);
+      switch (alias_info.type) {
+        case AllocationType::New: {
+          handleOutput(v->as<TensorView>());
+          break;
+        }
+        case AllocationType::ReuseBuffer: {
+          size_t num_visited = visited_alias_output.count(v);
+          if (num_visited == 0) {
+            visited_alias_output.insert(v);
+            handleOutput(v->as<TensorView>(), alias_info);
+          }
+          // An alias output can also be returned as a fusion output
+          // if it is already aliased or if not hide_output
+          if (num_visited > 0 || !alias_info.hide_output) {
+            handleOutput(v->as<TensorView>());
+          }
+          break;
+        }
+        default:
+          NVF_ERROR(false, "Unsupported AllocationType");
+      }
     }
 
     fd_->finalizeDefinition();
   }
+
+  // =================================================================================
+  //  Handle define_scalar and define_tensor variants
 
   // Add scalar value to Fusion Definition
   void handle(const Val* v) final {
@@ -168,24 +282,30 @@ class FusionTranslator : public OptInConstDispatch {
     map_val_to_fd_index_.emplace(tv, output());
   }
 
+  // =================================================================================
+  // Handle add_output variants
+
   // Add Tensor output to FusionDefinition
   void handleOutput(const TensorView* tv) {
     size_t output_index = map_val_to_fd_index_.at(tv);
     fd_->defineRecord(new OutputRecord<TensorView>(
-        {fd_->recordingState(output_index)}, serde::RecordType::OutputTv));
+        {fd_->recordingState(output_index)},
+        serde::RecordType::OutputTv,
+        tv->domain()->strideOrder()));
   }
 
-  // Add Broadcast operation to FusionDefinition
-  void handle(const BroadcastOp* bcast_op) final {
-    Tensor output =
-        fd_->defineTensor(bcast_op->out()->as<TensorView>()->nDims());
-    fd_->defineRecord(new BroadcastOpRecord(
-        {fd_->recordingState(map_val_to_fd_index_.at(bcast_op->in()))},
-        {fd_->recordingState(output())},
-        "ops.broadcast",
-        bcast_op->getBroadcastDimFlags()));
-    map_val_to_fd_index_.emplace(bcast_op->out(), output());
+  // Alias output Tensor with input tensor
+  void handleOutput(const TensorView* tv, const AliasInfo& alias_info) {
+    size_t output_index = map_val_to_fd_index_.at(tv);
+    size_t input_index = map_val_to_fd_index_.at(alias_info.aliased_io);
+    fd_->defineRecord(new OutputRecord<TensorView>(
+        {fd_->recordingState(output_index), fd_->recordingState(input_index)},
+        serde::RecordType::OutputTv));
   }
+
+  // =================================================================================
+  // Map CPP Expression classes to corresponding RecordFunctors in
+  // python_frontend
 
   // A generic function to map UnaryOp, BinaryOp, and TernaryOp to
   // python_frontend OpRecord
@@ -290,6 +410,18 @@ class FusionTranslator : public OptInConstDispatch {
           bop->lhs(),
           bop->rhs());
     }
+  }
+
+  // Add Broadcast operation to FusionDefinition
+  void handle(const BroadcastOp* bcast_op) final {
+    Tensor output =
+        fd_->defineTensor(bcast_op->out()->as<TensorView>()->nDims());
+    fd_->defineRecord(new BroadcastOpRecord(
+        {fd_->recordingState(map_val_to_fd_index_.at(bcast_op->in()))},
+        {fd_->recordingState(output())},
+        "ops.broadcast",
+        bcast_op->getBroadcastDimFlags()));
+    map_val_to_fd_index_.emplace(bcast_op->out(), output());
   }
 
  private:
