9f4d68f7fbf2691706de0781a9b8ecff6e427f87
Gao, Xiang <qasdfgtyuiop@gmail.com>
2024-09-23 23:35:18 -0700

    save

--- tests/cpp/test_mma.cpp
+++ tests/cpp/test_mma.cpp
@@ -739,7 +739,7 @@ TEST_P(HopperRS, MultipleTile) {
   {
     tv2c->split(-3, getM(macro));
     tv2c->split(-2, getN(macro));
-    tv2c->split(-1, getN(macro));
+    tv2c->split(-1, getK(macro));
     // [Mo, Mi, No, Ni, Ko, Ki] -> [Mo, No, Ko, Mi, Ni, Ki]
     tv2c->reorder({{-5, -3}, {-3, -2}});
     auto s = mma_utils::MmaSwizzler::scheduleMmaOutputAllocation(
@@ -1470,6 +1470,117 @@ TEST_P(HopperSS, FullSwizzle) {
 
   auto inputs =
       matmulAtInput3DHopperSS(m, n, k1, layout, data_type_to_aten(dtype));
+
+  FusionExecutor fe;
+  fe.compileFusion(
+      &fusion, {inputs.first, inputs.second}, LaunchParams(), matmul_cparams);
+  auto cg_outputs = fe.runFusion({inputs.first, inputs.second});
+  auto tref = atMatmul(
+      inputs.first.squeeze().to(at::kFloat),
+      inputs.second.squeeze().to(at::kFloat),
+      layout);
+  EXPECT_TRUE(at::allclose(cg_outputs[0], tref, 1e-5, 1e-5));
+}
+
+TEST_P(HopperSS, MultipleTile) {
+  Fusion fusion;
+  FusionGuard fg(&fusion);
+
+  auto shapes = matmulAtInputShape3DHopperSS(
+      2*getM(macro), 2*getN(macro), 2*getK(macro), layout);
+
+  auto tv0 = makeConcreteTensor(shapes.first, dtype);
+  auto tv1 = makeConcreteTensor(shapes.second, dtype);
+  fusion.addInput(tv0);
+  fusion.addInput(tv1);
+
+  // Just doing a gmem->smem copy
+  tv0 = set(tv0);
+  tv0->setMemoryType(MemoryType::Shared);
+  tv1 = set(tv1);
+  tv1->setMemoryType(MemoryType::Shared);
+
+  int axes = 0;
+  switch (layout) {
+    case MmaLayout::NT:
+      axes = 0;
+      break;
+    case MmaLayout::TT:
+    case MmaLayout::NN:
+      axes = 1;
+      break;
+    case MmaLayout::TN:
+      axes = 2;
+      break;
+    default:
+      NVF_ERROR("Invalid layout");
+  }
+  auto tv2 = fusedMultiplySum(tv0, tv1, {axes});
+
+  // Reorder the accumulator as [M, N, K]
+  switch (layout) {
+    case MmaLayout::TT:
+      // [M, K, N] -> [M, N, K]
+      tv2->reorder({{-2, -1}});
+      break;
+    case MmaLayout::TN:
+      // [M, N, K]
+      break;
+    case MmaLayout::NT:
+      // [K, M, N] -> [M, N, K]
+      tv2->reorder({{-3, -1}});
+      break;
+    case MmaLayout::NN:
+      // [N, K, M] -> [M, N, K]
+      tv2->reorder({{-1, -3}});
+      break;
+    default:
+      NVF_ERROR("Invalid layout");
+  }
+  tv2->commitLeafToLogical();
+
+  fusion.addOutput(tv2);
+
+  auto mma_ops = ir_utils::getOpsOfType<MmaOp>(&fusion);
+  NVF_CHECK(
+      1 == mma_ops.size(),
+      "Invalid number of MmaOp instances in fusion definition, expected 1, got ",
+      mma_ops.size());
+  mma_ops.front()->setMacro(macro);
+
+  auto tv2c = tv2->cacheBefore();
+
+  // Bring related dims to innermost, that is:
+  // - Reorder tv0 as [1, M, K] or [1, K, M]
+  // - Reorder tv1 as [1, N, K] or [1, K, N]
+  moveInnerBroadcastLeft(tv0);
+  moveInnerBroadcastLeft(tv1);
+
+  tv0->applyMmaSwizzle(swizzle_a);
+  tv1->applyMmaSwizzle(swizzle_b);
+
+  naivelyParallelize(tv0);
+  naivelyParallelize(tv1);
+
+  {
+    tv2c->split(-3, getM(macro));
+    tv2c->split(-2, getN(macro));
+    tv2c->split(-1, getK(macro));
+    auto s = mma_utils::MmaSwizzler::scheduleMmaOutputAllocation(
+        tv2c->getLoopDomain());
+    tv2c->setAllocationDomain(s.as<IterDomain*>(), true);
+    tv2c->axis(-1)->parallelize(ParallelType::Mma);
+    tv2c->axis(-2)->parallelize(ParallelType::Mma);
+    tv2c->axis(-3)->parallelize(ParallelType::Mma);
+  }
+  {
+    auto s = mma_utils::MmaSwizzler::scheduleMmaOutputAllocation(
+        tv2->getLoopDomain());
+    tv2->setLoopDomain(s.as<IterDomain*>());
+  }
+
+  auto inputs = matmulAtInput3DHopperSS(
+      2*getM(macro), 2*getN(macro), 2*getK(macro), layout, data_type_to_aten(dtype));
 
   FusionExecutor fe;
   fe.compileFusion(
