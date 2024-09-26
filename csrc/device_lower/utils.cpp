3b9a7e38eecee120297e5afed931e40043c0d6e6
Naoya Maruyama <naoyam@users.noreply.github.com>
2024-09-25 18:47:39 -0700

    Extend MaxPosCalculartor::getMaxProducerPosFromConsumer to support non-conventional loop domains (#2983)
    Part of #2902. See the linked design doc for the background context.
    
    ~Stacked on #2984~
    Stacked on #2987 
    
    The IdModel-based analysis is required for non-conventional loop domains
    as the dependency from the logical to loop domains may not be just one
    way from logical to loop. However, the IdModel-based analysis does not
    support broadcast forwarding, so we can't just completely switch to the
    new method.
    
    For now, I think it makes most sense to use the existing method whenever
    logical and loop domains are generated in the conventional scheduling
    primitives such as split and merge. The new method is only used
    otherwise.
    
    I also considered implementing the broadcast forwarding in the
    IdModel-based approach as well, but it doesn't seem to me worthwhile
    doing so at this moment. I thought maybe using the Permissive graph
    would make it trivial, but there seem to be (not a small number of)
    subtle corner cases of mappings that may not be considered in the
    Permissive graph, e.g., if the indexed domains should be mapped or not.
    
    It should be certainly possible to extend the Permissive graph to
    address these corner cases, but another factor I think we should
    consider is that the broadcast forwarding itself may not be necessary
    with the setLoopDomain-based approach. We should probably set the right
    loop domain from the beginning instead of trying to make different loop
    domains to be inlinable.

--- csrc/device_lower/utils.cpp
+++ csrc/device_lower/utils.cpp
@@ -942,24 +942,6 @@ std::array<UnitDim, 2> getMmaLayout(const MmaOp* expr) {
   return layout;
 }
 
-bool hasRootToLoopLinearTransformations(const TensorView* tv) {
-  auto root = tv->getMaybeRootDomain();
-  auto loop = tv->getLoopDomain();
-  std::vector<Val*> loop_val(loop.begin(), loop.end());
-  auto all_ids_vec =
-      DependencyCheck::getAllValsBetween({root.begin(), root.end()}, loop_val);
-  std::unordered_set<Val*> all_ids_set(all_ids_vec.begin(), all_ids_vec.end());
-  auto alloc = tv->getMaybeAllocationDomain();
-  auto logical = tv->getLogicalDomain();
-  bool all_alloc_id_on_path = std::all_of(
-      alloc.begin(), alloc.end(), [&](Val* v) { return all_ids_set.count(v); });
-  bool all_logical_id_on_path =
-      std::all_of(logical.begin(), logical.end(), [&](Val* v) {
-        return all_ids_set.count(v);
-      });
-  return all_alloc_id_on_path && all_logical_id_on_path;
-}
-
 bool isReductionInitExpr(const Expr* expr) {
   // False if its output isn't a TensorView
   if (!ir_utils::isTvOp(expr)) {
