e7ac869a698ad92b14803f83951285fa717b9d9c
Liqiang Lu <liqiangxl@gmail.com>
2024-09-27 18:13:45 +0000

    add predicate

--- csrc/device_lower/analysis/predicate_elimination.cpp
+++ csrc/device_lower/analysis/predicate_elimination.cpp
@@ -239,24 +239,6 @@ class PredicateChcker : public IterVisitor {
         predicateNonDivisibleRootDomains(expr) ||
         predicateNonDivisibleSplit(expr) || predicateExpandReduce(expr) ||
         predicateRNGOp(expr);
-    if (ir_utils::isCpAsyncOp(expr)) {
-      std::cout << "needs_predicate_ " << needs_predicate_smem_access
-                << ", expr: " << expr->toString() << std::endl;
-    }
-    // A cp.async op would need a predicate for either the global
-    //  input or its shared mem output, or both.
-    // Due to the WAR discussed in [Predicate Inversion for CpAsync],
-    //  we currently cannot support use cases where both the gmem read
-    //  and the smem write need to be predicated.
-    // Adding a check here would make the exclusion of such case as precise as
-    //  possible and avoid duplication of predicateSharedMemAccess
-    //  logic. But this part along with [Predicate Inversion for CpAsync]
-    //  should be cleaned up all together when we extend predicate/masking
-    //  logic to cover this usage.
-    // NVF_ERROR(
-    //     !(ir_utils::isCpAsyncOp(expr) && needs_predicate_smem_access),
-    //     "predicate removal: unsupported use case of cp.async");
-
     if (needs_predicate_) {
       return;
     }
