9b89d0944feda2f82a584429e86bdc26dbf0622d
Gao, Xiang <qasdfgtyuiop@gmail.com>
2024-10-03 16:21:20 -0700

    save

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # python setup.py build --debinfo
-NVFUSER_EXTERNAL_SRC=__tmp_kernel_none_f0_c0_r0_g0.cu ./bin/test_matmul --gtest_filter=*HSH_NT_128BSwizzle*
+NVFUSER_EXTERNAL_SRC=__tmp_kernel_none_f0_c0_r0_g0.cu NVFUSER_TEST_RANDOM_SEED=0 NVFUSER_TEST_ATEN_RANDOM_SEED=0 ./bin/test_matmul --gtest_filter=*HSH_NT_128BSwizzle*
