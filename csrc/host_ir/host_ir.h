9421a10cfc18a92189fd7fa20f325929d514f531
snordmann <snordmann@nvidia.com>
2024-10-04 06:41:20 -0700

    add explanatory comment

--- csrc/host_ir/host_ir.h
+++ csrc/host_ir/host_ir.h
@@ -211,6 +211,19 @@ class Synchronize : public Expr {
   }
 };
 
+// For ProcessGroupNCCL, startCoalescing and endCoalescing correspond to
+// ncclGroupStart and ncclGroupEnd respectively. Those calls group p2p calls
+// that need to be progressed together -- one global work handle returned by
+// endCoalescing needs to be progressed. This has the following main advantages:
+// 1) calls are progressed concurrently
+// 2) since NICs are two-sided, a send and a recv calls need to be coalesced to
+//    achieve full BW.
+// 3) If not coalesced, we can easily reach a deadlock if the
+//    send/recv pairs are not ordered correctly.
+// It is in general preferable to coalesce send/recv calls. The only drawback is
+// that we don't have a fine-grain control on synchronicity, in other words, we
+// can only synchronize with the grouped communication at once.
+// Remark: ProcessGroupUCC does not implement coalesced groups for now
 class StartCoalescing : public Expr {
  public:
   using Expr::Expr;
