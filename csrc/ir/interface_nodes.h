c7c5de8c94d43374b44f45c62ed0a2aeffbc9348
Gao, Xiang <qasdfgtyuiop@gmail.com>
2024-10-07 09:35:57 -0700

    Cleanup `applyMmaSwizzleForTMALoad` (#3077)
    In the past, we had a `permute_outer_dim` parameter for
    `applyMmaSwizzleForTMALoad` because strides in the Mma Op's matrix
    descriptor were hard-coded. With hardcoded strides, we had zero
    flexibility, and had to use multiple TMA to load smem buffer into a very
    specific format. After https://github.com/NVIDIA/Fuser/pull/3002, the
    strides were computed based on the schedule, so we do have the
    flexibility to choose a memory layout that uses less TMA instructions to
    load.

--- csrc/ir/interface_nodes.h
+++ csrc/ir/interface_nodes.h
@@ -427,9 +427,7 @@ class NVF_API TensorView : public Val {
   //! Transforms the innermost iterdomains according to the given mma swizzle,
   //!  this should be used on the tvs that are inputs of a MmaOp or are loaded
   //!  using TMA.
-  void applyMmaSwizzleForTMALoad(
-      MmaInputSmemSwizzle swizzle,
-      bool permute_outer_dim = true);
+  void applyMmaSwizzleForTMALoad(MmaInputSmemSwizzle swizzle);
 
   //! Returns if this tensor view has swizzle operator on its tensor domain.
   //!  This is the temporary flag for indicating that the new swizzle
