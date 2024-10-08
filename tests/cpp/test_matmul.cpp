2e95a413ac440c52c3a2860beac35063ac88ab77
Gao, Xiang <qasdfgtyuiop@gmail.com>
2024-10-08 13:48:40 -0700

    save

--- tests/cpp/test_matmul.cpp
+++ tests/cpp/test_matmul.cpp
@@ -3613,7 +3613,7 @@ TEST_F(HopperMatmulTest, HSH_NT_128BSwizzle) {
   constexpr auto swizzle = MmaInputSmemSwizzle::B128;
   const auto dtype = DataType::Half;
 
-  const int64_t cta_m = 1 * getM(macro);
+  const int64_t cta_m = 2 * getM(macro);
   const int64_t cta_n = 1 * getN(macro);
 
   auto tv0 = makeContigConcreteTensor({-1, -1, 1}, dtype);
