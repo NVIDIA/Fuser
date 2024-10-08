a3be1c24e66f25fba5ca6b255cf084c0d7db5b87
Gao, Xiang <qasdfgtyuiop@gmail.com>
2024-10-08 15:00:49 -0700

    save

--- __tmp_kernel_none_f0_c0_r0_g0.cu
+++ __tmp_kernel_none_f0_c0_r0_g0.cu
@@ -10875,8 +10875,8 @@ __global__ void nvfuser_none_f0_c0_r0_g0(Tensor<__half, 3, 3> T0, Tensor<__half,
       T10[((3 + i25) % 4)] = mbarrier::arrive(toSmem((&T9[((3 + i25) % 4)])));
     }
     mbarrier::wait(toSmem((&T9[i32])), T10[i32]);
-    asm volatile("wgmma.fence.sync.aligned;\n");
-    asm volatile("fence.proxy.async;\n");
+    // asm volatile("wgmma.fence.sync.aligned;\n");
+    // asm volatile("fence.proxy.async;\n");
     asm volatile(
       "{\n"
       "  .reg .pred p0; \n"
@@ -11038,8 +11038,8 @@ __global__ void nvfuser_none_f0_c0_r0_g0(Tensor<__half, 3, 3> T0, Tensor<__half,
     for(nvfuser_index_t i38 = 0; i38 < (ceilDiv(64, 64)); ++i38) {
       mbarrier::wait(toSmem((&T9[(i34 % 4)])), T10[(i34 % 4)]);
     }
-    asm volatile("wgmma.fence.sync.aligned;\n");
-    asm volatile("fence.proxy.async;\n");
+    // asm volatile("wgmma.fence.sync.aligned;\n");
+    // asm volatile("fence.proxy.async;\n");
     asm volatile(
       "{\n"
       "  .reg .pred p0; \n"
