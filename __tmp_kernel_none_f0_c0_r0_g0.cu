c3fa4f7693156b7dfe4459caf0237f3d53a36a6b
Xiang Gao <xgao@nvidia.com>
2024-10-09 16:53:18 -0700

    save

--- __tmp_kernel_none_f0_c0_r0_g0.cu
+++ __tmp_kernel_none_f0_c0_r0_g0.cu
@@ -10877,6 +10877,7 @@ __global__ void nvfuser_none_f0_c0_r0_g0(Tensor<__half, 3, 3> T0, Tensor<__half,
       T10[((3 + i25) % 4)] = mbarrier::arrive(toSmem((&T9[((3 + i25) % 4)])));
     }
     mbarrier::wait(toSmem((&T9[i32])), T10[i32]);
+    continue;
     asm volatile(
       "{\n"
       "  .reg .pred p0; \n"
@@ -11032,6 +11033,7 @@ __global__ void nvfuser_none_f0_c0_r0_g0(Tensor<__half, 3, 3> T0, Tensor<__half,
     i37 = i6 + (8192 * i35);
     mbarrier::wait(toSmem((&T7[(i34 % 4)])), T8[(i34 % 4)]);
     mbarrier::wait(toSmem((&T9[(i34 % 4)])), T10[(i34 % 4)]);
+    continue;
     asm volatile(
       "{\n"
       "  .reg .pred p0; \n"
