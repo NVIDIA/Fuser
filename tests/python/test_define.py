acfbe26092e52f846727fc5eb5d3b4bcd5bb1485
Jingyue Wu <wujingyue@gmail.com>
2024-10-09 12:06:45 -0700

    Add license.

--- tests/python/test_define.py
+++ tests/python/test_define.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024-present NVIDIA CORPORATION & AFFILIATES.
+# All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+
 import torch
 
 from nvfuser import FusionDefinition
