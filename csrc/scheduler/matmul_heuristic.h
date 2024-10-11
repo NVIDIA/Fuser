1dbea6f8a84db18760ab095ca2d14b1439f746a6
Jacob Hinkle <jhinkle@nvidia.com>
2024-10-11 18:13:25 -0400

    lintrunner -a (after lintrunner init)

--- csrc/scheduler/matmul_heuristic.h
+++ csrc/scheduler/matmul_heuristic.h
@@ -22,7 +22,7 @@ namespace nvfuser {
 class MatmulParams : public HeuristicParams {
  public:
   MatmulParams()
-      : HeuristicParams(SchedulerType::Matmul), supported_vec_size(){};
+      : HeuristicParams(SchedulerType::Matmul), supported_vec_size() {};
   //! A list of possible strategies used to define along which axis
   //!  parallelization will be done.
   enum class TileRasterizationOrder { RowMajor = 0, ColumnMajor = 1 };
