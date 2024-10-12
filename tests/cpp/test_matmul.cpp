9e639d56e93e8f758be48af0a17d60550186f91f
Gao, Xiang <qasdfgtyuiop@gmail.com>
2024-10-12 13:07:42 -0700

    save

--- tests/cpp/test_matmul.cpp
+++ tests/cpp/test_matmul.cpp
@@ -3608,14 +3608,14 @@ TEST_F(HopperMatmulTest, HSH_NT_128BSwizzle) {
   FusionGuard fg(&fusion);
 
   constexpr int64_t M = 1024 * 16, N = 1024 * 16, K = 1024;
-  constexpr auto macro = MmaMacro::Hopper_64_256_16;
+  constexpr auto macro = MmaMacro::Hopper_64_192_16;
   constexpr auto layout = MmaLayout::NT; // [K, M] x [K, N] -> [M, N]
   constexpr auto swizzle = MmaInputSmemSwizzle::B128;
   const auto dtype = DataType::Half;
 
-  constexpr int64_t stages = 5;
-  constexpr int64_t prefetch = 3;
-  const int64_t cta_m = 1 * getM(macro);
+  constexpr int64_t stages = 4;
+  constexpr int64_t prefetch = 2;
+  const int64_t cta_m = 3 * getM(macro);
   const int64_t cta_n = 1 * getN(macro);
 
   auto tv0 = makeContigConcreteTensor({-1, -1, 1}, dtype);
