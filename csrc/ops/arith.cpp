03285767be20e566c68d21aaa6e2970de024897f
Jingyue Wu <wujingyue@gmail.com>
2024-10-14 14:50:30 -0700

    Deduplicate tensor_sizes and shape. (#3179)
    They have the same implementation -- not sure why they have co-existed.
    I prefer `shape` because it's shorter. To reduce blast radius, I only
    removed `tensor_sizes` from the C++ APIs. The python API,
    `fd.ops.tensor_sizes`, remains the same.

--- csrc/ops/arith.cpp
+++ csrc/ops/arith.cpp
@@ -1738,17 +1738,6 @@ TensorView* expand_as(TensorView* inp, TensorView* other) {
   return out_tensor;
 }
 
-std::vector<Val*> tensor_sizes(TensorView* inp) {
-  auto iter_domains = TensorDomain::noReductions(inp->getLogicalDomain());
-  std::vector<Val*> sizes(iter_domains.size(), nullptr);
-
-  for (auto idx : c10::irange(iter_domains.size())) {
-    sizes[idx] = iter_domains[idx]->getMaybeExpandedExtent();
-  }
-
-  return sizes;
-}
-
 std::vector<Val*> shape(TensorView* inp) {
   auto iter_domains = TensorDomain::noReductions(inp->getLogicalDomain());
   std::vector<Val*> shape;
