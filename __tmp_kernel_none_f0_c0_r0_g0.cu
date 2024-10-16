36273828c2a5721c1c70f98db9bb30a829be563d
Xiang Gao <xgao@nvidia.com>
2024-10-16 14:23:23 -0700

    save

--- __tmp_kernel_none_f0_c0_r0_g0.cu
+++ __tmp_kernel_none_f0_c0_r0_g0.cu
@@ -11015,7 +11015,7 @@ __global__ void nvfuser_none_f0_c0_r0_g0(Tensor<__half, 3, 3> T0, Tensor<__half,
        "n"(1),
        "n"(1)
     );
-    __syncthreads();
+    // __syncthreads();
     asm volatile("wgmma.commit_group.sync.aligned;\n");
     asm volatile("wgmma.wait_group.sync.aligned %0;\n"::"n"(0LL):"memory");
   }
