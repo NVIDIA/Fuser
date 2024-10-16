ef5985c961d01612bb7901c22cc4b209afb5016a
Jingyue Wu <wujingyue@gmail.com>
2024-10-15 21:21:06 -0700

    Use NVFuserTest.

--- tests/python/test_alias.py
+++ tests/python/test_alias.py
@@ -5,17 +5,19 @@
 import torch
 
 from nvfuser import FusionDefinition
+from utils import NVFuserTest
 
 
-def test_squeeze():
-    with FusionDefinition() as fd:
-        inp = fd.define_tensor([1, 1, 2], contiguity=True)
-        out = fd.ops.squeeze(inp, [0, 1])
-        out = fd.ops.slice(out, [1], [2])
-        fd.add_output(out)
+class TestAlias(NVFuserTest):
+    def test_squeeze(self):
+        def fusion_func(fd: FusionDefinition):
+            inp = fd.define_tensor([1, 1, 2], contiguity=True)
+            out = fd.ops.squeeze(inp, [0, 1])
+            out = fd.ops.slice(out, [1], [2])
+            fd.add_output(out)
 
-    inp_tensor = torch.randn(1, 1, 2, device="cuda")
-    out_tensor = fd.execute([inp_tensor])[0]
-    torch.testing.assert_close(
-        out_tensor, inp_tensor.squeeze([0, 1])[1:2], rtol=0, atol=0
-    )
+        in_tensor = torch.randn(1, 1, 2, device="cuda")
+        out_tensors, _ = self.exec_nvfuser(fusion_func, [in_tensor])
+        torch.testing.assert_close(
+            out_tensors[0], in_tensor.squeeze([0, 1])[1:2], rtol=0, atol=0
+        )
