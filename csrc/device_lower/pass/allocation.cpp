5ad6412ac86d87ed1937c85e3416054c0549983e
Xiang Gao <xgao@nvidia.com>
2024-10-16 17:45:19 -0700

    save

--- csrc/device_lower/pass/allocation.cpp
+++ csrc/device_lower/pass/allocation.cpp
@@ -636,6 +636,10 @@ class AllocationInserter : public kir::ExprMutator {
   void handle(ForLoop* fl) final {
     ExprMutator::handle(fl);
 
+    std::cout << "handle(ForLoop* fl)" << std::endl;
+    std::cout << "fl->iter_domain() = " << fl->iter_domain() << std::endl;
+    std::cout << fl->toString() << std::endl;
+
     auto circular_buffer_tvs =
         GpuLower::current()->circularBufferInfo().getCircularBufferTvs(fl);
 
