NVIDIA/Fuser

commit 4a3360f233e0d9580089ff6278947dc25690adea

ops.define_tensor can take contiguity as single boolean. (#3145)
As a follow-up to
https://github.com/NVIDIA/Fuser/pull/3116#discussion_r1789980138.

csrc/python_frontend/python_bindings.cpp
@@ -515,6 +515,20 @@ void clone(FusionDefinition& from, FusionDefinition& to) {
   translate(from.preschedFusion(), &to);
 }
 
+namespace {
+void verifyShape(const std::vector<int64_t>& shape) {
+  for (size_t i = 0; i < shape.size(); ++i) {
+    NVF_CHECK(
+        shape[i] >= -1,
+        "The value ",
+        shape[i],
+        " at index ",
+        i,
+        " was neither symbolic(-1), zero_element(0), broadcast(1), or static(>1).");
+  }
+}
+} // namespace
+
 void initNvFuserPythonBindings(PyObject* module) {
   auto nvfuser = py::handle(module).cast<py::module>();
 
@@ -1005,34 +1019,17 @@ void initNvFuserPythonBindings(PyObject* module) {
           "define_tensor",
           [](FusionDefinition& self,
              const std::vector<int64_t>& shape,
-             std::vector<std::optional<bool>> contiguity = {},
+             const std::vector<std::optional<bool>>& contiguity,
              const PrimDataType dtype = DataType::Float,
              const bool is_cpu = false,
-             std::vector<int64_t> stride_order = {}) -> Tensor {
-            FUSER_PERF_SCOPE("FusionDefinition.define_tensor (default)");
+             const std::vector<int64_t>& stride_order = {}) -> Tensor {
+            FUSER_PERF_SCOPE(
+                "FusionDefinition.define_tensor (contiguity as vector)");
             NVF_CHECK(
                 !self.completed(),
                 "Attempting to add to a completed definition!");
 
-            for (size_t i = 0; i < shape.size(); ++i) {
-              NVF_CHECK(
-                  shape[i] >= -1,
-                  "The value ",
-                  shape[i],
-                  " at index ",
-                  i,
-                  " was neither symbolic(-1), zero_element(0), broadcast(1), or static(>1).");
-            }
-
-            if (contiguity.empty()) {
-              for (const auto dim_size : shape) {
-                if (dim_size == 1) {
-                  contiguity.emplace_back(std::nullopt);
-                } else {
-                  contiguity.emplace_back(false);
-                }
-              }
-            }
+            verifyShape(shape);
 
             Tensor out = self.defineTensor(shape.size());
             self.defineRecord(new TensorRecord(
@@ -1046,7 +1043,7 @@ void initNvFuserPythonBindings(PyObject* module) {
             return out;
           },
           py::arg("shape"),
-          py::arg("contiguity") = py::list(),
+          py::arg("contiguity"),
           py::arg("dtype") = DataType::Float,
           py::arg("is_cpu") = false,
           py::arg("stride_order") = py::list(),
@@ -1054,11 +1051,55 @@ void initNvFuserPythonBindings(PyObject* module) {
       .def(
           "define_tensor",
           [](FusionDefinition& self,
-             std::vector<int64_t>& sizes,
-             std::vector<int64_t>& strides,
-             PrimDataType dtype = DataType::Float,
-             bool static_sizes = false,
-             bool is_cpu = false) -> Tensor {
+             const std::vector<int64_t>& shape,
+             // Contiguity for non-broadcast dimensions.
+             const bool contiguity = false,
+             const PrimDataType dtype = DataType::Float,
+             const bool is_cpu = false,
+             const std::vector<int64_t>& stride_order = {}) -> Tensor {
+            FUSER_PERF_SCOPE(
+                "FusionDefinition.define_tensor (contiguity as bool)");
+            NVF_CHECK(
+                !self.completed(),
+                "Attempting to add to a completed definition!");
+
+            verifyShape(shape);
+
+            std::vector<std::optional<bool>> contiguity_vec;
+            contiguity_vec.reserve(shape.size());
+            for (const auto dim_size : shape) {
+              if (dim_size == 1) {
+                contiguity_vec.emplace_back(std::nullopt);
+              } else {
+                contiguity_vec.emplace_back(contiguity);
+              }
+            }
+
+            Tensor out = self.defineTensor(shape.size());
+            self.defineRecord(new TensorRecord(
+                {self.recordingState(out())},
+                shape,
+                contiguity_vec,
+                dtype,
+                is_cpu,
+                stride_order));
+
+            return out;
+          },
+          py::arg("shape"),
+          py::arg("contiguity") = false,
+          py::arg("dtype") = DataType::Float,
+          py::arg("is_cpu") = false,
+          py::arg("stride_order") = py::list(),
+          py::return_value_policy::reference)
+      .def(
+          "define_tensor",
+          [](FusionDefinition& self,
+             const std::vector<int64_t>& sizes,
+             const std::vector<int64_t>& strides,
+             const PrimDataType dtype = DataType::Float,
+             const bool static_sizes = false,
+             const bool is_cpu = false) -> Tensor {
             FUSER_PERF_SCOPE("FusionDefinition.define_tensor (integration)");
             NVF_CHECK(
                 !self.completed(),

tests/python/test_define.py
@@ -1,0 +1,40 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024-present NVIDIA CORPORATION & AFFILIATES.
+# All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+
+import torch
+
+from nvfuser import FusionDefinition
+
+
+def test_define_tensor_contiguous():
+    with FusionDefinition() as fd:
+        inp = fd.define_tensor([2, 3], contiguity=True)
+        out = fd.ops.add(inp, inp)
+        fd.add_output(out)
+
+    inp_tensor = torch.randn(2, 3, device="cuda")
+    out_tensor = fd.execute([inp_tensor])[0]
+    torch.testing.assert_close(out_tensor, inp_tensor * 2)
+
+
+def test_define_tensor_noncontiguous():
+    with FusionDefinition() as fd:
+        inp = fd.define_tensor([2, 3])
+        out = fd.ops.add(inp, inp)
+        fd.add_output(out)
+
+    inp_tensor = torch.randn(8, device="cuda").as_strided([2, 3], [4, 1])
+    out_tensor = fd.execute([inp_tensor])[0]
+    torch.testing.assert_close(out_tensor, inp_tensor * 2)
+
+
+def test_define_tensor_broadcast():
+    with FusionDefinition() as fd:
+        inp = fd.define_tensor([1, 2, 1], contiguity=True)
+        out = fd.ops.add(inp, inp)
+        fd.add_output(out)
+
+    inp_tensor = torch.randn(1, 2, 1, device="cuda").as_strided([1, 2, 1], [0, 1, 0])
+    out_tensor = fd.execute([inp_tensor])[0]
+    torch.testing.assert_close(out_tensor, inp_tensor * 2)

tests/python/test_multidevice.py
@@ -128,17 +128,17 @@ def test_sdpa(mpi_test):
         def definition(self) -> None:
             self.q = self.define_tensor(
                 shape=[d, -1, -1, -1, -1],
-                contiguity=[True if d > 1 else None, True, True, True, True],
+                contiguity=True,
                 dtype=DataType.BFloat16,
             )
             self.k = self.define_tensor(
                 shape=[d, -1, -1, -1, -1],
-                contiguity=[True if d > 1 else None, True, True, True, True],
+                contiguity=True,
                 dtype=DataType.BFloat16,
             )
             self.v = self.define_tensor(
                 shape=[d, -1, -1, -1, -1],
-                contiguity=[True if d > 1 else None, True, True, True, True],
+                contiguity=True,
                 dtype=DataType.BFloat16,
             )
             # TODO(#3123): support sharded dropout and change this to a
@@ -220,67 +220,67 @@ class TransformerForwardFusion(FusionDefinition):
         mlp_dropout_rng_seed = self.define_scalar(None, dtype=DataType.Int)
         self.input = self.define_tensor(
             shape=[b, s, e],
-            contiguity=[True if b > 1 else None, True, True],
+            contiguity=True,
             dtype=DataType.BFloat16,
         )
         self.layernorm0_weight = self.define_tensor(
             shape=[e],
-            contiguity=[True],
+            contiguity=True,
             dtype=DataType.BFloat16,
         )
         self.layernorm0_bias = self.define_tensor(
             shape=[e],
-            contiguity=[True],
+            contiguity=True,
             dtype=DataType.BFloat16,
         )
         self.mha_linear0_weight = self.define_tensor(
             shape=[e * 3, e],
-            contiguity=[True, True],
+            contiguity=True,
             dtype=DataType.BFloat16,
         )
         self.mha_linear0_bias = self.define_tensor(
             shape=[e * 3],
-            contiguity=[True],
+            contiguity=True,
             dtype=DataType.BFloat16,
         )
         self.mha_linear1_weight = self.define_tensor(
             shape=[e, e],
-            contiguity=[True, True],
+            contiguity=True,
             dtype=DataType.BFloat16,
         )
         self.mha_linear1_bias = self.define_tensor(
             shape=[e],
-            contiguity=[True],
+            contiguity=True,
             dtype=DataType.BFloat16,
         )
         self.layernorm1_weight = self.define_tensor(
             shape=[e],
-            contiguity=[True],
+            contiguity=True,
             dtype=DataType.BFloat16,
         )
         self.layernorm1_bias = self.define_tensor(
             shape=[e],
-            contiguity=[True],
+            contiguity=True,
             dtype=DataType.BFloat16,
         )
         self.mlp_linear0_weight = self.define_tensor(
             shape=[d, e * 4 // d, e],
-            contiguity=[True if d > 1 else None, True, True],
+            contiguity=True,
             dtype=DataType.BFloat16,
         )
         self.mlp_linear0_bias = self.define_tensor(
             shape=[d, e * 4 // d],
-            contiguity=[True if d > 1 else None, True],
+            contiguity=True,
             dtype=DataType.BFloat16,
         )
         self.mlp_linear1_weight = self.define_tensor(
             shape=[d, e, e * 4 // d],
-            contiguity=[True if d > 1 else None, True, True],
+            contiguity=True,
             dtype=DataType.BFloat16,
         )
         self.mlp_linear1_bias = self.define_tensor(
             shape=[e],
-            contiguity=[True],
+            contiguity=True,
             dtype=DataType.BFloat16,
         )
         S17 = self.define_scalar(0.00000, dtype=DataType.Double)

commit 2ec2b926a0c589f7b72bd7a3abce7a49111c5620

Fill contiguity according to stride_order.  (#3222)
This will fix a bug introduced by my
https://github.com/NVIDIA/Fuser/pull/3145.

csrc/python_frontend/python_bindings.cpp
@@ -1118,13 +1118,20 @@ void initNvFuserPythonBindings(PyObject* module) {
 
             verifyShape(shape);
 
-            std::vector<std::optional<bool>> contiguity_vec;
-            contiguity_vec.reserve(shape.size());
-            for (const auto dim_size : shape) {
-              if (dim_size == 1) {
-                contiguity_vec.emplace_back(std::nullopt);
+            const auto rank = static_cast<int64_t>(shape.size());
+            std::vector<std::optional<bool>> contiguity_vec(rank);
+            // This duplicates some code around
+            // https://github.com/NVIDIA/Fuser/blob/b60f2341bbe2ec276b1fe60f4f25a4a5b093faa9/csrc/python_frontend/fusion_record.h#L1370.
+            // Alternatively, I can extend TensorRecord to allow contiguity as
+            // a boolean. If you think it's worth doing, I'm happy to pursue it
+            // in a separate PR.
+            for (const auto index : c10::irange(rank)) {
+              const auto contig_index =
+                  stride_order.empty() ? index : rank - 1 - stride_order[index];
+              if (shape[index] == 1) {
+                contiguity_vec[contig_index] = std::nullopt;
               } else {
-                contiguity_vec.emplace_back(contiguity);
+                contiguity_vec[contig_index] = contiguity;
               }
             }
 

tests/python/test_define.py
@@ -5,36 +5,47 @@
 import torch
 
 from nvfuser import FusionDefinition
+from utils import NVFuserTest
 
 
-def test_define_tensor_contiguous():
-    with FusionDefinition() as fd:
-        inp = fd.define_tensor([2, 3], contiguity=True)
-        out = fd.ops.add(inp, inp)
-        fd.add_output(out)
+# I tried to merge the tests to opinfo and [failed](https://github.com/NVIDIA/Fuser/issues/3225).
+class TestDefine(NVFuserTest):
+    def test_contiguous(self):
+        def fusion_func(fd: FusionDefinition):
+            inp = fd.define_tensor([2, 3], contiguity=True)
+            out = fd.ops.add(inp, inp)
+            fd.add_output(out)
 
-    inp_tensor = torch.randn(2, 3, device="cuda")
-    out_tensor = fd.execute([inp_tensor])[0]
-    torch.testing.assert_close(out_tensor, inp_tensor * 2)
+        in_tensor = torch.randn(2, 3, device="cuda")
+        out_tensors, _ = self.exec_nvfuser(fusion_func, [in_tensor])
+        torch.testing.assert_close(out_tensors[0], in_tensor * 2)
 
+    def test_noncontiguous(self):
+        def fusion_func(fd: FusionDefinition):
+            inp = fd.define_tensor([2, 3])
+            out = fd.ops.add(inp, inp)
+            fd.add_output(out)
 
-def test_define_tensor_noncontiguous():
-    with FusionDefinition() as fd:
-        inp = fd.define_tensor([2, 3])
-        out = fd.ops.add(inp, inp)
-        fd.add_output(out)
+        in_tensor = torch.randn(8, device="cuda").as_strided([2, 3], [4, 1])
+        out_tensors, _ = self.exec_nvfuser(fusion_func, [in_tensor])
+        torch.testing.assert_close(out_tensors[0], in_tensor * 2)
 
-    inp_tensor = torch.randn(8, device="cuda").as_strided([2, 3], [4, 1])
-    out_tensor = fd.execute([inp_tensor])[0]
-    torch.testing.assert_close(out_tensor, inp_tensor * 2)
+    def test_broadcast(self):
+        def fusion_func(fd: FusionDefinition):
+            inp = fd.define_tensor([1, 2, 1], contiguity=True)
+            out = fd.ops.add(inp, inp)
+            fd.add_output(out)
 
+        in_tensor = torch.randn(1, 2, 1, device="cuda").as_strided([1, 2, 1], [0, 1, 0])
+        out_tensors, _ = self.exec_nvfuser(fusion_func, [in_tensor])
+        torch.testing.assert_close(out_tensors[0], in_tensor * 2)
 
-def test_define_tensor_broadcast():
-    with FusionDefinition() as fd:
-        inp = fd.define_tensor([1, 2, 1], contiguity=True)
-        out = fd.ops.add(inp, inp)
-        fd.add_output(out)
+    def test_contiguity_with_stride_order(self):
+        def fusion_func(fd: FusionDefinition):
+            inp = fd.define_tensor([1, 2], contiguity=True, stride_order=[0, 1])
+            out = fd.ops.add(inp, inp)
+            fd.add_output(out)
 
-    inp_tensor = torch.randn(1, 2, 1, device="cuda").as_strided([1, 2, 1], [0, 1, 0])
-    out_tensor = fd.execute([inp_tensor])[0]
-    torch.testing.assert_close(out_tensor, inp_tensor * 2)
+        in_tensor = torch.randn(1, 2, device="cuda")
+        out_tensors, _ = self.exec_nvfuser(fusion_func, [in_tensor])
+        torch.testing.assert_close(out_tensors[0], in_tensor * 2)