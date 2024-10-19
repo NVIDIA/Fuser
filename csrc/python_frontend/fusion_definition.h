7e307956828913904ba2c200e504b3c00889b8a6
root <26priya11@gmail.com>
2024-10-18 21:32:36 -0700

    python enable options

--- csrc/python_frontend/fusion_definition.h
+++ csrc/python_frontend/fusion_definition.h
@@ -189,7 +189,8 @@ class NVF_API FusionDefinition : public FusionState {
       std::optional<int8_t> device,
       bool override_user_schedule,
       bool capture_debug_output,
-      bool profile) const;
+      bool profile,
+      std::vector<std::string> enable_options) const;
   //! Return debugging output captured through exeuction with
   //! capture_debug_output=true
   std::optional<std::string> getDebugOutput() const {
--- csrc/python_frontend/python_bindings.cpp
+++ csrc/python_frontend/python_bindings.cpp
@@ -916,7 +916,8 @@ void initNvFuserPythonBindings(PyObject* module) {
              std::optional<int64_t> device,
              bool override_user_schedule,
              bool capture_debug_output,
-             bool profile) {
+             bool profile,
+             std::vector<std::string> enable_options) {
             std::vector<c10::IValue> inputs;
             for (py::handle obj : iter) {
               // Allows for a Vector of Sizes to be inputed as a list/tuple
@@ -941,7 +942,8 @@ void initNvFuserPythonBindings(PyObject* module) {
                 int8_device,
                 override_user_schedule,
                 capture_debug_output,
-                profile);
+                profile,
+                enable_options);
           },
           py::arg("inputs"),
           py::kw_only(),
@@ -949,6 +951,7 @@ void initNvFuserPythonBindings(PyObject* module) {
           py::arg("override_user_schedule") = false,
           py::arg("capture_debug_output") = false,
           py::arg("profile") = false,
+          py::arg("enable_options") = py::none(),
           py::return_value_policy::reference)
       .def_static(
           "_profile",
