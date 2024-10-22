ff48c03c9a5eea1a11a1d70fdc1e1b6701e624f1
Gao, Xiang <qasdfgtyuiop@gmail.com>
2024-10-22 13:52:47 -0700

    save

--- __tmp_kernel_none_f0_c0_r0_g0.cu
+++ __tmp_kernel_none_f0_c0_r0_g0.cu
@@ -10893,7 +10893,7 @@ nvfuser_none_f0_c0_r0_g0(Tensor<__half, 3, 3> T0, Tensor<__half, 3, 3> T1, const
   #pragma unroll
   for(nvfuser_index_t i20 = 0; i20 < 4; ++i20) {
     if ((((((nvfuser_index_t)threadIdx.x) < 32ULL) && (((nvfuser_index_t)threadIdx.y) == 0ULL)) && (((nvfuser_index_t)threadIdx.z) == 0ULL)) && Hopper::electSync(4294967295U)) {
-      mbarrier::init(toSmem((&T7[i20])), 2U);
+      mbarrier::init(toSmem((&T7[i20])), 1U);
     }
   }
   __syncthreads();
@@ -10906,14 +10906,11 @@ nvfuser_none_f0_c0_r0_g0(Tensor<__half, 3, 3> T0, Tensor<__half, 3, 3> T1, const
     unsigned i24;
     i24 = i9 + (4096 * i21);
     if ((((((nvfuser_index_t)threadIdx.x) < 32ULL) && (((nvfuser_index_t)threadIdx.y) == 0ULL)) && (((nvfuser_index_t)threadIdx.z) == 0ULL)) && Hopper::electSync(4294967295U)) {
-      mbarrier::arriveExpectTX(toSmem((&T7[i21])), 8192U);
+      mbarrier::arriveExpectTX(toSmem((&T7[i21])), 8192U+4096U);
       #pragma unroll
       for(nvfuser_index_t i25 = 0; i25 < 4; ++i25) {
         Hopper::cpAsyncBulkTensorTileG2S((Hopper::CpAsyncBulkTensorTileG2SIndex<2>{ ptr4, (Array<nvfuser_index_t, 2, 1>{(i5 + (64 * i25)), i22}), toSmem((&T7[i21])) }), (i23 + (2048 * i25)));
       }
-    }
-    if ((((((nvfuser_index_t)threadIdx.x) < 32ULL) && (((nvfuser_index_t)threadIdx.y) == 0ULL)) && (((nvfuser_index_t)threadIdx.z) == 0ULL)) && Hopper::electSync(4294967295U)) {
-      mbarrier::arriveExpectTX(toSmem((&T7[i21])), 4096U);
       #pragma unroll
       for(nvfuser_index_t i26 = 0; i26 < 2; ++i26) {
         Hopper::cpAsyncBulkTensorTileG2S((Hopper::CpAsyncBulkTensorTileG2SIndex<2>{ ptr7, (Array<nvfuser_index_t, 2, 1>{(i8 + (64 * i26)), i22}), toSmem((&T7[i21])) }), (i24 + (2048 * i26)));
@@ -10937,14 +10934,11 @@ nvfuser_none_f0_c0_r0_g0(Tensor<__half, 3, 3> T0, Tensor<__half, 3, 3> T1, const
     unsigned i34;
     i34 = i6 + (8192 * i32);
     if ((((((nvfuser_index_t)threadIdx.x) < 32ULL) && (((nvfuser_index_t)threadIdx.y) == 0ULL)) && (((nvfuser_index_t)threadIdx.z) == 0ULL)) && Hopper::electSync(4294967295U)) {
-      mbarrier::arriveExpectTX(toSmem((&T7[((3 + i27) % 4)])), 8192U);
+      mbarrier::arriveExpectTX(toSmem((&T7[((3 + i27) % 4)])), 8192U+4096U);
       #pragma unroll
       for(nvfuser_index_t i25 = 0; i25 < 4; ++i25) {
         Hopper::cpAsyncBulkTensorTileG2S((Hopper::CpAsyncBulkTensorTileG2SIndex<2>{ ptr4, (Array<nvfuser_index_t, 2, 1>{(i5 + (64 * i25)), i28}), toSmem((&T7[((3 + i27) % 4)])) }), (i30 + (2048 * i25)));
       }
-    }
-    if ((((((nvfuser_index_t)threadIdx.x) < 32ULL) && (((nvfuser_index_t)threadIdx.y) == 0ULL)) && (((nvfuser_index_t)threadIdx.z) == 0ULL)) && Hopper::electSync(4294967295U)) {
-      mbarrier::arriveExpectTX(toSmem((&T7[((3 + i27) % 4)])), 4096U);
       #pragma unroll
       for(nvfuser_index_t i26 = 0; i26 < 2; ++i26) {
         Hopper::cpAsyncBulkTensorTileG2S((Hopper::CpAsyncBulkTensorTileG2SIndex<2>{ ptr7, (Array<nvfuser_index_t, 2, 1>{(i8 + (64 * i26)), i28}), toSmem((&T7[((3 + i27) % 4)])) }), (i31 + (2048 * i26)));
