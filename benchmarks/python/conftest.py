ad925af01b89c60747d65418c794f9c5532b52d3
Jacob Hinkle <jhinkle@nvidia.com>
2024-10-24 15:27:04 -0400

    Apply decorator to all tests during modifyitems hook

--- benchmarks/python/conftest.py
+++ benchmarks/python/conftest.py
@@ -104,6 +104,11 @@ def pytest_collection_modifyitems(session, config, items):
     run_thunder = config.getoption("--benchmark-thunder")
     run_torchcompile = config.getoption("--benchmark-torchcompile")
 
+    from nvfuser.pytorch_utils import retry_on_oom_or_skip_test
+
+    for item in items:
+        item.obj = retry_on_oom_or_skip_test(item.obj)
+
     if not run_eager:
         skip_eager = pytest.mark.skip(reason="need --benchmark-eager option to run")
         for item in items:
@@ -133,34 +138,3 @@ def pytest_collection_modifyitems(session, config, items):
         for item in items:
             if "thunder" in item.nodeid:
                 item.add_marker(skip_thunder)
-
-
-def pytest_runtest_call(item: "Item") -> None:
-    """Called to run the test for test item (the call phase).
-
-    The default implementation calls ``item.runtest()``.
-    """
-    import torch
-
-    retry = False
-    try:
-        item.runtest()
-    except torch.OutOfMemoryError:
-        retry = True
-
-    if not retry:
-        return
-
-    # We have hit an OOM error, so clear the cache and retry
-    import gc
-
-    gc.collect()
-    torch.cuda.empty_cache()
-
-    try:
-        item.runtest()
-    except torch.OutOfMemoryError as e:
-        # If we hit an OOM this time, then skip the test
-        import pytest
-
-        pytest.skip(f"Test failed due to OutOfMemoryError: {e}")
