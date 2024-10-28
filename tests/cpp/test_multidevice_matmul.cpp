8a56b2f3ab891b51f80b6eef132d5d8dbd5b6eb4
mcowan <mcowan@nvidia.com>
2024-10-28 14:18:08 -0700

    skip mul sum matmul on blackwell

--- tests/cpp/test_multidevice_matmul.cpp
+++ tests/cpp/test_multidevice_matmul.cpp
@@ -62,6 +62,10 @@ TEST_F(DistributedMatmulTest, MulSum_LayoutTN_NoComms) {
   // MmaLayout::TN A(T), B(N), C(T)
   // A and C are sharded on dimension M
   // Tests local matmul with no communication
+  auto dev_prop = at::cuda::getCurrentDeviceProperties();
+  if (dev_prop->major > 10) {
+    GTEST_SKIP() << "Require Ampere or Hopper for generated matmul";
+  }
   auto fusion = std::make_unique<Fusion>();
   FusionGuard fg(fusion.get());
   auto mesh = DeviceMesh::createForNumDevices(num_devices_);
