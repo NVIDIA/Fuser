91f918ab5a894758e6fca5caf693b09e3d5c78eb
Jacob Hinkle <jhinkle@nvidia.com>
2024-10-30 10:04:17 -0400

    Add script to check for non-determinism
    For example, try running the following commands:
    ```bash
    rm -rf /tmp/nvfuser_kernel_db
    tools/check_determinism.sh -- pytest -vs tests/python/test_ops.py::test_correctness_var_mean_float64
    rm -rf /tmp/nvfuser_kernel_db
    DEBUG_SERDE=disable tools/check_determinism.sh -- pytest -vs tests/python/test_ops.py::test_correctness_var_mean_float64
    ```
    Note that this script will delete any existing *.cu files in the current
    directory.

---
+++ tools/check_determinism.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+usage() {
+  echo "Usage: $0 [-h] [-n NUMREPS=10] -- command to run]"
+}
+
+
+while getopts "n:h" arg
+do
+  case $arg in
+    n)
+      NUMREPS=$OPTARG
+      shift
+      ;;
+    h | ?)
+      usage
+      exit 1
+      ;;
+  esac
+done
+# getopts stops parsing if it sees "--". We can detect that case and record command
+while [[ $# -gt 0 ]]
+do
+  if [[ "$1" == "--" ]]
+  then
+    shift
+    break
+  fi
+  shift
+done
+CMD=$*
+
+export NVFUSER_DUMP=cuda_to_file
+
+KERNELDIR=$(mktemp -d)
+
+cleanup() {
+    rm -rf "$KERNELDIR"
+}
+
+trap "cleanup" EXIT
+
+FIRSTREPDIR="$KERNELDIR/1"
+
+retval=0
+for rep in $(seq 1 "$NUMREPS")
+do
+    # $CMD does not need to succeed for us to analyze it
+    set +e
+    $CMD
+    set -e
+
+    REPDIR="$KERNELDIR/$rep"
+    mkdir -p "$REPDIR"
+    mv ./*.cu "$REPDIR/"
+
+    NUMFIRST=$(find "$FIRSTREPDIR" -name \*.cu | wc -l)
+    NUMREP=$(find "$REPDIR" -name \*.cu | wc -l)
+    if [[ $NUMREP -ne $NUMFIRST ]]
+    then
+        echo "Created $NUMFIRST kernels on first repetition and $NUMREP on repetition $rep"
+        retval=1
+        continue
+    fi
+    for newkernel in "$REPDIR"/*.cu
+    do
+        basename=$(basename "$newkernel")
+        firstkernel="$FIRSTREPDIR/$basename"
+        if [ ! -f "$firstkernel" ]
+        then
+            echo "Kernel file $newkernel in repetition $rep does not exist in first repetition"
+            retval=1
+            continue
+        fi
+        set +e
+        diff "$firstkernel" "$newkernel"
+        diffstatus=$?
+        set -e
+        if [[ $diffstatus -ne 0 ]]
+        then
+            printf 'Diff of %s from rep 1 to rep %d (above) is non-zero\n' "$basename" "$rep"
+            retval=1
+            continue
+        fi
+    done
+    if [[ $retval -ne 0 ]]
+    then
+        # Stop repetitions after first failure
+        break
+    else
+        echo "Generated kernels all match"
+    fi
+done
+
+exit $retval
