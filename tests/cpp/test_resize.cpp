2a1ff76f40b4856c2308c321b879ebb11b0baa0e
jjsjann123 <jiej@nvidia.com>
2024-11-05 18:00:43 -0800

    clangformat

--- tests/cpp/test_resize.cpp
+++ tests/cpp/test_resize.cpp
@@ -2282,9 +2282,7 @@ TEST_F(ResizeTest, FusionSqueezeSymbolic) {
   NVF_CHECK(ref0.equal(cg_outputs[0]));
 
   EXPECT_THAT(
-      [&]() {
-        fec.runFusionWithInputs({t0, 10});
-      },
+      [&]() { fec.runFusionWithInputs({t0, 10}); },
       ThrowsMessage<nvfError>(
           HasSubstr("must concretize to IterType::Broadcast but found")));
 }
