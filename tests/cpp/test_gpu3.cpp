33115b9b0dc664c41ba7c80a27a95de732f5a24c
Naoya Maruyama <nmaruyama@nvidia.com>
2024-11-07 21:17:26 -0800

    test fix

--- tests/cpp/test_gpu3.cpp
+++ tests/cpp/test_gpu3.cpp
@@ -8943,11 +8943,14 @@ TEST_F(NVFuserTest, ReplaceSymbolicSizesPreferSimplerExtents) {
   fusion.addInput(tv0);
   auto tv1 = makeSymbolicTensor(2);
   fusion.addInput(tv1);
-  auto i0 = IrBuilder::create<Val>(DataType::Index);
-  fusion.addInput(i0);
 
-  auto tv2 = reshape(tv0, {i0});
-  auto tv3 = reshape(tv1, {i0});
+  auto tv2 = reshape(
+      tv0,
+      {mul(
+          mul(tv0->axis(0)->extent(), tv0->axis(1)->extent()),
+          tv0->axis(2)->extent())});
+  auto tv3 =
+      reshape(tv1, {mul(tv1->axis(0)->extent(), tv1->axis(1)->extent())});
   auto tv4 = add(tv2, tv3);
   fusion.addOutput(tv4);
 
@@ -8958,7 +8961,6 @@ TEST_F(NVFuserTest, ReplaceSymbolicSizesPreferSimplerExtents) {
   expr_eval.bind(tv0->axis(2)->extent(), 8L);
   expr_eval.bind(tv1->axis(0)->extent(), 8L);
   expr_eval.bind(tv1->axis(1)->extent(), 8L);
-  expr_eval.bind(i0, 64L);
 
   auto initial_info = DynamicTransform::getInitialInfo(&fusion);
   auto info = DynamicTransformConcretizationInfo(&initial_info, &expr_eval);
