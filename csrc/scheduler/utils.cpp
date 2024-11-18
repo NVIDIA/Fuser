b1d9eb8c75557dc68990b12b898dd01cfa53bb97
jjsjann123 <jiej@nvidia.com>
2024-11-18 13:50:29 -0800

    fixing segfault

--- csrc/scheduler/utils.cpp
+++ csrc/scheduler/utils.cpp
@@ -1218,13 +1218,16 @@ std::vector<TensorView*> cacheInputs(Fusion* fusion, bool unroll) {
         cached_uses.push_back(use);
       } else {
         // propagate allocation domain on pad op
+        if (!use->as<PadOp>()->out()->isA<TensorView>()) {
+          continue;
+        }
         auto pad_out_tv = use->as<PadOp>()->out()->as<TensorView>();
         if (pad_out_tv->hasAllocation()) {
           continue;
         }
         std::optional<std::vector<int64_t>> alloc_permutation =
             ir_utils::computePermutation(
-                tv->getLogicalDomain(), tv->getAllocationDomain());
+                tv->getLogicalDomain(), tv->getMaybeAllocationDomain());
         if (alloc_permutation.has_value()) {
           pad_out_tv->setAllocationDomain(
               ir_utils::applyPermutation(
