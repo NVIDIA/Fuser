5fcd764f29783ffdeb2bc61415b426960431348f
protonu <pbasu@nvidia.com>
2024-11-19 09:52:51 -0800

    formatting

--- csrc/scheduler/mma_utils.h
+++ csrc/scheduler/mma_utils.h
@@ -249,8 +249,10 @@ class MmaSwizzler {
 //! marked parallel type bulk.
 void scheduleTMAStoreForMmaOutput(TensorView* tv, int64_t m, int64_t n);
 
-
-void scheduleStMatrixForMmaOutput(TensorView* tv, int64_t tile_m, int64_t tile_n);
+void scheduleStMatrixForMmaOutput(
+    TensorView* tv,
+    int64_t tile_m,
+    int64_t tile_n);
 
 void checkDimSize(
     TensorView* tv,
