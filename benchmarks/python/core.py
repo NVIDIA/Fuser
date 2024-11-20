ab655ff9b3535d826f712649f7dd4a61b725b3b4
root <26priya11@gmail.com>
2024-11-19 22:10:48 -0800

    wip with executor change

--- benchmarks/python/core.py
+++ benchmarks/python/core.py
@@ -46,6 +46,16 @@ def clear_dynamo_cache() -> None:
 def unary_bwd_torch(inputs: List):  # [output, grad_out]
     inputs[0].backward(inputs[1], retain_graph=True)
 
+def with_executor(executor: str, fwd_fn: Callable) -> Callable:
+    assert executor in ["eager", "torchcompile", "thunder"]
+    if executor == 'eager':
+        return fwd_fn
+    if executor == 'torchcompile':
+        return torch.compile(fwd_fn)
+    if executor == 'thunder':
+        return thunder.jit(
+            fwd_dn, nv_enable_bookend=False, executors=[nvfuserex]
+        )            
 
 def with_executor(executor: str, fwd_fn: Callable) -> Callable:
     assert executor in ["eager", "torchcompile", "thunder"]
