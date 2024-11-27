27689708c7e15f91c9ac1383f434284464bf1990
Jingyue Wu <wujingyue@gmail.com>
2024-11-27 11:17:17 -0800

    Comment

--- csrc/multidevice/utils.h
+++ csrc/multidevice/utils.h
@@ -151,7 +151,20 @@ void reorderDIDToFront(TensorView*);
 // dimension this DID-parallelization should be attributed to.
 // - The function assumes that all Splits from logical to the DID-parallel
 // IterDomains in allocation are even. This is because there are currently no
-// ways to pass in the global shape without an API overhaul.
+// ways to pass in the global shape.
+//
+// Despite these limitations, I took this approach as a shortcut to fix #3282,
+// which blocked many other tasks. I'm however open to other better, long-term
+// solutions. Some alternatives considered in #3282 are:
+// - Try to bind `at::Tensor`s to allocation domains instead of logical. Many
+// `*Op::evaluate` methods (e.g.
+// https://github.com/NVIDIA/Fuser/blob/2415d904d1e9a5da7ca6fb1a55d3045bbd510341/csrc/ir/nodes.cpp#L4321-L4329)
+// assume the input/output `at::Tensor`s have the same dimension order as the
+// logical domain. Doing so would have to change them all.
+// - Try to pass into FusionExecutorCache both logical (global) shapes and
+// allocated (local) tensors for sharded TensorViews. The logical shapes would
+// have to be passed through FusionKernelRuntime, FusionExecutor,
+// ExpressionEvaluator, and so on, which is an API overhaul.
 std::vector<int64_t> unshardedSizes(
     const TensorView* tv,
     c10::IntArrayRef sizes);
