c132dbec6c4fa311440096b9a2f11252dc3dbc79
Naoya Maruyama <nmaruyama@nvidia.com>
2024-11-27 19:42:54 -0800

    cleanup

--- csrc/id_model/indexing_traversal.cpp
+++ csrc/id_model/indexing_traversal.cpp
@@ -9,8 +9,6 @@
 #include <id_model/indexing_traversal.h>
 #include <ir/utils.h>
 
-#include <fstream>
-
 namespace nvfuser {
 
 IndexingTraversal::IndexingTraversal(
@@ -70,8 +68,6 @@ std::optional<IndexingTraversal::ExprPath> IndexingTraversal::
   auto consumer_tv = ir_utils::getTvOutput(expr);
   NVF_ERROR(consumer_tv != nullptr);
 
-  std::cerr << "IndexingTraversal: trying resize war: "
-            << consumer_tv->toString() << "\n";
   IdModel local_model(
       std::vector<Expr*>{consumer_tv->definition()},
       /*additional_tvs=*/{},
@@ -93,25 +89,36 @@ std::optional<IndexingTraversal::ExprPath> IndexingTraversal::
 
   // from_ids are loop domains, which are representative
   // domains of loop groups and not necessarily domains of any
-  // of the producer and the consumer
-  std::vector<IterDomain*> consumer_from_domains(from_ids.size());
+  // of the producer and the consumer.  In that case, find an ID out
+  // of the global group that is mapped in the local graph.
+  ValGroups from_groups;
   for (const auto i : c10::irange(from_ids.size())) {
-    auto consumer_id = consumer_tv->getLoopDomain().at(i);
     auto from_id = from_ids.at(i);
+    if (local_graph.hasGroup(from_id)) {
+      from_groups.pushBack(local_graph.toGroup(from_id));
+      continue;
+    }
+    bool found = false;
+    const auto& global_group = graph.toGroup(from_id);
+    for (const auto& vg : local_graph.disjointValSets().disjointSets()) {
+      if (global_group->has(vg->front())) {
+        from_groups.pushBack(vg);
+        found = true;
+        break;
+      }
+    }
+    // If not found, it should mean it's promoted to some IDs of
+    // further consumer tensors. This WAR does not work then. We could
+    // simply fall back to the default ValGraph-based path, but that
+    // might hit the resize indexing issue (#3455). For now, this is
+    // considered an error.
     NVF_ERROR(
-        graph.disjointValSets().strictAreMapped(consumer_id, from_id),
-        "Expected strict mapping: ",
-        consumer_id->toString(),
-        ", ",
-        from_id->toString());
-    consumer_from_domains.at(i) = consumer_id;
+        found, "Indexing path for resize not found: ", from_id->toString());
   }
 
-  auto from_groups = local_graph.toGroups(consumer_from_domains);
-
-  // to_domains may not point to IDs of this tensor. If it's an
-  // allocation ID, it may be a loop promotion ID.
-  // auto to_groups = local_graph.toGroups(to_domains);
+  // Similarly, to_ids may not be IDs found in any of the producer and
+  // consumer tensors of this expr. For example, if it's an allocation
+  // ID, it may be a loop promotion ID.
   ValGroups to_groups;
   for (auto to_id : to_ids) {
     if (local_graph.hasGroup(to_id)) {
@@ -132,30 +139,14 @@ std::optional<IndexingTraversal::ExprPath> IndexingTraversal::
     NVF_ERROR(found, "Indexing path for resize not found: ", to_id->toString());
   }
 
-  std::cerr << "Resize war from: " << nvfuser::toString(from_groups) << " -> "
-            << nvfuser::toString(to_groups) << "\n";
-  std::cerr << expr->toString();
-
   IndexingTraversal traversal(
       expr,
       local_graph,
       {from_groups.vector().begin(), from_groups.vector().end()},
-      {to_groups.vector().begin(), to_groups.vector().end()});
-  // Local graph may not have all info necessary for
-  // indexing. If traversal fails, just giving up taking this
-  // WAR. This isn't ideal, but the WAR itself isn't ideal either...
-  traversal.require_all_to_visited_ = false;
+      {to_groups.vector().begin(), to_groups.vector().end()},
+      /*require_all_to_visited=*/true);
   traversal.traverse();
-  auto path = traversal.getShortestExprPath().first;
-  auto path_outputs = getOutputsOfExprPath(local_graph, path);
-  if (std::any_of(
-          to_groups.begin(), to_groups.end(), [&](const ValGroup& to_group) {
-            return !path_outputs.has(to_group) && !from_groups.has(to_group);
-          })) {
-    // Some to_group wasn't reached. Not a valid path.
-    std::cerr << "Unreachable node detected\n";
-    return std::nullopt;
-  }
+  auto [path, all_visited] = traversal.getShortestExprPath();
 
   for (const auto& [g, d] : path) {
     if (g->front()->isA<Resize>()) {
