f27ce14f4cefc48fd1e468bb08f8667a7378cf94
Gao, Xiang <qasdfgtyuiop@gmail.com>
2024-12-03 09:14:37 -0800

    revert

--- tests/cpp/test_circular_buffering.cpp
+++ tests/cpp/test_circular_buffering.cpp
@@ -1266,16 +1266,10 @@ TEST_P(TmaCircularBufferingTest, Pointwise) {
   tv3->circularBuffer(
       number_of_stages, prefetch_distance, circular_buffer_type);
 
-  // Circular Buffer with set operation.
-  // Note that in order to use warp specialization, all circilar buffers must be
-  // loaded by TMA, so for this test we disable circular buffering of set op if
-  // we are testing warp specialization.
-  if (!std::holds_alternative<WarpSpecialized>(circular_buffer_type)) {
-    // Load TV1 into shared memory
-    tv4->setMemoryType(MemoryType::Shared);
-    tv4->circularBuffer(
-        number_of_stages, prefetch_distance, circular_buffer_type);
-  }
+  // Circular Buffer with set operation
+  tv4->axis(0)->parallelize(ParallelType::BIDx);
+  tv4->circularBuffer(
+      number_of_stages, prefetch_distance, circular_buffer_type);
 
   // Split reference to parallelize TMA tile
   reference->split(-1, 32);
