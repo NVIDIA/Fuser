9d3734865af1837b61678d2444590693b2199f18
Naoya Maruyama <naoyam@users.noreply.github.com>
2024-12-06 01:53:52 -0800

    add scheduler_tools::scheduleLoopDomainsBy (#3523)
    Extracted from #3425 
    
    A simple scheduling utility that replays a single transform expr on the
    loop domain of a given tensor. It is conceptually similar to the
    existing transform propagator, but this interface can also replay a
    transform expr not just as a forward expr but also as a backward expr.
    
    The existing `scheduler_tools::scheduleLoopDomainsLike` can also be used
    to propagate loop domains across tensors, but I encountered with a
    couple of issues due to the resize mapping [issue](#3455). This
    `scheduleLoopDomainsBy` gives more explicit control to set loop domains
    with resize ops.

--- csrc/scheduler/tools/loop_domain_scheduler.h
+++ csrc/scheduler/tools/loop_domain_scheduler.h
@@ -11,6 +11,7 @@
 
 namespace nvfuser {
 
+class Expr;
 class TensorView;
 class IterDomain;
 
@@ -22,6 +23,41 @@ namespace scheduler_tools {
 void scheduleLoopDomainsLike(
     const std::vector<TensorView*>& tvs,
     const std::vector<IterDomain*>& ref_loop_dom);
+
+// Replay a transform expr on the loop domain of each of the given
+// tensors. If the input of the transform is exact mapped with the loop
+// domain, the transform is replayed as a forward op. If the output
+// is exact mapped with the loop domain, it's replayed as a backward
+// op. The loop domain of each tensor is updated with the replayed
+// transform expr. If it's replayed as a forward op, the outputs
+// replace the inputs in the loop domain. If it's replayed as a
+// backward op, the inputs replace the outputs in the loop domain. The
+// new IDs are inserted at the outermost position of the input IDs.
+//
+// For example, suppose a fusion has:
+//
+// t0 = makeSymbolicTensor(1);
+// t1 = sin(t0);
+// t2 = t1[1:]); // slice
+//
+// In this case, t2 has a resize op with a left expansion factor of
+// -1. This function can be used to propagate this resize onto t1 as
+// follows:
+//
+// scheduleLoopDomainsBy({t1}, t2->axis(0)->definition());
+//
+// Then the t1 domain should look like:
+//
+// Logical: i0
+//  resize i0 by {-1, 0} -> i1
+// Loop: i1
+//
+// Now that the loop domain of t1 and t2 are exact mapped, it's also
+// possible to inline t1 into the innermost position of t2. See
+// LoopDomainSchedulingTest.ScheduleLoopDomainsBy1 for more examples.
+void scheduleLoopDomainsBy(
+    const std::vector<TensorView*>& tvs,
+    Expr* transform);
 
 } // namespace scheduler_tools
 } // namespace nvfuser
