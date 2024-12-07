c7b646cd27ee20f365678736c784c33d4e0c71b1
Gao, Xiang <qasdfgtyuiop@gmail.com>
2024-12-06 18:11:39 -0800

    format

--- runtime/basic_type_traits.cu
+++ runtime/basic_type_traits.cu
@@ -47,7 +47,7 @@ template <class _Tp, class _Up>
 struct is_same : public false_type {};
 template <class _Tp>
 struct is_same<_Tp, _Tp> : public true_type {};
-template<class T, class U >
+template <class T, class U>
 constexpr bool is_same_v = is_same<T, U>::value;
 
 // is_integral, for some types.
