07be8b9b3a2279d86c8287b1411fbc46262831aa
Gao, Xiang <qasdfgtyuiop@gmail.com>
2024-12-09 14:24:41 -0800

    Update all reduction primitives to take a block_dim parameter (#3536)
    Stacked on https://github.com/NVIDIA/Fuser/pull/3541
    
    This PR is mostly mechanical. It just update all the reduction
    primitives with a new parameter `block_dim`, which is just `blockDim`
    for a kernel without warp specialization. For a kernel with warp
    specialization, we use a value for `block_dim`, which is the block
    dimension of the compute warps.
    
    For better performance, it is beneficial if we can know whether we are
    using the `blockDim` or a custom value at compile time. To achieve this,
    I added a new helper class:
    
    ```C++
    struct DefaultBlockDim {
      const uint32_t x, y, z;
      __device__ DefaultBlockDim() : x(blockDim.x), y(blockDim.y), z(blockDim.z) {}
      __device__ operator dim3() const {
        return blockDim;
      }
    };
    ```
    When a function takes `block_dim`, its type will be a template type
    `BlockDimT`, which is either `DefaultBlockDim` or `dim3`, where
    `DefaultBlockDim` means we are using `blockDim`, and `dim3` means we are
    using the provided custom value.
    
    With this PR, I would consider all our reductions and broadcasts
    compatible with warp specialization. I expect them to just work with
    warp specialization. In the future, we may want to use TMA + warp
    specialization for reduction and persistent kernels.

--- runtime/broadcast.cu
+++ runtime/broadcast.cu
@@ -16,30 +16,40 @@ namespace broadcast {
 // inp_val: Per-thread source value. Only valid when the thread is a source.
 // out: Per-thread output location
 //
-template <bool X_THREAD, bool Y_THREAD, bool Z_THREAD, bool Aligned, typename T>
+template <
+    bool X_THREAD,
+    bool Y_THREAD,
+    bool Z_THREAD,
+    bool Aligned,
+    typename T,
+    typename BlockDimT>
 __device__ void blockBroadcast(
     T& out,
     const T& inp_val,
     T* shared_mem,
-    bool read_write_pred) {
+    bool read_write_pred,
+    // block_dim is basically just blockDim (wrapped as DefaultBlockDim) if
+    // there is no warp specialization in the kernel. If there is warp
+    // specialization, block_dim is the the dimension of the compute warps.
+    BlockDimT block_dim) {
   const bool has_valid_data = (!X_THREAD || threadIdx.x == 0) &&
       (!Y_THREAD || threadIdx.y == 0) && (!Z_THREAD || threadIdx.z == 0);
 
   const auto shared_offset =
       index_utils::maskedOffset<!X_THREAD, !Y_THREAD, !Z_THREAD>(
-          threadIdx, blockDim);
+          threadIdx, block_dim);
 
   if (has_valid_data && read_write_pred) {
     shared_mem[shared_offset] = inp_val;
   }
 
-  block_sync::sync<Aligned>();
+  block_sync::sync<Aligned>(block_dim);
 
   if (read_write_pred) {
     out = shared_mem[shared_offset];
   }
 
-  block_sync::sync<Aligned>();
+  block_sync::sync<Aligned>(block_dim);
 }
 
 } // namespace broadcast
