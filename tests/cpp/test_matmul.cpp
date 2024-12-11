f5f2ab5511619934e41c45665d2fa9ad5be424d9
Jacob Hinkle <1454944+jacobhinkle@users.noreply.github.com>
2024-12-11 09:41:56 -0500

    Allow inlining past loop broadcasts for MmaOp (#3416)
    Stacked on #3414 
    
    This PR enables us to inline an MmaOp properly when its inputs are
    missing broadcast dimensions. We do this by always allowing inlining
    past loop broadcasts or their transforms. For example
    ```
    tv0:
      logical [ iS1{i0} ]
      loop [ iS1{i0} bS5{1} ]
    tv1:
      logical [ iS2{i1} ]
      loop [ bS6{1} iS2{i1} ]
    tv2 = foo(tv0, tv1)
      logical [ iS3{i0} iS4{i1} ]
    ```
    As long as the operation `foo` properly maps its arguments despite the
    missing logical dimensions (as `MmaOp` does as of #3391), then we should
    be able to fully inline this case because the loop broadcasts `bS5` and
    `bS6` are imaginary in the sense that they don't impact indexing.

--- tests/cpp/test_matmul.cpp
+++ tests/cpp/test_matmul.cpp
@@ -3657,7 +3657,6 @@ TEST_F(HopperMatmulTest, HSH_NT_128BSwizzle) {
   const auto dtype = DataType::Half;
 
   constexpr bool use_smem_epilogue = false;
-  constexpr bool use_warp_specialization = true;
 
   constexpr int64_t stages = 4;
   constexpr int64_t prefetch = 3;
@@ -3801,13 +3800,8 @@ TEST_F(HopperMatmulTest, HSH_NT_128BSwizzle) {
 
   inlineMost();
 
-  if (use_warp_specialization) {
-    tv0c->circularBuffer(stages, prefetch, WarpSpecialized(ParallelType::TIDy));
-    tv1c->circularBuffer(stages, prefetch, WarpSpecialized(ParallelType::TIDy));
-  } else {
-    tv0c->circularBuffer(stages, prefetch);
-    tv1c->circularBuffer(stages, prefetch);
-  }
+  tv0c->circularBuffer(stages, prefetch);
+  tv1c->circularBuffer(stages, prefetch);
 
   auto inputs =
       matmulAtInput3DHopperSS(M, N, K, layout, data_type_to_aten(dtype));
@@ -3948,7 +3942,31 @@ TEST_F(HopperMatmulTest, HSH_NT_128BSwizzle_NoBroadcasts) {
   }
   tv3->axis(-1)->parallelize(ParallelType::Vectorize);
 
+  {
+    // Check using a copy that improperly aligned axis are not inlined
+    Fusion tmp_fusion;
+    IrCloner ir_cloner = Fusion::copy(&fusion, &tmp_fusion);
+    FusionGuard tmp_fg(&tmp_fusion);
+    // [Mo, No, Ko, Mio, Nio, Mii, Nii, Ki]
+    // Swap the No and Ko axes, but only in tv2, the mma output
+    // [Mo, Ko, No, Mio, Nio, Mii, Nii, Ki]
+    // This should mean the smem operands are now inlined at position 1 instead
+    // of 3
+    ir_cloner.clone(tv2)->reorder({{2, 1}, {1, 2}});
+    inlineMost();
+    tmp_fusion.printMath();
+    ir_cloner.clone(tv2)->reorder({{2, 1}, {1, 2}});
+    EXPECT_EQ(ir_cloner.clone(tv0c)->getComputeAtPosition(), 1);
+    // The outermost loop dim of tv1c is a broadcast Mo axis, so
+    // tv1c->inlineAt(1) does not inline past that axis and we wind up with
+    // compute-at position 0.
+    EXPECT_EQ(ir_cloner.clone(tv1c)->getComputeAtPosition(), 0);
+  }
+
   inlineMost();
+
+  EXPECT_EQ(tv0c->getComputeAtPosition(), 3);
+  EXPECT_EQ(tv1c->getComputeAtPosition(), 3);
 
   if (stages > 1) {
     tv0c->circularBuffer(stages, prefetch);
