156a7a52ec7c391cfe5cce48cb51ee00a128aa01
jjsjann123 <alex.jann2012@gmail.com>
2024-12-15 20:53:07 -0800

    add hf_mistral_nemo

--- benchmarks/python/test_rope.py
+++ benchmarks/python/test_rope.py
@@ -698,6 +698,173 @@ def hf_phi3_rope():
     return HfPhi3Rope(cfg).cuda().bfloat16(), inputs, grads
 
 
+def hf_mistral_nemo_rope():
+    from transformers.models.mistral import MistralConfig
+    
+    mistral_cfg_str = r'''{
+      "_name_or_path": "mistralai/Mistral-Nemo-Base-2407",
+      "architectures": [
+        "MistralForCausalLM"
+      ],
+      "attention_dropout": 0.0,
+      "bos_token_id": 1,
+      "eos_token_id": 2,
+      "head_dim": 128,
+      "hidden_act": "silu",
+      "hidden_size": 5120,
+      "initializer_range": 0.02,
+      "intermediate_size": 14336,
+      "max_position_embeddings": 128000,
+      "model_type": "mistral",
+      "num_attention_heads": 32,
+      "num_hidden_layers": 40,
+      "num_key_value_heads": 8,
+      "rms_norm_eps": 1e-05,
+      "rope_theta": 1000000.0,
+      "sliding_window": null,
+      "tie_word_embeddings": false,
+      "torch_dtype": "bfloat16",
+      "transformers_version": "4.43.3",
+      "use_cache": true,
+      "vocab_size": 131072
+    }
+    '''
+    
+    class MistralRotaryEmbedding(nn.Module):
+        def __init__(self, dim, max_position_embeddings=2048, base=10000.0, device=None):
+            super().__init__()
+    
+            self.dim = dim
+            self.max_position_embeddings = max_position_embeddings
+            self.base = base
+            inv_freq = 1.0 / (self.base ** (torch.arange(0, self.dim, 2, dtype=torch.int64).float().to(device) / self.dim))
+            self.register_buffer("inv_freq", inv_freq, persistent=False)
+    
+        @torch.no_grad()
+        # copied from transformers.models.llama.modeling_llama.LlamaRotaryEmbedding.forward
+        # TODO(joao): add me back asap :)
+        def forward(self, x, position_ids):
+            # x: [bs, num_attention_heads, seq_len, head_size]
+            inv_freq_expanded = self.inv_freq[None, :, None].float().expand(position_ids.shape[0], -1, 1)
+            position_ids_expanded = position_ids[:, None, :].float()
+            # Force float32 since bfloat16 loses precision on long contexts
+            # See https://github.com/huggingface/transformers/pull/29285
+            device_type = x.device.type
+            device_type = device_type if isinstance(device_type, str) and device_type != "mps" else "cpu"
+            with torch.autocast(device_type=device_type, enabled=False):
+                freqs = (inv_freq_expanded.float() @ position_ids_expanded.float()).transpose(1, 2)
+                emb = torch.cat((freqs, freqs), dim=-1)
+                cos = emb.cos()
+                sin = emb.sin()
+            return cos.to(dtype=x.dtype), sin.to(dtype=x.dtype)
+    
+    
+    def rotate_half(x):
+        """Rotates half the hidden dims of the input."""
+        x1 = x[..., : x.shape[-1] // 2]
+        x2 = x[..., x.shape[-1] // 2 :]
+        return torch.cat((-x2, x1), dim=-1)
+    
+    def apply_rotary_pos_emb(q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
+        """Applies Rotary Position Embedding to the query and key tensors.
+    
+        Args:
+            q (`torch.Tensor`): The query tensor.
+            k (`torch.Tensor`): The key tensor.
+            cos (`torch.Tensor`): The cosine part of the rotary embedding.
+            sin (`torch.Tensor`): The sine part of the rotary embedding.
+            position_ids (`torch.Tensor`, *optional*):
+                Deprecated and unused.
+            unsqueeze_dim (`int`, *optional*, defaults to 1):
+                The 'unsqueeze_dim' argument specifies the dimension along which to unsqueeze cos[position_ids] and
+                sin[position_ids] so that they can be properly broadcasted to the dimensions of q and k. For example, note
+                that cos[position_ids] and sin[position_ids] have the shape [batch_size, seq_len, head_dim]. Then, if q and
+                k have the shape [batch_size, heads, seq_len, head_dim], then setting unsqueeze_dim=1 makes
+                cos[position_ids] and sin[position_ids] broadcastable to the shapes of q and k. Similarly, if q and k have
+                the shape [batch_size, seq_len, heads, head_dim], then set unsqueeze_dim=2.
+        Returns:
+            `tuple(torch.Tensor)` comprising of the query and key tensors rotated using the Rotary Position Embedding.
+        """
+        cos = cos.unsqueeze(unsqueeze_dim)
+        sin = sin.unsqueeze(unsqueeze_dim)
+        q_embed = (q * cos) + (rotate_half(q) * sin)
+        k_embed = (k * cos) + (rotate_half(k) * sin)
+        return q_embed, k_embed
+    
+    def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
+        """
+        This is the equivalent of torch.repeat_interleave(x, dim=1, repeats=n_rep). The hidden states go from (batch,
+        num_key_value_heads, seqlen, head_dim) to (batch, num_attention_heads, seqlen, head_dim)
+        """
+        batch, num_key_value_heads, slen, head_dim = hidden_states.shape
+        if n_rep == 1:
+            return hidden_states
+        hidden_states = hidden_states[:, :, None, :, :].expand(batch, num_key_value_heads, n_rep, slen, head_dim)
+        return hidden_states.reshape(batch, num_key_value_heads * n_rep, slen, head_dim)
+    
+    class MistralNemoRope(nn.Module):
+        def __init__(self, config: MistralConfig):
+            super().__init__()
+            self.config = config
+    
+            self.attention_dropout = config.attention_dropout
+            self.hidden_size = config.hidden_size
+            self.num_heads = config.num_attention_heads
+            self.head_dim = config.head_dim
+            self.num_key_value_heads = config.num_key_value_heads
+            self.num_key_value_groups = self.num_heads // self.num_key_value_heads
+            self.max_position_embeddings = config.max_position_embeddings
+            self.rope_theta = config.rope_theta
+            self.is_causal = True
+    
+            self.rotary_emb = MistralRotaryEmbedding(
+                self.head_dim,
+                max_position_embeddings=self.max_position_embeddings,
+                base=self.rope_theta,
+            )
+    
+        def forward(
+            self,
+            query_in_states: torch.Tensor,
+            key_in_states: torch.Tensor,
+            value_in_states: torch.Tensor,
+            position_ids: torch.Tensor
+        ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+            past_key_value = None 
+            bsz, q_len, _ = query_in_states.size()
+    
+            query_states = query_in_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+            key_states = key_in_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+            value_states = value_in_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+    
+            cos, sin = self.rotary_emb(value_states, position_ids)
+            query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
+    
+            if past_key_value is not None:
+                assert False
+    
+            key_states = repeat_kv(key_states, self.num_key_value_groups)
+            value_states = repeat_kv(value_states, self.num_key_value_groups)
+            return query_states, key_states, value_states
+    
+    cfg = MistralConfig.from_dict(json.loads(mistral_cfg_str))
+    cfg.batch_size = 1
+    cfg.seq_len = 4096
+    
+    head_dim = cfg.hidden_size // cfg.num_attention_heads
+    def inputs():
+        q = torch.randn(cfg.batch_size, cfg.seq_len, cfg.num_attention_heads * cfg.head_dim, device='cuda', dtype=torch.bfloat16, requires_grad=True)
+        k = torch.randn(cfg.batch_size, cfg.seq_len, cfg.num_key_value_heads * cfg.head_dim, device='cuda', dtype=torch.bfloat16, requires_grad=True)
+        v = torch.randn(cfg.batch_size, cfg.seq_len, cfg.num_key_value_heads * cfg.head_dim, device='cuda', dtype=torch.bfloat16, requires_grad=True)
+        position_ids = torch.arange(0, cfg.seq_len, device='cuda').unsqueeze(0)
+        return q, k, v, position_ids
+    def grads():
+        grad = torch.randn(cfg.batch_size, cfg.num_attention_heads, cfg.seq_len, cfg.head_dim, device='cuda', dtype=torch.bfloat16, requires_grad=False)
+        return grad
+    
+    return MistralNemoRope(cfg).cuda().bfloat16(), inputs, grads
+
+
 
 
 # { 'name_benchmark' : (fn, [[sizes0, optional_strides0, dtype0], [sizes1, dtype1], ...]) }
@@ -710,8 +877,8 @@ rope_setup = {
         hf_qwen2_rope,
     "hf_phi3_rope":
         hf_phi3_rope,
-    #"hf_mistral_nemo_rope":
-    #    hf_mistral_nemo_rope,
+    "hf_mistral_nemo_rope":
+        hf_mistral_nemo_rope,
 }
 
 
@@ -722,7 +889,7 @@ rope_setup = {
         "llama_3_8B_rope",
         "hf_qwen2_rope",
         "hf_phi3_rope",
-        #"hf_mistral_nemo_rope",
+        "hf_mistral_nemo_rope",
     ],
 )
 @pytest.mark.parametrize("executor", ["eager", "torchcompile", "thunder"])
@@ -750,7 +917,7 @@ def test_rope_variations_fwd_benchmark(
         "llama_3_8B_rope",
         "hf_qwen2_rope",
         "hf_phi3_rope",
-        #"hf_mistral_nemo_rope",
+        "hf_mistral_nemo_rope",
     ],
 )
 @pytest.mark.parametrize("executor", ["eager", "torchcompile", "thunder"])
