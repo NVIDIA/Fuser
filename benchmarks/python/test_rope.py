fixing bwd io inputs computation

benchmarks/python/test_rope.py
@@ -310,13 +310,13 @@ def llama_hf_rope(config_str):
         )
         return grad
 
-    # Manual IOBytes computes the theoretical total bandwidth for bwd kernel.
+    # Manual IOBytes computes the total bandwidth for thunder backward trace.
     def iobytes():
         n_elements = 0
         # adding size of q.grad + k.grad
         n_elements += 2 * cfg.batches * cfg.n_head * cfg.seq_length * cfg.head_size
         # adding size of cos, sin
-        n_elements += 2 * cfg.seq_length * rope_n_elem
+        n_elements += 2 * cfg.seq_length * cfg.rope_n_elem
         # adding size of qkv.grad
         n_elements += cfg.batches * cfg.seq_length * cfg.head_size * (cfg.n_head + 2 * cfg.n_query_groups)
         # scale by dtype size
@@ -518,11 +518,13 @@ def hf_qwen2_rope():
         )
         return grad
 
-    # Manual IOBytes computes the theoretical total bandwidth for bwd kernel.
+    # Manual IOBytes computes the total bandwidth for thunder backward trace.
     def iobytes():
         n_elements = 0
-        # adding size of query_states.grad + key_states.grad +  value_states.grad
+        # adding size of query_states.grad + key_states.grad + value_states.grad
         n_elements += 3 * cfg.batch_size * cfg.num_attention_heads * cfg.seq_len * head_dim
+        # adding size of query_states + key_states
+        n_elements += 2 * cfg.batch_size * cfg.num_attention_heads * cfg.seq_len * head_dim
         # adding size of cos, sin
         n_elements += 2 * cfg.batch_size * cfg.seq_len * head_dim
         # adding size of q.grad
@@ -872,15 +874,17 @@ def hf_phi3_rope():
         )
         return grad
 
-    # Manual IOBytes computes the theoretical total bandwidth for bwd kernel.
+    # Manual IOBytes computes the total bandwidth for thunder backward trace.
     def iobytes():
         n_elements = 0
         # adding size of query_states.grad + key_states.grad +  value_states.grad
         n_elements += 3 * cfg.batch_size * cfg.num_attention_heads * cfg.seq_len * head_dim
         # adding size of qkv.grad
         n_elements += cfg.batch_size * cfg.seq_len * (cfg.num_attention_heads * head_dim + 2 * (cfg.num_key_value_heads * head_dim))
-        # scale by dtype size and adding positional_ids
-        return n_elements * torch.bfloat16.itemsize + cfg.seq_len * torch.int
+        # matmul output size
+        n_elements_matmul_out = head_dim / 2 * cfg.seq_len
+        # totoal io sizes
+        return n_elements * torch.bfloat16.itemsize + n_elements_matmul_out * torch.float32.itemsize
 
     return HfPhi3Rope(cfg).cuda().bfloat16(), inputs, grads, iobytes
 
@@ -1108,7 +1112,7 @@ def hf_mistral_nemo_rope():
         )
         return grad
 
-    # Manual IOBytes computes the theoretical total bandwidth for bwd kernel.
+    # Manual IOBytes computes the total bandwidth for thunder backward trace.
     def iobytes():
         n_elements = 0
         # adding size of query_states.grad + key_states.grad +  value_states.grad
@@ -1117,8 +1121,10 @@ def hf_mistral_nemo_rope():
         n_elements += cfg.batch_size * cfg.seq_len * cfg.num_attention_heads * cfg.head_dim
         # adding size of k.grad, v.grad
         n_elements += 2 *cfg.batch_size * cfg.seq_len * cfg.num_key_value_heads * cfg.head_dim
-        # scale by dtype size
-        return n_elements * torch.bfloat16.itemsize + cfg.seq_len * torch.int
+        # matmul output size
+        n_elements_matmul_out = head_dim / 2 * cfg.seq_len
+        # totoal io sizes
+        return n_elements * torch.bfloat16.itemsize + n_elements_matmul_out * torch.float32.itemsize
 
     return MistralNemoRope(cfg).cuda().bfloat16(), inputs, grads, iobytes
 