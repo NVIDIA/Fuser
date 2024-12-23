83b51bc74b3c3907a990b718f00409d8e1191118
Nicholas Sarkauskas <nsarkauskas@PDX01-M03-I40-DGX-A100-DL-2.nvidia.com>
2024-12-23 15:21:19 -0800

    add at_inputs, compiling

--- benchmarks/cpp/transformer.cpp
+++ benchmarks/cpp/transformer.cpp
@@ -22,6 +22,7 @@
 #include <sstream>
 
 #include <benchmarks/cpp/utils.h>
+#include <csrc/multidevice/utils.h>
 #include <tests/cpp/utils.h>
 #include <tests/cpp/multidevice_transformer.h>
 
@@ -53,6 +54,61 @@ static void setupTransformerForward(Fusion* fusion, DataType dtype) {
   model->setupForward(fusion, dtype, /*sequence_parallel*/false);
 }
 
+static std::vector<at::Tensor> reference_mlp(
+    at::Tensor x,
+    at::Tensor w0,
+    at::Tensor b0,
+    at::Tensor w1,
+    at::Tensor b1) {
+  auto at_dtype = w0.dtype();
+  auto linear0 = at::linear(x, w0, b0);
+  auto gelu = at::gelu(linear0.to(at::kFloat), "tanh").to(at_dtype);
+  auto linear1 = at::linear(gelu, w1, b1).to(at::kFloat);
+  auto [dropout, mask] = at::native_dropout(linear1, kDropoutProb, true);
+  return {linear0, gelu, linear1, dropout, mask};
+}
+
+static std::vector<at::Tensor> reference_mha(
+    at::Tensor x,
+    at::Tensor w0,
+    at::Tensor b0,
+    at::Tensor w1,
+    at::Tensor b1) {
+  auto linear0 = at::linear(x, w0, b0);
+  auto qkv = linear0.view({B, S, 3 * E}).split(E, 2);
+  for (auto i = 0; i < 3; i++) {
+    qkv[i] = qkv[i].reshape({B, S, H, E / H}).transpose(1, 2);
+  }
+  auto sdpa_out = at::_scaled_dot_product_flash_attention(
+      qkv[0], qkv[1], qkv[2], kSdpaProb, true, false, kSdpaScale);
+  auto sdpa = std::get<0>(sdpa_out);
+  // Reassemble heads (B, H, S, E/H) to (B, S, H, E/H) to (B, S, E)
+  auto y = sdpa.transpose(1, 2).reshape({B * S, E});
+  auto linear1 = at::linear(y, w1, b1).to(at::kFloat);
+  auto [dropout, mask] = at::native_dropout(linear1, kDropoutProb, true);
+  return {linear0, sdpa, linear1, dropout, mask};
+}
+
+static at::Tensor transformerShardTensor_Mesh(
+    at::Tensor tensor,
+    const int64_t axis,
+    const DeviceMesh& mesh,
+    Communicator* communicator_) {
+  const auto device_id = communicator_->deviceId();
+  return nvfuser::shardTensor(tensor, axis, mesh, device_id);
+}
+
+static at::Tensor transformerShardTensor(at::Tensor tensor, TensorView* tv, Communicator* communicator_) {
+  if (!isSharded(tv)) {
+    return tensor;
+  }
+  NVF_ERROR(tv->hasDeviceMesh(), "`tv` has no DeviceMesh: ", tv);
+  return transformerShardTensor_Mesh(
+      tensor,
+      getShardedLogicalAxis(tv, ParallelType::DIDx),
+      tv->getDeviceMesh(), communicator_);
+}
+
 static void NvFuserScheduler_TransformerFwd(
     benchmark::State& benchmark_state,
     FusionExecutorCache* executor_cache,
@@ -76,6 +132,67 @@ static void NvFuserScheduler_TransformerFwd(
 
   benchmark_state.SetBytesProcessed(
       bytes * int64_t(benchmark_state.iterations()));*/
+
+  Communicator* communicator_ = &Communicator::getInstance(); // nick TODO call Communicator::getInstance().cleanup() somewhere before program exit
+  const int64_t D = communicator_->size(); // number of devices
+
+  at::ScalarType at_dtype = data_type_to_aten(dtype);
+  const auto mesh = DeviceMesh::createForNumDevices(D);
+  constexpr float kEps = 1e-5;
+  std::vector<int64_t> norm_shape{E};
+
+  const auto options =
+      at::TensorOptions().dtype(at_dtype).device(communicator_->device());
+  auto x_ = at::randn({B * S, E}, options);
+  auto ln0_w_ = at::randn(E, options).to(at::kFloat);
+  auto ln0_b_ = at::randn(E, options).to(at::kFloat);
+  auto mha_w0_ = at::randn({3 * E, E}, options) * kParamScale;
+  auto mha_b0_ = at::randn({3 * E}, options) * kParamScale;
+  auto mha_w1_ = at::randn({E, E}, options) * kParamScale;
+  auto mha_b1_ = at::randn({E}, options) * kParamScale;
+  auto ln1_w_ = at::randn(E, options).to(at::kFloat);
+  auto ln1_b_ = at::randn(E, options).to(at::kFloat);
+  auto mlp_w0_ = at::randn({4 * E, E}, options) * kParamScale;
+  auto mlp_b0_ = at::randn({4 * E}, options) * kParamScale;
+  auto mlp_w1_ = at::randn({E, 4 * E}, options) * kParamScale;
+  auto mlp_b1_ = at::randn({E}, options) * kParamScale;
+
+  at::manual_seed(getATenRandomSeed());
+  auto x_float_ = x_.to(at::kFloat);
+  auto ln0_ = at::native_layer_norm(x_float_, norm_shape, ln0_w_, ln0_b_, kEps);
+  auto ln0_out_ = std::get<0>(ln0_);
+
+  auto mha_out_ = reference_mha(
+      ln0_out_.to(at_dtype), mha_w0_, mha_b0_, mha_w1_, mha_b1_)[3];
+
+  auto resid0_ = mha_out_ + x_float_;
+  auto ln1_ = at::native_layer_norm(resid0_, norm_shape, ln1_w_, ln1_b_, kEps);
+  auto ln1_out_ = std::get<0>(ln1_);
+
+  auto mlp_out_ = reference_mlp(
+      ln1_out_.to(at_dtype), mlp_w0_, mlp_b0_, mlp_w1_, mlp_b1_)[3];
+  auto at_out = (resid0_ + mlp_out_).to(at_dtype);
+
+  std::vector<c10::IValue> at_inputs = {
+      x_,
+      ln0_w_,
+      ln0_b_,
+      transformerShardTensor_Mesh(mha_w0_.view({3, E, E}), 1, mesh, communicator_).view({1, 3 * E / D, E}),
+      transformerShardTensor_Mesh(mha_b0_.view({3, E}), 1, mesh, communicator_).view({1, 3 * E / D}),
+      transformerShardTensor_Mesh(mha_w1_, 1, mesh, communicator_).unsqueeze(0),
+      mha_b1_,
+      ln1_w_,
+      ln1_b_,
+      transformerShardTensor_Mesh(mlp_w0_, 0, mesh, communicator_).unsqueeze(0),
+      transformerShardTensor_Mesh(mlp_b0_, 0, mesh, communicator_).unsqueeze(0),
+      transformerShardTensor_Mesh(mlp_w1_, 1, mesh, communicator_).unsqueeze(0),
+      mlp_b1_};
+
+  auto bytes =
+      runBenchmarkIterations(benchmark_state, executor_cache, at_inputs);
+
+  benchmark_state.SetBytesProcessed(
+      bytes * int64_t(benchmark_state.iterations()));
 }
 
 //------------------------------------------------------------------------------
