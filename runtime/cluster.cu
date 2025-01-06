96ab0b0a2d422a12f3cd53a36a6e10ec608f2d9d
Ryan Spring <rspring@nvidia.com>
2025-01-06 12:44:53 -0800

    lint

--- runtime/cluster.cu
+++ runtime/cluster.cu
@@ -15,19 +15,19 @@ namespace Hopper {
 // there are no memory ordering and visibility guarantees provided for the
 // memory accesses performed prior to barrier.cluster.arrive.
 void cluster_arrive_relaxed() {
-  asm volatile("barrier.cluster.arrive.relaxed.aligned;\n" : : );
+  asm volatile("barrier.cluster.arrive.relaxed.aligned;\n" : :);
 }
 
 // A thread arrives at barrier but it does not have to wait for threads in other
 // participating warps.
 void cluster_arrive() {
-  asm volatile("barrier.cluster.arrive.aligned;\n" : : );
+  asm volatile("barrier.cluster.arrive.aligned;\n" : :);
 }
 
 // A thread waits for all non-exited threads of the cluster to perform
 // cluster_arrive.
 void cluster_wait() {
-  asm volatile("barrier.cluster.wait.aligned;\n" : : );
+  asm volatile("barrier.cluster.wait.aligned;\n" : :);
 }
 
 // Synchronize threads in cluster
@@ -39,36 +39,36 @@ void cluster_sync() {
 // Returns the dim3 grid size in terms of number of clusters.
 dim3 cluster_grid_dims() {
   uint32_t x, y, z;
-  asm volatile("mov.u32 %0, %%nclusterid.x;\n" : "=r"(x) : );
-  asm volatile("mov.u32 %0, %%nclusterid.y;\n" : "=r"(y) : );
-  asm volatile("mov.u32 %0, %%nclusterid.z;\n" : "=r"(z) : );
+  asm volatile("mov.u32 %0, %%nclusterid.x;\n" : "=r"(x) :);
+  asm volatile("mov.u32 %0, %%nclusterid.y;\n" : "=r"(y) :);
+  asm volatile("mov.u32 %0, %%nclusterid.z;\n" : "=r"(z) :);
   return {x, y, z};
 }
 
 // Returns the dim3 cluster rank in the grid.
 dim3 cluster_id_in_grid() {
   uint32_t x, y, z;
-  asm volatile("mov.u32 %0, %%clusterid.x;\n" : "=r"(x) : );
-  asm volatile("mov.u32 %0, %%clusterid.y;\n" : "=r"(y) : );
-  asm volatile("mov.u32 %0, %%clusterid.z;\n" : "=r"(z) : );
+  asm volatile("mov.u32 %0, %%clusterid.x;\n" : "=r"(x) :);
+  asm volatile("mov.u32 %0, %%clusterid.y;\n" : "=r"(y) :);
+  asm volatile("mov.u32 %0, %%clusterid.z;\n" : "=r"(z) :);
   return {x, y, z};
 }
 
 // Returns the relative dim3 block rank local to the cluster.
 dim3 block_id_in_cluster() {
   uint32_t x, y, z;
-  asm volatile("mov.u32 %0, %%cluster_ctaid.x;\n" : "=r"(x) : );
-  asm volatile("mov.u32 %0, %%cluster_ctaid.y;\n" : "=r"(y) : );
-  asm volatile("mov.u32 %0, %%cluster_ctaid.z;\n" : "=r"(z) : );
+  asm volatile("mov.u32 %0, %%cluster_ctaid.x;\n" : "=r"(x) :);
+  asm volatile("mov.u32 %0, %%cluster_ctaid.y;\n" : "=r"(y) :);
+  asm volatile("mov.u32 %0, %%cluster_ctaid.z;\n" : "=r"(z) :);
   return {x, y, z};
 }
 
 // Returns the dim3 cluster shape.
 dim3 cluster_shape() {
   uint32_t x, y, z;
-  asm volatile("mov.u32 %0, %%cluster_nctaid.x;\n" : "=r"(x) : );
-  asm volatile("mov.u32 %0, %%cluster_nctaid.y;\n" : "=r"(y) : );
-  asm volatile("mov.u32 %0, %%cluster_nctaid.z;\n" : "=r"(z) : );
+  asm volatile("mov.u32 %0, %%cluster_nctaid.x;\n" : "=r"(x) :);
+  asm volatile("mov.u32 %0, %%cluster_nctaid.y;\n" : "=r"(y) :);
+  asm volatile("mov.u32 %0, %%cluster_nctaid.z;\n" : "=r"(z) :);
   return {x, y, z};
 }
 
@@ -83,8 +83,8 @@ uint32_t block_rank_in_cluster() {
 uint32_t map_shared_rank(uint32_t smemAddr, uint32_t rank) {
   uint32_t result;
   asm volatile("mapa.shared::cluster.u32  %0, %1, %2;\n"
-              : "=r"(result)
-              : "r"(smemAddr), "r"(rank));
+               : "=r"(result)
+               : "r"(smemAddr), "r"(rank));
   return result;
 }
 
