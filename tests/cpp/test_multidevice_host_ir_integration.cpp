72162c37115f7a011ff776ec907098fc142bb521
Nicholas Sarkauskas <nsarkauskas@PDX01-M03-I40-DGX-A100-DL-1.nvidia.com>
2025-01-10 13:39:20 -0800

    add file for integration gtest

---
+++ tests/cpp/test_multidevice_host_ir_integration.cpp
@@ -0,0 +1,46 @@
+// clang-format off
+/*
+* SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
+* All rights reserved.
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+// clang-format on
+#include <fusion.h>
+#include <host_ir/container.h>
+#include <host_ir/executor.h>
+#include <ir/all_nodes.h>
+#include <ops/all_ops.h>
+#include <tests/cpp/multidevice.h>
+
+namespace nvfuser {
+
+namespace hir {
+
+using MultiDeviceHostIrIntegrationTestParams = std::tuple<bool, bool>;
+
+class MultiDeviceHostIrIntegrationTest
+    : public MultiDeviceTest,
+      public testing::WithParamInterface<MultiDeviceHostIrIntegrationTestParams> {};
+
+TEST_P(MultiDeviceHostIrIntegrationTest, test_kernel) {
+  //auto [use_fusion_executor_cache, with_sharding_annotations] = GetParam();
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    Manual,
+    MultiDeviceHostIrIntegrationTest,
+    testing::Combine(testing::Bool(), testing::Bool()),
+    [](const testing::TestParamInfo<MultiDeviceHostIrTestParams>& info)
+        -> std::string {
+      std::string s;
+      s += std::get<0>(info.param) ? "useFusionExecutorCache"
+                                   : "useFusionExecutor";
+      s += "_";
+      s += std::get<1>(info.param) ? "withShardingAnnotations"
+                                   : "withoutShardingAnnotations";
+      return s;
+    });
+
+} // namespace hir
+
+} // namespace nvfuser
