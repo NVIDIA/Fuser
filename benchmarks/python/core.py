dd302c957b0caa22f55e3f070c7d79b484a531a3
root <26priya11@gmail.com>
2025-01-15 19:08:31 -0800

    default exec variable

--- benchmarks/python/core.py
+++ benchmarks/python/core.py
@@ -22,6 +22,7 @@ BENCHMARK_CONFIG = {"rounds": 10, "warmup_rounds": 1, "num_inputs": None}
 L2_CACHE_SIZE = DEVICE_PROPERTIES["gpu_l2_bytes"]
 PEAK_BANDWIDTH_GBPS = DEVICE_PROPERTIES["gpu_peak_bandwidth_gbps"]
 
+DEFAULT_EXECUTORS = ['eager', 'torchcompile', 'thunder']
 
 def clear_l2_cache() -> None:
     """
