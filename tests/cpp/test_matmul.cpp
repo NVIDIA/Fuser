d3011a1d1944a5045cb90323c5eb0270443da6aa
Ryan Spring <rspring@nvidia.com>
2025-01-17 10:23:39 -0800

    update nt config

--- tests/cpp/test_matmul.cpp
+++ tests/cpp/test_matmul.cpp
@@ -4027,8 +4027,8 @@ TEST_F(HopperMatmulTest, HSH_NT_UseScheduler) {
   auto out_ref = at::matmul(a_ref.squeeze().t(), b_ref.squeeze()).to(at::kHalf);
 
   MatMulTileOptions gemm_tile;
-  gemm_tile.cta_tile = GemmTile(128, 256, 16);
-  gemm_tile.warp_tile = GemmTile(64, 256, 16);
+  gemm_tile.cta_tile = GemmTile(128, 256, 64);
+  gemm_tile.warp_tile = GemmTile(64, 256, 64);
 
   MatmulParams mparams;
   mparams.supported_vec_size = {8, 8, 8};
