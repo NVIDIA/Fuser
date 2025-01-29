5cfad143d80ed55ab5d7c82b33fe4f59e586fd3d
Christian Sarofeen <csarofeen@nvidia.com>
2025-01-29 13:48:23 -0800

    Forgot license.

--- csrc/ir/evaluate.cpp
+++ csrc/ir/evaluate.cpp
@@ -1,3 +1,11 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+
 #include <expr_evaluator.h>
 #include <instrumentation.h>
 #include <ir/all_nodes.h>
