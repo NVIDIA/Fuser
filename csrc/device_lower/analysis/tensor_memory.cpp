30b14b36065394c04fca9c389392eeb3cfe277aa
Xiang Gao <qasdfgtyuiop@gmail.com>
2025-01-30 12:05:10 -0800

    comment

--- csrc/device_lower/analysis/tensor_memory.cpp
+++ csrc/device_lower/analysis/tensor_memory.cpp
@@ -54,8 +54,9 @@ TensorMemoryInfo computeTMemInfo(Fusion* fusion) {
     }
   }
 
-  // Compute the allocation information for tensor memory. That is, for each
-  // partition, we create a Region object and fill in the necessary information.
+  // Step 2: Compute the allocation information for tensor memory. That is, for
+  // each partition, we create a Region object and fill in the necessary
+  // information.
   auto& regions = result.allocation.regions;
   for (const auto& partition : partitions) {
     regions.emplace_back();
