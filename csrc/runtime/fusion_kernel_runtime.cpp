e99b427cbe75c9e6aa3389827b93078f487c234e
Nicholas Sarkauskas <nsarkauskas@SJC-DC2-DGX1-1.nvidia.com>
2025-01-31 13:43:32 -0800

    update

--- csrc/runtime/fusion_kernel_runtime.cpp
+++ csrc/runtime/fusion_kernel_runtime.cpp
@@ -427,19 +427,17 @@ void FusionKernelRuntime::compileFusionParallel(KernelArgumentHolder args) {
     for (int64_t run_order_id = 0; run_order_id < num_groups; ++run_order_id) {
       auto group_to_run = runtime_workspace_.group_run_order.at(run_order_id);
       auto fusion_to_run = segmented_fusion_->makeFusion(group_to_run).second;
-      auto in_clone = ir_cloner.clone(group_to_run->inputs());
-      auto out_clone = ir_cloner.clone(group_to_run->outputs());
+      auto in_clone = ir_cloner.clone(fusion_to_run->inputs());
+      auto out_clone = ir_cloner.clone(fusion_to_run->outputs());
       if (hic->hasKernelExecutor(run_order_id)) {
         auto launch_kernel = IrBuilder::create<nvfuser::hir::LaunchKernel>(
             run_order_id, std::vector<Val*>{in_clone}, std::vector<Val*>{out_clone});
         hic->pushBackTopLevelExprs(launch_kernel);
-        std::cout << "pushed back launch kernel" << std::endl;
       } else {
         // push back segment's exprs into the container as top level expressions
         for (auto *expr : fusion_to_run->exprs()) {
           auto cloned_expr = ir_cloner.clone(expr);
           hic->pushBackTopLevelExprs(cloned_expr);
-          std::cout << "pushed back some other expr" << std::endl;
         }
       }
       for (auto &input : in_clone) {
@@ -733,8 +731,6 @@ void FusionKernelRuntime::compileKernel(
   auto group_id = sg->groupId();
   auto heuristic_params = schedulers().at(group_id).get();
 
-  std::cout << "in compileKernel" << std::endl;
-
   // Check that the heuristics are matched, in the case of segmented fusion
   NVF_ERROR(!sg || heuristic_params->scheduler_type == sg->schedulerType());
 
@@ -763,10 +759,6 @@ void FusionKernelRuntime::compileKernel(
       auto ke = std::make_unique<KernelExecutor>();
       ke->compile(fusion_to_run.get(), args, heuristic_params->lparams, heuristic_params->cparams, heuristic_params->scheduler_type);
       hic->setKernelExecutor(group_id, std::move(ke));
-      std::cout << "setting index " << group_id << " to compiled kernel" << std::endl;
-    } else {
-      std::cout << "kernel executor not supported" << std::endl;
-      //nick
     }
   } else {
     // Initialize associated executors
