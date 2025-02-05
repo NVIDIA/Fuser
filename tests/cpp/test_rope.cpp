1b8fc0fc0825f9540904d1b096367afd23b47ebc
Naoya Maruyama <nmaruyama@nvidia.com>
2025-02-04 20:08:16 -0800

    renable resize sched

--- tests/cpp/test_rope.cpp
+++ tests/cpp/test_rope.cpp
@@ -49,7 +49,7 @@ struct RopeConfig {
 class RopeTest : public NVFuserFixtureParamTest<RopeConfig> {
  protected:
   void SetUp() override {
-    // EnableOptionsGuard::getCurOptions().set(EnableOption::ResizeScheduler);
+    EnableOptionsGuard::getCurOptions().set(EnableOption::ResizeScheduler);
     NVFuserTest::SetUp();
   }
 };
