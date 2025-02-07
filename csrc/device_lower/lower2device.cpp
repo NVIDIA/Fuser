ee715075b1e1f7d2ca7d83f24e6460848eafaf32
Naoya Maruyama <nmaruyama@nvidia.com>
2025-02-06 18:05:16 -0800

    Always build an IdModel during lowreing

--- csrc/device_lower/lower2device.cpp
+++ csrc/device_lower/lower2device.cpp
@@ -491,15 +491,12 @@ void GpuLower::analysis(Fusion* fusion) {
   // functionality should be affected. New IterDomains may be created,
   // so it is expected that generated code may use diffrent variable
   // names
-  if (idModelOptions().buildIdModel()) {
-    // Enable validation in the DEBUG build mode
-    id_model_ = std::make_unique<IdModel>(
-        fusion_,
-        /*build_graphs=*/true,
-        /*allow_self_mapping=*/false,
-        /*validate=*/false);
-    id_model_->validateAndPropagatePType();
-  }
+  id_model_ = std::make_unique<IdModel>(
+      fusion_,
+      /*build_graphs=*/true,
+      /*allow_self_mapping=*/false,
+      /*validate=*/false);
+  id_model_->validateAndPropagatePType();
 
   resolveComputeWith(fusion_);
   dumpExprsIfEnabled(fusion_->exprs(), "resolveComputeWith");
