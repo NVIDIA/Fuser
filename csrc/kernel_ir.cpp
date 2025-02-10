ad53cbaef348e682bb892a338a955ca4979e0bb6
Gao, Xiang <qasdfgtyuiop@gmail.com>
2025-02-10 01:36:11 -0800

    fix

--- csrc/kernel_ir.cpp
+++ csrc/kernel_ir.cpp
@@ -416,25 +416,21 @@ std::string Asm::toInlineString(int indent_size) const {
 }
 
 const std::string Asm::utility() const {
-  static const std::unordered_map<
-      std::string,
-      std::pair<std::string, std::unordered_set<int64_t>>>
-      ptx_to_utility{
-          {"tcgen05.wait::ld.sync.aligned", "waitTMemLoad"},
-          {"tcgen05.wait::st.sync.aligned", "waitTMemStore"},
-          {"tcgen05.ld.sync.aligned.32x32b.x1.b32", "loadTMem"},
-          {"tcgen05.st.sync.aligned.32x32b.x1.b32", "storeTMem"},
-          {"tcgen05.alloc.cta_group::1.sync.aligned.shared::cta.b32",
-           "allocTMem"},
-          {"tcgen05.relinquish_alloc_permit.cta_group::1.sync.aligned",
-           "relinquishTMemAllocPermit"},
-          {"wgmma.fence.sync.aligned", "wgmmaFence"},
-          {"fence.proxy.async", "fenceAsyncProxy"},
-          {"wgmma.commit_group.sync.aligned", "wgmmaCommit"},
-          {"wgmma.wait_group.sync.aligned", "wgmmaWait"},
-          {"stmatrix.sync.aligned.x1.m8n8.shared.b16", "stmatrix1"},
-          {"stmatrix.sync.aligned.x2.m8n8.shared.b16", "stmatrix2"},
-          {"stmatrix.sync.aligned.x4.m8n8.shared.b16", "stmatrix4"}};
+  static const std::unordered_map<std::string, std::string> ptx_to_utility{
+      {"tcgen05.wait::ld.sync.aligned", "waitTMemLoad"},
+      {"tcgen05.wait::st.sync.aligned", "waitTMemStore"},
+      {"tcgen05.ld.sync.aligned.32x32b.x1.b32", "loadTMem"},
+      {"tcgen05.st.sync.aligned.32x32b.x1.b32", "storeTMem"},
+      {"tcgen05.alloc.cta_group::1.sync.aligned.shared::cta.b32", "allocTMem"},
+      {"tcgen05.relinquish_alloc_permit.cta_group::1.sync.aligned",
+       "relinquishTMemAllocPermit"},
+      {"wgmma.fence.sync.aligned", "wgmmaFence"},
+      {"fence.proxy.async", "fenceAsyncProxy"},
+      {"wgmma.commit_group.sync.aligned", "wgmmaCommit"},
+      {"wgmma.wait_group.sync.aligned", "wgmmaWait"},
+      {"stmatrix.sync.aligned.x1.m8n8.shared.b16", "stmatrix1"},
+      {"stmatrix.sync.aligned.x2.m8n8.shared.b16", "stmatrix2"},
+      {"stmatrix.sync.aligned.x4.m8n8.shared.b16", "stmatrix4"}};
   const std::string& code = this->code();
   auto it = ptx_to_utility.find(code);
   if (it != ptx_to_utility.end()) {
