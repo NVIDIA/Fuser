a1ee54f6a58730becf7034837217564ddec4eae2
Xiang Gao <qasdfgtyuiop@gmail.com>
2025-02-11 15:47:24 -0800

    tmem

---
+++ doc/dev/tmem.md
@@ -0,0 +1,1 @@
+../../tests/cpp/tutorial_tmem.cpp
---
+++ tests/cpp/tutorial_tmem.cpp
@@ -0,0 +1,13 @@
+
+/*<span style="display:none">
+
+Please ignore the lines around here.
+
+blabla
+
+<span style="display:none"> */ //\
+```cpp
+int main() {}
+
+//\
+```
