cc0cfec13f6430630a9c08782c0e63e7ac70471f
Naoya Maruyama <nmaruyama@nvidia.com>
2025-02-12 08:56:37 -0800

    clang-tidy

--- csrc/runtime/executor_kernel_arg.cpp
+++ csrc/runtime/executor_kernel_arg.cpp
@@ -110,7 +110,7 @@ void KernelArgumentHolder::pushTensorProxy(
 c10::ArrayRef<c10::IValue> KernelArgumentHolder::toArrayRef() const {
   std::vector<c10::IValue> ival_array;
   ival_array.reserve(arguments_.size());
-  for (auto arg : arguments_) {
+  for (const auto& arg : arguments_) {
     ival_array.push_back(PolymorphicValue_functions::toIValue(*arg));
   }
   return c10::ArrayRef<c10::IValue>(ival_array);
