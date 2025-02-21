9dc2db04999665219d0f714b0c56cba822debde1
Jacob Hinkle <jhinkle@nvidia.com>
2025-02-21 11:03:17 -0500

    Remove NVF_API

--- csrc/interval_analysis.h
+++ csrc/interval_analysis.h
@@ -44,7 +44,7 @@ namespace nvfuser {
 //! [0, 6] since we know that b must be a multiple of 2, so c must be 0, 2, 4,
 //! or 6 only. This kind of analysis is not provided by the simplistic
 //! propagation using a BoundedInt interval at each stage.
-struct NVF_API BoundedInt {
+struct BoundedInt {
   int64_t min = 0L;
   int64_t max = 0L;
 
@@ -58,77 +58,77 @@ struct NVF_API BoundedInt {
   //!   All numbers in this range are of the form 0b10101XXX
   //!     different_bits = 0b110
   //!     num_common_bits = 61
-  NVF_API int64_t countCommonHighBits() const;
+  int64_t countCommonHighBits() const;
 
-  NVF_API BoundedInt operator+(const BoundedInt& other) const;
-  NVF_API BoundedInt operator+(const int64_t other) const;
-  NVF_API BoundedInt operator-(const BoundedInt& other) const;
-  NVF_API BoundedInt operator-(const int64_t other) const;
-  NVF_API BoundedInt operator-() const;
-  NVF_API BoundedInt operator*(const BoundedInt& other) const;
-  NVF_API BoundedInt operator*(const int64_t other) const;
-  NVF_API BoundedInt operator/(const BoundedInt& other) const;
-  NVF_API BoundedInt operator/(const int64_t other) const;
-  NVF_API BoundedInt operator%(const BoundedInt& other) const;
-  NVF_API BoundedInt operator%(const int64_t other) const;
+  BoundedInt operator+(const BoundedInt& other) const;
+  BoundedInt operator+(const int64_t other) const;
+  BoundedInt operator-(const BoundedInt& other) const;
+  BoundedInt operator-(const int64_t other) const;
+  BoundedInt operator-() const;
+  BoundedInt operator*(const BoundedInt& other) const;
+  BoundedInt operator*(const int64_t other) const;
+  BoundedInt operator/(const BoundedInt& other) const;
+  BoundedInt operator/(const int64_t other) const;
+  BoundedInt operator%(const BoundedInt& other) const;
+  BoundedInt operator%(const int64_t other) const;
 
-  NVF_API BoundedInt operator^(const BoundedInt& other) const;
-  NVF_API BoundedInt operator&(const BoundedInt& other) const;
-  NVF_API BoundedInt operator|(const BoundedInt& other) const;
-  NVF_API BoundedInt operator~() const;
-  NVF_API BoundedInt operator>>(const BoundedInt& other) const;
-  NVF_API BoundedInt operator<<(const BoundedInt& other) const;
+  BoundedInt operator^(const BoundedInt& other) const;
+  BoundedInt operator&(const BoundedInt& other) const;
+  BoundedInt operator|(const BoundedInt& other) const;
+  BoundedInt operator~() const;
+  BoundedInt operator>>(const BoundedInt& other) const;
+  BoundedInt operator<<(const BoundedInt& other) const;
 
-  NVF_API bool operator==(const BoundedInt& other) const {
+  bool operator==(const BoundedInt& other) const {
     return min == other.min && max == other.max;
   }
-  NVF_API bool operator!=(const BoundedInt& other) const {
+  bool operator!=(const BoundedInt& other) const {
     return !(*this == other);
   }
 };
 
-NVF_API BoundedInt ceilDiv(const BoundedInt& numer, const BoundedInt& denom);
+BoundedInt ceilDiv(const BoundedInt& numer, const BoundedInt& denom);
 
-NVF_API std::ostream& operator<<(std::ostream& out, const BoundedInt& b);
+std::ostream& operator<<(std::ostream& out, const BoundedInt& b);
 
 //! This class traverses the expressions in a kir::Kernel and defines a
 //! BoundedInt for each integer scalar encountered. The range is determined by
 //! the scalar's definition along with the rules defined in BoundedInt.
-class NVF_API ScalarBoundsCalculator : kir::IrVisitor {
+class ScalarBoundsCalculator : kir::IrVisitor {
  public:
-  NVF_API ScalarBoundsCalculator(
+  ScalarBoundsCalculator(
       kir::Kernel* kernel,
       ExpressionEvaluator& expr_eval,
       const LaunchParams& launch_params);
 
-  NVF_API ~ScalarBoundsCalculator() override = default;
+  ~ScalarBoundsCalculator() override = default;
 
   //! Look at all casts (T)x where x is of type nvfuser_index_t, to ensure that
   //! these casts are safe i.e. that the bounds of x do not overflow those
   //! representable by T.
-  NVF_API bool castsFromIndexAreSafe() const;
+  bool castsFromIndexAreSafe() const;
 
   //! NamedScalar bounds are set using the launch_params_. For example
   //! `blockDim.x` is set to the [blockDim.x, blockDim.x] and `threadIdx.x` is
   //! set to [0, blockDim.x - 1].
-  NVF_API void setBoundsForNamedScalar(NamedScalar* scalar);
+  void setBoundsForNamedScalar(NamedScalar* scalar);
 
   using kir::IrVisitor::dispatch;
   //! These public methods are useful for processing an individual statement to
   //! get bounds of all its producers
-  NVF_API void dispatch(Statement* statement) final;
-  NVF_API void dispatch(Expr* expr) final;
-  NVF_API void dispatch(Val* val) final;
+  void dispatch(Statement* statement) final;
+  void dispatch(Expr* expr) final;
+  void dispatch(Val* val) final;
 
-  NVF_API void setBounds(Val* val, const BoundedInt& bounds);
-  NVF_API void setBounds(Val* val, int64_t min, int64_t max);
-  NVF_API void setAsUnbounded(Val* val);
+  void setBounds(Val* val, const BoundedInt& bounds);
+  void setBounds(Val* val, int64_t min, int64_t max);
+  void setAsUnbounded(Val* val);
 
   //! Non-recursive function to look up bounds if they have been recorded
   //! already. For NamedScalars, also look in parallel dimension map and bound
   //! if it has not already been bounded. Finally, try and evaluate constants.
   //! If all this fails, return nullopt.
-  NVF_API std::optional<BoundedInt> maybeGetBounds(Val* val);
+  std::optional<BoundedInt> maybeGetBounds(Val* val);
 
  private:
   //! Evaluate val using our ExpressionEvaluator
@@ -136,11 +136,11 @@ class NVF_API ScalarBoundsCalculator : kir::IrVisitor {
 
   using kir::IrVisitor::handle;
 
-  NVF_API void handle(ForLoop* loop) final;
-  NVF_API void handle(LoadStoreOp* lsop) final;
-  NVF_API void handle(UnaryOp* uop) final;
-  NVF_API void handle(BinaryOp* bop) final;
-  NVF_API void handle(TernaryOp* top) final;
+  void handle(ForLoop* loop) final;
+  void handle(LoadStoreOp* lsop) final;
+  void handle(UnaryOp* uop) final;
+  void handle(BinaryOp* bop) final;
+  void handle(TernaryOp* top) final;
 
  private:
   ExpressionEvaluator& expr_eval_;
