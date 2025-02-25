5f7653559932ac8fcd32b4766a965707308a151d
snordmann <snordmann@nvidia.com>
2025-02-25 01:21:00 -0800

    minor comments

--- tests/cpp/test_multidevice_ipc.cpp
+++ tests/cpp/test_multidevice_ipc.cpp
@@ -1,6 +1,6 @@
 // clang-format off
 /*
-* SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
+* SPDX-FileCopyrightText: Copyright (c) 2025-present NVIDIA CORPORATION & AFFILIATES.
 * All rights reserved.
 * SPDX-License-Identifier: BSD-3-Clause
 */
@@ -11,14 +11,10 @@
 #include <host_ir/container.h>
 #include <host_ir/executor.h>
 #include <ir/all_nodes.h>
-#include <multidevice/ipc_handle.h>
 #include <ops/all_ops.h>
 #include <tests/cpp/multidevice.h>
-#include <tests/cpp/multidevice_kernels.h>
 
 namespace nvfuser {
-
-#define CUDA_CALL(call) ASSERT_EQ((call), cudaSuccess)
 
 template <typename T>
 std::vector<uint8_t> toBytes(const T& data) {
@@ -32,126 +28,136 @@ const T& fromBytes(const std::vector<uint8_t>& bytes) {
   return *reinterpret_cast<const T*>(bytes.data());
 }
 
-class GpuCommTest : public MultiDeviceTest {};
+using IpcTest = MultiDeviceTest;
 
-TEST_F(GpuCommTest, IpcMemHandle) {
-  // Allocate GPU memory
-  constexpr size_t size = sizeof(int64_t);
+TEST_F(IpcTest, IpcMemHandle) {
+  // Allocate and setup GPU buffers
+  constexpr size_t kBufferSize = sizeof(int64_t);
   const int64_t num_devices = communicator_->size();
   const int64_t rank = communicator_->deviceId();
   void* d_ptr;
-  CUDA_CALL(cudaMalloc(&d_ptr, size));
-
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaMalloc(&d_ptr, kBufferSize));
   const int64_t value = rank;
-  CUDA_CALL(cudaMemcpy(d_ptr, &value, size, cudaMemcpyHostToDevice));
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaMemcpy(d_ptr, &value, kBufferSize, cudaMemcpyHostToDevice));
 
+  // Export Ipc Handle
   cudaIpcMemHandle_t ipc_handle;
-  CUDA_CALL(cudaIpcGetMemHandle(&ipc_handle, d_ptr));
-
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaIpcGetMemHandle(&ipc_handle, d_ptr));
+  // As a convenience, we use the TCP store to exchange out-of-band the IPC handle as raw data
   auto store = communicator_->getTcpStore();
   store->set("ipc_handle_" + std::to_string(rank), toBytes(ipc_handle));
+
+  // Wait for all ranks to finish exporting the IPC handle
   communicator_->barrier();
+
+  // Import Ipc Handle
   auto peer_ipc_handle = fromBytes<cudaIpcMemHandle_t>(
       store->get("ipc_handle_" + std::to_string((rank + 1) % num_devices)));
-
   void* peer_d_ptr;
-  CUDA_CALL(cudaIpcOpenMemHandle(
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaIpcOpenMemHandle(
       &peer_d_ptr, peer_ipc_handle, cudaIpcMemLazyEnablePeerAccess));
 
+  // Validate
   int64_t peer_value;
-  CUDA_CALL(cudaMemcpy(&peer_value, peer_d_ptr, size, cudaMemcpyDeviceToHost));
-
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaMemcpy(&peer_value, peer_d_ptr, kBufferSize, cudaMemcpyDeviceToHost));
   EXPECT_EQ((value + 1) % num_devices, peer_value);
 
   // Clean up
-  CUDA_CALL(cudaIpcCloseMemHandle(peer_d_ptr));
-  CUDA_CALL(cudaFree(d_ptr));
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaIpcCloseMemHandle(peer_d_ptr));
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaFree(d_ptr));
 }
 
-TEST_F(GpuCommTest, IpcMemHandlePtrArithmeticAtReceiver) {
-  // TLDR; We can do pointer arithmetic on the receiver side.
+TEST_F(IpcTest, IpcMemHandlePtrArithmeticAtReceiver) {
+  // TL;DR: We can do pointer arithmetic on the importer side. IOW, the pointer can be used as a regular pointer on the importer side.
 
-  // Allocate GPU memory
-  constexpr size_t size = 2 * sizeof(int64_t);
+  // Allocate GPU memory. Set up a buffer with two int values.
+  constexpr size_t kBufferSize = 2 * sizeof(int64_t);
   const int64_t num_devices = communicator_->size();
   const int64_t rank = communicator_->deviceId();
   const int64_t peer_rank = (rank + 1) % num_devices;
   void* d_ptr;
-  CUDA_CALL(cudaMalloc(&d_ptr, size));
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaMalloc(&d_ptr, kBufferSize));
 
+  // Set up the buffer
   std::vector<int64_t> values;
   values.push_back(2 * rank);
   values.push_back(2 * rank + 1);
-  CUDA_CALL(cudaMemcpy(d_ptr, values.data(), size, cudaMemcpyHostToDevice));
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaMemcpy(d_ptr, values.data(), kBufferSize, cudaMemcpyHostToDevice));
 
+  // Export Ipc Handle
   cudaIpcMemHandle_t ipc_handle;
-  CUDA_CALL(cudaIpcGetMemHandle(&ipc_handle, d_ptr));
-
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaIpcGetMemHandle(&ipc_handle, d_ptr));
   auto store = communicator_->getTcpStore();
   store->set("ipc_handle_" + std::to_string(rank), toBytes(ipc_handle));
+
+  // Wait for all ranks to finish exporting the IPC handle
   communicator_->barrier();
+
+  // Import Ipc Handle
   auto peer_ipc_handle = fromBytes<cudaIpcMemHandle_t>(
       store->get("ipc_handle_" + std::to_string(peer_rank)));
-
   int64_t* peer_d_ptr;
-  CUDA_CALL(cudaIpcOpenMemHandle(
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaIpcOpenMemHandle(
       (void**)&peer_d_ptr, peer_ipc_handle, cudaIpcMemLazyEnablePeerAccess));
 
+  // Validate, by reading the second value in the buffer (c.f. the "+1" offset)
   int64_t peer_value;
-  CUDA_CALL(cudaMemcpy(
-      &peer_value, peer_d_ptr + 1, size / 2, cudaMemcpyDeviceToHost));
-
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaMemcpy(
+      &peer_value, peer_d_ptr + 1, kBufferSize / 2, cudaMemcpyDeviceToHost));
   EXPECT_EQ(2 * peer_rank + 1, peer_value);
 
   // Clean up
-  CUDA_CALL(cudaIpcCloseMemHandle(peer_d_ptr));
-  CUDA_CALL(cudaFree(d_ptr));
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaIpcCloseMemHandle(peer_d_ptr));
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaFree(d_ptr));
 }
 
-TEST_F(GpuCommTest, IpcMemHandlePtrArithmeticAtSender) {
-  // TLDR; We CANNOT do pointer arithmetic on the sender side! The IPC handle
+TEST_F(IpcTest, IpcMemHandlePtrArithmeticAtSender) {
+  // TL;DR: We CANNOT do pointer arithmetic on the exporter side! The IPC handle
   // points to the beginning of the allocated buffer.
 
-  // Allocate GPU memory
-  constexpr size_t size = 2 * sizeof(int64_t);
+  // Allocate GPU memory. Set up a buffer with two int values.
+  constexpr size_t kBufferSize = 2 * sizeof(int64_t);
   const int64_t num_devices = communicator_->size();
   const int64_t rank = communicator_->deviceId();
   const int64_t peer_rank = (rank + 1) % num_devices;
   int64_t* d_ptr;
-  CUDA_CALL(cudaMalloc(&d_ptr, size));
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaMalloc(&d_ptr, kBufferSize));
 
   std::vector<int64_t> values;
   values.push_back(2 * rank);
   values.push_back(2 * rank + 1);
-  CUDA_CALL(cudaMemcpy(d_ptr, values.data(), size, cudaMemcpyHostToDevice));
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaMemcpy(d_ptr, values.data(), kBufferSize, cudaMemcpyHostToDevice));
 
+  // Export Ipc Handle
   cudaIpcMemHandle_t ipc_handle;
-  CUDA_CALL(cudaIpcGetMemHandle(&ipc_handle, d_ptr + 1));
-
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaIpcGetMemHandle(&ipc_handle, d_ptr + 1));
   auto store = communicator_->getTcpStore();
   store->set("ipc_handle_" + std::to_string(rank), toBytes(ipc_handle));
+
+  // Wait for all ranks to finish exporting the IPC handle
   communicator_->barrier();
+
+  // Import Ipc Handle
   auto peer_ipc_handle = fromBytes<cudaIpcMemHandle_t>(
       store->get("ipc_handle_" + std::to_string(peer_rank)));
-
   int64_t* peer_d_ptr;
-  CUDA_CALL(cudaIpcOpenMemHandle(
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaIpcOpenMemHandle(
       (void**)&peer_d_ptr, peer_ipc_handle, cudaIpcMemLazyEnablePeerAccess));
 
+  // Validate, noticing that the pointer is not offset by 1, contrarily to the offset used in the exporter side.
   int64_t peer_value;
-  CUDA_CALL(
-      cudaMemcpy(&peer_value, peer_d_ptr, size / 2, cudaMemcpyDeviceToHost));
-
+  NVFUSER_CUDA_RT_SAFE_CALL(
+      cudaMemcpy(&peer_value, peer_d_ptr, kBufferSize / 2, cudaMemcpyDeviceToHost));
   EXPECT_EQ(
       2 * peer_rank,
       peer_value); // and not 2 * peer_rank + 1 as could be expected!
 
   // Clean up
-  CUDA_CALL(cudaIpcCloseMemHandle(peer_d_ptr));
-  CUDA_CALL(cudaFree(d_ptr));
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaIpcCloseMemHandle(peer_d_ptr));
+  NVFUSER_CUDA_RT_SAFE_CALL(cudaFree(d_ptr));
 }
 
-class StreamOpTest : public NVFuserTest {};
+using StreamOpTest = NVFuserTest;
 
 TEST_F(StreamOpTest, StreamWriteValue32) {
   cudaStream_t stream;
@@ -169,27 +175,6 @@ TEST_F(StreamOpTest, StreamWriteValue32) {
       &value, buf, sizeof(int), cudaMemcpyDeviceToHost, stream));
   NVFUSER_CUDA_RT_SAFE_CALL(cudaStreamSynchronize(stream));
   EXPECT_EQ(value, new_value);
-}
-
-TEST_F(GpuCommTest, Allgather) {
-  constexpr int64_t kTensorSize = 1024;
-
-  at::Tensor input =
-      at::full({kTensorSize}, communicator_->deviceId(), tensor_options);
-  auto outputs = std::vector<at::Tensor>(communicator_->size());
-  std::generate(outputs.begin(), outputs.end(), [&]() {
-    return at::empty({kTensorSize}, tensor_options);
-  });
-
-  // AllgatherThroughCudaMemcpyAsync(input, outputs, communicator_);
-
-  torch::cuda::synchronize();
-  communicator_->barrier();
-
-  for (int64_t i = 0; i < communicator_->size(); ++i) {
-    at::Tensor expected = at::full({kTensorSize}, i, tensor_options);
-    EXPECT_TRUE(outputs[i].equal(expected));
-  }
 }
 
 } // namespace nvfuser
