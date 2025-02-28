f6ba84ca86a468720aafd61707fd65a82e16b5e6
Jingyue Wu <wujingyue@gmail.com>
2025-02-28 13:40:01 -0800

    Revert "Temporarily skip the overlap test (#3759)"
    This reverts commit af107cfef4ceb1c54a5fdb7a1232960ac727fd5a.

--- tests/python/test_transformer_engine.py
+++ tests/python/test_transformer_engine.py
@@ -102,10 +102,7 @@ def test_transformer_layer(
     overlap: bool,
 ):
     if overlap and parallelism == Parallelism.TENSOR_PARALLEL:
-        pytest.skip("Tensor parallelism doesn't support overlapping.")
-
-    if overlap:
-        pytest.skip("Temporarily avoid an ongoing bug in TE (http://nv/evJ).")
+        pytest.skip("Tensor parallelism doesn't support overlapping")
 
     # Hyperparameters for GPT-3
     hidden_size = 12288
