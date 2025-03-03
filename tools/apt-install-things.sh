0d0b04460201c16c6c9f3518fccfaaf7387e9083
Gao, Xiang <qasdfgtyuiop@gmail.com>
2025-03-03 15:29:30 -0800

    Remove gcc-14 headers due to incompatibility

--- tools/apt-install-things.sh
+++ tools/apt-install-things.sh
@@ -7,3 +7,6 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
 sudo apt-get -y install ninja-build cuda-compiler-12-8 cuda-command-line-tools-12-8 cuda-libraries-dev-12-8 libnccl-dev clang-19 nlohmann-json3-dev
+
+# Remove headers of gcc-14 because it is too new and not very compatible with clang
+sudo apt-get -y remove gcc-14 g++14
