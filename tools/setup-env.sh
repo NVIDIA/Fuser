1b097e6da8a1da74bec09d33e8cebd222ae36eef
Gao, Xiang <qasdfgtyuiop@gmail.com>
2025-03-03 16:39:53 -0800

    Update setup-env.sh

--- tools/setup-env.sh
+++ tools/setup-env.sh
@@ -6,3 +6,5 @@ export CUDA_INSTALL_PATH=/usr/local/cuda
 export CC=clang
 export CXX=clang++
 export CXXFLAGS="--stdlib=libstdc++ -L/usr/lib/gcc/x86_64-linux-gnu/13 -I/usr/include/c++/13"
+export CPATH=/usr/include/c++/13:$CPATH
+
