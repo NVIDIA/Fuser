56e81266c94bd4fcfda2a26dd9c372a706d5c978
Gao, Xiang <qasdfgtyuiop@gmail.com>
2025-03-03 16:07:16 -0800

    Update apt-install-things.sh

--- tools/apt-install-things.sh
+++ tools/apt-install-things.sh
@@ -9,5 +9,5 @@ sudo apt-get update
 sudo apt-get -y install ninja-build cuda-compiler-12-8 cuda-command-line-tools-12-8 cuda-libraries-dev-12-8 libnccl-dev clang-19 nlohmann-json3-dev
 
 # Remove headers of gcc-14 because it is too new and not very compatible with clang
-sudo apt-get -y remove gcc-14 g++14
+sudo apt-get -y remove gcc-14 g++14 libstdc++-14-dev
 dpkg -S /usr/include/c++/14/cstdint
