9129d7260c05b5828137108caa54ab39e80b064f
Gao, Xiang <qasdfgtyuiop@gmail.com>
2025-03-03 16:24:14 -0800

    Remove redundant package installations in script

--- tools/apt-install-things.sh
+++ tools/apt-install-things.sh
@@ -6,8 +6,8 @@ set -e
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
-sudo apt-get -y install ninja-build cuda-compiler-12-8 cuda-command-line-tools-12-8 cuda-libraries-dev-12-8 libnccl-dev clang-19 libstdc++-13 libstdc++-13-dev nlohmann-json3-dev
+sudo apt-get -y install ninja-build cuda-compiler-12-8 cuda-command-line-tools-12-8 cuda-libraries-dev-12-8 libnccl-dev clang-19 libstdc++-13-dev nlohmann-json3-dev
 
 # Remove headers of gcc-14 because it is too new and not very compatible with clang
-sudo apt-get -y remove gcc-14 g++14 libstdc++-14-dev libstdc++-14
+sudo apt-get -y remove gcc-14 g++14 libstdc++-14-dev
 dpkg -S /usr/include/c++/14/cstdint
