aca7ac778d2f12a9175da233968a313c6e77ee7b
Gao, Xiang <qasdfgtyuiop@gmail.com>
2025-03-03 20:45:31 -0800

    Update apt-install-things.sh

--- tools/apt-install-things.sh
+++ tools/apt-install-things.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -eX
 
 # Remove headers of gcc-14 because it is too new and not very compatible with clang
 #sudo apt-get -y remove gcc-14 g++14 libstdc++-14-dev
@@ -12,3 +12,6 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
 sudo apt-get -y install ninja-build cuda-compiler-12-8 cuda-command-line-tools-12-8 cuda-libraries-dev-12-8 libnccl-dev
+
+echo | clang++ -E -dM -x c++ - | grep stdint
+ls -l /usr/include/stdint.h
