fba4289ab0cc9259b8ea59fc696cd8f5ad95beda
Xiang Gao <qasdfgtyuiop@gmail.com>
2025-03-04 16:50:24 -0800

    save

--- tools/apt-install-things.sh
+++ tools/apt-install-things.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Remove headers of gcc-14 because it is too new and not very compatible with clang
-#sudo apt-get -y remove gcc-14 g++14 libstdc++-14-dev
-#sudo rm -rf /usr/include/c++/14 #/usr/lib/gcc/x86_64-linux-gnu/13 
+sudo apt-get -y remove gcc-13 g++13 libstdc++-13-dev gcc-12 g++12 libstdc++-12-dev
+#sudo rm -rf /usr/include/c++/14 #/usr/lib/gcc/x86_64-linux-gnu/13
 sudo apt-get -y install --reinstall build-essential clang-19 clang gcc-14 g++-14 nlohmann-json3-dev #g++-13 libstdc++-13-dev 
 
 # Install cuda
@@ -14,4 +14,4 @@ sudo apt-get update
 sudo apt-get -y install ninja-build cuda-compiler-12-8 cuda-command-line-tools-12-8 cuda-libraries-dev-12-8 libnccl-dev 
 
 tree /usr/include/c++/
-rm /usr/include/c++/12 /usr/include/c++/13
+# rm /usr/include/c++/12 /usr/include/c++/13
