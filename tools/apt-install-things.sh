b0bc73f3a86ed69e4bc00ae769dc55871ff22f9a
Xiang Gao <qasdfgtyuiop@gmail.com>
2025-03-04 17:05:48 -0800

    save

--- tools/apt-install-things.sh
+++ tools/apt-install-things.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 # Remove headers of gcc-14 because it is too new and not very compatible with clang
 sudo apt-get -y remove gcc-13 libstdc++-13-dev gcc-12 libstdc++-12-dev
 #sudo rm -rf /usr/include/c++/14 #/usr/lib/gcc/x86_64-linux-gnu/13
-sudo apt-get -y install --reinstall build-essential clang-19 clang gcc-14 nlohmann-json3-dev #g++-13 libstdc++-13-dev 
+sudo apt-get -y install --reinstall clang-19 gcc-14 nlohmann-json3-dev #g++-13 libstdc++-13-dev 
 
 # Install cuda
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
