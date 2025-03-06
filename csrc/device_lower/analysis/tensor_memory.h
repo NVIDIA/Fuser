2825ab253cb3bc58c5827c7cbd9b3071087c3a00
Gao, Xiang <qasdfgtyuiop@gmail.com>
2025-03-06 13:37:47 -0800

    new line

--- csrc/device_lower/analysis/tensor_memory.h
+++ csrc/device_lower/analysis/tensor_memory.h
@@ -168,6 +168,7 @@ struct TensorMemoryInfo {
   TMemAlllocationInfo allocation;
   std::unordered_map<TensorView*, TMemRegisterDataPath> load_data_path;
   std::unordered_map<TensorView*, TMemRegisterDataPath> store_data_path;
+
   bool hasTMemTensor() const {
     return !allocation.regions.empty();
   }
