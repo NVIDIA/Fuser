187b74ea53fdbf0154eecee045107c57ca787c95
Liqiang Lu <liqiangxl@gmail.com>
2025-03-07 06:55:31 -0800

    fix typo

--- csrc/fusion_segmenter.cpp
+++ csrc/fusion_segmenter.cpp
@@ -4838,9 +4838,10 @@ void SegmentCandidateFinder::finalize() {
   for (auto group : segmented_fusion_->groups()) {
     auto scheduler_type = group->schedulerType();
     // If the pre-upcasted tensor is a persistent buffer, revert privatized
-    // upcast changes the persistent tensor from the pre-upcasted tensor (e.g.
-    // bool) to the post-upcasted tensor (e.g. float) may leads a fusion can't
-    // be scheduled by persistent schedulders due to this increased buffer size.
+    // upcast may change the persistent tensor from the pre-upcasted type (e.g.,
+    // bool) to the post-upcasted type (e.g., float), which can increase buffer
+    // size. This increase may prevent the fusion from being scheduled by
+    // persistent schedulers.
     if (scheduler_type == SchedulerType::InnerPersistent ||
         scheduler_type == SchedulerType::OuterPersistent ||
         scheduler_type == SchedulerType::InnerOuterPersistent) {
