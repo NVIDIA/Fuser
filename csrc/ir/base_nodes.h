cb2f4470d43acd1fef5696dcefb8ebf932f17be8
Jingyue Wu <wujingyue@gmail.com>
2025-03-12 21:36:12 -0700

    friend IrContainer

--- csrc/ir/base_nodes.h
+++ csrc/ir/base_nodes.h
@@ -401,6 +401,7 @@ class NVF_API Val : public Statement {
 
  protected:
   friend Fusion;
+  friend IrContainer;
 
   // NOLINTNEXTLINE(cppcoreguidelines-non-private-member-variables-in-classes)
   const ValType vtype_;
@@ -424,7 +425,6 @@ class NVF_API Val : public Statement {
   //! return whether an insertion occurred.
   bool addUse(Expr*);
 
- public:
   //! Remove an expression from uses() if it is already present and return
   //! whether a removal occurred.
   bool removeUse(Expr*);
