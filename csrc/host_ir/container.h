415c8d6dbb1579d6d086e7fba76dec0080604a52
snordmann <snordmann@nvidia.com>
2025-03-13 08:12:35 -0700

    hir container fix alias in case of multiple indirections

--- csrc/host_ir/container.h
+++ csrc/host_ir/container.h
@@ -60,6 +60,9 @@ class HostIrContainer final : public Fusion {
   Stream* getDefaultStream();
 
   void markAlias(TensorView* original, const TensorView* new_alias) {
+    if (alias_.count(original)) {
+      original = alias_[original]->as<TensorView>();
+    }
     alias_[new_alias] = original;
   }
 
