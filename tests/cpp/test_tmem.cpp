826f5a1e21e2edf5b6e769d58255ea07e244862f
Gao, Xiang <qasdfgtyuiop@gmail.com>
2025-03-26 10:35:29 -0700

    Fix liveness of TMem address smem TV (#4142)
    To use TMem, we must have a smem tensor for storing the address of TMem,
    and uses `smem_tv[0] = tcgen05::alloc()` to store the allocated TMem
    address. The liveness of this smem tv should be from the
    `tcgen05::alloc` to the `tcgen05::dealloc`. However, we are currently
    unable to get that due to `tcgen05::dealloc(smem_tv[0])` is not a "TV
    Op". Due to this bug, the smem address tensor is allocated overlapping
    with other smem tensors. This PR fixes this bug by handling both TV Op
    or non-TV op correctly.

--- tests/cpp/test_tmem.cpp
+++ tests/cpp/test_tmem.cpp
@@ -323,14 +323,14 @@ TEST_F(TMemTest, InexactParallelType) {
 
   auto tv0 = makeContigConcreteTensor({2, 33});
   fusion.addInput(tv0);
-  auto tv1 = set(tv0); // gmem
+  auto tv1 = set(tv0); // smem
   auto tv2 = set(tv1); // register
   auto tv3 = set(tv2); // tmem
   auto tv4 = set(tv3); // register
   auto tv5 = set(tv4); // gmem
   fusion.addOutput(tv5);
 
-  tv1->setMemoryType(MemoryType::Global);
+  tv1->setMemoryType(MemoryType::Shared);
   tv3->setMemoryType(MemoryType::Tensor);
   tv3->definition()->as<LoadStoreOp>()->setOpType(LoadStoreOpType::StTMem);
   tv4->definition()->as<LoadStoreOp>()->setOpType(LoadStoreOpType::LdTMem);
