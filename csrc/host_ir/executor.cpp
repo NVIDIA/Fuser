abcff2a2bb0f984c844820389fcb776de01a1d4b
snordmann <snordmann@nvidia.com>
2025-04-01 09:30:54 -0700

    create a separate pass for converting expr to communications

--- csrc/host_ir/executor.cpp
+++ csrc/host_ir/executor.cpp
@@ -16,12 +16,12 @@
 #include <ir/utils.h>
 #include <multidevice/communication.h>
 #include <multidevice/utils.h>
+#include <preseg_passes/convert_op_to_communication.h>
 #include <options.h>
 #include <runtime/allocations.h>
 #include <runtime/executor_dispatch.h>
 #include <runtime/executor_kernel_arg.h>
 #include <runtime/fusion_kernel_runtime.h>
-
 namespace nvfuser {
 
 HostIrExecutor::HostIrExecutor(
@@ -70,9 +70,8 @@ void HostIrExecutor::compile(Fusion* fusion) {
     std::vector<Expr*> exprs = fusion->exprs();
     DeviceIdxType my_device_idx = communicator_ ? communicator_->deviceId() : 0;
     for (Expr* e : exprs) {
-      HostIrLower lower;
       std::vector<Expr*> communications =
-          lower.lower(cloner.clone(e), my_device_idx);
+          preseg_passes::ConvertOpToCommunication::ConvertSingleOpToCommunication(cloner.clone(e), my_device_idx, HostIrLowerParams());
       for (auto* communication : communications) {
         host_ir_container_->pushBackTopLevelExprs(communication);
       }
---
+++ csrc/preseg_passes/convert_op_to_communication.h
@@ -0,0 +1,35 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+#pragma once
+
+#include <fusion.h>
+#include <host_ir/lower.h>
+#include <preseg_passes/optimization_pass.h>
+
+namespace nvfuser::preseg_passes {
+
+class ConvertOpToCommunication : public OptimizationPass<ConvertOpToCommunication> {
+  friend class OptimizationPass<ConvertOpToCommunication>;
+ public:
+  static std::vector<Expr*> ConvertSingleOpToCommunication(Expr* c, DeviceIdxType my_device_idx, const HostIrLowerParams& params);
+
+  static void setParams(const HostIrLowerParams& params) {
+    params_ = params;
+  }
+
+ protected:
+  static void runPass(Fusion* fusion);
+  static constexpr std::string_view name() {
+    return "ConvertOpToCommunication";
+  }
+
+ private:
+  static HostIrLowerParams params_;
+};
+
+} // namespace nvfuser::preseg_passes
