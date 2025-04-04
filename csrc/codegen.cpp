7050ce98a409292c22adee418f9050428e2d0bcc
Ryan Spring <rspring@nvidia.com>
2025-04-04 10:33:43 -0700

    Assert that the LoadWarp has 128 threads.

--- csrc/codegen.cpp
+++ csrc/codegen.cpp
@@ -1276,11 +1276,18 @@ class CudaKernelGenerator : private kir::ConstIrVisitor {
   std::string genLoadBlockDim() {
     std::stringstream ss;
     const auto& pdim_map = kernel_->summary().parallel_dimension_map;
+    Val* tidx = pdim_map.getRawLoad(ParallelType::TIDx);
+    Val* tidy = pdim_map.getRawLoad(ParallelType::TIDy);
+    Val* tidz = pdim_map.getRawLoad(ParallelType::TIDz);
+    int64_t num_threads = tidx->value().as<int64_t>() +
+        tidy->value().as<int64_t>() + tidz->value().as<int64_t>();
+    NVF_ERROR(
+        num_threads == 128,
+        "Expected 128 threads in LoadWarp, but found ",
+        num_threads);
     NVF_ERROR(pdim_map.hasWarpSpecialization());
-    ss << "dim3(" << genInlineOrOne(pdim_map.getRawLoad(ParallelType::TIDx))
-       << ", " << genInlineOrOne(pdim_map.getRawLoad(ParallelType::TIDy))
-       << ", " << genInlineOrOne(pdim_map.getRawLoad(ParallelType::TIDz))
-       << ")";
+    ss << "dim3(" << genInlineOrOne(tidx) << ", " << genInlineOrOne(tidy)
+       << ", " << genInlineOrOne(tidz) << ")";
     return ss.str();
   }
 
