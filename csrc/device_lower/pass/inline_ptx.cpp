d834c8201da53806d239604851d0a57a0e02aa58
Xiang Gao <xgao@nvidia.com>
2025-04-08 17:06:50 -0700

    fix

--- csrc/device_lower/pass/inline_ptx.cpp
+++ csrc/device_lower/pass/inline_ptx.cpp
@@ -268,7 +268,7 @@ class LowerToInlinePtx : public kir::ExprMutator {
     }
     for (auto fl : for_loops_) {
       if (fl->isTrivial() ||
-          std::ranges::any_of(reduction_ids, [fl](IterDomain* id) {
+          !std::ranges::any_of(reduction_ids, [fl](IterDomain* id) {
             return GpuLower::current()
                 ->idModel()
                 .idGraph(IdMappingMode::LOOP)
