221b28f0ce1cd8e54dc3e34cb8b823b3109a88d9
snordmann <snordmann@nvidia.com>
2025-04-11 06:42:10 -0700

    guard TCPStore method with NVFUSER_DISTRIBUTED

--- csrc/multidevice/ipc_handle.cpp
+++ csrc/multidevice/ipc_handle.cpp
@@ -95,6 +95,7 @@ std::string IpcHandleCache::getTcpStoreKey(
 
 void IpcHandleCache::exchangeHandles(
     const std::vector<P2PCommunication*>& communications) {
+#ifdef NVFUSER_DISTRIBUTED
   Communicator* communicator = &Communicator::getInstance();
   const int64_t my_rank = communicator->deviceId();
 
@@ -151,6 +152,10 @@ void IpcHandleCache::exchangeHandles(
 
     insert(communication, std::move(ipc_handles));
   }
+#else // NVFUSER_DISTRIBUTED
+  NVF_ERROR(false, "NVFUSER_DISTRIBUTED is not defined");
+#endif // NVFUSER_DISTRIBUTED
 }
+
 
 } // namespace nvfuser
