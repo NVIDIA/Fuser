80b9392ec8026f1ef89ab029d451cf1ec376e17d
Naoya Maruyama <nmaruyama@nvidia.com>
2025-04-11 18:16:58 -0700

    always enable TensorIndexer

--- csrc/device_lower/lower2device.cpp
+++ csrc/device_lower/lower2device.cpp
@@ -427,6 +427,12 @@ IdModelOptions getIdModelOptions(Fusion* fusion) {
     }
   }
 
+  // Always use TensorIndexer unless not supported
+  options.setProducerIndex(true);
+  options.setConsumerIndex(true);
+  options.setInlinePredicate(true);
+  options.setUnswitchPredicate(true);
+
   // If not supported, disable use of TensorIndexer by default. It is
   // still used if explicitly opted-in (see, for example,
   // Index::getConsumerIndex)
