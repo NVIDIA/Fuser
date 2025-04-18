cc845dcf51e5b18d7b1ef12ee8433e5badb446e5
Jacob Hinkle <jhinkle@nvidia.com>
2025-04-18 13:50:16 -0400

    Allocate scan dims when needed

--- csrc/device_lower/pass/allocation.cpp
+++ csrc/device_lower/pass/allocation.cpp
@@ -50,8 +50,20 @@ bool mayRequireAllocation(const TensorView* tv, IterDomain* id) {
   // remain size one.
   // - Reduction: Check the original ID, not the promotion, which may
   //   be a reduction ID even though the original ID is not a reduction
+
+  if (id->isScan()) {
+    // Allocate IterType::Scan IDs only if they are outputs or not computeWith.
+    // We know tv must not have computeAt past the scan id, so without
+    // computeWith, we know the expression won't be inlined and we'll need to
+    // allocate the scan id.
+    if (tv->isFusionOutput()) {
+      return true;
+    }
+    return !tv->hasComputeWith();
+  }
+
   return !isPartitionedLoop(tv, id) && !isSizeOneDomain(id) &&
-      !id->isReduction() && !id->isScan() && !id->isStride();
+      !id->isReduction() && !id->isStride();
 }
 
 // Get the allocation stride of a given allocation domain
