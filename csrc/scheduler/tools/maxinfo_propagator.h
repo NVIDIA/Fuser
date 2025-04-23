b4bcb334e3138a88c1ebe6f75b2ecdc5c3cfa67d
Jingyue Wu <wujingyue@gmail.com>
2025-04-23 11:22:30 -0700

    Remove some unnecessary NVF_APIs

--- csrc/scheduler/tools/maxinfo_propagator.h
+++ csrc/scheduler/tools/maxinfo_propagator.h
@@ -147,11 +147,11 @@ class MaxInfoSpanningTree {
   std::shared_ptr<Information> reference_info_;
 
  public:
-  NVF_API MaxInfoSpanningTree(
+  MaxInfoSpanningTree(
       TensorView* reference,
       std::shared_ptr<Information> reference_info,
       Selector* selector = nullptr);
-  NVF_API void traverse(Propagator* propagator);
+  void traverse(Propagator* propagator);
   virtual ~MaxInfoSpanningTree() = default;
 };
 
@@ -164,7 +164,7 @@ class MaxInfoSpanningTree {
 // level. This information is stored as a vector of `IDInfo`, where each
 // item in the vector corresponds to one ID in the reference tensor's root
 // domain.
-class NVF_API MaxLogicalDomainInfoSpanningTree : public MaxInfoSpanningTree {
+class MaxLogicalDomainInfoSpanningTree : public MaxInfoSpanningTree {
  protected:
   // This is a struct storing how the information about a root ID in the
   // starting tensor is preserved during path-finding. If during path-finding,
@@ -285,7 +285,7 @@ class SpanningTreePrinter : public MaxInfoSpanningTree::Propagator {
 // Simple selector for selecting subgraphs to build spanning trees. The selector
 // allows propagation only to the given set of selected tensorviews, except for
 // sibiling propagation, which we should never block.
-class NVF_API SetSelector : public MaxInfoSpanningTree::Selector {
+class SetSelector : public MaxInfoSpanningTree::Selector {
   std::unordered_set<TensorView*> selected_;
 
  public:
