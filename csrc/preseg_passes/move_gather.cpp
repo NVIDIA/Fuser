65ddd8ed89b7d7d579c6204e561a3d12efd96c77
protonu <pbasu@nvidia.com>
2025-04-25 15:09:02 -0700

    more tests

--- csrc/preseg_passes/move_gather.cpp
+++ csrc/preseg_passes/move_gather.cpp
@@ -29,85 +29,98 @@ bool isUnaryPointwiseOp(const Expr* expr) {
 }
 
 bool hasGatherOp(const Fusion* fusion) {
-  // Iterate through all expressions in the fusion
-  for (auto expr : fusion->exprs()) {
-    // Check if the expression is a GatherOp
-    if (expr->isA<GatherOp>()) {
-      return true; // Found a GatherOp
-    }
-  }
-  return false; // No GatherOp found
+  auto exprs = fusion->exprs();
+  return ir_utils::filterByType<GatherOp>(exprs).size() > 0;
 }
 
-std::optional<Expr*> getProducerOfLookup(const GatherOp* gather_op) {
-  // Get the producer of the GatherOp
+std::optional<Expr*> getLookupTvDef(const GatherOp* gather_op) {
   if (gather_op->lookupTv()->isFusionInput()) {
     return std::nullopt;
   }
 
   auto producer = gather_op->lookupTv()->definition();
-  NVF_ERROR(
-      producer != nullptr, "GatherOp must have a producer, but found none.");
+  NVF_ERROR(producer != nullptr, "Def for Lookup tensor view is nullptr.");
 
-  if (isUnaryPointwiseOp(producer)) {
-    return producer;
-  }
-
-  if (producer->isA<SqueezeOp>()) {
+  if (isUnaryPointwiseOp(producer) || producer->isA<SqueezeOp>()) {
     return producer;
   }
 
   return std::nullopt;
 }
 
-GatherOp* moveGatherOp(Fusion* fusion, GatherOp* gather_op, Expr* def) {
+auto prepareIndexTv(Fusion* fusion, GatherOp* gather_op, Expr* def) {
+  if (def->isA<SqueezeOp>()) {
+    return unsqueeze(gather_op->indexTv(), 0);
+  }
+  return gather_op->indexTv();
+}
+
+auto addPostGatherSqueeze(
+    Fusion* fusion,
+    GatherOp* gather,
+    TensorView* new_gather_output,
+    Expr* def,
+    Expr* dest_expr) {
+  auto new_squeeze_output =
+      squeeze(new_gather_output, def->as<SqueezeOp>()->getSqueezeDimFlags());
+  dest_expr = new_gather_output->definition();
+  return new_squeeze_output;
+}
+
+auto addPostGatherOp(
+    Fusion* fusion,
+    GatherOp* gather,
+    TensorView* new_gather_output,
+    Expr* def,
+    Expr* dest_expr) {
+  IrCloner ir_cloner(fusion);
+  auto cloned_def = static_cast<Expr*>(def->clone(&ir_cloner));
+
+  cloned_def = ir_utils::replaceValInExprInputs(
+      cloned_def, cloned_def->input(0), dest_expr->output(0));
+
+  auto cloned_output_tv = ops::newValLike(
+      cloned_def->input(0)->as<TensorView>(), cloned_def->output(0)->dtype());
+
+  cloned_def =
+      ir_utils::transferDefinitionToNewOutputs(cloned_def, {cloned_output_tv});
+
+  return cloned_def->output(0);
+}
+
+auto addOrCloneNewNodeAfterGather(
+    Fusion* fusion,
+    GatherOp* gather,
+    TensorView* new_gather_output,
+    Expr* def,
+    Expr* dest_expr) {
+  auto output_of_post_gather_op = def->isA<SqueezeOp>()
+      ? addPostGatherSqueeze(fusion, gather, new_gather_output, def, dest_expr)
+      : addPostGatherOp(fusion, gather, new_gather_output, def, dest_expr);
+
+  for (auto expr : gather->output(0)->uses()) {
+    ir_utils::replaceValInExprInputs(
+        expr, gather->output(0), output_of_post_gather_op);
+  }
+}
+
+GatherOp* moveGatherOp(
+    Fusion* fusion,
+    GatherOp* gather_op,
+    Expr* def,
+    Expr* dest_expr) {
   IrCloner ir_cloner(fusion);
 
-  auto is_take_along_axis = gather_op->exactSizes();
-
-  auto unsqueeze_output = unsqueeze(gather_op->indexTv(), 0);
-
-  auto index_tv =
-      def->isA<SqueezeOp>() ? unsqueeze_output : gather_op->indexTv();
+  auto index_tv = prepareIndexTv(fusion, gather_op, def);
 
   auto dim_for_gather_op =
       def->isA<SqueezeOp>() ? gather_op->dim() + 1 : gather_op->dim();
 
-  auto new_gather_op_output = is_take_along_axis
-      ? takeAlongAxis(
-            static_cast<TensorView*>(def->input(0)),
-            index_tv,
-            dim_for_gather_op)
-      : gather(
-            static_cast<TensorView*>(def->input(0)),
-            dim_for_gather_op,
-            index_tv);
+  auto new_gather_op_output = takeAlongAxis(
+      static_cast<TensorView*>(def->input(0)), index_tv, dim_for_gather_op);
 
-  if (def->isA<SqueezeOp>()) {
-    auto new_squeeze_output = squeeze(
-        new_gather_op_output, def->as<SqueezeOp>()->getSqueezeDimFlags());
-
-    for (auto expr : gather_op->output(0)->uses()) {
-      ir_utils::replaceValInExprInputs(
-          expr, gather_op->output(0), new_squeeze_output);
-    }
-
-  } else {
-    auto cloned_def = static_cast<Expr*>(def->clone(&ir_cloner));
-    cloned_def = ir_utils::replaceValInExprInputs(
-        cloned_def, cloned_def->input(0), new_gather_op_output);
-
-    auto cloned_output_tv = ops::newValLike(
-        cloned_def->input(0)->as<TensorView>(), cloned_def->output(0)->dtype());
-
-    cloned_def = ir_utils::transferDefinitionToNewOutputs(
-        cloned_def, {cloned_output_tv});
-
-    for (auto expr : gather_op->output(0)->uses()) {
-      ir_utils::replaceValInExprInputs(
-          expr, gather_op->output(0), cloned_def->output(0));
-    }
-  }
+  addOrCloneNewNodeAfterGather(
+      fusion, gather_op, new_gather_op_output, def, dest_expr);
 
   fusion->removeExpr(gather_op);
 
@@ -115,18 +128,19 @@ GatherOp* moveGatherOp(Fusion* fusion, GatherOp* gather_op, Expr* def) {
 }
 
 void moveGatherOp(Fusion* fusion, GatherOp* gather_op) {
-  static int count = 0;
+  Expr* dest_expr = gather_op;
   do {
-    auto producer = getProducerOfLookup(gather_op);
+    auto producer = getLookupTvDef(gather_op);
     if (!producer.has_value() || producer.value() == nullptr) {
       return;
     }
 
     auto def = producer.value();
-    // Move the GatherOp to the producer
-    std::cout << "trying to move across " << def->toString() << std::endl;
-    gather_op = moveGatherOp(fusion, gather_op, def);
-    std::cout << "After moving GatherOp: Roumd: " << count++ << std::endl;
+    auto new_gather_op = moveGatherOp(fusion, gather_op, def, dest_expr);
+    if (new_gather_op == gather_op) {
+      return;
+    }
+    gather_op = new_gather_op;
     fusion->printMath();
   } while (true);
 }
@@ -141,7 +155,20 @@ void MoveGatherPass::runPass(Fusion* fusion) {
 
   auto exprs = fusion->exprs();
   auto gather_ops = ir_utils::filterByType<GatherOp>(exprs);
-  NVF_ERROR(gather_ops.size() == 1, "Only one GatherOp is supported.");
+
+  // We support this optimization on fusions
+  // with a single gather op
+  if (gather_ops.size() > 1) {
+    return;
+  }
+
+  // For now we'll only support take_along_axis
+  // not the general gather.
+  if (!gather_ops.vector().at(0)->exactSizes()) {
+    return;
+  }
+
   moveGatherOp(fusion, gather_ops.vector()[0]);
+  fusion->printMath();
 }
 } // namespace nvfuser::preseg_passes
