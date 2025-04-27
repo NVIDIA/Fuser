bfc7ba836400aa349fab473fa04bab204e9c5601
samnordmann <snordmann@nvidia.com>
2025-04-27 16:21:05 +0200

    add HirAliasSelect (#4301)
    # What
    Add a `SelectOp`-like HIR to express indexing into ATen tensor.
    
    # Why
    it is used in the context of stream lowering, see
    https://github.com/NVIDIA/Fuser/pull/4147 and
    especially the discussion in
    https://github.com/NVIDIA/Fuser/pull/4147#discussion_r2055365814

--- csrc/host_ir/executor.h
+++ csrc/host_ir/executor.h
@@ -138,6 +138,7 @@ class HostIrEvaluator final : public OptOutDispatch {
   void handle(BinaryOp* binary_op) override;
   void handle(ReductionOp* reduction_op) override;
   void handle(ShareMemHandles* share_mem_handles) override;
+  void handle(HirAliasSelect* hir_alias_select) override;
   void unhandled(Statement* stmt) override;
 
   c10::cuda::CUDAStream getCUDAStream(Stream* stream);
