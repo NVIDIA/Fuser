526ca786c23a9eb5cebba3b4c2a2fe5e96ea9e0a
Jacob Hinkle <jhinkle@nvidia.com>
2025-04-28 12:58:44 -0400

    Make the diff smaller for Ampere scheduler

--- csrc/scheduler/ampere_multi_matmul.cpp
+++ csrc/scheduler/ampere_multi_matmul.cpp
@@ -909,17 +909,17 @@ void AmpereMultipleMatmulScheduler::scheduleMmaResults() {
       mma_result->axis(num_device_dims_)->parallelize(ParallelType::BIDz);
     }
     switch (params_->cta_order) {
-      case MatmulParams::TileRasterizationOrder::RowMajor:
-        mma_result->axis(num_device_and_batch_dims_)
-            ->parallelize(ParallelType::BIDy);
-        mma_result->axis(num_device_and_batch_dims_ + 1)
-            ->parallelize(ParallelType::BIDx);
-        break;
       case MatmulParams::TileRasterizationOrder::ColumnMajor:
         mma_result->axis(num_device_and_batch_dims_)
             ->parallelize(ParallelType::BIDx);
         mma_result->axis(num_device_and_batch_dims_ + 1)
             ->parallelize(ParallelType::BIDy);
+        break;
+      case MatmulParams::TileRasterizationOrder::RowMajor:
+        mma_result->axis(num_device_and_batch_dims_)
+            ->parallelize(ParallelType::BIDy);
+        mma_result->axis(num_device_and_batch_dims_ + 1)
+            ->parallelize(ParallelType::BIDx);
         break;
       default:
         NVF_THROW("Invalid TileRasterizationOrder passed to Matmul scheduler");
