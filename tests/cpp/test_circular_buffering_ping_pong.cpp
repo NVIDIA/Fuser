2e028bb898418509339b31bd5da700c203b6f5e1
Ryan Spring <rspring@nvidia.com>
2025-05-14 12:20:01 -0700

    refactor test

--- tests/cpp/test_circular_buffering_ping_pong.cpp
+++ tests/cpp/test_circular_buffering_ping_pong.cpp
@@ -1,6 +1,6 @@
 // clang-format off
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
+ * SPDX-FileCopyrightText: Copyright (c) 2025-present NVIDIA CORPORATION & AFFILIATES.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -21,7 +21,7 @@ namespace nvfuser {
 // For non-GEMM kernel there is no ping-pong switching between tensor cores and
 // cuda cores, but multiple warp groups work on different tiles and are
 // scheduled and synchronized separately on the same SM.
-class PingPongTest : public NVFuserTest {
+class PingPongCircularBufferingTest : public NVFuserTest {
  protected:
   void SetUp() override {
     if (cudaArchGuardShouldSkip(9, 0)) {
@@ -31,24 +31,30 @@ class PingPongTest : public NVFuserTest {
   }
 };
 
-TEST_F(PingPongTest, SimpleFusion) {
+TEST_F(PingPongCircularBufferingTest, SimpleFusion) {
   Fusion fusion;
   FusionGuard fg(&fusion);
-  int rows_per_stage = 4, compute_warp_groups = 2, circular_loop = 12;
+
+  int64_t rows_per_stage = 4;
+  int64_t compute_warp_groups = 2;
+  int64_t circular_loop = 12;
   int sm_count = at::cuda::getCurrentDeviceProperties()->multiProcessorCount;
   const int dim0 =
       rows_per_stage * compute_warp_groups * sm_count * circular_loop;
   const int dim1 = 128;
+
   int stages = 6;
-  // auto tv0 = makeContigTensor(2, DataType::Float);
-  auto tv0 = makeContigConcreteTensor({dim0, dim1}, DataType::Float);
+
+  TensorView* tv0 = makeContigConcreteTensor({dim0, dim1}, DataType::Float);
   fusion.addInput(tv0);
-  auto tv1 = set(tv0);
+
+  TensorView* tv1 = set(tv0);
+  TensorView* tv2 = set(tv1);
+  TensorView* tv3 = add(tv2, tv2);
+  fusion.addOutput(tv3);
+
   tv1->definition()->as<LoadStoreOp>()->setOpType(LoadStoreOpType::CpAsyncBulk);
   tv1->setMemoryType(MemoryType::Shared);
-  auto tv2 = set(tv1);
-  auto tv3 = add(tv2, tv2);
-  fusion.addOutput(tv3);
 
   Fusion fusion_copy = fusion;
   auto options = at::TensorOptions().device(at::kCUDA, 0);
@@ -58,29 +64,30 @@ TEST_F(PingPongTest, SimpleFusion) {
     tv->split(0, rows_per_stage);
     tv->split(0, sm_count);
     tv->split(0, compute_warp_groups);
+
     // [I, 2, 132, 4]
     tv->axis(0)->parallelize(ParallelType::Serial);
-    tv->axis(1)->parallelize(ParallelType::TIDy);
-    tv->axis(2)->parallelize(ParallelType::BIDy);
+    tv->axis(2)->parallelize(ParallelType::BIDx);
     tv->axis(3)->parallelize(ParallelType::Unroll);
-    tv->axis(4)->parallelize(ParallelType::TIDx);
-    if (tv == tv1) {
-      tv->axis(1)->parallelize(ParallelType::Serial);
-      tv->axis(4)->parallelize(ParallelType::Bulk);
-    }
   }
+
+  tv1->axis(1)->parallelize(ParallelType::Serial);
+  tv1->axis(4)->parallelize(ParallelType::Bulk);
+
+  for (auto tv : {tv2, tv3}) {
+    tv->axis(1)->parallelize(ParallelType::TIDy);
+    tv->axis(4)->parallelize(ParallelType::TIDx);
+  }
+
   // [I, 2, 132, 4] --> [132, I, 2, 4]
-  std::unordered_map<int64_t, int64_t> reorder_map;
-  reorder_map[0] = 1;
-  reorder_map[1] = 2;
-  reorder_map[2] = 0;
+  std::unordered_map<int64_t, int64_t> reorder_map = {{0, 1}, {1, 2}, {2, 0}};
   for (auto tv : {tv1, tv2, tv3}) {
     tv->reorder(reorder_map);
   }
   fusion.printMath();
 
-  inlineSelectedAt({tv1}, tv2, 2);
-  inlineSelectedAt({tv2}, tv2, 3);
+  inlineSelectedAt({tv1}, tv2, /*reference_pos=*/2);
+  inlineSelectedAt({tv2}, tv2, /*reference_pos=*/3);
 
   tv1->circularBuffer(stages, stages - 1, WarpSpecialized(ParallelType::TIDy));
 
