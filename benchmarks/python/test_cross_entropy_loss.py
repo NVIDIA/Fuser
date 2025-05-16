beca294254e210947c484e549753fe8cc8ed428e
protonu <pbasu@nvidia.com>
2025-05-16 16:44:42 -0700

    adding a mini benchmark for cross entropy loss

--- benchmarks/python/test_cross_entropy_loss.py
+++ benchmarks/python/test_cross_entropy_loss.py
@@ -80,3 +80,58 @@ def test_cross_entropy_bwd_benchmark(
         [outputs[0], test_case.grads(), *fwd_inputs, *list(model.parameters())],
         iobytes=test_case.grad_iobytes(),
     )
+
+
+problem_sizes = [8000, 32064, 132035, 180000]
+
+
+def mini_model(logits, labels):
+    labels = torch.nn.functional.pad(labels, (0, 1))
+    labels = labels[1 : labels.shape[-1]]
+    return torch.nn.functional.cross_entropy(logits, labels)
+
+
+@pytest.mark.parametrize(
+    "executor", ["eager", "torchcompile", "thunder", "thunder-torchcompile"]
+)
+@pytest.mark.parametrize("vocab_size", problem_sizes)
+def test_cross_entropy_mini_benchmark_fwd(benchmark, executor: str, vocab_size: int):
+    if executor == "torchcompile":
+        clear_dynamo_cache()
+
+    def fwd_call(inp):
+        return mini_model(*inp)
+
+    inputs = torch.rand((8192, vocab_size), device="cuda", requires_grad=True)
+    target = torch.randint(
+        0, vocab_size - 1, (8192,), device="cuda", requires_grad=False
+    )
+
+    kwargs = {}
+    inputs = [inputs, target]
+    fwd_fn = with_executor(executor, fwd_call, **kwargs)
+    run_benchmark(benchmark, fwd_fn, inputs)
+
+
+@pytest.mark.parametrize(
+    "executor", ["eager", "torchcompile", "thunder", "thunder-torchcompile"]
+)
+@pytest.mark.parametrize("vocab_size", problem_sizes)
+def test_cross_entropy_mini_benchmark_bwd(benchmark, executor: str, vocab_size: int):
+    if executor == "torchcompile":
+        clear_dynamo_cache()
+
+    def fwd_call(inp):
+        return mini_model(*inp)
+
+    input = torch.rand((8192, vocab_size), device="cuda", requires_grad=True)
+    target = torch.randint(
+        0, vocab_size - 1, (8192,), device="cuda", requires_grad=False
+    )
+
+    kwargs = {}
+    inputs = [input, target]
+    fwd_fn = with_executor(executor, fwd_call, **kwargs)
+    outputs = fwd_fn(inputs)
+    grads = torch.tensor(1, device="cuda", dtype=torch.float32, requires_grad=False)
+    run_benchmark(benchmark, unary_bwd_torch, [outputs, grads, inputs])
