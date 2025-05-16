2c3bc1e3bbe047f741a8d85d05daeaf72ca5083c
root <26priya11@gmail.com>
2025-05-16 15:36:34 -0700

    undo change

--- csrc/host_ir/executor.cpp
+++ csrc/host_ir/executor.cpp
@@ -166,8 +166,7 @@ KernelArgumentHolder HostIrExecutor::run(
         communicator_->deviceId(),
         backend,
         in_tensor,
-        out_tensor,
-        expr_eval);
+        out_tensor);
     if (work != nullptr) {
       work->wait();
     }
@@ -504,8 +503,7 @@ void HostIrEvaluator::handle(Communication* communication) {
       communicator_->deviceId(),
       backend,
       input_tensor,
-      output_tensor,
-      expr_evaluator_);
+      output_tensor);
 }
 
 void HostIrEvaluator::handle(P2PCommunication* communication) {
