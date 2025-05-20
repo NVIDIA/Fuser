8f3e87dfab1abd995e6d755177d394c5766ac89c
protonu <pbasu@nvidia.com>
2025-05-20 09:23:16 -0700

    increasing the span of vocab sizes

--- benchmarks/python/test_cross_entropy_loss.py
+++ benchmarks/python/test_cross_entropy_loss.py
@@ -6,7 +6,10 @@ import pytest
 import torch
 
 from .core import run_benchmark, with_executor, unary_bwd_torch, clear_dynamo_cache
-from .cross_entropy_loss import cross_entropy_loss_setup
+from .cross_entropy_loss import (
+    cross_entropy_loss_setup,
+    SyntheticMiniModel,
+)
 
 
 @pytest.mark.parametrize(
@@ -82,33 +85,21 @@ def test_cross_entropy_bwd_benchmark(
     )
 
 
-problem_sizes = [8000, 32064, 132035, 180000]
-
-
-def mini_model(logits, labels):
-    labels = torch.nn.functional.pad(labels, (0, 1))
-    labels = labels[1 : labels.shape[-1]]
-    return torch.nn.functional.cross_entropy(logits, labels)
-
-
 @pytest.mark.parametrize(
     "executor", ["eager", "torchcompile", "thunder", "thunder-torchcompile"]
 )
-@pytest.mark.parametrize("vocab_size", problem_sizes)
+@pytest.mark.parametrize("vocab_size", SyntheticMiniModel.generate_vocab_sizes())
 def test_cross_entropy_mini_benchmark_fwd(benchmark, executor: str, vocab_size: int):
     if executor == "torchcompile":
         clear_dynamo_cache()
 
     def fwd_call(inp):
-        return mini_model(*inp)
+        return SyntheticMiniModel.mini_model(*inp)
 
-    inputs = torch.rand((8192, vocab_size), device="cuda", requires_grad=True)
-    target = torch.randint(
-        0, vocab_size - 1, (8192,), device="cuda", requires_grad=False
-    )
+    input, target = SyntheticMiniModel.inputs(8192, vocab_size)
 
     kwargs = {}
-    inputs = [inputs, target]
+    inputs = [input, target]
     fwd_fn = with_executor(executor, fwd_call, **kwargs)
     run_benchmark(benchmark, fwd_fn, inputs)
 
@@ -116,22 +107,19 @@ def test_cross_entropy_mini_benchmark_fwd(benchmark, executor: str, vocab_size: 
 @pytest.mark.parametrize(
     "executor", ["eager", "torchcompile", "thunder", "thunder-torchcompile"]
 )
-@pytest.mark.parametrize("vocab_size", problem_sizes)
+@pytest.mark.parametrize("vocab_size", SyntheticMiniModel.generate_vocab_sizes())
 def test_cross_entropy_mini_benchmark_bwd(benchmark, executor: str, vocab_size: int):
     if executor == "torchcompile":
         clear_dynamo_cache()
 
     def fwd_call(inp):
-        return mini_model(*inp)
+        return SyntheticMiniModel.mini_model(*inp)
 
-    input = torch.rand((8192, vocab_size), device="cuda", requires_grad=True)
-    target = torch.randint(
-        0, vocab_size - 1, (8192,), device="cuda", requires_grad=False
-    )
+    input, target = SyntheticMiniModel.inputs(8192, vocab_size)
 
     kwargs = {}
     inputs = [input, target]
     fwd_fn = with_executor(executor, fwd_call, **kwargs)
     outputs = fwd_fn(inputs)
-    grads = torch.tensor(1, device="cuda", dtype=torch.float32, requires_grad=False)
+    grads = SyntheticMiniModel.grads()
     run_benchmark(benchmark, unary_bwd_torch, [outputs, grads, inputs])
