234dcbe21c3cc91228341b500704b331f388ede7
snordmann <snordmann@nvidia.com>
2025-05-20 04:29:17 -0700

    fix bug

--- csrc/host_ir/pass/convert_op_to_communication.cpp
+++ csrc/host_ir/pass/convert_op_to_communication.cpp
@@ -37,7 +37,7 @@ void ConvertOpToCommunication::passImplementation(Fusion* fusion) {
       if (expr->isA<Communication>()) {
         auto* communication = expr->as<Communication>();
         TensorView* tv = communication->out();
-        if (tv->getDeviceMesh().has(my_device_index) == 0) {
+        if (tv->getDeviceMesh().has(my_device_index)) {
           auto* allocate =
               IrBuilder::create<kir::Allocate>(tv, MemoryType::Global);
           new_top_level_exprs.push_back(allocate);
