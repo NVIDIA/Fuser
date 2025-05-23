9b13e5fd4c2933f2e6e280631c973acddafe3228
jjsjann123 <jiej@nvidia.com>
2025-05-23 16:14:35 -0700

    err missing parenthesis

--- benchmarks/python/test_scatter_reduce.py
+++ benchmarks/python/test_scatter_reduce.py
@@ -50,7 +50,7 @@ def test_scatter_reduce_fwd_baseline_benchmark(
     if executor == "torchcompile":
         clear_dynamo_cache()
 
-    hidden_states = torch.randn((seq_length * topk_hidden[0], topk_hidden[1]), device="cuda", requires_grad=True))
+    hidden_states = torch.randn((seq_length * topk_hidden[0], topk_hidden[1]), device="cuda", requires_grad=True)
     logits = torch.randn(seq_length * topk_hidden[0], device="cuda")
     idxs = logits.argsort()
     topk_weight = torch.randn((seq_length, topk_hidden[0]), device="cuda")
