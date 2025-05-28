4371a08e163b026ab578e183b6ee372ece4d7500
Priya Mishra <26priya11@gmail.com>
2025-05-28 14:22:29 -0700

    only include device and stream parallel type

--- csrc/multidevice/utils.cpp
+++ csrc/multidevice/utils.cpp
@@ -127,28 +127,22 @@ bool isSharded(const TensorView* tv) {
 
 namespace {
 
-// Collect parallelized IterDomains in `domain` and return them as a
-// ParallelType-to-IterDomain map. Excludes `Serial` parallel type and device
-// parallelization (DID) on reduction iterdomains.
-std::unordered_map<ParallelType, IterDomain*> mapParallelTypeToId(
+// Collect device and stream parallelized IterDomains in `domain` and return
+// them as a ParallelType-to-IterDomain map. Excludes device parallelization
+// (DID) on reduction iterdomains.
+std::unordered_map<ParallelType, IterDomain*> mapDeviceAndStreamParallelTypeToId(
     const std::vector<IterDomain*>& domain) {
   const std::unordered_set<ParallelType>& parallel_types =
-      allParallelTypesExcept({ParallelType::Serial});
+      deviceAndStreamParallelTypes();
 
   std::unordered_map<ParallelType, IterDomain*> parallel_type_to_id;
   parallel_type_to_id.reserve(parallel_types.size());
 
   for (IterDomain* id : domain) {
-    if (!id->isParallelized()) {
+    const ParallelType parallel_type = id->getParallelType();
+    if (parallel_types.count(parallel_type) == 0) {
       continue;
     }
-    const ParallelType parallel_type = id->getParallelType();
-    NVF_ERROR(
-        parallel_types.count(parallel_type) > 0,
-        "Unexpected parallel type on IterDomain: ",
-        id,
-        " in ",
-        toDelimitedString(domain));
 
     // rDIDx{i0}, usually a product of an Allreduce or a ReduceScatter, is
     // treated as replicated. This way `iDIDx{i0} => rDIDx{i0}` is considered
@@ -189,7 +183,7 @@ int64_t getShardedLogicalAxis(
     const TensorView* tv,
     const ParallelType parallel_type) {
   std::unordered_map<ParallelType, IterDomain*> parallel_type_to_id =
-      mapParallelTypeToId(tv->getMaybeAllocationDomain());
+      mapDeviceAndStreamParallelTypeToId(tv->getMaybeAllocationDomain());
   IterDomain* alloc_id = getOrDefault(parallel_type_to_id, parallel_type);
   if (alloc_id == nullptr) {
     return -1;
@@ -512,9 +506,9 @@ bool haveDifferentShardings(
   // Create indices for producer logical IDs and consumer root IDs. As an
   // optimization, we create indices only for those that DIDs depend on.
   std::unordered_map<ParallelType, IterDomain*> p_parallel_type_to_id =
-      mapParallelTypeToId(producer->getLoopDomain());
+      mapDeviceAndStreamParallelTypeToId(producer->getLoopDomain());
   std::unordered_map<ParallelType, IterDomain*> c_parallel_type_to_id =
-      mapParallelTypeToId(consumer->getLoopDomain());
+      mapDeviceAndStreamParallelTypeToId(consumer->getLoopDomain());
   for (const auto parallel_type : kParallelTypeDIDs) {
     if (IterDomain* p_loop_id =
             getOrDefault(p_parallel_type_to_id, parallel_type)) {
