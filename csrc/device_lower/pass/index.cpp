aaaa06542cc2687dfd4c429367c0d0de3dd7991a
Ryan Spring <rspring@nvidia.com>
2025-05-27 18:10:17 -0700

    Add abstract domains to fusion managed data

--- csrc/device_lower/pass/index.cpp
+++ csrc/device_lower/pass/index.cpp
@@ -2159,6 +2159,7 @@ void IndexLowering::handle(const LoadStoreOp* ldst) {
         auto m_tile = ldst->fusion()->getManaged<int64_t>("ldst_matrix_m_tile");
         auto n_tile = ldst->fusion()->getManaged<int64_t>("ldst_matrix_n_tile");
 
+        /*
         auto m = ldst->fusion()->getManaged<int64_t>("ldst_matrix_m_smem");
         auto n = ldst->fusion()->getManaged<int64_t>("ldst_matrix_n_smem");
         MmaInputSmemSwizzle swizzle = getSwizzle(in_tv);
@@ -2176,11 +2177,16 @@ void IndexLowering::handle(const LoadStoreOp* ldst) {
           default:
             NVF_ERROR("Unsupported Swizzle Type for StMatrix");
         }
-        /*
-        Val* index =
-            GpuLower::current()->tensorIndexer().getLinearIndex(in_tv, ldst,
-        for_loops_); in = IrBuilder::create<kir::TensorIndex>(in_tv, index);
         */
+
+        NVF_ERROR(ldst->fusion()->hasManaged("ldmatrix_smem"));
+        std::vector<IterDomain*> smem_domain =
+            ldst->fusion()->getManaged<std::vector<nvfuser::IterDomain*>>(
+                "ldmatrix_smem");
+
+        Val* index = GpuLower::current()->tensorIndexer().getLinearIndex(
+            in_tv, ldst, for_loops_);
+        in = IrBuilder::create<kir::TensorIndex>(in_tv, index);
 
         auto num_regs = (m_tile) / 8 * (n_tile) / 8;
         auto as_type = ArrayType{
