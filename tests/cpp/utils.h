b615141796180a238e0d0827f2db4994b20c4903
Naoya Maruyama <nmaruyama@nvidia.com>
2025-05-30 09:31:23 -0700

    Enable TensorIndexer for matmul-related tests

--- tests/cpp/utils.h
+++ tests/cpp/utils.h
@@ -560,6 +560,7 @@ class HopperBase : public NVFuserTest {
       GTEST_SKIP() << "skipping tests on non-Hopper GPUs";
     }
     NVFuserTest::SetUp();
+    EnableOptionsGuard::getCurOptions().set(EnableOption::IdModel, {"all"});
   }
 };
 
@@ -570,6 +571,7 @@ class BlackwellBase : public NVFuserTest {
       GTEST_SKIP() << "skipping tests on non-Blackwell GPUs";
     }
     NVFuserTest::SetUp();
+    EnableOptionsGuard::getCurOptions().set(EnableOption::IdModel, {"all"});
   }
 };
 
@@ -581,6 +583,7 @@ class TmaBase : public NVFuserTest {
       GTEST_SKIP() << "skipping tests on pre-Hopper GPUs";
     }
     NVFuserTest::SetUp();
+    EnableOptionsGuard::getCurOptions().set(EnableOption::IdModel, {"all"});
   }
 };
 
