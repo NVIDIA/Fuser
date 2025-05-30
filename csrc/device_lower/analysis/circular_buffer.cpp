82c92b246fe4212a576ecd45bba7e18c7a78eaf6
Ryan Spring <rspring@nvidia.com>
2025-05-30 11:17:12 -0700

    Use stage_slice_position to get inner_loop_index

--- csrc/device_lower/analysis/circular_buffer.cpp
+++ csrc/device_lower/analysis/circular_buffer.cpp
@@ -583,14 +583,15 @@ int64_t getForLoopIndex(
 Val* CircularBufferInfo::getLinearIndex(
     TensorView* circular_buffer_tv,
     const std::vector<ForLoop*>& loops) const {
-  int64_t inner_loop_index =
-      getForLoopIndex(circular_buffer_tv, loops, /*is_inner_most_axis=*/true);
-
   // short-circuit: return index for inner-most for-loop if not warp specialized
   // with register sharing
-  auto circular_buffer_type = circular_buffer_tv->circularBufferOptions().type;
-  bool is_warp_specialized = std::holds_alternative<WarpSpecialized>(circular_buffer_type);
+  const auto& circular_buffer_type =
+      circular_buffer_tv->circularBufferOptions().type;
+  bool is_warp_specialized =
+      std::holds_alternative<WarpSpecialized>(circular_buffer_type);
   if (!is_warp_specialized) {
+    int64_t inner_loop_index =
+        getForLoopIndex(circular_buffer_tv, loops, /*is_inner_most_axis=*/true);
     return loops[inner_loop_index]->indexOrStartIfTrivial();
   }
 
@@ -600,10 +601,16 @@ Val* CircularBufferInfo::getLinearIndex(
   int64_t outer_loop_index =
       getForLoopIndex(circular_buffer_tv, loops, /*is_inner_most_axis=*/false);
 
+  // The inner_loop_index is the for-loop where the mbarrier arrive and wait
+  // operations are inserted in circular buffering pass. Use
+  // stage_slice_position if available. Otherwise, the default value is the
+  // first serial for-loop to the left of compute_at_position.
+  int64_t inner_loop_index = (warp_specialized.stage_slice_position.has_value())
+      ? warp_specialized.stage_slice_position.value() - 1
+      : getForLoopIndex(circular_buffer_tv, loops, /*is_inner_most_axis=*/true);
+
   // Calculate insertion position.
-  int64_t insertion_position = warp_specialized.stage_slice_position.has_value()
-          ? warp_specialized.stage_slice_position.value() - 1
-                : inner_loop_index - outer_loop_index + 1;
+  int64_t insertion_position = inner_loop_index - outer_loop_index + 1;
   return getLinearIndexRelativeForLoopStack(
       loops, insertion_position, /*start=*/outer_loop_index);
 }
