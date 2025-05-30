5140d78cdf835b23dd8ff867e2de5cb5e4c5b98d
Liqiang Lu <liqiangxl@gmail.com>
2025-05-30 09:38:05 -0700

    fix smem index

--- csrc/device_lower/analysis/circular_buffer.cpp
+++ csrc/device_lower/analysis/circular_buffer.cpp
@@ -588,8 +588,9 @@ Val* CircularBufferInfo::getLinearIndex(
 
   // short-circuit: return index for inner-most for-loop if not warp specialized
   // with register sharing
-  bool is_warp_specialized = std::holds_alternative<WarpSpecialized>(
-      circular_buffer_tv->circularBufferOptions().type);
+  auto circular_buffer_type = circular_buffer_tv->circularBufferOptions().type;
+  bool is_warp_specialized =
+      std::holds_alternative<WarpSpecialized>(circular_buffer_type);
   if (!is_warp_specialized) {
     return loops[inner_loop_index]->indexOrStartIfTrivial();
   }
@@ -600,7 +601,10 @@ Val* CircularBufferInfo::getLinearIndex(
       getForLoopIndex(circular_buffer_tv, loops, /*is_inner_most_axis=*/false);
 
   // Calculate insertion position.
-  int64_t insertion_position = inner_loop_index - outer_loop_index + 1;
+  auto warp_specialized = std::get<WarpSpecialized>(circular_buffer_type);
+  int64_t insertion_position = warp_specialized.stage_slice_position.has_value()
+      ? warp_specialized.stage_slice_position.value() - 1
+      : inner_loop_index - outer_loop_index + 1;
   return getLinearIndexRelativeForLoopStack(
       loops, insertion_position, /*start=*/outer_loop_index);
 }
