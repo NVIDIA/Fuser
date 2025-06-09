a5b65edcb8ae420717dbf9273304f81810561336
Jingyue Wu <wujingyue@gmail.com>
2025-06-09 10:07:19 -0700

    comment

--- python/nvfuser/testing/benchmark_utils.py
+++ python/nvfuser/testing/benchmark_utils.py
@@ -20,6 +20,12 @@ def get_benchmark_fn(func, /, profile: bool):
 
 # Returns two functors, the first with profiler off and the second with profiler
 # on. The first functor is usually used for warmup and the second for actual
-# benchmarking.
+# benchmarking. This way, one
+# can collect stats of the first few non-warmup benchmark iterations using
+# ```bash
+# mpirun -np 1 nsys profile --capture-range=cudaProfilerApi --capture-range-end=repeat:<iterations> pytest tests/python/multidevice/<test_file>.py -k <filter> --only-mpi : -np <processes - 1> pytest tests/python/multidevice/<test_file>.py -k <filter> --only-mpi
+# ```
+# and then display the stats using e.g. `nsys stats --report=cuda_gpu_kern_sum
+# report1.nsys-rep`.
 def get_benchmark_fns(func):
     return get_benchmark_fn(func, profile=False), get_benchmark_fn(func, profile=True)
