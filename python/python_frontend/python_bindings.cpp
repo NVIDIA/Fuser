901749ae490b5f0eb6684407c4709c1942eafbdc
Jacob Hinkle <jhinkle@nvidia.com>
2025-06-12 08:12:14 -0400

    Add num_clusters to python interface for MatmulParams

--- python/python_frontend/python_bindings.cpp
+++ python/python_frontend/python_bindings.cpp
@@ -648,6 +648,7 @@ void defineHeuristicParamBindings(py::module& nvfuser) {
       .PARAM(MatmulParams, circular_buffering_strategy)
       .PARAM(MatmulParams, cta_order)
       .PARAM(MatmulParams, cluster_dims)
+      .PARAM(MatmulParams, num_clusters)
       .PARAM(MatmulParams, mma_macro);
 
 #undef PARAM
