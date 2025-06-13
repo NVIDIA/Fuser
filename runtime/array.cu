305b453e6385cb762b38cd3f7a195e2a27a93355
Xiang Gao <xgao@nvidia.com>
2025-06-13 00:41:46 -0700

    Use bits for load functions

--- runtime/array.cu
+++ runtime/array.cu
@@ -43,6 +43,11 @@ __device__ void arraySet(scalar_t* buff, scalar_t val) {
   }
 }
 
+template <typename scalar_t>
+constexpr int64_t vecSizeBit(int64_t vec_size) {
+  return vec_size * sizeof(scalar_t);
+}
+
 template <typename scalar_t, int vec_size>
 __device__ void loadGeneric(scalar_t* to, scalar_t* from) {
   // It would be really nice to use memcpy here, but one example was failing
@@ -56,7 +61,9 @@ __device__ void loadGeneric(scalar_t* to, scalar_t* from) {
   //   to[i] = from[i];
   // }
 
-  switch (sizeof(scalar_t) * vec_size) {
+  constexpr int64_t vec_size_bit = vecSizeBit<scalar_t>(vec_size);
+  static_assert(vec_size_bit % 8 == 0, "vec_size_bit must be a multiple of 8");
+  switch (vec_size_bit) {
     case 1:
       *reinterpret_cast<uchar1*>(to) = *reinterpret_cast<uchar1*>(from);
       break;
@@ -87,30 +94,32 @@ template <
 __device__ void loadGenericVolatile(
     typename MaybeVolatile<scalar_t, is_volatile_to>::type* to,
     typename MaybeVolatile<scalar_t, is_volatile_from>::type* from) {
-  switch (sizeof(scalar_t) * vec_size) {
+  constexpr int64_t vec_size_bit = vecSizeBit<scalar_t>(vec_size);
+  static_assert(vec_size_bit % 8 == 0, "vec_size_bit must be a multiple of 8");
+  switch (vec_size_bit) {
     // Reinterpret cast like this with volatile types only works for C++
     // fundamental types otherwise the = operator is not defined
-    case 1:
+    case 8:
       *reinterpret_cast<
           typename MaybeVolatile<unsigned char, is_volatile_to>::type*>(to) =
           *reinterpret_cast<
               typename MaybeVolatile<unsigned char, is_volatile_from>::type*>(
               from);
       break;
-    case 2:
+    case 16:
       *reinterpret_cast<typename MaybeVolatile<short, is_volatile_to>::type*>(
           to) =
           *reinterpret_cast<
               typename MaybeVolatile<short, is_volatile_from>::type*>(from);
       break;
-    case 4:
+    case 32:
       *reinterpret_cast<
           typename MaybeVolatile<unsigned int, is_volatile_to>::type*>(to) =
           *reinterpret_cast<
               typename MaybeVolatile<unsigned int, is_volatile_from>::type*>(
               from);
       break;
-    case 8:
+    case 64:
       *reinterpret_cast<typename MaybeVolatile<double, is_volatile_to>::type*>(
           to) =
           *reinterpret_cast<
@@ -123,13 +132,15 @@ template <typename scalar_t, int vec_size, bool is_volatile>
 __device__ void loadLocalToGlobal(
     typename MaybeVolatile<scalar_t, is_volatile>::type* to,
     scalar_t* from) {
-  switch (sizeof(scalar_t) * vec_size) {
-    case 1:
-    case 2:
-    case 4:
+  constexpr int64_t vec_size_bit = vecSizeBit<scalar_t>(vec_size);
+  static_assert(vec_size_bit % 8 == 0, "vec_size_bit must be a multiple of 8");
+  switch (vec_size_bit) {
+    case 8:
+    case 16:
+    case 32:
       loadGenericVolatile<scalar_t, vec_size, is_volatile, false>(to, from);
       break;
-    case 8: {
+    case 64: {
       uint2 const& data = *reinterpret_cast<uint2*>(from);
       if (is_volatile) {
         asm volatile(
@@ -146,7 +157,7 @@ __device__ void loadLocalToGlobal(
       }
       break;
     }
-    case 16: {
+    case 128: {
       uint4 const& data = *reinterpret_cast<uint4*>(from);
       if (is_volatile) {
         asm volatile(
@@ -201,13 +212,15 @@ template <typename scalar_t, int vec_size, bool is_volatile, CacheOp cache_op>
 __device__ void loadGlobalToLocal(
     scalar_t* to,
     typename MaybeVolatile<scalar_t, is_volatile>::type* from) {
-  switch (sizeof(scalar_t) * vec_size) {
-    case 1:
-    case 2:
-    case 4:
+  constexpr int64_t vec_size_bit = vecSizeBit<scalar_t>(vec_size);
+  static_assert(vec_size_bit % 8 == 0, "vec_size_bit must be a multiple of 8");
+  switch (vec_size_bit) {
+    case 8:
+    case 16:
+    case 32:
       loadGenericVolatile<scalar_t, vec_size, false, is_volatile>(to, from);
       break;
-    case 8: {
+    case 64: {
       if (is_volatile) {
         uint2& data = *reinterpret_cast<uint2*>(to);
         asm volatile("ld.volatile.global.v2.s32 {%0,%1}, [%2];"
@@ -219,7 +232,7 @@ __device__ void loadGlobalToLocal(
       }
       break;
     }
-    case 16: {
+    case 128: {
       if (is_volatile) {
         uint4& data = *reinterpret_cast<uint4*>(to);
         asm volatile("ld.volatile.global.v4.s32 {%0,%1,%2,%3}, [%4];"
@@ -242,17 +255,19 @@ template <
 __device__ void loadGlobalToGlobal(
     typename MaybeVolatile<scalar_t, is_volatile_to>::type* to,
     typename MaybeVolatile<scalar_t, is_volatile_from>::type* from) {
-  switch (sizeof(scalar_t) * vec_size) {
+  constexpr int64_t vec_size_bit = vecSizeBit<scalar_t>(vec_size);
+  static_assert(vec_size_bit % 8 == 0, "vec_size_bit must be a multiple of 8");
+  switch (vec_size_bit) {
     // Reinterpret cast like this with volatile types only works for C++
     // fundamental types otherwise the = operator is not defined
-    case 1:
-    case 2:
-    case 4:
     case 8:
+    case 16:
+    case 32:
+    case 64:
       loadGenericVolatile<scalar_t, vec_size, is_volatile_to, is_volatile_from>(
           to, from);
       break;
-    case 12: {
+    case 96: {
       uint3 local_intermediate;
       loadGlobalToLocal<
           scalar_t,
@@ -264,7 +279,7 @@ __device__ void loadGlobalToGlobal(
           to, reinterpret_cast<scalar_t*>(&local_intermediate));
       break;
     }
-    case 16: {
+    case 128: {
       uint4 local_intermediate;
       loadGlobalToLocal<
           scalar_t,
