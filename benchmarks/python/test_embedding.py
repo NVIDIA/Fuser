79fed599ddacaaccf69557cebc74deb80c5c8906
Ivan Yashchuk <IvanYashchuk@users.noreply.github.com>
2025-06-13 14:54:19 +0300

    Add test_embedding_rmsnorm_inference

--- benchmarks/python/test_embedding.py
+++ benchmarks/python/test_embedding.py
@@ -15,7 +15,7 @@ from .core import (
     DEFAULT_EXECUTORS,
 )
 from .global_params import FLOAT_DTYPES
-from .torch_ops import embedding, embedding_indexing
+from .torch_ops import embedding, embedding_indexing, rmsnorm, rmsnorm_rsqrt
 
 
 # (vocab, hidden) configurations seen in models.
@@ -42,6 +42,11 @@ SEQ_LENGTHS = [
 FNS = [
     pytest.param(embedding, id="embedding"),
     pytest.param(embedding_indexing, id="embedding_indexing"),
+]
+
+RMS_NORM_FNS = [
+    pytest.param(rmsnorm, id="rmsnorm"),
+    pytest.param(rmsnorm_rsqrt, id="rmsnorm_rsqrt"),
 ]
 
 
@@ -105,4 +110,41 @@ def test_embedding_bwd_baseline_benchmark(
         benchmark,
         unary_bwd_torch,
         [outputs, grads, *fwd_inputs],
+    )
+
+# Almost all transformer models use rmsnorm after the embedding layer and nvFuser should be able to fuse this.
+# To run this benchmark and group results by embedding size and sequence length, use:
+# pytest --benchmark-group-by=group,param:vocab_hidden,param:seq_length,param:dtype test_embedding.py  -k "test_embedding_rmsnorm_inference" --benchmark-eager --benchmark-thunder --benchmark-torchcompile
+@pytest.mark.parametrize("executor", DEFAULT_EXECUTORS)
+@pytest.mark.parametrize("seq_length", SEQ_LENGTHS)
+@pytest.mark.parametrize("vocab_hidden", EMBEDDING_CONFIGS)
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
+@pytest.mark.parametrize("embedding_fn", FNS)
+@pytest.mark.parametrize("rmsnorm_fn", RMS_NORM_FNS)
+def test_embedding_rmsnorm_inference(
+    benchmark,
+    seq_length: int,
+    vocab_hidden: tuple,
+    dtype: torch.dtype,
+    embedding_fn: Callable,
+    rmsnorm_fn: Callable,
+    executor: str,
+):
+    kwargs = {}
+    if executor == "torchcompile":
+        clear_dynamo_cache()
+
+    indices = torch.randint(0, vocab_hidden[0], (seq_length,), device="cuda")
+    embedding_table = torch.randn(vocab_hidden, device="cuda", dtype=dtype)
+    rmsnorm_weights = torch.randn(vocab_hidden[1], device="cuda", dtype=dtype)
+
+    def fn(inputs: list):
+        indices, embedding_table, rmsnorm_weights = inputs
+        return rmsnorm_fn([embedding_fn([indices, embedding_table]), rmsnorm_weights])
+
+    benchmark_fn = with_executor(executor, fn, **kwargs)
+    run_benchmark(
+        benchmark,
+        benchmark_fn,
+        [indices, embedding_table, rmsnorm_weights],
     )
