2ff635fbfa5adf05d04bd2321485da69eed5007e
Ryan Spring <rspring@nvidia.com>
2025-06-25 12:57:00 -0700

    Create nvfp4_scaled_mm

--- cutlass/fp8_blockwise_moe_kernel.cu
+++ cutlass/fp8_blockwise_moe_kernel.cu
@@ -1,3 +1,10 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
 #include <cutlass_utils.h>
 #include <nvf_cutlass.h>
 
--- cutlass/nvf_cutlass.h
+++ cutlass/nvf_cutlass.h
@@ -31,4 +31,18 @@ void fp8_blockwise_scaled_grouped_mm(
     const torch::Tensor& expert_offsets,
     const torch::Tensor& workspace);
 
+void nvfp4_scaled_mm(
+    torch::Tensor& output,
+    const torch::Tensor& a,
+    const torch::Tensor& b,
+    const torch::Tensor& scales_a,
+    const torch::Tensor& scales_b,
+    const torch::Tensor& alpha);
+
+void nvfp4_quantize(
+    torch::Tensor& output,
+    torch::Tensor& output_scale,
+    const torch::Tensor& input,
+    const torch::Tensor& input_scale);
+
 } // namespace nvfuser::cutlass_kernels
