fa917255fbe669df1cd1db9dc3c8c2ca6ceface9
Liqiang Lu <liqiangxl@gmail.com>
2025-06-27 05:47:55 -0700

    remove pybms

--- benchmarks/python/test_embedding.py
+++ benchmarks/python/test_embedding.py
@@ -1,8 +1,6 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025-present NVIDIA CORPORATION & AFFILIATES.
 # All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
-from typing import Callable
-
 import pytest
 
 import torch
@@ -15,7 +13,7 @@ from .core import (
     DEFAULT_EXECUTORS,
 )
 from .global_params import FLOAT_DTYPES
-from .torch_ops import embedding, embedding_indexing, rmsnorm, rmsnorm_rsqrt
+from .torch_ops import embedding
 
 
 # (vocab, hidden) configurations seen in models.
@@ -38,17 +36,6 @@ SEQ_LENGTHS = [
     28672,
     32768,
 ]
-
-FNS = [
-    pytest.param(embedding, id="embedding"),
-    pytest.param(embedding_indexing, id="embedding_indexing"),
-]
-
-RMS_NORM_FNS = [
-    pytest.param(rmsnorm, id="rmsnorm"),
-    pytest.param(rmsnorm_rsqrt, id="rmsnorm_rsqrt"),
-]
-
 
 
 @pytest.mark.parametrize("executor", DEFAULT_EXECUTORS)
@@ -111,58 +98,4 @@ def test_embedding_bwd_baseline_benchmark(
         benchmark,
         unary_bwd_torch,
         [outputs, grads, *fwd_inputs],
-    )
-
-
-
-
-
-# Almost all transformer models use rmsnorm after the embedding layer and nvFuser should be able to fuse this.
-# To run this benchmark and group results by embedding size and sequence length, use:
-# pytest --benchmark-group-by=group,param:vocab_hidden,param:seq_length,param:dtype test_embedding.py  -k "test_embedding_rmsnorm_inference" --benchmark-eager --benchmark-thunder --benchmark-torchcompile
-# pytest --benchmark-group-by=group,param:vocab_hidden,param:seq_length,param:dtype test_embedding.py  -k "test_embedding_rmsnorm_inference" --benchmark-thunder
-# NVFUSER_DUMP=scheduler_params,python_definition pytest -vvvs --benchmark-group-by=group,param:vocab_hidden,param:seq_length,param:dtype ./benchmarks/python/test_embedding.py  -k "test_embedding_rmsnorm_inference and 32064 and float16 and rsqrt and embedding-dtype and 16384" --benchmark-thunder --benchmark-json ./tmp.json 2>&1 |tee 1.log
-@pytest.mark.parametrize("executor", DEFAULT_EXECUTORS)
-@pytest.mark.parametrize("seq_length", SEQ_LENGTHS)
-@pytest.mark.parametrize("vocab_hidden", EMBEDDING_CONFIGS)
-@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
-@pytest.mark.parametrize("embedding_fn", FNS)
-@pytest.mark.parametrize("rmsnorm_fn", RMS_NORM_FNS)
-def test_embedding_rmsnorm_inference(
-    benchmark,
-    seq_length: int,
-    vocab_hidden: tuple,
-    dtype: torch.dtype,
-    embedding_fn: Callable,
-    rmsnorm_fn: Callable,
-    executor: str,
-):
-    kwargs = {}
-    if executor == "torchcompile":
-        clear_dynamo_cache()
-
-    indices = torch.randint(0, vocab_hidden[0], (seq_length,), device="cuda")
-    embedding_table = torch.randn(vocab_hidden, device="cuda", dtype=dtype)
-    rmsnorm_weights = torch.randn(vocab_hidden[1], device="cuda", dtype=dtype)
-
-    def fn(inputs: list):
-        indices, embedding_table, rmsnorm_weights = inputs
-        return rmsnorm_fn(
-            [embedding_fn([indices, embedding_table]).float(), rmsnorm_weights]
-        ).to(dtype)
-
-    benchmark_fn = with_executor(executor, fn, **kwargs)
-
-    def compute_iobytes(seq_length : int, hidden_size: int, dtype: torch.dtype):
-        # indices(seq_length)
-        # embedding(seq_length, hidden_size), should not use vocab size
-        # rmsnorm_weights(hidden_size)
-        # output(seq_length, hidden_size)
-        return int(torch.int.itemsize * seq_length + dtype.itemsize * (2 * seq_length * hidden_size + hidden_size))
-
-    run_benchmark(
-        benchmark,
-        benchmark_fn,
-        [indices, embedding_table, rmsnorm_weights],
-        iobytes=compute_iobytes(seq_length, vocab_hidden[1], dtype),
     )
