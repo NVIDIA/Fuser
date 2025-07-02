4de70510a0735d12453205859b28c4d7a9d15405
Xiang Gao <xgao@nvidia.com>
2025-07-02 12:02:53 -0700

    save

--- csrc/device_lower/validation.cpp
+++ csrc/device_lower/validation.cpp
@@ -609,37 +609,39 @@ class VectorizeValidator : public OptInDispatch {
         "Tv has no definition, cannot validate vectorization:",
         tv);
 
-    auto vector_word_size = v_id->extent()->evaluate().as<int64_t>();
-    auto vector_size_bit =
-        dataTypeSizeBit(
-            tv->getDataType().value(), GpuLower::current()->indexType()) *
-        vector_word_size;
+    if (tv_def->isA<LoadStoreOp>()) {
+      auto vector_word_size = v_id->extent()->evaluate().as<int64_t>();
+      auto vector_size_bit =
+          dataTypeSizeBit(
+              tv->getDataType().value(), GpuLower::current()->indexType()) *
+          vector_word_size;
 
-    // Except for TMem, allow half2, float2, float4 and same sized vtypes.
-    std::vector<int64_t> allowed_vector_sizes_bit = {8, 16, 32, 64, 128};
-    // TMem can vectorize up to 4096 bits.
-    if (auto ldst = dynamic_cast<LoadStoreOp*>(tv_def); ldst != nullptr &&
-        (ldst->opType() == LoadStoreOpType::LdTMem ||
-         ldst->opType() == LoadStoreOpType::StTMem)) {
-      allowed_vector_sizes_bit.push_back(256);
-      allowed_vector_sizes_bit.push_back(512);
-      allowed_vector_sizes_bit.push_back(1024);
-      allowed_vector_sizes_bit.push_back(2048);
-      allowed_vector_sizes_bit.push_back(4096);
+      // Except for TMem, allow half2, float2, float4 and same sized vtypes.
+      std::vector<int64_t> allowed_vector_sizes_bit = {8, 16, 32, 64, 128};
+      // TMem can vectorize up to 4096 bits.
+      if (auto ldst = dynamic_cast<LoadStoreOp*>(tv_def); ldst != nullptr &&
+          (ldst->opType() == LoadStoreOpType::LdTMem ||
+          ldst->opType() == LoadStoreOpType::StTMem)) {
+        allowed_vector_sizes_bit.push_back(256);
+        allowed_vector_sizes_bit.push_back(512);
+        allowed_vector_sizes_bit.push_back(1024);
+        allowed_vector_sizes_bit.push_back(2048);
+        allowed_vector_sizes_bit.push_back(4096);
+      }
+
+      NVF_CHECK(
+          std::find(
+              allowed_vector_sizes_bit.begin(),
+              allowed_vector_sizes_bit.end(),
+              vector_size_bit) != allowed_vector_sizes_bit.end(),
+          "Tried to vectorize a dim resulting in a word size of ",
+          vector_size_bit,
+          " bits, however, vector sizes starting from and including ",
+          allowed_vector_sizes_bit.front(),
+          " bits upto and including ",
+          allowed_vector_sizes_bit.back(),
+          " bits are supported.");
     }
-
-    NVF_CHECK(
-        std::find(
-            allowed_vector_sizes_bit.begin(),
-            allowed_vector_sizes_bit.end(),
-            vector_size_bit) != allowed_vector_sizes_bit.end(),
-        "Tried to vectorize a dim resulting in a word size of ",
-        vector_size_bit,
-        " bits, however, vector sizes starting from and including ",
-        allowed_vector_sizes_bit.front(),
-        " bits upto and including ",
-        allowed_vector_sizes_bit.back(),
-        " bits are supported.");
 
     auto consumer_vectorized_id = getAndValidateVectorizedIdInAllocationDomain(
         v_id, tv, "consumer", tv_def, vector_size_bit);
