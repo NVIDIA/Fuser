998b484d8b66bc3a18dceb5e0e04b4947c53329a
Liqiang Lu <liqiangxl@gmail.com>
2025-07-03 13:43:32 -0700

    don't use preset alloc domain for 1d tma loaded tv

--- csrc/device_lower/pass/allocation.cpp
+++ csrc/device_lower/pass/allocation.cpp
@@ -188,20 +188,21 @@ class AllocationDomainSetup : private kir::IrVisitor {
         // domain is just a permutation of the loop domain, use the
         // set allocation domain. This seems to happen only with
         // AllocationDomainTest.TransposedIntermediate.
-        if (std::any_of(
-                tv->getAllocationDomain().begin(),
-                tv->getAllocationDomain().end(),
-                [](IterDomain* allocation_domain) {
-                  return dynamic_cast<Swizzle*>(
-                             allocation_domain->definition()) != nullptr ||
-                      allocation_domain->getParallelType() ==
-                      ParallelType::Bulk;
-                }) ||
-            std::is_permutation(
-                tv->getLoopDomain().begin(),
-                tv->getLoopDomain().end(),
-                tv->getAllocationDomain().begin(),
-                tv->getAllocationDomain().end())) {
+        if (!ir_utils::isCpAsyncBulk1D(tv->definition()) &&
+            (std::any_of(
+                 tv->getAllocationDomain().begin(),
+                 tv->getAllocationDomain().end(),
+                 [](IterDomain* allocation_domain) {
+                   return dynamic_cast<Swizzle*>(
+                              allocation_domain->definition()) != nullptr ||
+                       allocation_domain->getParallelType() ==
+                       ParallelType::Bulk;
+                 }) ||
+             std::is_permutation(
+                 tv->getLoopDomain().begin(),
+                 tv->getLoopDomain().end(),
+                 tv->getAllocationDomain().begin(),
+                 tv->getAllocationDomain().end()))) {
           use_set_allocation_domain = true;
         }
 
