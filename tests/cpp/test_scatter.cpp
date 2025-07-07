8ef8e3e50e8f8ba61028ddc6b1b50228d81bbe7c
Naoya Maruyama <nmaruyama@nvidia.com>
2025-07-07 16:15:23 -0700

    cleanup

--- tests/cpp/test_scatter.cpp
+++ tests/cpp/test_scatter.cpp
@@ -135,8 +135,7 @@ TEST_F(ScatterTest, BlockCounting) {
   Fusion& fusion = *fusion_ptr.get();
   FusionGuard fg(&fusion);
 
-  // const int64_t m = 64;
-  const int64_t m = 8;
+  const int64_t m = 64;
   const int64_t n = 5;
 
   auto tv0 = makeContigConcreteTensor({n}, DataType::Int);
@@ -166,8 +165,43 @@ TEST_F(ScatterTest, BlockCounting) {
   ke.compile(&fusion, {t0});
   auto outputs = ke.run({t0});
 
-  std::cerr << "t0: " << t0 << "\n";
-  std::cerr << "out: " << outputs[0].as<at::Tensor>() << "\n";
+  testValidate(&fusion, outputs, {t0}, __LINE__, __FILE__);
+}
+
+TEST_F(ScatterTest, GridCounting) {
+  auto fusion_ptr = std::make_unique<Fusion>();
+  Fusion& fusion = *fusion_ptr.get();
+  FusionGuard fg(&fusion);
+
+  const int64_t m = 64;
+  const int64_t n = 5;
+
+  auto tv0 = makeContigConcreteTensor({n}, DataType::Int);
+  fusion.addInput(tv0);
+
+  auto tv1 = set(tv0);
+  auto tv2 = zeros({IrBuilder::create<Val>(m)}, DataType::Int);
+  auto tv3 = ones({IrBuilder::create<Val>(n)}, DataType::Int);
+  auto tv4 = scatter(tv2, 0, tv1, tv3);
+  fusion.addOutput(tv4);
+
+  tv1->axis(0)->parallelize(ParallelType::BIDx);
+  tv2->axis(0)->parallelize(ParallelType::BIDx);
+  tv3->axis(0)->parallelize(ParallelType::BIDx);
+  tv4->axis(0)->parallelize(ParallelType::BIDx);
+
+  // Scatter input must be the same type as the output
+  tv2->setMemoryType(MemoryType::Global);
+
+  fusion.print();
+  fusion.printKernel();
+
+  auto options = at::TensorOptions().dtype(at::kLong).device(at::kCUDA, 0);
+  auto t0 = at::randperm(m, options).slice(0, 0, n);
+
+  KernelExecutor ke;
+  ke.compile(&fusion, {t0});
+  auto outputs = ke.run({t0});
 
   testValidate(&fusion, outputs, {t0}, __LINE__, __FILE__);
 }
