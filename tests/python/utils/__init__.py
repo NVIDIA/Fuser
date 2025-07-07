9c7d2deb52e1ac365f55c3bb14fd90772c417bff
Ryan Spring <rspring@nvidia.com>
2025-07-07 13:14:55 -0700

    Move nvfuser/testing/utils.py to tests/python folder

---
+++ tests/python/utils/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025-present NVIDIA CORPORATION & AFFILIATES.
+# All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+
+from . import utils
+from .utils import *  # noqa: F401,F403
