acbac2e6564073788138c6152e3a5d1a79b9816f
Xiang Gao <xgao@nvidia.com>
2025-07-10 21:59:49 -0700

    fix

--- csrc/scheduler/normalization_utils.cpp
+++ csrc/scheduler/normalization_utils.cpp
@@ -1195,7 +1195,7 @@ bool compileTimeCheck(Fusion* fusion, SchedulerType scheduler_type) {
     if (tv->dtype() != DataType::Index &&
         dataTypeSizeBit(tv->dtype()) % 8 != 0) {
       scheduler_debug_utils::canScheduleRejectReason(
-          schedulerType(), "Does not support sub-byte data types.");
+          scheduler_type, "Does not support sub-byte data types.");
       return false;
     }
   }
