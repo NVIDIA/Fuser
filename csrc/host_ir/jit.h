17b7c6ddcab8ec6b43d379a1e2b732b833454b72
Henry <52993433+wolfcomos@users.noreply.github.com>
2025-07-14 10:25:05 -0700

    Update jit.h

--- csrc/host_ir/jit.h
+++ csrc/host_ir/jit.h
@@ -14,12 +14,10 @@ namespace nvfuser {
 
 constexpr int64_t kHostIrJitCompileThreads = 4;
 struct HostIrJitImpl;
-struct HostIrJitImpl;
 
 class HostIrJit {
  public:
   HostIrJit(
-      std::unique_ptr<hir::HostIrContainer> container,
       std::unique_ptr<hir::HostIrContainer> container,
       int num_threads = kHostIrJitCompileThreads);
 
@@ -30,13 +28,9 @@ class HostIrJit {
   const hir::HostIrContainer& container() const;
   KernelArgumentHolder runWithInputs(const KernelArgumentHolder& args);
 
-  const std::vector<Val*>& inputs() const;
-  const std::vector<Val*>& outputs() const;
-  const hir::HostIrContainer& container() const;
   ~HostIrJit();
 
  private:
-  std::unique_ptr<HostIrJitImpl> pimpl_;
   std::unique_ptr<HostIrJitImpl> pimpl_;
 };
 } // namespace nvfuser
