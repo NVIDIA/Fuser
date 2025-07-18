04278cb9dc0fe8b09b2718ee0b1d84947ccc2201
Jacob Hinkle <jhinkle@nvidia.com>
2025-07-18 15:01:46 -0400

    Fix broken merge

--- tests/cpp/test_matmul.cpp
+++ tests/cpp/test_matmul.cpp
@@ -5746,7 +5746,7 @@ TEST_F(HopperMatmulTest, PingPongPersistent) {
   mparams.circular_buffer_options.smem_circular_buffer_prefetch_gap = 2;
   mparams.splitk_factor = 1;
   mparams.use_smem_epilogue = true;
-  mparams.cluster_dims = {1, 2, 1};
+  mparams.cluster_dims = {1, 2};
   mparams.promote_prologue_smem_reuse = true;
   mparams.grid_traversal_factor = {16, 8};
 
@@ -5800,7 +5800,7 @@ TEST_F(HopperMatmulTest, MisalignedReadTMAStore) {
   mparams.circular_buffer_options.smem_circular_buffer_stage = 0;
   mparams.splitk_factor = 1;
   mparams.use_smem_epilogue = true;
-  mparams.cluster_dims = {1, 1, 1};
+  mparams.cluster_dims = {1, 1};
   mparams.promote_prologue_smem_reuse = false;
 
   SchedulerEntry::makeSchedulerInstance(SchedulerType::Matmul)
