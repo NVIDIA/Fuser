eb9c1f318d7199111692f6984f8252fc515ea7e5
Priya Mishra <26priya11@gmail.com>
2025-07-23 15:54:44 -0700

    lintrunner

--- csrc/preseg_passes/decompose_reshardings.cpp
+++ csrc/preseg_passes/decompose_reshardings.cpp
@@ -247,7 +247,8 @@ void canonicalizeLoopDomain(TensorView* tv) {
 void decomposeRowParallelLinearWithBias(Fusion* fusion) {
   // Iterate backwards over fusion expressions to avoid processing
   // expressions that have been deleted.
-  // Recall that replaceValInAllExprInputsAndFusionOutputs invalidates consumers.
+  // Recall that replaceValInAllExprInputsAndFusionOutputs invalidates
+  // consumers.
   for (Expr* e : fusion->exprs() | std::views::reverse) {
     auto* linear_op = dynamic_cast<LinearOp*>(e);
     if (linear_op == nullptr) {
