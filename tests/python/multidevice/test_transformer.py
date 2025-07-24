3249cf34e63b093d151aa4eeb68a77b1575bcab4
Priya Mishra <26priya11@gmail.com>
2025-07-24 16:29:39 -0700

    cleanup

--- tests/python/multidevice/test_transformer.py
+++ tests/python/multidevice/test_transformer.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 import pytest
 
-from sympy import E
 import torch
 import torch.nn.functional as F
 
@@ -103,33 +102,27 @@ def test_grouped_mlp(multidevice_test):
     torch.testing.assert_close(out.cpu(), expected_out, rtol=1.0, atol=float("inf"))
 
 
-# The following two benchmarks micro-benchmarks the forward pass and the
-# backprop of a sharded Transformer block used in GPT-3.
+# The following benchmarks the fusion generated from NanoGPTBlockBenchmark in Thunder.
+# To generate the fusion, use the following snippet which turns on the necessary options
+# to get a single nvfuser definition.
+# ```
+#   bench = NanoGPTBlockBenchmark(
+#     config="gpt2", device="cuda:0", dtype=thunder.bfloat16, requires_grad=True
+#   )
+#   args, kwargs = bench.make_batch()
 #
-# The single-GPU nvFusions are
-# dumped from Thunder. To regenerate the nvFusions and the inputs, run the
-# following:
+#   jfn = thunder.jit(
+#       bench.fn(),
+#       executors=[nvfuserex],
+#       nv_enable_sdpa=True,
+#       nv_enable_matmul=True,
+#       nv_enable_linear=True,
+#       disable_replace_uniform=True,
+#   )
+#   out = jfn(*args, **kwargs)
+#   print (thunder.last_traces(jfn)[-1].python_ctx()['nvFusion0'].last_used)
+# ```
 #
-# 1. `git clone https://github.com/Lightning-AI/lightning-thunder.git`
-# 2. `git fetch origin wjy/sharded`
-# 3. `git checkout wjy/sharded`
-#    This branch adds the GPT-3 block benchmark, turns on certain knobs so the
-#    entire Transformer block fits into one nvFusion, and prints out the repro.
-# 4. `pytest thunder/benchmarks/targets.py -k 'test_nanogpt_block[backward-thunder]' -s`
-#
-# In stdout, you'll find the forward nvFusion executed once followed by the
-# backward nvFusion executed many times.
-#
-# For future reference, the nvFusions below are generated with Thunder version
-# https://github.com/Lightning-AI/lightning-thunder/commit/953a91477cec792b6e694650cd2466b871af812d.
-# The Thunder traces are
-# https://gist.github.com/wujingyue/b111aa8b8d92067fc6004f5d0488dd27.
-#
-# Based on the single-GPU nvFusions, more changes are applied to generate the multi-GPU nvFusions.
-# 1. Replace magic values with variables for flexibility and readability.
-# 2. Split device dimensions and parallelize them.
-# 3. Decompose the second linear layer in MLP so the matmul result can be allreduced.
-# 4. Rename the inputs and outputs for readability.
 class TransformerForwardFusion(FusionDefinition):
     def __init__(self, num_devices, batch, sequence, head, hidden):
         super().__init__()
@@ -147,58 +140,158 @@ class TransformerForwardFusion(FusionDefinition):
             self._head,
             self._hidden,
         )
-        self.inp = self.define_tensor(shape=[b, s, e], contiguity=[True, True, True], dtype=DataType.BFloat16, is_cpu=False, stride_order=[2, 1, 0])
-        self.layernorm0_weight = self.define_tensor(shape=[e], contiguity=[True], dtype=DataType.BFloat16, is_cpu=False, stride_order=[0])
-        self.layernorm0_bias = self.define_tensor(shape=[e], contiguity=[True], dtype=DataType.BFloat16, is_cpu=False, stride_order=[0])
-        self.mha_linear0_weight = self.define_tensor(shape=[e * 3, e], contiguity=[True, True], dtype=DataType.BFloat16, is_cpu=False, stride_order=[1, 0])
-        self.mha_linear0_bias = self.define_tensor(shape=[e * 3], contiguity=[True], dtype=DataType.BFloat16, is_cpu=False, stride_order=[0])
-        self.mha_linear1_weight = self.define_tensor(shape=[e, e], contiguity=[True, True], dtype=DataType.BFloat16, is_cpu=False, stride_order=[1, 0])
-        self.mha_linear1_bias = self.define_tensor(shape=[e], contiguity=[True], dtype=DataType.BFloat16, is_cpu=False, stride_order=[0])
-        self.layernorm1_weight = self.define_tensor(shape=[e], contiguity=[True], dtype=DataType.BFloat16, is_cpu=False, stride_order=[0])
-        self.layernorm1_bias = self.define_tensor(shape=[e], contiguity=[True], dtype=DataType.BFloat16, is_cpu=False, stride_order=[0])
-        self.mlp_linear0_weight = self.define_tensor(shape=[e * 4, e], contiguity=[True, True], dtype=DataType.BFloat16, is_cpu=False, stride_order=[1, 0])
-        self.mlp_linear0_bias = self.define_tensor(shape=[e * 4], contiguity=[True], dtype=DataType.BFloat16, is_cpu=False, stride_order=[0])
-        self.mlp_linear1_weight = self.define_tensor(shape=[e, e * 4], contiguity=[True, True], dtype=DataType.BFloat16, is_cpu=False, stride_order=[1, 0])
-        self.mlp_linear1_bias = self.define_tensor(shape=[e], contiguity=[True], dtype=DataType.BFloat16, is_cpu=False, stride_order=[0])
-        
+        self.inp = self.define_tensor(
+            shape=[b, s, e],
+            contiguity=[True, True, True],
+            dtype=DataType.BFloat16,
+            is_cpu=False,
+            stride_order=[2, 1, 0],
+        )
+        self.layernorm0_weight = self.define_tensor(
+            shape=[e],
+            contiguity=[True],
+            dtype=DataType.BFloat16,
+            is_cpu=False,
+            stride_order=[0],
+        )
+        self.layernorm0_bias = self.define_tensor(
+            shape=[e],
+            contiguity=[True],
+            dtype=DataType.BFloat16,
+            is_cpu=False,
+            stride_order=[0],
+        )
+        self.mha_linear0_weight = self.define_tensor(
+            shape=[e * 3, e],
+            contiguity=[True, True],
+            dtype=DataType.BFloat16,
+            is_cpu=False,
+            stride_order=[1, 0],
+        )
+        self.mha_linear0_bias = self.define_tensor(
+            shape=[e * 3],
+            contiguity=[True],
+            dtype=DataType.BFloat16,
+            is_cpu=False,
+            stride_order=[0],
+        )
+        self.mha_linear1_weight = self.define_tensor(
+            shape=[e, e],
+            contiguity=[True, True],
+            dtype=DataType.BFloat16,
+            is_cpu=False,
+            stride_order=[1, 0],
+        )
+        self.mha_linear1_bias = self.define_tensor(
+            shape=[e],
+            contiguity=[True],
+            dtype=DataType.BFloat16,
+            is_cpu=False,
+            stride_order=[0],
+        )
+        self.layernorm1_weight = self.define_tensor(
+            shape=[e],
+            contiguity=[True],
+            dtype=DataType.BFloat16,
+            is_cpu=False,
+            stride_order=[0],
+        )
+        self.layernorm1_bias = self.define_tensor(
+            shape=[e],
+            contiguity=[True],
+            dtype=DataType.BFloat16,
+            is_cpu=False,
+            stride_order=[0],
+        )
+        self.mlp_linear0_weight = self.define_tensor(
+            shape=[e * 4, e],
+            contiguity=[True, True],
+            dtype=DataType.BFloat16,
+            is_cpu=False,
+            stride_order=[1, 0],
+        )
+        self.mlp_linear0_bias = self.define_tensor(
+            shape=[e * 4],
+            contiguity=[True],
+            dtype=DataType.BFloat16,
+            is_cpu=False,
+            stride_order=[0],
+        )
+        self.mlp_linear1_weight = self.define_tensor(
+            shape=[e, e * 4],
+            contiguity=[True, True],
+            dtype=DataType.BFloat16,
+            is_cpu=False,
+            stride_order=[1, 0],
+        )
+        self.mlp_linear1_bias = self.define_tensor(
+            shape=[e],
+            contiguity=[True],
+            dtype=DataType.BFloat16,
+            is_cpu=False,
+            stride_order=[0],
+        )
+
         T13 = self.ops.cast(self.inp, dtype=DataType.Float)
-        T14, layernorm0_mean = self.ops.var_mean(T13, dims=[2], correction=0, keepdim=False)
+        T14, layernorm0_mean = self.ops.var_mean(
+            T13, dims=[2], correction=0, keepdim=False
+        )
         T20 = self.ops.broadcast_in_dim(T14, shape=[b, s, 1], broadcast_dims=[0, 1])
-        T25 = self.ops.broadcast_in_dim(layernorm0_mean, shape=[b, s, 1], broadcast_dims=[0, 1])
+        T25 = self.ops.broadcast_in_dim(
+            layernorm0_mean, shape=[b, s, 1], broadcast_dims=[0, 1]
+        )
         S26 = self.define_scalar(1.00000e-05, dtype=DataType.Double)
         T27 = self.ops.add(T20, S26)
         layernorm0_rstd = self.ops.rsqrt(T27)
         T33 = self.ops.broadcast_in_dim(T25, shape=[b, s, e], broadcast_dims=[0, 1, 2])
         T34 = self.ops.sub(T13, T33)
-        T39 = self.ops.broadcast_in_dim(layernorm0_rstd, shape=[b, s, e], broadcast_dims=[0, 1, 2])
+        T39 = self.ops.broadcast_in_dim(
+            layernorm0_rstd, shape=[b, s, e], broadcast_dims=[0, 1, 2]
+        )
         T40 = self.ops.mul(T34, T39)
-        T45 = self.ops.broadcast_in_dim(self.layernorm0_weight, shape=[b, s, e], broadcast_dims=[2])
+        T45 = self.ops.broadcast_in_dim(
+            self.layernorm0_weight, shape=[b, s, e], broadcast_dims=[2]
+        )
         T46 = self.ops.cast(T45, dtype=DataType.Float)
         T47 = self.ops.mul(T40, T46)
-        T52 = self.ops.broadcast_in_dim(self.layernorm0_bias, shape=[b, s, e], broadcast_dims=[2])
+        T52 = self.ops.broadcast_in_dim(
+            self.layernorm0_bias, shape=[b, s, e], broadcast_dims=[2]
+        )
         T53 = self.ops.cast(T52, dtype=DataType.Float)
         T54 = self.ops.add(T47, T53)
         T55 = self.ops.cast(T54, dtype=DataType.BFloat16)
-        mha_linear0_out = self.ops.linear(T55, self.mha_linear0_weight, self.mha_linear0_bias)
-        
+        mha_linear0_out = self.ops.linear(
+            T55, self.mha_linear0_weight, self.mha_linear0_bias
+        )
+
         # Reshape before slice to avoid slicing a tensor along sharded dimension.
         # This is different from the single-GPU definition obtained from Thunder.
-        T57 = self.ops.reshape(mha_linear0_out, new_shape=[b, s, h, 3*e//h])
-        T69 = self.ops.slice(T57, start_indices=[0, 0, 0, 0], end_indices=[b, s, h, e//h])
-        T82 = self.ops.slice(T57, start_indices=[0, 0, 0, e//h], end_indices=[b, s, h, 2*e//h])
-        T95 = self.ops.slice(T57, start_indices=[0, 0, 0, 2*e//h], end_indices=[b, s, h, 3*e//h])
-        
+        T57 = self.ops.reshape(mha_linear0_out, new_shape=[b, s, h, 3 * e // h])
+        T69 = self.ops.slice(
+            T57, start_indices=[0, 0, 0, 0], end_indices=[b, s, h, e // h]
+        )
+        T82 = self.ops.slice(
+            T57, start_indices=[0, 0, 0, e // h], end_indices=[b, s, h, 2 * e // h]
+        )
+        T95 = self.ops.slice(
+            T57, start_indices=[0, 0, 0, 2 * e // h], end_indices=[b, s, h, 3 * e // h]
+        )
+
         T102 = self.ops.permute(T82, dims=[0, 2, 1, 3])
         T109 = self.ops.permute(T69, dims=[0, 2, 1, 3])
         T116 = self.ops.permute(T95, dims=[0, 2, 1, 3])
-        
+
         S117 = self.define_scalar(0.100000, dtype=DataType.Double)
         S118 = self.define_scalar(True, dtype=DataType.Bool)
-        sdpa_out, sdpa_logsum_exp, sdpa_seed, sdpa_offset = self.ops.sdpfa_fwd(T109, T102, T116, S117, S118, None)
+        sdpa_out, sdpa_logsum_exp, sdpa_seed, sdpa_offset = self.ops.sdpfa_fwd(
+            T109, T102, T116, S117, S118, None
+        )
         T123 = self.ops.permute(sdpa_out, dims=[0, 2, 1, 3])
         T124 = self.ops.stride_order(T123, stride_order=[3, 2, 1, 0])
         T129 = self.ops.reshape(T124, new_shape=[b, s, e])
-        mha_linear1_out = self.ops.linear(T129, self.mha_linear1_weight, self.mha_linear1_bias)
+        mha_linear1_out = self.ops.linear(
+            T129, self.mha_linear1_weight, self.mha_linear1_bias
+        )
         S131 = self.define_scalar(0.00000, dtype=DataType.Double)
         S132 = self.define_scalar(1.00000, dtype=DataType.Double)
         T137 = self.ops.uniform(S131, S132, shape=[b, s, e], dtype=DataType.BFloat16)
@@ -210,24 +303,38 @@ class TransformerForwardFusion(FusionDefinition):
         S143 = self.define_scalar(1.11111, dtype=DataType.Double)
         T144 = self.ops.mul(T142, S143)
         T145 = self.ops.add(T13, T144)
-        T146, layernorm1_mean = self.ops.var_mean(T145, dims=[2], correction=0, keepdim=False)
+        T146, layernorm1_mean = self.ops.var_mean(
+            T145, dims=[2], correction=0, keepdim=False
+        )
         T152 = self.ops.broadcast_in_dim(T146, shape=[b, s, 1], broadcast_dims=[0, 1])
-        T157 = self.ops.broadcast_in_dim(layernorm1_mean, shape=[b, s, 1], broadcast_dims=[0, 1])
+        T157 = self.ops.broadcast_in_dim(
+            layernorm1_mean, shape=[b, s, 1], broadcast_dims=[0, 1]
+        )
         S158 = self.define_scalar(1.00000e-05, dtype=DataType.Double)
         T159 = self.ops.add(T152, S158)
         layernorm1_rstd = self.ops.rsqrt(T159)
-        T165 = self.ops.broadcast_in_dim(T157, shape=[b, s, e], broadcast_dims=[0, 1, 2])
+        T165 = self.ops.broadcast_in_dim(
+            T157, shape=[b, s, e], broadcast_dims=[0, 1, 2]
+        )
         T166 = self.ops.sub(T145, T165)
-        T171 = self.ops.broadcast_in_dim(layernorm1_rstd, shape=[b, s, e], broadcast_dims=[0, 1, 2])
+        T171 = self.ops.broadcast_in_dim(
+            layernorm1_rstd, shape=[b, s, e], broadcast_dims=[0, 1, 2]
+        )
         T172 = self.ops.mul(T166, T171)
-        T177 = self.ops.broadcast_in_dim(self.layernorm1_weight, shape=[b, s, e], broadcast_dims=[2])
+        T177 = self.ops.broadcast_in_dim(
+            self.layernorm1_weight, shape=[b, s, e], broadcast_dims=[2]
+        )
         T178 = self.ops.cast(T177, dtype=DataType.Float)
         T179 = self.ops.mul(T172, T178)
-        T184 = self.ops.broadcast_in_dim(self.layernorm1_bias, shape=[b, s, e], broadcast_dims=[2])
+        T184 = self.ops.broadcast_in_dim(
+            self.layernorm1_bias, shape=[b, s, e], broadcast_dims=[2]
+        )
         T185 = self.ops.cast(T184, dtype=DataType.Float)
         T186 = self.ops.add(T179, T185)
         T187 = self.ops.cast(T186, dtype=DataType.BFloat16)
-        mlp_linear0_out = self.ops.linear(T187, self.mlp_linear0_weight, self.mlp_linear0_bias)
+        mlp_linear0_out = self.ops.linear(
+            T187, self.mlp_linear0_weight, self.mlp_linear0_bias
+        )
         T189 = self.ops.cast(mlp_linear0_out, dtype=DataType.Float)
         T190 = self.ops.mul(T189, T189)
         T191 = self.ops.mul(T190, T189)
@@ -243,7 +350,9 @@ class TransformerForwardFusion(FusionDefinition):
         T201 = self.ops.add(S200, T199)
         T202 = self.ops.mul(T193, T201)
         T203 = self.ops.cast(T202, dtype=DataType.BFloat16)
-        mlp_linear1_out = self.ops.linear(T203, self.mlp_linear1_weight, self.mlp_linear1_bias)
+        mlp_linear1_out = self.ops.linear(
+            T203, self.mlp_linear1_weight, self.mlp_linear1_bias
+        )
         S205 = self.define_scalar(0.00000, dtype=DataType.Double)
         S206 = self.define_scalar(1.00000, dtype=DataType.Double)
         T211 = self.ops.uniform(S205, S206, shape=[b, s, e], dtype=DataType.BFloat16)
@@ -270,7 +379,6 @@ class TransformerForwardFusion(FusionDefinition):
         self.add_output(mlp_linear0_out)
         self.add_output(mlp_dropout_mask)
         self.add_output(out)
-        
 
     def multidevice_schedule(self):
         mesh = nvfuser.DeviceMesh(range(self._num_devices))
@@ -299,7 +407,6 @@ class TransformerForwardFusion(FusionDefinition):
         ]:
             self.sched.split(tv, 0, self._num_devices, False)
             self.sched.parallelize(tv, 0, nvfuser.ParallelType.mesh_x)
-
 
         for tv in [
             self.mha_linear1_weight,
@@ -366,7 +473,7 @@ def test_transformer_forward(multidevice_test, benchmark):
     mlp_linear1_weight = torch.testing.make_tensor(
         e, e * 4, dtype=torch.bfloat16, device="cpu"
     )
-    
+
     # See TransformerForwardFusion.definition for the meanings of these
     # arguments. They are passed in in the same order as the `define_scalar`s
     # and `define_tensor`s.
@@ -430,6 +537,33 @@ def test_transformer_forward(multidevice_test, benchmark):
     benchmark.pedantic(benchmark_fn, rounds=5)
 
 
+# The following micro-benchmarks the
+# backprop of a sharded Transformer block used in GPT-3.
+#
+# The single-GPU nvFusions are
+# dumped from Thunder. To regenerate the nvFusions and the inputs, run the
+# following:
+#
+# 1. `git clone https://github.com/Lightning-AI/lightning-thunder.git`
+# 2. `git fetch origin wjy/sharded`
+# 3. `git checkout wjy/sharded`
+#    This branch adds the GPT-3 block benchmark, turns on certain knobs so the
+#    entire Transformer block fits into one nvFusion, and prints out the repro.
+# 4. `pytest thunder/benchmarks/targets.py -k 'test_nanogpt_block[backward-thunder]' -s`
+#
+# In stdout, you'll find the forward nvFusion executed once followed by the
+# backward nvFusion executed many times.
+#
+# For future reference, the nvFusions below are generated with Thunder version
+# https://github.com/Lightning-AI/lightning-thunder/commit/953a91477cec792b6e694650cd2466b871af812d.
+# The Thunder traces are
+# https://gist.github.com/wujingyue/b111aa8b8d92067fc6004f5d0488dd27.
+#
+# Based on the single-GPU nvFusions, more changes are applied to generate the multi-GPU nvFusions.
+# 1. Replace magic values with variables for flexibility and readability.
+# 2. Split device dimensions and parallelize them.
+# 3. Decompose the second linear layer in MLP so the matmul result can be allreduced.
+# 4. Rename the inputs and outputs for readability.
 # All tensors are replicated to all devices at this moment; future PRs will try
 # to shard them.
 class TransformerBackwardFusion(FusionDefinition):
