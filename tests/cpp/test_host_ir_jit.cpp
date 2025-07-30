6c4ee8915e20e2571ebbbbde94cd5fea3addda55
Hanlin Bi <hanlinb@nvidia.com>
2025-07-29 22:55:03 -0700

    init files

--- tests/cpp/test_host_ir_jit.cpp
+++ tests/cpp/test_host_ir_jit.cpp
@@ -23,24 +23,77 @@ namespace hir {
 
 using HostIrJitTest = NVFuserTest;
 // Build with: python setup.py install --build-with-host-ir-jit
-TEST_F(HostIrJitTest, TestHostIrJit) {
-  auto hic = std::make_unique<HostIrContainer>(1);
-  FusionGuard::setCurFusion(hic.get());
+TEST_F(HostIrJitTest, PointwiseScheduler) {
+  auto fusion_ptr = std::make_unique<Fusion>();
+  {
+    // defining simple fusion
+    auto fusion = fusion_ptr.get();
+    FusionGuard fg(fusion);
 
-  auto hic_in = makeSymbolicTensor(2);
-  auto hic_out = set(hic_in);
+    TensorView* tv0 =
+        TensorViewBuilder().ndims(2).contiguity({true, true}).build();
+    fusion->addInput(tv0);
+    auto tv1 = add(tv0, IrBuilder::create<Val>(1.0, DataType::Float));
+    fusion->addOutput(tv1);
+  }
 
-  hic->addInput(hic_in);
-  hic->addOutput(hic_out);
+  {
+    Fusion fusion_clone = *fusion_ptr;
+    Fusion* fusion = &fusion_clone;
+    FusionGuard fg(fusion);
+    // create inputs
+    auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
+    at::Tensor t0 = at::empty_strided({1024, 128}, {128, 1}, options);
 
-  HostIrJit jit(std::move(hic));
-  auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
-  at::Tensor t0 = at::randn({32, 32}, options);
-  auto output_args = jit.runWithInput({{hic_in, t0}});
-  auto output = output_args[0].as<at::Tensor>();
+    KernelArgumentHolder runtime_inputs({t0});
+    SchedulerType scheduler_type = SchedulerType::PointWise;
 
-  EXPECT_EQ(output.sizes(), t0.sizes());
-  EXPECT_EQ(output.strides(), t0.strides());
+    std::cout << "before scheduling fusion" << std::endl;
+    fusion->printMath();
+    fusion->printTransforms();
+
+    // scheduling fusion using pointwise scheduler
+    auto heuristic_params = SchedulerEntry::scheduleWith(
+        fusion, scheduler_type, runtime_inputs, /*validate_scheduler=*/true);
+
+    std::cout << "after scheduling fusion" << std::endl;
+    fusion->printMath();
+    fusion->printTransforms();
+
+    // print heuristic params
+    std::cout << heuristic_params->toString() << std::endl;
+
+    // compile and run
+    auto ke = std::make_unique<KernelExecutor>();
+    ke->compile(fusion, runtime_inputs, heuristic_params->lparams);
+    auto cg_outputs = ke->run(runtime_inputs, {}, heuristic_params->lparams);
+
+    testValidate(fusion, cg_outputs, {t0}, __LINE__, __FILE__);
+  }
+
+  {
+    Fusion fusion_clone = *fusion_ptr;
+    Fusion* fusion = &fusion_clone;
+    FusionGuard fg(fusion);
+    auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
+    // a different problem size
+    at::Tensor t0 = at::empty_strided({2048, 128}, {128, 1}, options);
+
+    KernelArgumentHolder runtime_inputs({t0});
+    SchedulerType scheduler_type = SchedulerType::PointWise;
+    // note: we probably would want to use the get heuristic function
+    // instead of running schedule again.
+    auto heuristic_params = SchedulerEntry::scheduleWith(
+        fusion, scheduler_type, runtime_inputs, /*validate_scheduler=*/true);
+
+    // print heuristic params
+    std::cout << heuristic_params->toString() << std::endl;
+
+    auto ke = std::make_unique<KernelExecutor>();
+    ke->compile(fusion, runtime_inputs, heuristic_params->lparams);
+    auto cg_outputs = ke->run(runtime_inputs, {}, heuristic_params->lparams);
+    testValidate(fusion, cg_outputs, {t0}, __LINE__, __FILE__);
+  }
 }
 
 } // namespace hir
