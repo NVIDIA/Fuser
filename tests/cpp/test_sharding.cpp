78408acda707cb53377db250ad5c708bf76adf57
Jingyue Wu <wujingyue@gmail.com>
2025-08-01 22:01:06 -0700

    Change the underlying storage of DeviceMesh to Tensor (#4898)

--- tests/cpp/test_sharding.cpp
+++ tests/cpp/test_sharding.cpp
@@ -209,10 +209,7 @@ TEST_P(ShardingTest, ComputeIndex) {
 
 TEST_F(ShardingTest, MultiDimDeviceMesh) {
   DeviceMesh mesh({3, 4, 1, 0, 8, 2}, {2, 3});
-  // Shape not consistent with number of devices
-  EXPECT_ANY_THROW(DeviceMesh({1, 2}, {2, 3}));
-  // Duplicates in DeviceMesh
-  EXPECT_ANY_THROW(DeviceMesh({1, 2, 0, 2}, {2, 3}));
+  EXPECT_ANY_THROW(DeviceMesh({1, 2, 0, 2}, {2, 2}));
 
   std::vector<int64_t> local_indices_8 = {1, 1};
   std::vector<int64_t> local_indices_1 = {0, 2};
