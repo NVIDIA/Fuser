8e294ba36081470ff35ee3d99efe727886517170
Jacob Hinkle <jhinkle@nvidia.com>
2025-08-13 08:54:55 -0400

    First draft of task sorter

---
+++ csrc/graph/task_graph.cpp
@@ -0,0 +1,222 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+#include <exceptions.h>
+#include <graph/task_graph.h>
+#include <utils.h>
+
+#include <set>
+
+namespace nvfuser {
+
+void TaskGraph::validateSteps(const std::vector<Step>& steps) const {
+  // First find any Data in the graph that has no definition. This must be
+  // preallocated before running the program, so we initialize allocated and
+  // high_water_mark to the sum of their sizes.
+  TaskGraph::Size allocated = getInitialAllocation();
+  TaskGraph::Size high_water_mark = allocated;
+
+  std::vector<TaskId> future_uses = num_uses_;
+  std::vector<DataId> outstanding_dependencies = num_dependencies_;
+
+  // Now we are ready to process steps
+  for (const Step& step : steps) {
+    const Task& task = getTask(step.task);
+
+    // Allocate outputs
+    for (const DataId output_id : task.outputs) {
+      const Data& data = getData(output_id);
+      if (!data.input_alias.has_value()) {
+        // Don't allocate outputs if they are reusing input memory
+        allocated += data.size;
+      }
+    }
+
+    // Add temporary space
+    allocated += task.temp_space;
+
+    // This is the most space we will use, so update high water mark here
+    high_water_mark = std::max(high_water_mark, allocated);
+    NVF_ERROR(step.high_water_mark == high_water_mark);
+
+    // reduce use count for inputs and free them if possible
+    for (const DataId input_id : task.inputs) {
+      if (--future_uses.at((size_t)input_id)) {
+        // There are no more uses for this Data, so free it if we're allowed to
+        const Data& data = getData(input_id);
+        if (data.can_free) {
+          allocated -= data.size;
+        }
+      }
+    }
+
+    // step.allocated indicates how much space is allocated _upon completion_ of this step
+    NVF_ERROR(step.allocated == allocated);
+  }
+}
+
+namespace {
+
+//! [Backtracking algorithm to find optimal topological ordering]
+//!
+//! If validate==true, then we will validate the steps vector after every
+//! backtracking step.
+//!
+//! c.f. https://en.wikipedia.org/wiki/Topological_sorting#Kahn's_algorithm
+class TaskSorter {
+ public:
+  TaskSorter(const TaskGraph& graph, bool validate, int64_t max_iters)
+      : graph_(graph), validate_(validate), max_iters_(max_iters) {
+    sort();
+  }
+
+  const std::vector<TaskGraph::Step>& steps() const {
+    return steps_;
+  }
+
+ private:
+  inline void validate() const {
+    if (validate_) {
+      graph_.validateSteps(steps_);
+    }
+  }
+
+  //! This pushes a step indicating that we should execute the given task next.
+  void advance(TaskGraph::TaskId task_id) {
+    TaskGraph::Size allocated = 0;
+    TaskGraph::Size high_water_mark = 0;
+    if (steps_.empty()) {
+      // (Re-)Initialize allocated and high_water_mark to starting values
+      allocated = graph_.getInitialAllocation();
+      high_water_mark = allocated;
+    } else {
+      allocated = steps_.back().allocated;
+      high_water_mark = steps_.back().high_water_mark;
+    }
+
+    // Compute the new allocated amount and high water mark for this step
+    const TaskGraph::Task& task = graph_.getTask(task_id);
+    
+    for (const TaskGraph::DataId output_id : task.outputs) {
+      const TaskGraph::Data& output = graph_.getData(output_id);
+      // Allocate outputs if not aliased
+      if (!output.input_alias.has_value()) {
+        allocated += output.size;
+      }
+
+      // Update outstanding_dependencies_ and ready_tasks_ for each use
+      for (const TaskGraph::TaskId use_id : output.uses) {
+        if (--outstanding_dependencies_.at((size_t)use_id) == 0) {
+          ready_tasks_.insert(use_id);
+        }
+      }
+    }
+
+    // Add temp space
+    allocated += task.temp_space;
+
+    // Update high water mark
+    high_water_mark = std::max(high_water_mark, allocated);
+
+    // Decrement future_uses_ and deallocate dead inputs
+    for (const TaskGraph::DataId input_id : task.inputs) {
+      const TaskGraph::Data& input = graph_.getData(input_id);
+      if (--future_uses_.at((size_t)input_id) == 0) {
+        if (input.can_free) {
+          allocated -= input.size;
+        }
+      }
+    }
+
+    steps_.emplace_back(task_id, allocated, high_water_mark);
+  }
+
+  //! Backtrack a single step. This returns the TaskId of the step that was
+  //! popped.
+  TaskGraph::TaskId backtrack() {
+    validate();
+    TaskGraph::TaskId last_task_id = steps_.back().task;
+    const TaskGraph::Task& last_task = graph_.getTask(last_task_id);
+    steps_.pop_back();
+
+    ready_tasks_.erase(last_task_id);
+
+    // Update outstanding_dependencies to reflect that the outputs of last_task are no longer available
+    for (const TaskGraph::DataId& output_id: last_task.outputs) {
+      const TaskGraph::Data& output = graph_.getData(output_id);
+      for (const TaskGraph::TaskId use_id : output.uses) {
+        outstanding_dependencies_.at((size_t)use_id)++;
+      }
+    }
+
+    // Update future_uses to reflect that the inputs to last_task will need to compute last_task later
+    for (const TaskGraph::DataId& input_id: last_task.inputs) {
+      future_uses_.at((size_t)input_id)++;
+    }
+
+    return last_task_id;
+  }
+
+  void sort() {
+    // Set up outstanding_dependencies_, future_uses_, and ready_tasks_
+    outstanding_dependencies_.reserve(graph_.numTasks());
+    for (const TaskGraph::TaskId task_id : arange(graph_.numTasks())) {
+      const TaskGraph::Task& task = graph_.getTask(task_id);
+      TaskGraph::DataId inputs_to_compute = 0;
+      for (const TaskGraph::DataId data_id : task.inputs) {
+        const TaskGraph::Data& data = graph_.getData(data_id);
+        if (data.definition.has_value()) {
+          // Skip counting input data since these are available before we start
+          inputs_to_compute++; 
+        }
+      }
+      outstanding_dependencies_.push_back(inputs_to_compute);
+      if (inputs_to_compute == 0) {
+        ready_tasks_.insert(task_id);
+      }
+    }
+
+    future_uses_.reserve(graph_.numData());
+    for (const TaskGraph::DataId data_id : arange(graph_.numData())) {
+      const TaskGraph::Data& data = graph_.getData(data_id);
+      future_uses_.push_back(data.uses.size());
+    }
+
+    for (int64_t _ : arange(max_iters_)) {
+    }
+
+    // Validate final result
+    NVF_ERROR(steps_.size() == graph_.numTasks());
+    validate();
+  }
+
+ private:
+  const TaskGraph& graph_;
+  bool validate_;
+  int64_t max_iters_;
+  std::vector<TaskGraph::Step> steps_;
+
+  //! There is one entry here for each task and indicating how many
+  //! dependencies are currently unmet. When this reaches zero the task becomes ready.
+  std::vector<TaskGraph::DataId> outstanding_dependencies_;
+
+  //! There is one entry here for each Data and indicating how many uses there
+  //! are remaining. When it reaches zero, the Data can be freed if allowed.
+  std::vector<TaskGraph::TaskId> future_uses_;
+
+  //! This holds all candidates for the next step, sorted by ID
+  std::set<TaskGraph::TaskId> ready_tasks_;
+};
+
+} // namespace
+
+std::vector<TaskGraph::Step> TaskGraph::findOptimalOrder() const {
+  TaskSorter sorter(*this, /*validate=*/true);
+  return sorter.steps();
+}
+
+} // namespace nvfuser
---
+++ csrc/graph/task_graph.h
@@ -0,0 +1,105 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+#pragma once
+
+#include <cstdint>
+#include <optional>
+#include <vector>
+
+namespace nvfuser {
+
+class TaskGraph {
+ public:
+  using TaskId = int16_t;
+  using DataId = int16_t;
+  using Size = int64_t;
+
+  //! A Task consumes some input Data and produces some output Data. To do so, it might use some intermediate space.
+  struct Task {
+    std::vector<DataId> inputs;
+    std::vector<DataId> outputs;
+    //! This amount of temporary space is required only while executing the Task and is immediately freed afterward
+    Size temp_space = 0;
+  };
+
+  struct Data {
+    std::optional<TaskId> definition;
+    std::vector<TaskId> uses;
+    // If set, this means we do not allocate a new output when executing this Data's definition, instead we re-use the space from the specified input. Note that this implies an ordering constraint which we will check, since the definition must be the last use of the aliased input.
+    std::optional<DataId> input_alias;
+    Size size;
+
+    //! This indicates whether we are able to free this data after its last use. For a segmented fusion, unsegmented fusion inputs and outputs cannot be freed (with the exception of an aliased input), while any intermediate tensors should be freed as soon as possible.
+    bool can_free = true;
+  };
+
+  TaskGraph(const std::vector<Task>& tasks, const std::vector<Data>& data) : tasks_(tasks), data_(data) {
+    // Initialize the counts of future uses of data and unmet dependencies of tasks. These are the out-degrees of Data and in-degrees of Tasks, respectively.
+    num_dependencies_.reserve(tasks_.size());
+    for (const Task& task : tasks_) {
+      num_dependencies_.push_back((DataId)task.inputs.size());
+    }
+    num_uses_.reserve(data_.size());
+    for (const Data& data : data_) {
+      num_dependencies_.push_back((TaskId)data.uses.size());
+      if (!data.definition.has_value()) {
+        initial_allocation_ += (Size)data.size;
+      }
+    }
+  }
+
+  //! This represents the execution of a single Task in a given ordering. It tracks some cumulative state representing the amount of space required up to this point.
+  struct Step {
+    TaskId task;
+
+    //! This is the sum of all Data that is active _after_ execution of this task and after any inputs with no more uses are freed.
+    Size allocated;
+
+    //! This is the maximum active space used until this step is completed.
+    Size high_water_mark;
+  };
+
+  TaskId numTasks() const {
+    return (TaskId)tasks_.size();
+  }
+
+  const Task& getTask(TaskId id) const {
+    return tasks_.at((size_t)id);
+  }
+
+  TaskId numData() const {
+    return (DataId)data_.size();
+  }
+
+  const Data& getData(DataId id) const {
+    return data_.at((size_t)id);
+  }
+
+  Size getInitialAllocation() const {
+    return initial_allocation_;
+  }
+
+  //! Given a list of steps, recompute the active space and high water mark. This is useful for validating that our backtracking algorithm does not corrupt this data. Raises an exception if corruption is detected.
+  void validateSteps(const std::vector<Step>& steps) const;
+
+  //! This does an exhaustive search of all possible orderings using a modified Kahn's algorithm to efficiently traverse the set of possible topological orderings.
+  std::vector<Step> findOptimalOrder() const;
+
+ private:
+  std::vector<Task> tasks_;
+  std::vector<Data> data_;
+
+  //! How much data is allocated by data that has no definition, i.e. input data
+  Size initial_allocation_;
+
+  std::vector<TaskId> num_uses_;
+  std::vector<DataId> num_dependencies_;
+};
+
+
+} // namespace nvfuser
