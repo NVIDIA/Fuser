28b613313009b2df97ea8d76847085c44a646eb8
Ryan Spring <rspring@nvidia.com>
2025-08-14 20:28:20 -0700

    create test_nvfp4_gemm_epilogue

--- tests/python/direct/test_cutlass_nvfp4_gemm.py
+++ tests/python/direct/test_cutlass_nvfp4_gemm.py
@@ -93,3 +93,56 @@ def test_nvfp4_gemm(
     )
 
     torch.testing.assert_close(out, expected_out.to(dtype=dtype), atol=1e-1, rtol=1e-1)
+
+
+@pytest.mark.parametrize("dtype", [torch.float16, torch.bfloat16])
+@pytest.mark.parametrize(
+    "shape", [(128, 128, 64), (128, 128, 128), (256, 128, 64), (128, 256, 128)]
+)
+@torch.inference_mode()
+def test_nvfp4_gemm_epilogue(
+    dtype: torch.dtype,
+    shape: tuple[int, int, int],
+) -> None:
+    m, n, packed_k = shape
+    k = packed_k * 2
+    block_size = 16
+    a_dtype = torch.randn((m, k), dtype=dtype, device="cuda")
+    b_dtype = torch.randn((n, k), dtype=dtype, device="cuda")
+
+    a_global_scale = (
+        (FLOAT8_E4M3_MAX * FLOAT4_E2M1_MAX) / torch.amax(a_dtype.flatten(), dim=-1)
+    ).to(torch.float32)
+    b_global_scale = (
+        (FLOAT8_E4M3_MAX * FLOAT4_E2M1_MAX) / torch.amax(b_dtype.flatten(), dim=-1)
+    ).to(torch.float32)
+    alpha = 1.0 / (a_global_scale * b_global_scale)
+    global_normconst = torch.tensor(2, dtype=torch.float, device="cuda")
+
+
+    a_fp4, a_scale_linear = pytorch_nvfp4_quantize(a_dtype, a_global_scale)
+    b_fp4, b_scale_linear = pytorch_nvfp4_quantize(b_dtype, b_global_scale)
+    a_scale_interleaved = linear_to_swizzled_128_4(a_scale_linear)
+    b_scale_interleaved = linear_to_swizzled_128_4(b_scale_linear)
+
+    '''
+    expected_out = get_ref_results(
+        a_fp4,
+        b_fp4,
+        a_scale_interleaved,
+        b_scale_interleaved,
+        a_global_scale,
+        b_global_scale,
+        m,
+        n,
+        dtype,
+        block_size,
+        "cuda",
+    )
+    '''
+
+    out_fp4, out_scale_interleaved = nvf_cutlass.nvfp4_scaled_mm_epilogue(
+        a_fp4, b_fp4, a_scale_interleaved, b_scale_interleaved, alpha, global_normconst
+    )
+
+    #torch.testing.assert_close(out, expected_out.to(dtype=dtype), atol=1e-1, rtol=1e-1)
