d41a46620fbd0ba0121a0282431dde915e432dba
jjsjann123 <jiej@nvidia.com>
2025-08-20 16:39:46 -0700

    wip

--- tests/python/test_narrow_precision.py
+++ tests/python/test_narrow_precision.py
@@ -109,3 +109,208 @@ def test_scaled_mm(
         * alpha
     )
     assert o.allclose(ref_o, 1e-2, 1e-2)
+def round_up(x, y):
+    return (x + y - 1) // y * y
+
+
+def activation_scale_to_nvfp4(x, g_sf, offsets, blockscale_offsets, block_size):
+    m = x.size(0)
+    k = x.size(1)
+    g = g_sf.size(0)
+    padded_m_size = blockscale_offsets[g - 1] + round_up(m - offsets[g - 1], 128)
+    block_scale = torch.empty(
+        (padded_m_size, k // block_size), dtype=torch.float8_e4m3fn, device="cuda:0"
+    )
+    v_scaled = torch.empty((m, k // 2), dtype=torch.float4_e2m1fn_x2, device="cuda:0")
+    for i in range(len(g_sf)):
+        l = offsets[i]
+        if i == g - 1:
+            r = m
+        else:
+            r = offsets[i + 1]
+        l_sf = blockscale_offsets[i]
+        r_sf = l_sf + r - l
+        v_scaled[l:r], block_scale[l_sf:r_sf] = pytorch_nvfp4_quantize(x[l:r], g_sf[i])
+    return v_scaled, block_scale
+
+
+@pytest.mark.skipif(
+    is_pre_blackwell(), reason="Only supported on blackwell and newer devices."
+)
+@pytest.mark.parametrize("config", [[1024, 128, 256]]])
+@pytest.mark.parametrize("tokens_per_expert_neg_one", [[115, 144, 8]])
+@pytest.mark.parametrize("out_dtype", [torch.bfloat16])
+def test_cutlass_nvfp4_grouped_mm(
+    config,
+    tokens_per_expert_neg_one,
+    out_dtype,
+):
+    INPUT_DTYPE = torch.uint8
+    BLOCK_SIZE = 16
+
+    # k dimension is multiple of 128 to avoid padding
+    m, n, k = config
+    tokens_per_expert = tokens_per_expert_neg_one
+    tokens_per_expert.append(m - sum(tokens_per_expert))
+    g = len(tokens_per_expert)
+
+    mat1 = torch.testing.make_tensor((m, k), dtype=torch.float32, device="cuda:0")
+    # format is g, n, k instead of g, k, n
+    mat2 = torch.testing.make_tensor(
+        (g, n, k), dtype=torch.float32, device="cuda:0"
+    )
+
+    offsets = torch.empty((g,), dtype=torch.int32, device="cuda:0")
+    blockscale_offsets = torch.empty((g,), dtype=torch.int32, device="cuda:0")
+    problem_sizes = torch.empty((g, 3), dtype=torch.int32, device="cuda:0")
+
+    # prepare quantization for mat2
+    mat2_gs = torch.empty((g,), dtype=torch.float32, device="cuda:0")
+    scale2 = torch.empty(
+        (g, n, k // BLOCK_SIZE), dtype=torch.float8_e4m3fn, device="cuda:0"
+    )
+
+    acc_tokens = 0
+    rounded_acc_tokens = 0
+    mat2_scaled = torch.empty(
+        (g, n, k // 2), dtype=torch.float4_e2m1fn_x2, device="cuda:0"
+    )
+
+    for i in range(g):
+        mat2_gs[i] = FLOAT4_E2M1_MAX * FLOAT8_E4M3_MAX / mat2[i].max()
+        offsets[i] = acc_tokens
+        blockscale_offsets[i] = rounded_acc_tokens
+        acc_tokens += tokens_per_expert[i]
+        # Note: we technically don't need to round up, since k is perfectly sized.
+        rounded_acc_tokens += round_up(tokens_per_expert[i], 128)
+
+        problem_sizes[i][0] = tokens_per_expert[i]
+        problem_sizes[i][1] = n
+        problem_sizes[i][2] = k
+
+        scaled_mat2_i, bs_mat2_i = pytorch_nvfp4_quantize(mat2[i], mat2_gs[i])
+        mat2_scaled[i] = scaled_mat2_i
+        scale2[i] = bs_mat2_i
+
+    # prepare quantization for mat1
+    # note: following sglang implementation, not computing global scaling factor for mat1
+    #       similarly, we don't need to apply mat1_gs to alpha
+    mat1_gs = torch.ones((g,), dtype=torch.float32, device="cuda:0")
+    mat1, scale1 = activation_scale_to_nvfp4(
+        mat1, mat1_gs, offsets, blockscale_offsets, BLOCK_SIZE
+    )
+
+    ab_strides = torch.full((g,), k, dtype=torch.int64, device="cuda:0")
+    c_strides = torch.full((g,), n, dtype=torch.int64, device="cuda:0")
+
+    def nvfuser_fusion_id0(fd: FusionDefinition) -> None:
+        mat1 = fd.define_tensor(
+            shape=[-1, -1],
+            contiguity=True,
+            dtype=DataType.Float4_e2m1fn,
+            is_cpu=False,
+        )
+        mat2 = fd.define_tensor(
+            shape=[-1, -1, -1],
+            contiguity=True,
+            dtype=DataType.Float4_e2m1fn,
+            is_cpu=False,
+            stride_order=[2, 0, 1],
+        )
+        scale1 = fd.define_tensor(
+            shape=[-1, -1],
+            contiguity=True,
+            dtype=DataType.Float8_e4m3fn,
+            is_cpu=False,
+        )
+        scale2 = fd.define_tensor(
+            shape=[-1, -1, -1],
+            contiguity=True,
+            dtype=DataType.Float8_e4m3fn,
+            is_cpu=False,
+        )
+        alpha = fd.define_tensor(
+            shape=[-1], contiguity=True, dtype=DataType.Float, is_cpu=False
+        )
+        problem_sizes = fd.define_tensor(
+            shape=[-1, -1], contiguity=True, dtype=DataType.Int32, is_cpu=False
+        )
+        offsets = fd.define_tensor(
+            shape=[-1], contiguity=True, dtype=DataType.Int32, is_cpu=False
+        )
+        blockscale_offsets = fd.define_tensor(
+            shape=[-1], contiguity=True, dtype=DataType.Int32, is_cpu=False
+        )
+        out = fd.ops.cutlass_nvfp4_grouped_mm(
+            mat1,
+            mat2,
+            scale1,
+            scale2,
+            alpha,
+            problem_sizes,
+            offsets,
+            blockscale_offsets,
+            DataType.BFloat16,
+        )
+        fd.add_output(out)
+
+    with FusionDefinition() as fd:
+        nvfuser_fusion_id0(fd)
+
+    inputs = [
+        mat1.view(torch.float4_e2m1fn_x2),
+        mat2_scaled.view(torch.float4_e2m1fn_x2).transpose(-1, -2),
+        scale1,
+        scale2,
+        mat2_gs,
+        problem_sizes,
+        offsets,
+        blockscale_offsets,
+    ]
+
+    o = fd.execute(inputs)[0]
+
+    o_decomposed_ref = torch.empty(m, n, dtype=torch.bfloat16, device="cuda:0")
+    for i in range(g):
+        l = offsets[i]
+        l_sf = blockscale_offsets[i]
+        if i == g - 1:
+            r = m
+        else:
+            r = offsets[i + 1]
+        r_sf = round_up(tokens_per_expert[i], 128) + l_sf
+        # for some reason I cannot feed mat2_gs[i] as alpha in the torch kernel. this triggers a cublas invalid value error
+        o_decomposed_ref[l:r] = (
+            torch._scaled_mm(
+                mat1[l:r],
+                mat2_scaled[i].transpose(-1, -2),
+                scale1[l_sf:r_sf],
+                scale2[i],
+                None,
+                None,
+                torch.bfloat16,
+            )
+            * mat2_gs[i]
+        )
+
+    # I think we have higher error because we are not fusing the scaling factor
+    assert torch.allclose(o_decomposed_ref, o, atol=1e-2, rtol=1e-2)
+
+    # TODO: remove this, it's not relevant here.
+    o_ref = torch.empty(m, n, dtype=torch.bfloat16, device="cuda:0")
+    import nvfuser_direct
+
+    nvfuser_direct.nvf_cutlass.nvfp4_scaled_grouped_mm(
+        o_ref,
+        mat1.view(INPUT_DTYPE),
+        mat2_scaled.view(INPUT_DTYPE),
+        scale1,
+        scale2,
+        mat2_gs,
+        ab_strides,
+        c_strides,
+        problem_sizes,
+        offsets,
+        blockscale_offsets,
+    )
+    assert torch.allclose(o_ref, o_decomposed_ref, atol=1e-2, rtol=1e-2)
