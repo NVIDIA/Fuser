491e6cacd44fb9641baf01b9994f06046dca3883
Naoya Maruyama <nmaruyama@nvidia.com>
2025-08-22 09:48:25 -0700

    comment

--- csrc/scheduler/greedy.h
+++ csrc/scheduler/greedy.h
@@ -16,6 +16,54 @@ class Fusion;
 class SchedulerRuntimeInfo;
 class HeuristicDataCache;
 
+// The Greedy scheduler aims to maximize fusion while accommodating
+// scheduling constraints. Unlike some of the
+// existing schedulers such as pointwise, a given fusion is scheduled
+// as a collection of disjoint sets of tensors. Each set
+// consists of tensors that can be scheduled uniformly.
+// When a scheduling conflict occurs between tensor sets, it is
+// resolved by staging conflicting tensors to shared memory.
+//
+// The scheduler currently uses certain operations, such as argsort
+// and scan, as a trigger to be enabled. Additionally, only a limited
+// set of operations are allowed to exist in the given fusion. See
+// canScheduleCompileTime for the current set of required and
+// supported operations.
+//
+// The scheduling strategy is primarily based on two principles:
+//
+// 1. Avoid grid synchronizations
+// 2. Accommodate a wide variety of operations
+//
+// The first principle means that BID types must be used uniformly
+// across a fusion. More specifically, all iter domains parallelized
+// with BID types must be mapped in the loop graph.
+//
+// The scheduler attempts to achieve the second principle through a
+// three-step process:
+// - First, it schedules all constrained tensors--tensor with specific
+//   scheduling requirements.For example,
+//   ArgsortOp and ScanOp currently require the sorted or scanned iter
+//   domain to be parallelized with TID.
+// - Next, the schedule of each constrained tensor is propagated
+//   to a subset of tensors determined by a disjoint fusion partitioning
+//   (see below).
+// - Finally, because constrained tensors may have different
+//   scheduling requirements, adjacent disjoint sets
+//   might be scheduled differently with respect to TID types. The scheduler
+//   uses SyncMap to identify conflicting producer-consumer pairs of tensors and
+//   copies the producers to the shared memory to resolve these conflicts. Since
+//   the first principle only permits conflicts with the TID types, the shared
+//   memory followed by syncthreads is sufficient to resolve them.
+//
+// To partition a fusion into disjoint sets, each constrained
+// tensor is marked as a reference and forms an initial set. The
+// remaining non-constrained tensors are then iteratively added to a subset
+// whose reference has matching iter domains, which allows the schedule of
+// the reference tensor to be propagated uniformly.
+//
+// There are still a number of limitations. For a full list of issues
+// we plan to address, see https://github.com/NVIDIA/Fuser/issues/5030.
 class GreedyScheduler : public SchedulerEntry {
  public:
   bool canScheduleCompileTime(Fusion* fusion) override;
