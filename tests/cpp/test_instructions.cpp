d2a8b9c9a0b5278ede2fdf65ce766b3f4f24acfa
Liqiang Lu <liqiangxl@gmail.com>
2025-08-24 14:27:55 -0700

    test

--- tests/cpp/test_instructions.cpp
+++ tests/cpp/test_instructions.cpp
@@ -49,4 +49,35 @@ TEST_F(InstructionsTest, FmaxfGeneratesMaxInstruction) {
   testValidate(fusion.get(), output, {t0}, __LINE__, __FILE__);
 }
 
+TEST_F(InstructionsTest, FmaxfFastMath) {
+  auto fusion = std::make_unique<Fusion>();
+  FusionGuard fg(fusion.get());
+
+  auto tv0 = makeSymbolicTensor(2);
+  fusion->addInput(tv0);
+  auto tv1 = max(tv0, {1});
+  fusion->addOutput(tv1);
+
+  auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
+  auto t0 = at::randn({4, 8}, options);
+
+  KernelExecutor ke;
+  {
+    DebugDumpOptionsGuard debug_dump_options_guard;
+    DebugDumpOptionsGuard::getCurOptions().set(DebugDumpOption::Ptx);
+    EnableOptionsGuard enable_opt_guard;
+    EnableOptionsGuard::getCurOptions().set(EnableOption::FastMath);
+    ke.compile(fusion.get(), {t0});
+  }
+
+  // Verify PTX.
+  const executor_utils::CudaExecutable* compiled_kernel =
+      ke.compiledKernel()->cudaExecutable().get();
+  std::string ptx_string(
+      compiled_kernel->ptx.begin(), compiled_kernel->ptx.end());
+  EXPECT_TRUE(ptx_string.find("max.ftz.f32") != std::string::npos);
+
+  const auto output = ke.run({t0});
+  testValidate(fusion.get(), output, {t0}, __LINE__, __FILE__);
+}
 } // namespace nvfuser
