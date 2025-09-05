f149b4bbf4b39e6fb9d237a90667ee2bbb014d8c
Xiang Gao <xgao@nvidia.com>
2025-09-05 15:23:43 -0700

    save

--- csrc/runtime/fusion_cache_utils.cpp
+++ csrc/runtime/fusion_cache_utils.cpp
@@ -127,8 +127,8 @@ void ArgumentManager::updateWithSegmentOutputs(
     auto tv = dynamic_cast<TensorView*>(group_outputs[group_out_i]);
     if (tv) {
       const at::Tensor& tensor = group_runtime_outputs[group_out_i].as<at::Tensor>();
-      const std::vector<int64_t> sizes = tensor.sizes();
-      const std::vector<int64_t> strides = tensor.strides();
+      const std::vector<int64_t> sizes = tensor.sizes().vector();
+      const std::vector<int64_t> strides = tensor.strides().vector();
       tv->domain()->setContiguity(_computeContiguity(sizes, strides));
     }
   }
