030e3578b20788d45dbeb05aea8cde77f9f136e9
jjsjann123 <alex.jann2012@gmail.com>
2025-09-05 03:10:18 -0700

    PR4: Adding automatic scheduler
    test case added
    
    wip
    
    prevent cacheAndForkOutputs
    
    disabl cacheInputs for offsets TVs
    
    change domain stuff in reference TV
    
    revert unused changes
    
    err something isn't working right
    
    wip

--- csrc/scheduler/registry.cpp
+++ csrc/scheduler/registry.cpp
@@ -57,14 +57,14 @@ bool checkCanSchedule(Fusion* fusion, SchedulerType scheduler_type) {
           ArgsortOp,
           GroupedMmaOp,
           CutlassNvfp4GroupedMmaOp,
-          // TODO: remove this once we have a scheduler for it
-          PreprocessGroupedMatmulInputSf,
           TopKOp,
           ScanOp>(fusion)) {
     scheduler_debug_utils::canScheduleRejectReason(
         scheduler_type, "Has unsupported ops");
     return false;
   }
+
+  // TODO: check PreprocessGroupedMatmulInputSf's output is in global memory / fusion output
 
   // Fusions with `MatmulOp, LinearOp, MmaOp` can only be accepted by Matmul
   // scheduler.
@@ -80,6 +80,7 @@ bool checkCanSchedule(Fusion* fusion, SchedulerType scheduler_type) {
         scheduler_type, "Connected fusion graph check failed!");
     return false;
   }
+
   if (IterDomainGraph(fusion, /*allow_self_mapping=*/true).hasSelfMapping()) {
     scheduler_debug_utils::canScheduleRejectReason(
         scheduler_type, "Iter domain graph check failed!");
