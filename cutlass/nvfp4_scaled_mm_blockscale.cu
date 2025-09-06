21f32c98c0f41be8705b55b8b6a50510de58fb68
Ryan Spring <rspring@nvidia.com>
2025-09-06 10:08:29 -0700

    Remove unnecessary imports and fix comments in cutlass kernels

--- cutlass/nvfp4_scaled_mm_blockscale.cu
+++ cutlass/nvfp4_scaled_mm_blockscale.cu
@@ -13,17 +13,13 @@
 #include <c10/cuda/CUDAGuard.h>
 #include <torch/torch.h>
 
-#include "cute/tensor.hpp"
 #include "cutlass/cutlass.h"
 #include "cutlass/detail/sm100_blockscaled_layout.hpp"
 #include "cutlass/epilogue/collective/collective_builder.hpp"
 #include "cutlass/epilogue/thread/linear_combination.h"
 #include "cutlass/gemm/collective/collective_builder.hpp"
 #include "cutlass/gemm/device/gemm_universal_adapter.h"
-#include "cutlass/gemm/dispatch_policy.hpp"
 #include "cutlass/gemm/kernel/gemm_universal.hpp"
-#include "cutlass/gemm/kernel/tile_scheduler_params.h"
-#include "cutlass/tensor_ref.h"
 #include "cutlass/util/packed_stride.hpp"
 
 namespace nvfuser::cutlass_kernels {
