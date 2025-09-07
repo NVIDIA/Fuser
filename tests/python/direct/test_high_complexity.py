ef0bf21c7919cd4df290b7be1973f6ed4b75cd2d
Ryan Spring <rspring@nvidia.com>
2025-09-07 10:51:35 -0700

    Create test_cat_symbolic

--- tests/python/direct/test_high_complexity.py
+++ tests/python/direct/test_high_complexity.py
@@ -7,7 +7,7 @@ import torch
 import torch._refs as refs
 import torch._prims as prims
 
-from nvfuser_direct import FusionDefinition
+from nvfuser_direct import FusionDefinition, DataType
 
 
 def test_broadcast_in_dim_with_dynamic_shapes(nvfuser_direct_test):
@@ -87,3 +87,86 @@ def test_broadcast_in_dim_with_dynamic_shapes(nvfuser_direct_test):
         inputs[0], prims.broadcast_in_dim(inputs[1], inputs[0].size(), [2])
     )
     nvfuser_direct_test.assertEqual(eager_out, nvf_out[0])
+
+
+# Test that symbolic IterDomains can be concatenated
+# https://github.com/NVIDIA/Fuser/issues/1554
+def test_cat_symbolic(nvfuser_direct_test):
+    inputs = [
+        0.29730177875068026,
+        0.29730177875068026,
+        4,
+        64,
+        768,
+        4,
+        64,
+        768,
+        2,
+        torch.randn([4, 6, 64, 128], dtype=torch.float32, device="cuda"),
+        torch.randn([4, 6, 64, 128], dtype=torch.float32, device="cuda"),
+        torch.randn([4, 64, 768], dtype=torch.float32, device="cuda"),
+    ]
+
+    def fusion_func(fd: FusionDefinition) -> None:
+        S0 = fd.define_scalar(None, dtype=DataType.Double)
+        S1 = fd.define_scalar(None, dtype=DataType.Double)
+        S2 = fd.define_scalar(None, dtype=DataType.Int)
+        S3 = fd.define_scalar(None, dtype=DataType.Int)
+        S4 = fd.define_scalar(None, dtype=DataType.Int)
+        S5 = fd.define_scalar(None, dtype=DataType.Int)
+        S6 = fd.define_scalar(None, dtype=DataType.Int)
+        S7 = fd.define_scalar(None, dtype=DataType.Int)
+        S8 = fd.define_scalar(None, dtype=DataType.Int)
+        T9 = fd.define_tensor(
+            shape=[-1, -1, -1, -1],
+            contiguity=[True, True, True, True],
+            dtype=DataType.Float,
+            is_cpu=False,
+            stride_order=[3, 2, 1, 0],
+        )
+        T10 = fd.define_tensor(
+            shape=[-1, -1, -1, -1],
+            contiguity=[True, True, True, True],
+            dtype=DataType.Float,
+            is_cpu=False,
+            stride_order=[3, 2, 1, 0],
+        )
+        T11 = fd.define_tensor(
+            shape=[-1, -1, -1],
+            contiguity=[True, True, True],
+            dtype=DataType.Float,
+            is_cpu=False,
+            stride_order=[2, 1, 0],
+        )
+        T12 = fd.ops.mul(T10, S1)
+        T13 = fd.ops.permute(T12, dims=[0, 1, 3, 2])
+        T14 = fd.ops.mul(T9, S0)
+        T15 = fd.ops.permute(T14, dims=[0, 2, 1, 3])
+        S16 = fd.define_scalar(4, dtype=DataType.Int)
+        S17 = fd.define_scalar(64, dtype=DataType.Int)
+        S18 = fd.define_scalar(768, dtype=DataType.Int)
+        T20 = fd.ops.reshape(T15, new_shape=[S16, S17, S18])
+        T21 = fd.ops.permute(T13, dims=[0, 2, 1, 3])
+        S22 = fd.define_scalar(4, dtype=DataType.Int)
+        S23 = fd.define_scalar(64, dtype=DataType.Int)
+        S24 = fd.define_scalar(768, dtype=DataType.Int)
+        T26 = fd.ops.reshape(T21, new_shape=[S22, S23, S24])
+        T27 = fd.ops.cat([T20, T26, T11], dim=2)
+        T28 = fd.ops.sum(T27, [0, 1], keepdim=False, dtype=DataType.Null)
+        fd.add_output(T27)
+        fd.add_output(T28)
+
+    nvf_out, _ = nvfuser_direct_test.exec_nvfuser(fusion_func, inputs)
+
+    t12 = inputs[1] * inputs[-2]
+    t13 = torch.permute(t12, [0, 1, 3, 2])
+    t14 = inputs[0] * inputs[-3]
+    t15 = torch.permute(t14, [0, 2, 1, 3])
+    t20 = torch.reshape(t15, [4, 64, 768])
+    t21 = torch.permute(t13, [0, 2, 1, 3])
+    t26 = torch.reshape(t21, [4, 64, 768])
+    t27 = torch.cat([t20, t26, inputs[-1]], dim=2)
+    t28 = t27.sum([0, 1])
+
+    nvfuser_direct_test.assertEqual(nvf_out[0], t27)
+    nvfuser_direct_test.assertEqual(nvf_out[1], t28)
