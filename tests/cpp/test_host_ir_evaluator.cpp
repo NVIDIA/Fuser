57e34247f3ac062fa8eb03d00f1c26b1b1e3b117
Jingyue Wu <wujingyue@gmail.com>
2025-09-10 16:25:14 -0700

    Move the test from test_stream to test_host_ir_evaluator

--- tests/cpp/test_host_ir_evaluator.cpp
+++ tests/cpp/test_host_ir_evaluator.cpp
@@ -196,4 +196,32 @@ TEST_F(HostIrEvaluatorTest, MatmulInLoop) {
       << out_tensor << " vs " << expected_out_tensor;
 }
 
+TEST_F(HostIrEvaluatorTest, AddInLoop) {
+  constexpr int64_t c = 3;
+  Fusion fusion;
+  {
+    FusionGuard fg(&fusion);
+    TensorView* in = makeContigTensor(2);
+    TensorView* out = add(in, in);
+    fusion.addInput(in);
+    fusion.addOutput(out);
+    in->outer_split(1, c);
+    in->axis(1)->parallelize(ParallelType::Stream);
+    out->outer_split(1, c);
+    out->axis(1)->parallelize(ParallelType::Stream);
+  }
+
+  auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA);
+  at::Tensor in_tensor = at::randn({5, c * 2}, options);
+  at::Tensor out_tensor = at::zeros_like(in_tensor);
+
+  KernelExecutor ke;
+  ke.compile(&fusion, {in_tensor});
+  constexpr int64_t kStreamIndex = 1;
+  ke.run({in_tensor, kStreamIndex}, {out_tensor});
+
+  std::cout << "in_tensor: " << in_tensor << std::endl;
+  std::cout << "out_tensor: " << out_tensor << std::endl;
+}
+
 } // namespace nvfuser::hir
