b2a3b46cd021fa0ec101f7942d0a9b380f771ae7
jjsjann123 <jiej@nvidia.com>
2025-09-10 16:42:49 -0700

    restore skip checks

--- csrc/ops/indexing.cpp
+++ csrc/ops/indexing.cpp
@@ -366,12 +366,16 @@ TensorView* preprocessGroupedMatmulInputSf(
   // Create the output tensor with logical domain matching inputs
   TensorView* out_tv = IrBuilder::create<TensorView>(
       IrBuilder::create<TensorDomain>(
-          /*logcial_domain=*/out_logical_dom),
-      input->getDataType().value());
-  // Set allocation domain with padding logic.
-  out_tv->setAllocationDomain(
-      out_alloc_dom,
-      TensorDomain::getContiguityFilledWith(out_alloc_dom, true));
+         /*root_domain=*/std::vector<IterDomain*>(),
+         /*logical_domain=*/out_logical_dom,
+         /*allocation=*/out_alloc_dom,
+         /*loop_domain=*/out_logical_dom,
+         /*alternate_loop_domain=*/std::nullopt,
+         /*contiguity=*/
+         TensorDomain::getContiguityFilledWith(out_alloc_dom, true),
+         /*additional_ids=*/std::vector<IterDomain*>(),
+         /*skip_checks=*/true),
+     input->getDataType().value());
 
   IrBuilder::create<PreprocessGroupedMatmulInputSf>(
       out_tv,
