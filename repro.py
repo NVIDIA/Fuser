1faae140e73f4b248676a7a1b693814d6355528f
Jingyue Wu <wujingyue@gmail.com>
2025-09-15 12:10:50 -0700

    Simplify

--- repro.py
+++ repro.py
@@ -1,12 +1,17 @@
 # torchrun --local-ranks-filter=0 --nnodes 1 --nproc-per-node 2 test_nvfuser_dtensor_stride_mismatch.py
 
-import torch
-from thunder.tests.make_tensor import make_tensor
-from torch.distributed._tensor import DeviceMesh, distribute_tensor, DTensor
-from torch.distributed.tensor.placement_types import Placement, Shard, Replicate
+import nvfuser_direct as nvfd
 import os
+import torch
 import torch.distributed as dist
+
+from thunder.executors.nvfuserex_impl import compute_contiguity
+from nvfuser_direct import FusionDefinition, DataType
+from thunder.tests.make_tensor import make_tensor
+from torch.distributed.tensor import DeviceMesh, distribute_tensor, DTensor
+from torch.distributed.tensor.placement_types import Placement, Shard, Replicate
 from typing import cast
+
 
 LOCAL_RANK = int(os.environ["LOCAL_RANK"])
 num_devices = 2
@@ -25,36 +30,12 @@ a = make_tensor(
 ).as_strided(shape, strides, offset)
 a = a.detach()
 
-print(a.shape, "A SHAPE")  # torch.Size([5, 6, 2])
-print(a.stride(), "A STRIDE")  # (1, 1, 7)
-
-# in_dtensor = distribute_tensor(a, mesh, [Shard(2)])  # Works
-# in_dtensor = distribute_tensor(a, mesh, [Shard(1)])  # Works
 in_dtensor = distribute_tensor(a, mesh, [Replicate()])
 
-print(in_dtensor.shape, "DTENSOR SHAPE")  # torch.Size([5, 6, 2])
-print(in_dtensor.stride(), "DTENSOR STRIDE")  # (1, 1, 7)
-print(in_dtensor.is_contiguous(), "DTENSOR CONTIGUOUS")  # False
-
-# NOTE: The strides for local tensor. They are contiguous.
-print(in_dtensor._local_tensor.shape, "LOCAL TENSOR SHAPE")  # torch.Size([5, 6, 2])
-print(in_dtensor._local_tensor.stride(), "LOCAL TENSOR STRIDE")  # (12, 2, 1)
-print(in_dtensor._local_tensor.is_contiguous(), "LOCAL TENSOR CONTIGUOUS")  # True
-
-# Check PyTorch eager
-expected = torch.neg(in_dtensor)
-
-
-from nvfuser_direct import FusionDefinition, DataType
-import nvfuser_direct as nvfd
-from thunder.executors.nvfuserex_impl import compute_contiguity
 
 fd = FusionDefinition()
 
 contiguity, stride_order = compute_contiguity(in_dtensor.shape, in_dtensor.stride())
-
-print(contiguity, "CONTIGUITY")
-print(stride_order, "STRIDE ORDER")
 
 
 # This is roughly what thunder does.
