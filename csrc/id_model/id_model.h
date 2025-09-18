92b3713dbc781bfe14730dd2154b657e93c3e705
Ryan Spring <rspring@nvidia.com>
2025-09-18 10:41:13 -0700

    Add test_tutorial_id_model_reshape_analysis

--- csrc/id_model/id_model.h
+++ csrc/id_model/id_model.h
@@ -104,7 +104,7 @@ StatefulInliningInfo buildStatefulInliningInfo(
 // IdMappingMode::LOOP
 //   Subgraph of the permissive graph. Maps only CA and their
 //   dependent domains.
-class IdModel : public PolymorphicBase {
+class NVF_API IdModel : public PolymorphicBase {
  public:
   // Sometimes fusion inputs or outputs are disconnected from expressions, in
   // those cases we still may want to send in some additional tensor views from
@@ -126,7 +126,7 @@ class IdModel : public PolymorphicBase {
   //
   // The validate parameter is a temporary option during the
   // transition from the current ComputeAtMap.
-  IdModel(
+  NVF_API IdModel(
       Fusion* fusion,
       bool build_graphs = false,
       bool allow_self_mapping = true,
@@ -212,7 +212,7 @@ class IdModel : public PolymorphicBase {
   ValGraph& buildGraph(IdMappingMode mode);
 
   // Build a graph if not already built
-  ValGraph& maybeBuildGraph(IdMappingMode mode);
+  NVF_API ValGraph& maybeBuildGraph(IdMappingMode mode);
 
   // Query if the specified graph is already built
   bool hasGraph(IdMappingMode mode) const;
