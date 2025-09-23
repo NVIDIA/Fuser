c2a3aeb79dfb34d2c7bdca476408abf37a6d269a
jjsjann123 <jiej@nvidia.com>
2025-09-22 17:29:10 -0700

    why is it so hard for me

--- tests/cpp/test_layout_op.cpp
+++ tests/cpp/test_layout_op.cpp
@@ -127,7 +127,7 @@ TEST_F(LayoutOpTest, AllocationDomainSplitVectorizationFactor) {
   auto&& [io, ii] = IterDomain::split(
       out->axis(1), IrBuilder::create<Val>(16L, DataType::Index), true);
   out->setAllocationDomain(
-      {out->axis(0), io, ii, out->axis(2)}, {false, true, true});
+      {out->axis(0), io, ii, out->axis(2)}, {false, true, true, true});
 
   auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
   // because of the split on the middle dimension, we only have the fastest
