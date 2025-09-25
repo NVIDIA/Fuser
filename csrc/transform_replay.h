6e6e5b6ceeda921e5a44302cd85e52538942e079
Jingyue Wu <wujingyue@gmail.com>
2025-09-24 22:48:53 -0700

    Comment

--- csrc/transform_replay.h
+++ csrc/transform_replay.h
@@ -232,10 +232,13 @@ class TransformReplay {
   //
   // This method is often used to propagate transforms from one TV to another
   // that's not necessarily a producer/consumer. It assumes enough similarity
-  // between `self` and `new_self`. By default, the logical domains of the two
-  // TVs have to match exactly, e.g., same length, same extents, same IterTypes,
-  // and same ParallelTypes. For convenience, we allow the reduction IDs to
-  // mismatch and thus the `ignore_reductions` flag. For example,
+  // between `self` and `new_self`. When self's logical and new_self's logical
+  // have the same length, their logical IterDomains have to match exactly,
+  // e.g., same extents, same IterTypes and same ParallelTypes. When one of
+  // them is longer than the other, the extra IterDomains must be reductions.
+  // This way, we can still map the rest without ambiguity.
+  //
+  // For example,
   // ```
   // in: logical=[i{n}, r{m}], loop=[iDIDy{d}, i{n/d}, rDIDx{d}, r{m/d}]
   // out = in + 1.0: logical=[i{n}]
@@ -243,10 +246,6 @@ class TransformReplay {
   // `out` and `in` are similar enough to selfReplay so `out` gets a loop domain
   // of [iDIDy{d}, i{n/d}]. However, to achieve that, we'll have to ignore
   // `in`'s reduction dimensions.
-  //
-  // In practice, `ignore_reductions=true` is used more often than `false`. I
-  // made `false` default merely because a true "self" replay needn't and
-  // shouldn't ignore anything.
   static void selfReplay(const TensorDomain* self, TensorDomain* new_self);
 
   // Returns the loop position in producer that matches with `consumer_pos` in
