217e2fb8783ad2826659fd50db5b6e3211a46a2c
Priya Mishra <52657555+Priya2698@users.noreply.github.com>
2025-10-02 00:55:28 -0700

    Reorder maps for loop and logical domain in pointwise schedulers (#5237)
    Pointwise scheduler uses `maybeReorderAsAllocationMap` to generate a map
    for reordering loop and logical domain as the allocation domain. It
    computes a permutation between loop and allocation domain. Loop and
    logical domain are assumed to be the same, as is generally the case for
    single-GPU programs at this stage.
    
    For multi-GPU, so far this meant having any empty map. Recall, that the
    `FinalizeMultideviceDomainsPass` reorders loop as allocation to avoid
    existing limitations.
    
    With #4381, loop and allocation can be different in order or
    parallelization. This implies that the map generated to reorder loop as
    allocation domain for multi-device fusions cannot be used for logical
    domain. This PR generates different maps for logical and loop domain.
    
    **Reordering logical as allocation domain:**
    
    Allocation domain is assumed to post-dominate logical domain. We
    canonicalize the allocation domain such that it is a permutation of
    logical:
    > When `allocation` is a split of `logical`, walks backwards from
    `allocation` to `logical` to find a permutation of `logical` that
    satisfies the order in `allocation`.
    
    **Reordering loop as allocation domain:**
    
    Loop domain is assumed to post-dominate allocation domain. We traverse
    the transforms between allocation and loop domain such that it becomes a
    permutation of loop domain. Caveat, this will not be the case if
    allocation and loop are disjoint or allocation contains transforms
    absent in loop domain. I am adding an assert to catch such cases early
    and modify our implementation.

--- tests/cpp/test_multidevice_sharding.cpp
+++ tests/cpp/test_multidevice_sharding.cpp
@@ -1117,4 +1117,43 @@ TEST_F(MultiDeviceTest, AllocationPermutationOfLoop) {
   EXPECT_TRUE(at::allclose(nvf_out, sharded_inp));
 }
 
+TEST_F(MultiDeviceTest, PointwiseSchedulerReordering) {
+  auto fusion = std::make_unique<Fusion>();
+  FusionGuard fg(fusion.get());
+
+  const int d = communicator_->size();
+  auto mesh = DeviceMesh::createForNumDevices(d);
+
+  TensorView* tv0 = makeContigConcreteTensor({5, 3 * d});
+  Val* s0 = IrBuilder::create<Val>(1.0);
+  TensorView* tv1 = add(tv0, s0);
+
+  fusion->addInput(tv0);
+  fusion->addOutput(tv1);
+
+  for (auto* tv : {tv0, tv1}) {
+    tv->setDeviceMesh(mesh);
+    // Loop domain: [5, 3*d] -> [5, d, 3]
+    tv->outer_split(1, d);
+    tv->axis(1)->parallelize(ParallelType::DIDx);
+    // Allocation domain: [d, 3, 5]
+    tv->setAllocationDomain({tv->axis(1), tv->axis(2), tv->axis(0)}, true);
+    reorderParallelizedToFront(tv);
+  }
+
+  // Disable the pass to verify we can run a fusion where allocation domain
+  // is a permutation of loop domain. This pass can currently not be modified
+  // due to other issues listed in #4381.
+  preseg_passes::OptimizationPassGuard<
+      preseg_passes::FinalizeMultideviceDomainsPass>
+      optimization_guard(false);
+
+  FusionExecutorCache executor_cache(std::move(fusion));
+  at::Tensor inp = at::randn({3 * d, 5}, tensor_options);
+  at::Tensor sharded_inp = shardTensor(inp, 0, mesh).t();
+  at::Tensor nvf_out =
+      executor_cache.runFusionWithInputs({sharded_inp})[0].as<at::Tensor>();
+  EXPECT_TRUE(at::allclose(nvf_out, sharded_inp + 1.0));
+}
+
 } // namespace nvfuser
