4dff7be81f530579d3b56524c9ec643f158c5556
tbqh <bzitelli@nvidia.com>
2025-10-07 11:07:01 -0700

    Add fmin_fmax_promotion preseg pass

--- tests/cpp/test_math_opt.cpp
+++ tests/cpp/test_math_opt.cpp
@@ -109,4 +109,112 @@ INSTANTIATE_TEST_SUITE_P(
       return sanitizeTestName(ss.str());
     });
 
+using FMinFMaxPromotionTest = NVFuserFixtureParamTest<int>;
+TEST_P(FMinFMaxPromotionTest, Test) {
+  // Test and validate max reductions under a couple different topologies.
+  // Ensure some topologies include "fmax" in the kernel.
+
+  int testIndex = GetParam();
+
+  auto fusion = std::make_unique<Fusion>();
+  FusionGuard fg(fusion.get());
+
+  auto tv0 = makeSymbolicTensor(2);
+  fusion->addInput(tv0);
+  auto tv1 = makeSymbolicTensor(2);
+  fusion->addInput(tv1);
+  auto tv2 = makeSymbolicTensor(2);
+  fusion->addInput(tv2);
+
+  bool expectFMax = false;
+
+  if (testIndex == 1) {
+    TensorView* tv3 = add(max(tv0, {0, 1}), tv0);
+    TensorView* tv4 = add(tv3, sum(tv3, {0, 1}));
+    fusion->addOutput(tv4);
+    expectFMax = true;
+  }
+
+  if (testIndex == 2) {
+    TensorView* tv3 = add(max(tv0, {0, 1}), sum(tv0, {0, 1}));
+    fusion->addOutput(tv3);
+    expectFMax = true;
+  }
+
+  if (testIndex == 3) {
+    TensorView* tv3 = add(tv0, tv1);
+    TensorView* tv4 = add(tv3, broadcast(max(tv3, {1}), {false, true}));
+    TensorView* tv5 = broadcast(sum(add(tv4, tv2), {1}), {false, true});
+    TensorView* tv6 = add(tv4, tv5);
+    fusion->addOutput(tv6);
+    expectFMax = true;
+  }
+
+  if (testIndex == 4) {
+    TensorView* tv3 = add(max(tv0, {1}), sum(tv1, {1}));
+    fusion->addOutput(tv3);
+    expectFMax = false;
+  }
+
+  if (testIndex == 5) {
+    TensorView* tv3 = add(tv0, tv1);
+    TensorView* tv4 = broadcast(max(tv3, {1}), {false, true});
+    TensorView* tv5 = broadcast(sum(add(tv4, tv2), {1}), {false, true});
+    TensorView* tv6 = add(tv4, tv5);
+    fusion->addOutput(tv6);
+    expectFMax = false;
+  }
+
+  if (testIndex == 6) {
+    TensorView* tv3 = add(tv0, broadcast(max(tv0, {1}), {false, true}));
+    fusion->addOutput(tv3);
+    expectFMax = false;
+  }
+
+  if (testIndex == 7) {
+    TensorView* tv3 = add(abs(max(tv0, {0, 1})), sum(tv0, {0, 1}));
+    fusion->addOutput(tv3);
+    expectFMax = false;
+  }
+
+  fusion->printMath();
+
+  auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
+  auto t0 = at::randn({16, 32}, options);
+  t0[0][0] = std::numeric_limits<float>::quiet_NaN();
+
+  auto t1 = at::randn({16, 32}, options);
+  auto t2 = at::randn({16, 32}, options);
+
+  FusionExecutorCache executor_cache(std::move(fusion));
+  auto outputs = executor_cache.runFusionWithInputs({t0, t1, t2});
+
+  testValidate(
+      executor_cache.fusion(), outputs, {t0, t1, t2}, __LINE__, __FILE__);
+
+  auto kernel_runtime = executor_cache.getMostRecentKernelRuntime();
+
+  auto& group = kernel_runtime->fusionSegments()->groups()[0];
+
+  const auto* ke =
+      kernel_runtime->executors().at(group->groupId())->as<KernelExecutor>();
+  std::string kernel_code = ke->compiledKernel()->kernelString();
+
+  if (expectFMax) {
+    EXPECT_THAT(kernel_code, ::testing::HasSubstr("fmax("));
+  } else {
+    EXPECT_THAT(kernel_code, ::testing::Not(::testing::HasSubstr("fmax(")));
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    MathOptTest,
+    FMinFMaxPromotionTest,
+    ::testing::Values(1, 2, 3, 4, 5, 6, 7),
+    [](const testing::TestParamInfo<int>& info) -> std::string {
+      std::stringstream ss;
+      ss << info.param;
+      return sanitizeTestName(ss.str());
+    });
+
 } // namespace nvfuser
