dfaf172e99c552bf5643ec763d194906aa850c24
jjsjann123 <jiej@nvidia.com>
2025-10-08 13:59:56 -0700

    clean up

--- tests/python/test_moe.py
+++ tests/python/test_moe.py
@@ -24,6 +24,7 @@ from python.direct_utils import (
     dequantize_to_dtype,
 )
 
+
 # Sizes used in Llama 4 Maverick
 @dataclass
 class Config:
@@ -55,6 +56,7 @@ def _group_sizes_from_offsets(offsets: torch.Tensor, total_tokens: int) -> list[
     group_sizes.append(total_tokens - prev)
     return group_sizes
 
+
 # Required otherwise, there is a graph-break.
 _grouped_mm = torch.compiler.allow_in_graph(torch._grouped_mm)
 
@@ -73,7 +75,9 @@ def grouped_mm(a: torch.Tensor, b: torch.Tensor, offsets: torch.Tensor) -> torch
     return torch.cat(group_outs)
 
 
-@torch.library.custom_op("nvf_cutlass::f16a_nvfp4weight_scaled_grouped_mm", mutates_args=())
+@torch.library.custom_op(
+    "nvf_cutlass::f16a_nvfp4weight_scaled_grouped_mm", mutates_args=()
+)
 def nvfuser_f16a_nvfp4weight_scaled_grouped_mm(
     activation: torch.Tensor,
     fp4_weight: torch.Tensor,
@@ -84,17 +88,8 @@ def nvfuser_f16a_nvfp4weight_scaled_grouped_mm(
     problem_sizes: torch.Tensor,
     dropme: torch.Tensor,
 ) -> torch.Tensor:
-    #assert False # dequantize_to_dtype is way too slow to be usable.
-    #hp_weight = torch.empty(
-    #    (fp4_weight.size(0), fp4_weight.size(1), fp4_weight.size(2) * 2),
-    #    device=activation.device,
-    #    dtype=activation.dtype,
-    #)
-    #for i in range(fp4_weight.size(0)):
-    #    hp_weight[i] = dequantize_to_dtype(
-    #        fp4_weight[i], weight_scaling_factor[i], global_scale[i], activation.dtype, fp4_weight.device, 16
-    #    )
     return grouped_mm(activation, dropme, offsets)
+
 
 @torch.library.register_fake("nvf_cutlass::f16a_nvfp4weight_scaled_grouped_mm")
 def _(
@@ -107,11 +102,17 @@ def _(
     problem_sizes: torch.Tensor,
     dropme: torch.Tensor,
 ) -> torch.Tensor:
-    return torch.empty((activation.size(0), fp4_weight.size(2)), device=activation.device, dtype=activation.dtype)
+    return torch.empty(
+        (activation.size(0), fp4_weight.size(2)),
+        device=activation.device,
+        dtype=activation.dtype,
+    )
+
 
 from thunder.executors.nvfuserex_impl import lcdtype_to_nvdtype, getnv
 
 from nvfuser_direct import DataType
+
 
 def gmm_nvfuser(
     activation,
@@ -136,22 +137,26 @@ def gmm_nvfuser(
     # dynamic shape support has some concretization issue
     m_size = activation.shape[0]
     k_size = activation.shape[1]
-    k_tile_size = k_size //  16
+    k_tile_size = k_size // 16
 
     reshaped_mat1 = fd.ops.reshape(nv_act, [m_size, k_tile_size, 16])
     scale1 = fd.ops.abs(reshaped_mat1)
     scale1 = fd.ops.max(scale1, 2)
     scale1 = fd.ops.div(scale1, FLOAT4_E2M1_MAX)
     scale1 = fd.ops.clamp(scale1, FLOAT8_E4M3_EPS, FLOAT8_E4M3_MAX)
-    
+
     broadcast_scale1 = fd.ops.broadcast(scale1, [False, False, True])
     reshaped_scaled_mat1 = fd.ops.div(reshaped_mat1, broadcast_scale1)
-    reshaped_scaled_mat1 = fd.ops.clamp(reshaped_scaled_mat1, -FLOAT8_E4M3_MAX, FLOAT8_E4M3_MAX)
-    
+    reshaped_scaled_mat1 = fd.ops.clamp(
+        reshaped_scaled_mat1, -FLOAT8_E4M3_MAX, FLOAT8_E4M3_MAX
+    )
+
     scaled_mat1 = fd.ops.reshape(reshaped_scaled_mat1, [m_size, k_size])
     fp4_mat1 = fd.ops.cast(scaled_mat1, DataType.Float4_e2m1fn)
     fp8_scale1 = fd.ops.cast(scale1, DataType.Float8_e4m3fn)
-    layout_fp8_scale1 = fd.ops.preprocess_grouped_matmul_input_sf(fp8_scale1, nv_offsets, nv_blocksf_offsets)
+    layout_fp8_scale1 = fd.ops.preprocess_grouped_matmul_input_sf(
+        fp8_scale1, nv_offsets, nv_blocksf_offsets
+    )
     out = fd.ops.cutlass_nvfp4_grouped_mm(
         fp4_mat1,
         nv_fp4_w,
@@ -165,13 +170,18 @@ def gmm_nvfuser(
         DataType.BFloat16,
     )
     return out
-    
+
 
 from thunder.torch.custom_op import _register_custom_op
-_sym_of_nvfp4_scaled_grouped_mm = _register_custom_op(nvfuser_f16a_nvfp4weight_scaled_grouped_mm)
+
+_sym_of_nvfp4_scaled_grouped_mm = _register_custom_op(
+    nvfuser_f16a_nvfp4weight_scaled_grouped_mm
+)
 
 from thunder.torch.custom_op import _register_nvfuser_translator
+
 _register_nvfuser_translator(_sym_of_nvfp4_scaled_grouped_mm, gmm_nvfuser)
+
 
 class GroupedLinear(nn.Module):
     def __init__(self, groups: int, in_features: int, out_features: int):
@@ -180,17 +190,36 @@ class GroupedLinear(nn.Module):
         # Initialize the weight in the same way as nn.Linear
         nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
         alpha = torch.empty((groups,), dtype=torch.float32, requires_grad=False)
-        fp4_weight = torch.empty((groups, out_features , in_features // 2), dtype=torch.float4_e2m1fn_x2, requires_grad=False)
-        b_sf = torch.empty((groups, round_up(out_features, 128), round_up(in_features // 16, 4)), dtype=torch.float8_e4m3fn, requires_grad=False)
+        fp4_weight = torch.empty(
+            (groups, out_features, in_features // 2),
+            dtype=torch.float4_e2m1fn_x2,
+            requires_grad=False,
+        )
+        b_sf = torch.empty(
+            (groups, round_up(out_features, 128), round_up(in_features // 16, 4)),
+            dtype=torch.float8_e4m3fn,
+            requires_grad=False,
+        )
 
-        self.k = torch.tensor(in_features, dtype=torch.int32, requires_grad=False).unsqueeze(-1).expand((groups, 1))
-        self.n = torch.tensor(out_features, dtype=torch.int32, requires_grad=False).unsqueeze(-1).expand((groups, 1))
-
+        self.k = (
+            torch.tensor(in_features, dtype=torch.int32, requires_grad=False)
+            .unsqueeze(-1)
+            .expand((groups, 1))
+        )
+        self.n = (
+            torch.tensor(out_features, dtype=torch.int32, requires_grad=False)
+            .unsqueeze(-1)
+            .expand((groups, 1))
+        )
         transposed_weight = self.weight.transpose(-1, -2).contiguous()
         for i in range(groups):
-            global_factor = FLOAT4_E2M1_MAX * FLOAT8_E4M3_MAX / transposed_weight[i].max()
+            global_factor = (
+                FLOAT4_E2M1_MAX * FLOAT8_E4M3_MAX / transposed_weight[i].max()
+            )
             alpha[i] = 1.0 / global_factor
-            scaled_mat2_i, bs_mat2_i = pytorch_nvfp4_quantize(transposed_weight[i], global_factor)
+            scaled_mat2_i, bs_mat2_i = pytorch_nvfp4_quantize(
+                transposed_weight[i], global_factor
+            )
             fp4_weight[i] = scaled_mat2_i
             b_sf[i] = linear_to_swizzled_128_4(bs_mat2_i)
 
@@ -199,11 +228,26 @@ class GroupedLinear(nn.Module):
         self.b_sf = nn.Parameter(b_sf)
 
     def forward(
-        self, hidden_states: torch.Tensor, offsets: torch.Tensor, blockscale_offsets: torch.Tensor, tokens_per_expert: torch.Tensor
+        self,
+        hidden_states: torch.Tensor,
+        offsets: torch.Tensor,
+        blockscale_offsets: torch.Tensor,
+        tokens_per_expert: torch.Tensor,
     ) -> torch.Tensor:
         if torch.compiler.is_compiling():
-            problem_sizes = torch.cat((tokens_per_expert.unsqueeze(-1), self.n, self.k), dim=1).to(torch.int32)
-            return torch.ops.nvf_cutlass.f16a_nvfp4weight_scaled_grouped_mm(hidden_states, self.fp4_weight, self.b_sf, self.alpha, offsets, blockscale_offsets, problem_sizes, self.weight)
+            problem_sizes = torch.cat(
+                (tokens_per_expert.unsqueeze(-1), self.n, self.k), dim=1
+            ).to(torch.int32)
+            return torch.ops.nvf_cutlass.f16a_nvfp4weight_scaled_grouped_mm(
+                hidden_states,
+                self.fp4_weight,
+                self.b_sf,
+                self.alpha,
+                offsets,
+                blockscale_offsets,
+                problem_sizes,
+                self.weight,
+            )
 
         return grouped_mm(hidden_states, self.weight, offsets)
 
@@ -216,12 +260,24 @@ class GroupedSwiGLU(nn.Module):
         self.down_proj = GroupedLinear(groups, intermediate_size, hidden_size)
 
     def forward(
-        self, hidden_states: torch.Tensor, offsets: torch.Tensor, blockscale_offsets: torch.Tensor, tokens_per_expert: torch.Tensor
+        self,
+        hidden_states: torch.Tensor,
+        offsets: torch.Tensor,
+        blockscale_offsets: torch.Tensor,
+        tokens_per_expert: torch.Tensor,
     ) -> torch.Tensor:
         return self.down_proj(
-            F.silu(self.gate_proj(hidden_states, offsets, blockscale_offsets, tokens_per_expert))
-            * self.up_proj(hidden_states, offsets, blockscale_offsets, tokens_per_expert),
-            offsets, blockscale_offsets, tokens_per_expert
+            F.silu(
+                self.gate_proj(
+                    hidden_states, offsets, blockscale_offsets, tokens_per_expert
+                )
+            )
+            * self.up_proj(
+                hidden_states, offsets, blockscale_offsets, tokens_per_expert
+            ),
+            offsets,
+            blockscale_offsets,
+            tokens_per_expert,
         )
 
 
@@ -263,13 +319,18 @@ class Llama4MoE(nn.Module):
         # Without `torch.int32`, we see `RuntimeError: Offsets tensor must be integer (int32) tensor, but got torch.int64.`
         # from PyTorch when calling _grouped_mm.
         shifted_tokens_per_expert = torch.nn.functional.pad(tokens_per_expert, [1, 0])
-        
+
         offsets = torch.cumsum(shifted_tokens_per_expert, 0, dtype=torch.int32)  # [n]
         rounded_tokens_per_expert = (shifted_tokens_per_expert + 127) // 128 * 128
-        blockscale_offsets = torch.cumsum(rounded_tokens_per_expert, 0, dtype=torch.int32)  # [n]
+        blockscale_offsets = torch.cumsum(
+            rounded_tokens_per_expert, 0, dtype=torch.int32
+        )  # [n]
 
         outs_sorted_by_expert_id = self.routed_experts(
-            tokens_sorted_by_expert_id, offsets[:-1], blockscale_offsets[:-1], tokens_per_expert
+            tokens_sorted_by_expert_id,
+            offsets[:-1],
+            blockscale_offsets[:-1],
+            tokens_per_expert,
         )  # [s, h]
 
         token_ids_sorted_by_expert_inverse_id = torch.argsort(
@@ -331,9 +392,9 @@ def test_llama4_moe_thunderfx():
     with torch.no_grad():
         actual = tmodel(inp)
 
-    #assert len(tmodel._backend.subgraph_infos) == 1
-    #assert len(tmodel._backend.subgraph_infos[0].split_reasons) == 0
+    assert len(tmodel._backend.subgraph_infos) == 1
+    assert len(tmodel._backend.subgraph_infos[0].split_reasons) == 0
     # Uncomment to view thunder traces
-    print(tmodel.last_traces)
+    # print(tmodel.last_traces)
 
     torch.testing.assert_close(actual, expected, atol=1e-2, rtol=1e-2)
