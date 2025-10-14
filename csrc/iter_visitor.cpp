65b79e7051331a0dcbcd41d0408c7d57baebb3f5
Jacob Hinkle <jhinkle@nvidia.com>
2025-10-14 08:51:26 -0400

    clang-format

--- csrc/iter_visitor.cpp
+++ csrc/iter_visitor.cpp
@@ -71,8 +71,7 @@ class MemberStatements : public OptOutDispatch {
 
   void handle(TensorDomain* stmt) final {
     std::vector<IterDomain*> all_ids = stmt->allIDs();
-    next_stmts_.insert(
-        next_stmts_.end(), all_ids.begin(), all_ids.end());
+    next_stmts_.insert(next_stmts_.end(), all_ids.begin(), all_ids.end());
   }
 
   void handle(TensorView* tv) final {
