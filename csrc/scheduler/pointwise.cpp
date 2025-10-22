81dd726f5d13cd0c2618735bdccb89c339d6889c
jjsjann123 <jiej@nvidia.com>
2025-10-22 15:32:23 -0700

    bump vec size for fp4 types

--- csrc/scheduler/pointwise.cpp
+++ csrc/scheduler/pointwise.cpp
@@ -295,7 +295,10 @@ std::unique_ptr<PointwiseParams> getPointwiseHeuristics(
       });
 
   int64_t max_dtype_size_bit_for_vectorization = 0;
+  // ugly WAR.
+  bool has_sub_byte = false;
   for (auto inp : vectorizable_inputs_outputs_entry.get()) {
+    has_sub_byte |= dataTypeSizeBit(inp->getDataType().value()) < 8;
     max_dtype_size_bit_for_vectorization = std::max(
         max_dtype_size_bit_for_vectorization,
         dataTypeSizeBit(inp->getDataType().value(), index_type));
@@ -485,7 +488,7 @@ std::unique_ptr<PointwiseParams> getPointwiseHeuristics(
   }
 
   params->vectorization_factor = std::min(
-      max_vect_factor,
+      has_sub_byte ? max_vect_factor : 2,
       vectorize_helper::getVectorizationFactor(
           runtime_info,
           largest_out,
