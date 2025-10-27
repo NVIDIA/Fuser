a8e126ccb83af111c8bfa74687fab72d0e9cb9c0
Jingyue Wu <wujingyue@gmail.com>
2025-10-27 13:55:40 -0700

    Drop support for CUDA <12 and clean driver_api

--- csrc/driver_api.h
+++ csrc/driver_api.h
@@ -14,11 +14,9 @@
 // happen. To understand how the magic works, please refer to the cpp file's doc
 // "How does the magic work?"
 
-namespace nvfuser {
-
 // List of driver APIs that you want the magic to happen.
 //
-// The second argument is the **requested** driver API version.
+// The second argument of `fn` is the **requested** driver API version.
 // It must be lower than or equal to the **actual** driver API version. For max
 // compatibility, this requested version should be as low as possible
 // while still supporting the capabilities required by nvFuser.
@@ -33,75 +31,52 @@ namespace nvfuser {
 // Instead, `cudaGetDriverEntryPointByVersion(...,12000)` still returns
 // cuFoo_v2 despite of the CUDA 13 upgrade so nvFuser still functions.
 //
-// nvFuser is expected to support only CUDA_VERSION >= 11000, so I didn't try
+// nvFuser is expected to support only CUDA_VERSION >= 12000, so I didn't try
 // to go lower than that. When we increase the minimum supported version, we
 // can accordingly increase the requested versions. However, we don't have to
 // unless new driver capabilities are needed.
-#define NVF_ALL_DRIVER_API_WRAPPER_VERSION_INDEPENDENT(fn) \
-  fn(cuDeviceGetAttribute, 11000);                         \
-  fn(cuDeviceGetName, 11000);                              \
-  fn(cuFuncGetAttribute, 11000);                           \
-  fn(cuFuncSetAttribute, 11000);                           \
-  fn(cuGetErrorName, 11000);                               \
-  fn(cuGetErrorString, 11000);                             \
-  fn(cuLaunchCooperativeKernel, 11000);                    \
-  fn(cuLaunchKernel, 11000);                               \
-  fn(cuModuleGetFunction, 11000);                          \
-  fn(cuModuleLoadData, 11000);                             \
-  fn(cuModuleLoadDataEx, 11000);                           \
-  fn(cuModuleUnload, 11000);                               \
-  fn(cuMemGetAddressRange, 11000);                         \
-  fn(cuOccupancyMaxActiveBlocksPerMultiprocessor, 11000);  \
-  fn(cuOccupancyAvailableDynamicSMemPerBlock, 11000)
+#define NVF_FOR_EACH_DRIVER_API(fn)                       \
+  fn(cuDeviceGetAttribute, 12000);                        \
+  fn(cuDeviceGetName, 12000);                             \
+  fn(cuFuncGetAttribute, 12000);                          \
+  fn(cuFuncSetAttribute, 12000);                          \
+  fn(cuGetErrorName, 12000);                              \
+  fn(cuGetErrorString, 12000);                            \
+  fn(cuLaunchCooperativeKernel, 12000);                   \
+  fn(cuLaunchKernel, 12000);                              \
+  fn(cuLaunchKernelEx, 12000);                            \
+  fn(cuMemGetAddressRange, 12000);                        \
+  fn(cuModuleGetFunction, 12000);                         \
+  fn(cuModuleLoadData, 12000);                            \
+  fn(cuModuleLoadDataEx, 12000);                          \
+  fn(cuModuleUnload, 12000);                              \
+  fn(cuOccupancyAvailableDynamicSMemPerBlock, 12000);     \
+  fn(cuOccupancyMaxActiveBlocksPerMultiprocessor, 12000); \
+  fn(cuOccupancyMaxActiveClusters, 12000);                \
+  fn(cuStreamBatchMemOp, 12000);                          \
+  fn(cuStreamWaitValue32, 12000);                         \
+  fn(cuStreamWriteValue32, 12000);                        \
+  fn(cuTensorMapEncodeTiled, 12000)
 
-// Stream memory operations (e.g. cuStreamWriteValue32) are specified for both
-// 11 and 12+. In CUDA 11, these operations require NVreg_EnableStreamMemOPs=1
-// to be explicitly enabled. CUDA 12+ removed this requirement. Therefore, we
-// try to request version 12000 whenever it's available.
-//
-// Details: CUDA 11.7 introduced _v2 of these APIs, which removed the above
-// NVreg_EnableStreamMemOPs=1 requirement. In CUDA 12, these _v2 APIs are
-// integrated into the vanilla APIs and are therefore removed. Refer to
-// https://docs.nvidia.com/cuda/archive/11.7.1/cuda-driver-api/group__CUDA__MEMOP.html
-#if (CUDA_VERSION >= 12000)
-#define NVF_STREAM_DRIVER_API_WRAPPER(fn) \
-  fn(cuStreamWaitValue32, 12000);         \
-  fn(cuStreamWriteValue32, 12000);        \
-  fn(cuStreamBatchMemOp, 12000)
-#elif (CUDA_VERSION >= 11000)
-#define NVF_STREAM_DRIVER_API_WRAPPER(fn) \
-  fn(cuStreamWaitValue32, 11000);         \
-  fn(cuStreamWriteValue32, 11000);        \
-  fn(cuStreamBatchMemOp, 11000)
-#else
-#error "CUDA_VERSION < 11000 isn't supported."
-#endif
+// nvFuser hasn't yet used any driver APIs that are defined above a certain
+// CUDA 12.x version. If/When there's a need, use the following pattern
+// ```c++
+// #if CUDA_VERSION >= <version>
+// #define NVF_FOR_EACH_DRIVER_API_GE_<version>(fn) \
+//   fn(cuFoo, <version>); \
+//   fn(cuBar, <version>)
+// #else
+// #define NVF_FOR_EACH_DRIVER_API_GE_<version>(fn)
+// #endif
+// ```
+// and append NVF_FOR_EACH_DRIVER_API_GE_<version>(fn) to the definition of
+// NVF_FOR_EACH_DRIVER_API(fn)
 
-#if (CUDA_VERSION >= 11080)
-#define NVF_DRIVER_API_WRAPPER_CUDA_118(fn) \
-  fn(cuOccupancyMaxActiveClusters, 11080);  \
-  fn(cuLaunchKernelEx, 11080)
-#else
-#define NVF_DRIVER_API_WRAPPER_CUDA_118(fn)
-#endif
+#define NVF_DECLARE_DRIVER_API_WRAPPER(fn, requested_version) \
+  extern decltype(::fn)* fn
 
-#if (CUDA_VERSION >= 12000)
-#define NVF_DRIVER_API_WRAPPER_CUDA_120(fn) fn(cuTensorMapEncodeTiled, 12000)
-#else
-#define NVF_DRIVER_API_WRAPPER_CUDA_120(fn)
-#endif
-
-#define NVF_ALL_DRIVER_API_WRAPPER(fn)                \
-  NVF_ALL_DRIVER_API_WRAPPER_VERSION_INDEPENDENT(fn); \
-  NVF_STREAM_DRIVER_API_WRAPPER(fn);                  \
-  NVF_DRIVER_API_WRAPPER_CUDA_118(fn);                \
-  NVF_DRIVER_API_WRAPPER_CUDA_120(fn)
-
-#define DECLARE_DRIVER_API_WRAPPER(funcName, version) \
-  extern decltype(::funcName)* funcName
-
-NVF_ALL_DRIVER_API_WRAPPER(DECLARE_DRIVER_API_WRAPPER);
-
-#undef DECLARE_DRIVER_API_WRAPPER
-
+namespace nvfuser {
+NVF_FOR_EACH_DRIVER_API(NVF_DECLARE_DRIVER_API_WRAPPER);
 } // namespace nvfuser
+
+#undef NVF_DECLARE_DRIVER_API_WRAPPER
