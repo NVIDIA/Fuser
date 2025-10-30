0c6874fb2ecc8589136d0c0c26dba8ff6bca1381
Ryan Spring <rspring@nvidia.com>
2025-10-30 15:50:03 -0700

    Implement Fusion definition equality check

--- csrc/ir/base_nodes.h
+++ csrc/ir/base_nodes.h
@@ -283,6 +283,13 @@ class NVF_API Val : public Statement {
 
   size_t getHash() const final;
 
+  // checkDefinition determines if a two values will create the same fusion
+  // definition. If this value contains a scalar, check if other value has the
+  // same scalar. NaN values are considered equal. Unlike Val::sameAs,
+  // non-deterministic definitions are permitted if both Vals have the same
+  // definition.
+  virtual bool checkDefinition(const Val* other) const;
+
   std::string toString(int indent_size = 0) const override;
 
   std::string toInlineString(int indent_size = 0) const override;
@@ -395,6 +402,10 @@ class NVF_API Val : public Statement {
         getDataType() == other->as<Val>()->getDataType();
   }
 
+  // sameAs determines if a Statement will return the exact same outputs as this
+  // Val. Both this Val and other Statement must be deterministic. It checks if
+  // the other Statement is a Val and has the same definition, ValType, and
+  // DType.
   bool sameAs(const Statement* other) const override;
 
   void setEvaluatorIndex(int to) {
@@ -535,6 +546,11 @@ class NVF_API Expr : public Statement {
   // Check that if this and other are the same operator. This main difference
   // from sameAs is that sameOp does not check the inputs.
   virtual bool sameOp(const Expr* other) const;
+
+  // Check that if this and other have same definition. This main difference
+  // from sameAs is that checkDefinition checks the inputs with checkDefinition
+  // instead of sameAs.
+  virtual bool checkDefinition(const Expr* other) const;
 
   bool sameAs(const Statement* other) const override;
 
