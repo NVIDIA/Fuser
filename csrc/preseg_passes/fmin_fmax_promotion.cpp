95747f93a641193c2f262047051068ef2085d348
tbqh <bzitelli@nvidia.com>
2025-10-30 21:15:10 -0700

    Rewrite analysis algorithm to be a mixture of TensorView and IterDomain analysis. Can now handle arbitrary reductions, but not arbitrary broadcasts.

--- csrc/preseg_passes/fmin_fmax_promotion.cpp
+++ csrc/preseg_passes/fmin_fmax_promotion.cpp
@@ -19,56 +19,15 @@ namespace nvfuser::preseg_passes {
 
 namespace {
 
-// NanStatus are attached to TensorViews and propagated with a
-// downward-flow algorithm. The goal is to detect whether lost NANs will reach
-// the outputs of a fusion. If so, then it is not valid to do the fast min/max
-// promotion for a particular reduction axis.
-enum class NanStatus {
-  // "None" status corresponds to a lack of relevant information. This status is
-  // the default when looking up an un-tracked node in the NanStatusMap. This
-  // can happen when checking e.g. an input to a binary op, which is a TV from
-  // some other part of the fusion not traversed during analysis. "None" is the
-  // lowest precedence state, everything else overwrites it.
+enum class TensorStatus {
   None = 0,
-
-  // "Unreduced" is the state attached to the input of the target reduction op.
-  // It becomes "GoodReduced" if it passes through a safe reduction. It's safe
-  // in the sense that it is allowed to reach fusion outputs. But it won't fix
-  // a bad status.
-  Unreduced,
-
-  // "BadReduced" is attached to the output of the target reduction op. It
-  // corresponds to a TV that has its data modified by the fmin/fmax promotion.
-  // If it reaches an output node, we know we cannot do the fmin/fmax promotion
-  // because it might change fusion output data.
-  BadReduced,
-
-  // "Mixed" is a combination of "Unreduced" and "BadReduced" states. It
-  // contains the NANs from an "Unreduced" state, so a safe reduction will
-  // transform it into a "GoodReduced" state. But it is also downstream of an
-  // unrepaired bad reduction, so if it reaches an output, that output may have
-  // lost NAN data.
-  Mixed,
-
-  // "GoodReduced" is the status that is reached after "Unreduced" data passes
-  // through a safe reduction. It is the highest precedence state, and repairs
-  // all other states.
-  GoodReduced,
+  Bad,
+  Good,
 };
 
-// An example of what each status looks like is below, with *max* reduction:
-//
-// [0.0  1.0  2.0  3.0  NAN  5.0] <- Unreduced
-// [5.0, 5.0, 5.0, 5.0, 5.0, 5.0] <- BadReduced
-// [NAN, NAN, NAN, NAN, NAN, NAN] <- GoodReduced
-// [5.0  5.0  5.0  5.0  NAN  5.0] <- Mixed
-//
-// Note that "Mixed" appears the same as "Unreduced" - what is the difference?
-// The difference is that "Mixed" signals that a node is downstream of the bad
-// reduction. If the bad reduction had been a good reduction, then a TV with
-// a "Mixed" state would have data like a "GoodReduced" node.
-
-using NanStatusMap = std::unordered_map<TensorView*, NanStatus>;
+using TensorMap = std::unordered_map<TensorView*, TensorStatus>;
+using IterDomainMap =
+    std::unordered_map<TensorView*, std::unordered_set<IterDomain*>>;
 
 bool isSafeReduction(
     Expr* expr,
@@ -87,17 +46,18 @@ bool isSafeReduction(
 // targetRop IterDomain.
 bool reductionDomainIsCovered(
     ReductionOp* targetRop,
-    const ValGroup& reduction_id_group,
-    const std::unordered_set<ReductionOp*>& promotedRops) {
+    const std::unordered_set<ReductionOp*>& promotedRops,
+    int targetReduceIndex) {
   Fusion* fusion = targetRop->fusion();
 
   auto* in_tv = targetRop->input(0)->as<TensorView>();
   auto* out_tv = targetRop->output(0)->as<TensorView>();
 
-  NanStatusMap status_map;
+  TensorMap tensor_map;
+  IterDomainMap good_domains;
 
-  status_map[in_tv] = NanStatus::Unreduced;
-  status_map[out_tv] = NanStatus::BadReduced;
+  good_domains[in_tv].insert(in_tv->getLogicalDomain()[targetReduceIndex]);
+  tensor_map[out_tv] = TensorStatus::Bad;
 
   auto traversal =
       StmtSort::getExprsBetween({targetRop->input(0)}, fusion->outputs());
@@ -108,29 +68,23 @@ bool reductionDomainIsCovered(
       continue;
     }
 
-    bool anyUnreduced = false;
-    bool anyBadReduced = false;
-    bool anyMixed = false;
-    bool anyGoodReduced = false;
+    bool anyGoodInputs = false;
+    bool anyBadInputs = false;
 
     for (auto input : expr->inputs()) {
       if (auto* in_tv = dynamic_cast<TensorView*>(input)) {
-        NanStatus status = status_map[in_tv];
+        TensorStatus status = tensor_map[in_tv];
 
-        if (status == NanStatus::Unreduced) {
-          anyUnreduced = true;
+        if (status == TensorStatus::Good) {
+          anyGoodInputs = true;
         }
-        if (status == NanStatus::BadReduced) {
-          anyBadReduced = true;
-        }
-        if (status == NanStatus::Mixed) {
-          anyMixed = true;
-        }
-        if (status == NanStatus::GoodReduced) {
-          anyGoodReduced = true;
+        if (status == TensorStatus::Bad) {
+          anyBadInputs = true;
         }
       }
     }
+
+    TensorStatus status = TensorStatus::None;
 
     auto* out_tv = dynamic_cast<TensorView*>(expr->output(0));
 
@@ -139,56 +93,45 @@ bool reductionDomainIsCovered(
 
     if (!out_tv || !canBeAnalyzed) {
       // Analysis is blocked for this node, treat it like a fusion output.
-      if (anyBadReduced || anyMixed) {
+      if (anyBadInputs) {
         return false;
       } else {
         continue;
       }
     }
 
-    NanStatus status = NanStatus::None;
-
-    // Determine this node's status based on its inputs.
-    // Status is mostly propped based on priority. For example, GoodReduced
-    // beats all other states. There is also one combination rule with
-    // BadReduced and Unreduced combining to become Mixed.
-    if (anyGoodReduced) {
-      status = NanStatus::GoodReduced;
-    } else if (anyMixed) {
-      status = NanStatus::Mixed;
-    } else if (anyUnreduced && anyBadReduced) {
-      status = NanStatus::Mixed;
-    } else if (anyUnreduced) {
-      status = NanStatus::Unreduced;
-    } else if (anyBadReduced) {
-      status = NanStatus::BadReduced;
+    if (anyGoodInputs) {
+      status = TensorStatus::Good;
+    } else if (anyBadInputs) {
+      status = TensorStatus::Bad;
     }
 
-    // Here we handle the repair of squelched NAN's. This happens when
-    // IterDomain's with "Unreduced" data are safely reduced.
-    if (isSafeReduction(expr, promotedRops)) {
-      if (status == NanStatus::Unreduced || status == NanStatus::Mixed) {
-        for (IterDomain* out_id : out_tv->getLogicalDomain()) {
-          if (out_id->isReduction()) {
-            // This id_group check verifies that the reduction axis matches
-            // the axis of the target unsafe reduction.
-            if (reduction_id_group->has(out_id)) {
-              status = NanStatus::GoodReduced;
-              break;
-            }
+    for (auto input : expr->inputs()) {
+      if (auto* in_tv = dynamic_cast<TensorView*>(input)) {
+        PairwiseLogicalDomainMap p2c_map(in_tv, out_tv);
+        p2c_map.mapBroadcast(false);
+        auto p2c = p2c_map.mapProducerToConsumer();
+        for (IterDomain* in_id : good_domains[in_tv]) {
+          if (!p2c.contains(in_id)) {
+            continue;
+          }
+
+          IterDomain* out_id = p2c.at(in_id);
+          good_domains[out_tv].insert(out_id);
+
+          if (out_id->isReduction() && isSafeReduction(expr, promotedRops)) {
+            status = TensorStatus::Good;
           }
         }
       }
     }
 
-    status_map[out_tv] = status;
+    tensor_map[out_tv] = status;
   }
 
-  // Check whether any bad status reached output nodes
   auto output_tvs = ir_utils::filterByType<TensorView>(fusion->outputs());
   for (TensorView* out_tv : output_tvs) {
-    if (status_map[out_tv] == NanStatus::BadReduced ||
-        status_map[out_tv] == NanStatus::Mixed) {
+    if (tensor_map[out_tv] == TensorStatus::Bad) {
       return false;
     }
   }
@@ -205,15 +148,14 @@ bool reductionDomainIsCovered(
 // make the propagation code more complicated.
 bool minMaxOpIsCovered(
     ReductionOp* targetRop,
-    const ValGraph& graph,
     const std::unordered_set<ReductionOp*>& promotedRops) {
   auto* out_tv = targetRop->output(0)->as<TensorView>();
 
+  int i = -1;
   for (IterDomain* out_id : out_tv->getLogicalDomain()) {
+    ++i;
     if (out_id->isReduction()) {
-      const ValGroup& reduction_id_group = graph.toGroup(out_id);
-      if (!reductionDomainIsCovered(
-              targetRop, reduction_id_group, promotedRops)) {
+      if (!reductionDomainIsCovered(targetRop, promotedRops, i)) {
         return false;
       }
     }
@@ -226,10 +168,6 @@ bool minMaxOpIsCovered(
 
 void FMinFMaxPromotionPass::runPass(Fusion* fusion) {
   FusionGuard fusion_guard(fusion);
-
-  IdModel id_model(fusion, false);
-  id_model.buildGraph(IdMappingMode::EXACT);
-  const ValGraph& graph = id_model.idGraph(IdMappingMode::EXACT);
 
   std::unordered_set<ReductionOp*> promotedRops;
 
@@ -246,7 +184,7 @@ void FMinFMaxPromotionPass::runPass(Fusion* fusion) {
 
     if (reduction_type == BinaryOpType::Min ||
         reduction_type == BinaryOpType::Max) {
-      if (minMaxOpIsCovered(targetRop, graph, promotedRops)) {
+      if (minMaxOpIsCovered(targetRop, promotedRops)) {
         promotedRops.insert(targetRop);
       }
     }
