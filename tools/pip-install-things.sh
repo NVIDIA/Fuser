39e3a29ab1b57fb89ffc4efe0e03516b1b743bdc
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2025-11-03 14:10:34 -0800

    use torch's own cmake find cuda

--- tools/pip-install-things.sh
+++ tools/pip-install-things.sh
@@ -2,7 +2,4 @@
 
 pip3 install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu130
 
-# PyTorch's has a buildin Modules_CUDA which is super old. Remove it.
-# rm -rf $(python -c 'import torch; print(torch.__path__[0])')/share/cmake/Caffe2/Modules_CUDA_fix
-
 pip install -r requirements.txt
