6705930663a1139c28281148a434a4da197881e1
jjsjann123 <jiej@nvidia.com>
2025-11-03 22:18:45 -0800

    Minor fixes on pointwise scheduler support for fp4 (#5455)
    1. move sub-byte vectorization factor logic closer to its uses;
    2. update the validation for python tests;

--- tests/python/direct/test_narrow_precision.py
+++ tests/python/direct/test_narrow_precision.py
@@ -99,9 +99,9 @@ def test_scaled_mm(
         )
         fd.add_output(out)
 
-    o, _ = nvfuser_direct_test.exec_nvfuser(nvfuser_fusion_id0, inputs)
+    outputs, _ = nvfuser_direct_test.exec_nvfuser(nvfuser_fusion_id0, inputs)
 
-    ref_o = (
+    ref_outputs = (
         torch._scaled_mm(
             mat1,
             mat2.t(),
@@ -113,7 +113,7 @@ def test_scaled_mm(
         )
         * alpha
     )
-    torch.testing.assert_close(o[0], ref_o, rtol=1e-1, atol=1e-2)
+    torch.testing.assert_close(outputs[0], ref_outputs, rtol=1e-1, atol=1e-2)
 
 
 @pytest.mark.skipif(
@@ -248,7 +248,7 @@ def test_cutlass_nvfp4_grouped_mm(
         blockscale_offsets,
     ]
 
-    o, _ = nvfuser_direct_test.exec_nvfuser(nvfuser_fusion_id0, inputs)
+    outputs, _ = nvfuser_direct_test.exec_nvfuser(nvfuser_fusion_id0, inputs)
 
     o_decomposed_ref = torch.empty(m, n, dtype=torch.bfloat16, device="cuda:0")
     for i in range(g):
@@ -274,7 +274,7 @@ def test_cutlass_nvfp4_grouped_mm(
             * mat2_gs[i]
         )
 
-    torch.testing.assert_close(o_decomposed_ref, o[0], atol=1e-2, rtol=1e-2)
+    torch.testing.assert_close(o_decomposed_ref, outputs[0], atol=1e-2, rtol=1e-2)
 
 
 @pytest.mark.skipif(
@@ -304,6 +304,15 @@ def test_fp4_vectorization(
         T5 = fd.ops.reshape(T4, [32])
         fd.add_output(T5)
 
-    o, _ = nvfuser_direct_test.exec_nvfuser(nvfuser_fusion_id0, inputs)
+    outputs, _ = nvfuser_direct_test.exec_nvfuser(nvfuser_fusion_id0, inputs)
 
-    ref_o = to_fp4(inputs[0].to(torch.float) / inputs[1].unsqueeze(-1)).reshape(-1)
+    ref_outputs = to_fp4(inputs[0].to(torch.float) / inputs[1].unsqueeze(-1)).reshape(
+        -1
+    )
+
+    torch.testing.assert_close(
+        outputs[0].view(dtype=torch.uint8),
+        ref_outputs.view(dtype=torch.uint8),
+        rtol=1e-1,
+        atol=1e-2,
+    )
