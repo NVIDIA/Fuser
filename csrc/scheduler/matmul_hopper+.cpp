56ab27663fdc3f6c372fa70fabccf5bcb44005a6
Jacob Hinkle <jhinkle@nvidia.com>
2025-11-18 08:06:51 -0500

    lintrunner

--- csrc/scheduler/matmul_hopper+.cpp
+++ csrc/scheduler/matmul_hopper+.cpp
@@ -239,7 +239,8 @@ std::vector<MatmulDimRole> HopperPlus::reorderBlockTileTraversal(
         Mo_pos >= 0 || No_pos >= 0, "Either M or N role must be present.");
     NVF_ERROR(
         Mo_pos != No_pos, "The position of M and N roles must be different.");
-    NVF_ERROR(std::abs(Mo_pos - No_pos) == 1, "M and N roles must be consecutive.");
+    NVF_ERROR(
+        std::abs(Mo_pos - No_pos) == 1, "M and N roles must be consecutive.");
 
     bool is_M_present = Mo_pos >= 0;
     bool is_N_present = No_pos >= 0;
