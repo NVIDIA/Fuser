1f4639605a518fffc2202c7c8f23069438e02915
Jingyue Wu <wujingyue@gmail.com>
2025-11-18 12:18:04 -0800

    Improve code comments around markAliases (#5545)

--- csrc/scheduler/mark_aliases.h
+++ csrc/scheduler/mark_aliases.h
@@ -14,6 +14,17 @@ namespace nvfuser {
 // Marks aliases between fusion inputs and outputs. This respects existing
 // allocation domains **even when** they are empty (assuming default order). See
 // [Note on overriding empty allocation domains].
+//
+// This function is called by various schedulers (like pointwise and reduction)
+// to handle a partially alias-producing fusion. We could instead improve
+// MarkAliasesPreparePass to split a partially alias-producing fusion into an
+// alias-only segment and the rest. This way, the rest of the fusion (which has
+// fewer expressions) can potentially find a better scheduler or scheduling, and
+// we can completely remove this function for simplicity.
+//
+// This function is typically called at the end of scheduling. This is because
+// schedulers may add Exprs that affects aliasing:
+// https://github.com/NVIDIA/Fuser/issues/1401#issuecomment-1840137527.
 void markAliases(Fusion* fusion);
 
 } // namespace nvfuser
