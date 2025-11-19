3691bcda4f097c89daa5838272d55f0cba927ed5
Ryan Spring <rspring@nvidia.com>
2025-11-19 13:02:05 -0800

    Use NVRTC to jit compile bulk_load.cu

---
+++ cutlass/nvrtc/bulk_load.cu
@@ -0,0 +1,67 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+
+#include <cute/tensor.hpp>
+#include "cute.cuh"
+
+using namespace cute;
+
+__global__ void cute_bulk_copy(float const* g_in, float* g_out) {
+  auto smem_layout = make_layout(Shape<_32, _32>{}, GenRowMajor{});
+  auto gmem_layout = smem_layout;
+  // Use Shared Storage structure to allocate and distribute aligned SMEM
+  // addresses
+  extern __shared__ char shared_memory[];
+  using SharedStorage = SharedStorage<float, decltype(smem_layout)>;
+  SharedStorage& shared_storage =
+      *reinterpret_cast<SharedStorage*>(shared_memory);
+
+  // Construct SMEM tensor
+  Tensor sA =
+      make_tensor(make_smem_ptr(shared_storage.smem.data()), smem_layout);
+  // Construct the GMEM tensor
+  Tensor gA = make_tensor(make_gmem_ptr(g_in), gmem_layout);
+
+  // Shared memory barriers use 64bits in SMEM for synchronization
+  uint64_t* bulk_copy_mbar = shared_storage.bulk_copy_mbar;
+
+  //
+  // Perform the BULK_COPY load
+  //
+
+  auto blkcp = Copy_Traits<SM90_BULK_COPY_AUTO>{};
+
+  // Set the bytes transferred in this transaction (may involve multiple issues)
+  constexpr int transaction_bytes = size(sA) * sizeof(float);
+
+  if (threadIdx.x == 0) {
+    /// Initialize shared memory barrier
+    bulk_copy_mbar[0] = 0;
+    initialize_barrier(bulk_copy_mbar[0], 1 /*numThreads*/);
+    set_barrier_transaction_bytes(bulk_copy_mbar[0], transaction_bytes);
+
+    copy(blkcp.with(bulk_copy_mbar[0]), gA, sA);
+  }
+  __syncthreads();
+
+  /// Wait on the shared memory barrier until the phase bit flips from kPhaseBit
+  /// value
+  constexpr int kPhaseBit = 0;
+  wait_barrier(bulk_copy_mbar[0], kPhaseBit);
+
+  //
+  // Write out trivially
+  //
+
+  Tensor gA_out = make_tensor(make_gmem_ptr(g_out), gmem_layout);
+
+  // Output smem -> gmem
+  for (int i = threadIdx.x; i < size(sA); i += blockDim.x) {
+    gA_out(i) = sA(i);
+  }
+}
---
+++ cutlass/nvrtc/main.cpp
@@ -0,0 +1,143 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+
+#include <fstream>
+
+#include <ATen/ATen.h>
+#include <cute/tensor.hpp>
+#include <nvrtc.h>
+
+#include "cute.cuh"
+#include "utils.h"
+
+std::string read_file(const std::string& filename) {
+  std::ifstream file(filename);
+  if (!file.is_open()) {
+    std::cerr << "Failed to open file: " << filename << std::endl;
+    exit(1);
+  }
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  return buffer.str();
+}
+
+std::vector<std::byte> convertTensorToBytes(at::Tensor& tensor) {
+  std::vector<std::byte> buffer;
+  void* data = tensor.data_ptr();
+  buffer.reserve(sizeof(void*));
+  buffer.insert(
+      buffer.end(), (std::byte*)&data, (std::byte*)&data + sizeof(void*));
+  return buffer;
+}
+
+int main() {
+  // 1. Read the CUDA source file
+  std::string filename = "bulk_load.cu";
+  std::string cuda_source = read_file(filename);
+
+  // 2. Create the NVRTC Program
+  nvrtcProgram prog;
+  // Parameters: program ptr, source string, name for errors, num headers,
+  // headers, header names
+  nvrtcCreateProgram(
+      &prog, cuda_source.c_str(), filename.c_str(), 0, NULL, NULL);
+
+  const char* name_expression = "cute_bulk_copy";
+  nvrtcAddNameExpression(prog, name_expression);
+
+  // 3. Compile the Program
+  std::vector<const char*> opts;
+  opts.push_back("-std=c++20");
+  opts.push_back("-default-device");
+  opts.push_back("--gpu-architecture=compute_100a");
+  opts.push_back("-DCUTLASS_NVCC_ARCHS=\"100a\"");
+  opts.push_back("-I/usr/local/cuda/include/");
+  opts.push_back("-I/usr/local/cuda/include/cccl/");
+  opts.push_back("-I../../third_party/cutlass/include");
+  opts.push_back("-I../../third_party/cutlass/include/tools/util/include");
+  NVRTC_CHECK(nvrtcCompileProgram(prog, opts.size(), opts.data()));
+
+  const char* mangled_name;
+  nvrtcGetLoweredName(prog, name_expression, &mangled_name);
+
+  // 5. Get the PTX (The compiled binary)
+  size_t ptx_size;
+  nvrtcGetPTXSize(prog, &ptx_size);
+  assert(ptx_size > 0);
+
+  std::vector<char> ptx(ptx_size);
+  nvrtcGetPTX(prog, ptx.data());
+
+  // **********************************************************
+
+  // Create ATen arguments before getting CUfunction to create cuContext because
+  // manually creating cuContext fails
+  auto options = at::TensorOptions().dtype(at::kFloat).device(at::kCUDA, 0);
+  auto d_in = at::randn({32, 32}, options);
+  auto d_out = at::empty({32, 32}, options);
+
+  std::vector<std::vector<std::byte>> data;
+  std::vector<void*> pointers;
+
+  data.push_back(convertTensorToBytes(d_in));
+  pointers.emplace_back(data.back().data());
+
+  data.push_back(convertTensorToBytes(d_out));
+  pointers.emplace_back(data.back().data());
+
+  // Create module from binary file (FATBIN)
+  CUmodule cu_module;
+  CUDA_CHECK(cuModuleLoadData(&cu_module, (void*)ptx.data()));
+
+  // Get function handle from module
+  CUfunction kernel;
+  CUDA_CHECK(cuModuleGetFunction(&kernel, cu_module, mangled_name));
+
+  // Cleanup NVRTC after cuModuleGetFunction to avoid destroying mangled_name
+  nvrtcDestroyProgram(&prog);
+
+  CUDA_RUNTIME_CHECK(cudaDeviceSynchronize());
+
+  auto smem_layout = cute::make_layout(
+      cute::Shape<cute::_32, cute::_32>{}, cute::GenRowMajor{});
+  auto gmem_layout = smem_layout;
+  int32_t smem_size =
+      static_cast<int32_t>(sizeof(SharedStorage<float, decltype(smem_layout)>));
+
+  CUlaunchConfig config = {};
+  config.gridDimX = 1;
+  config.gridDimY = 1;
+  config.gridDimZ = 1;
+  config.blockDimX = 128;
+  config.blockDimY = 1;
+  config.blockDimZ = 1;
+  config.sharedMemBytes = smem_size;
+  config.attrs = nullptr;
+  config.numAttrs = 0;
+
+  CUDA_CHECK(cuLaunchKernelEx(&config, kernel, pointers.data(), nullptr));
+
+  CUDA_RUNTIME_CHECK(cudaDeviceSynchronize());
+
+  // Flatten the tensors for easier access
+  auto d_in_flat = d_in.flatten();
+  auto d_out_flat = d_out.flatten();
+
+  // Validate the results
+  for (int i = 0; i < cute::size(gmem_layout); ++i) {
+    int k = gmem_layout(i);
+    // TODO: Replace with ASSERT_EQ if using gtest framework
+    float in_val = d_in_flat[k].item<float>();
+    float out_val = d_out_flat[k].item<float>();
+    if (in_val != out_val) {
+      printf("d_in[%d] = %f, d_out[%d] = %f\n", k, in_val, k, out_val);
+    }
+  }
+
+  return 0;
+}
