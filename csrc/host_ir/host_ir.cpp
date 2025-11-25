abc5dbb7013c26beb4420202dc84e26e9bbd3cff
Michael Davis <michael.l.davis@hotmail.co.uk>
2025-11-24 22:00:24 -0800

    Bounds and argument safety.

--- csrc/host_ir/host_ir.cpp
+++ csrc/host_ir/host_ir.cpp
@@ -140,12 +140,19 @@ LaunchKernel::LaunchKernel(
     Val* cache_id,
     PrimDataType index_type)
     : Expr(passkey, inputs, outputs, {}), index_type_(index_type), compiled_kernel_(compiled_kernel) {
+  NVF_CHECK(
+      compiled_kernel != nullptr,
+      "LaunchKernel requires a non-null CompiledKernel pointer");
+  NVF_CHECK(
+      cache_id != nullptr, "LaunchKernel requires a non-null cache_id");
+
   addDataAttribute(group_id);
   addDataAttribute(launch_constraints);
   addDataAttribute(compiled_kernel->compileParams());
   addAttribute(cache_id);
 
   for (auto* val : inputs) {
+    NVF_CHECK(val != nullptr, "LaunchKernel input cannot be null");
     ArgInfo info;
     info.dtype = val->dtype();
     info.last_dim_adj = getLastDimAdjustment(info.dtype);
@@ -154,6 +161,7 @@ LaunchKernel::LaunchKernel(
   }
 
   for (auto* val : outputs) {
+    NVF_CHECK(val != nullptr, "LaunchKernel output cannot be null");
     ArgInfo info;
     info.dtype = val->dtype();
     info.last_dim_adj = getLastDimAdjustment(info.dtype);
