491e377217c8afd7abaf426135d9415013ba187c
Nick Sarkauskas <nsarkauskas@nvidia.com>
2025-11-25 17:22:33 +0200

    MM+RS Stream Parallel Type Lowering

--- csrc/host_ir/lower.cpp
+++ csrc/host_ir/lower.cpp
@@ -100,8 +100,9 @@ std::unique_ptr<hir::HostIrContainer> HostIrLower::lower(
   // since they are incompatible with MultiDeviceExecutor.
   OptimizationPass<preseg_passes::DecomposeReshardingsPass>::runPass(
       fusion.get());
-  OptimizationPass<preseg_passes::FinalizeMultideviceDomainsPass>::runPass(
-      fusion.get());
+  // nick: reorders sharded axis to the front, but the alg expects it to be the second axis for the matmul expr
+  //OptimizationPass<preseg_passes::FinalizeMultideviceDomainsPass>::runPass(
+  //    fusion.get());
 
   // Performs segmentation at the inter-device communications
   // Each SegmentedGroup represents a pipeline's stage, and can be either
