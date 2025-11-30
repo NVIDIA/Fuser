dba9df48837582cdc309a91968a18e052755639d
Jingyue Wu <wujingyue@gmail.com>
2025-11-30 08:00:46 -0800

    Remove an unnecessary NVF_API (#5599)

--- csrc/preseg_passes/optimization_pass.h
+++ csrc/preseg_passes/optimization_pass.h
@@ -47,7 +47,7 @@ class OptimizationPass {
     return flag_.load();
   }
 
-  NVF_API static void runPass(Fusion* fusion) {
+  static void runPass(Fusion* fusion) {
     if (!flag_.load()) {
       return;
     }
