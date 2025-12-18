195bd5e3ec9ba389e05b25c0520dcb0ca9eb1cf9
Naoya Maruyama <naoyam@users.noreply.github.com>
2025-12-18 13:32:49 -0800

    Partition operation for RaggedIterDomain (#5674)
    Adds the `Partition` iter domain op to represent the construction of a
    ragged iter domain from an iter domain. Similar to `IterDomain::split`,
    `RaggedIterDomain::partition` is added by using the `Partition` op.
    
    The offsets/extents parameter is still limited to 1D. Will be extended
    in a follow-up PR.

--- csrc/tensor_view.cpp
+++ csrc/tensor_view.cpp
@@ -561,6 +561,51 @@ TensorView* TensorView::merge(int64_t axis_o, int64_t axis_i) {
   return this;
 }
 
+// Partition "axis" into component and ragged dimensions based on
+// extents. Follow the pattern of TensorView::split.
+TensorView* TensorView::partition(int64_t axis, TensorView* extents) {
+  NVF_ERROR(
+      nDims() > 0,
+      "Tried to do partition on a 0-dim TensorView. ",
+      "Tensor: ",
+      toString());
+
+  axis = wrapDim(axis);
+
+  NVF_CHECK(
+      axis >= getMaxComputePosition(),
+      "Cannot partition axis within compute at position. Axis = ",
+      axis,
+      " computePosition = ",
+      getMaxComputePosition(),
+      ". Tensor: ",
+      toString());
+
+  NVF_CHECK(
+      axis >= getMaybeMaxProducerPosition(),
+      "Cannot partition axis within max producer position. Axis = ",
+      axis,
+      " maxProducerPosition = ",
+      getMaybeMaxProducerPosition(),
+      ". Tensor: ",
+      toString());
+
+  NVF_CHECK(
+      this->axis(axis)->getParallelType() == ParallelType::Serial,
+      "Partitioning an axis (",
+      this->axis(axis)->toString(),
+      ") of non-Serial parallel type is not supported at this time."
+      " Parallelization strategy must be set after calling partition: ",
+      toString());
+
+  if (extents->dtype() != DataType::Index) {
+    extents = castOp(DataType::Index, extents);
+  }
+
+  domain()->partition(axis, extents);
+  return this;
+}
+
 TensorView* TensorView::resize(
     int64_t axis,
     Val* left_expansion,
