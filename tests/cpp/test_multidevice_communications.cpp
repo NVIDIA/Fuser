64b29e661950bef67dcd1e8d77063e22ba6a03a7
Jingyue Wu <wujingyue@gmail.com>
2025-12-20 10:30:48 -0800

    Comment

--- tests/cpp/test_multidevice_communications.cpp
+++ tests/cpp/test_multidevice_communications.cpp
@@ -517,6 +517,7 @@ class CUDACommunicationTest : public MultiDeviceTest {
   }
 };
 
+// Disabled because it failed in GB200 CI: http://nv/e.n
 TEST_F(CUDACommunicationTest, DISABLED_Broadcast) {
   if (communicator_->size() < 2 || at::cuda::device_count() < 2) {
     GTEST_SKIP() << "This test needs at least 2 GPUs and 2 ranks.";
