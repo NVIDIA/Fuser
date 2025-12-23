2925d82c439bbfe0523a5c884df909c2c14f83ff
Michael Davis <michael.l.davis@hotmail.co.uk>
2025-12-23 14:27:38 -0800

    Remove duplicate install logic

--- cmake/DependencyUtilities.cmake
+++ cmake/DependencyUtilities.cmake
@@ -1,13 +1,21 @@
+set(NVFUSER_CMake_VERSION_REQUIRED       "3.8")
+set(NVFUSER_Ninja_VERSION_REQUIRED       "ANY")
+
+set(NVFUSER_GCC_VERSION_REQUIRED       "0.0")
+set(NVFUSER_Clang_VERSION_REQUIRED       "0.0")
+
+set(NVFUSER_CUDAToolkit_VERSION_REQUIRED  "12.6")
+
 set(NVFUSER_Python_VERSION_REQUIRED       "3.8")
 set(NVFUSER_Torch_VERSION_REQUIRED        "2.0")
 set(NVFUSER_pybind11_VERSION_REQUIRED     "2.0")
-set(NVFUSER_CUDAToolkit_VERSION_REQUIRED  "12.6")
+
 set(NVFUSER_LLVM_VERSION_REQUIRED         "18.1")
 
 # --------------------------
-# Find all dependencies (sandboxed - non-fatal)
+# Find all dependencies
 # --------------------------
-macro(find_nvfuser_dependencies_sandboxed)
+macro(find_nvfuser_dependencies)
 
   # --------------------------
   # Python
@@ -94,115 +102,29 @@ macro(find_nvfuser_dependencies_sandboxed)
 endmacro()
 
 # --------------------------
-# Find all dependencies (fatal on error)
-# --------------------------
-macro(find_nvfuser_dependencies)
-
-  # --------------------------
-  # Python
-  # --------------------------
-  message("")
-  message("Finding Python...")
-  find_package(Python ${NVFUSER_Python_VERSION_REQUIRED} COMPONENTS Interpreter Development REQUIRED)
-
-
-  # --------------------------
-  # Torch
-  # --------------------------
-  message("")
-  message("Finding Torch...")
-  execute_process(
-    COMMAND "${Python_EXECUTABLE}" -c "import torch; print(torch.utils.cmake_prefix_path)"
-    OUTPUT_VARIABLE TORCH_CMAKE_PATH
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    RESULT_VARIABLE TORCH_FIND_RESULT
-  )
-
-  if(NOT TORCH_FIND_RESULT EQUAL 0)
-    message(FATAL_ERROR "Failed to find PyTorch via Python. Make sure 'torch' is installed in the detected Python environment.")
-  endif()
-
-  # need this since the pytorch execution uses a different name
-  set(PYTHON_EXECUTABLE ${Python_EXECUTABLE})
-  list(APPEND CMAKE_PREFIX_PATH "${TORCH_CMAKE_PATH}")
-
-  # set CUDA_ARCH for cu tests.
-  if(TORCH_CUDA_ARCH_LIST)
-    set(ARCH_FLAGS)
-    cuda_select_nvcc_arch_flags(ARCH_FLAGS ${TORCH_CUDA_ARCH_LIST})
-    list(APPEND CUDA_NVCC_FLAGS ${ARCH_FLAGS})
-  endif()
-
-  # CXX flags is necessary since https://github.com/pytorch/pytorch/issues/98093
-  string(APPEND CMAKE_CXX_FLAGS " ${TORCH_CXX_FLAGS}")
-
-  find_package(Torch ${NVFUSER_Torch_VERSION_REQUIRED} REQUIRED)
-
-  
-  # --------------------------
-  # pybind11
-  # --------------------------
-  message("")
-  message("Finding pybind11...")
-  find_package(pybind11 REQUIRED)
-
-  # --------------------------
-  # CUDAToolkit
-  # --------------------------
-  message("")
-  message("Finding CUDAToolkit...")
-  find_package(CUDAToolkit REQUIRED COMPONENTS Cupti cuda_driver)
-
-  # --------------------------
-  # LLVM
-  # --------------------------
-  message("")
-  message("Finding LLVM...")
-  find_package(LLVM ${NVFUSER_LLVM_VERSION_REQUIRED} REQUIRED)
-
-  if (LLVM_FOUND)
-    llvm_map_components_to_libnames(LLVM_LIBS
-      support
-      core
-      orcjit
-      executionengine
-      irreader
-      nativecodegen
-      Target
-      Analysis
-      JITLink
-      Demangle
-    )
-  endif()
-
-endmacro()
-
-
-
-# --------------------------
 # Reporting
 # --------------------------
 
 if(NOT WIN32 OR MSVC)
-    string(ASCII 27 Esc)
-    set(ColorReset  "${Esc}[m")
-    set(ColorBold   "${Esc}[1m")
-    set(ColorRed    "${Esc}[31m")
-    set(ColorGreen  "${Esc}[32m")
-    set(ColorYellow "${Esc}[33m")
-    set(ColorBlue   "${Esc}[34m")
-    set(ColorMagenta "${Esc}[35m")
-    set(ColorCyan   "${Esc}[36m")
-    set(ColorWhite  "${Esc}[37m")
-    set(BoldRed     "${Esc}[1;31m")
-    set(BoldGreen   "${Esc}[1;32m")
-    set(BoldYellow  "${Esc}[1;33m")
-    set(BoldBlue    "${Esc}[1;34m")
-    set(BoldWhite   "${Esc}[1;37m")
+  string(ASCII 27 Esc)
+  set(ColorReset  "${Esc}[m")
+  set(ColorBold   "${Esc}[1m")
+  set(ColorRed    "${Esc}[31m")
+  set(ColorGreen  "${Esc}[32m")
+  set(ColorYellow "${Esc}[33m")
+  set(ColorBlue   "${Esc}[34m")
+  set(ColorMagenta "${Esc}[35m")
+  set(ColorCyan   "${Esc}[36m")
+  set(ColorWhite  "${Esc}[37m")
+  set(BoldRed     "${Esc}[1;31m")
+  set(BoldGreen   "${Esc}[1;32m")
+  set(BoldYellow  "${Esc}[1;33m")
+  set(BoldBlue    "${Esc}[1;34m")
+  set(BoldWhite   "${Esc}[1;37m")
 endif()
 
 function(message_colored color text)
-    message(STATUS "${color}${text}${ColorReset}")
+  message(STATUS "${color}${text}${ColorReset}")
 endfunction()
 
 macro(report name location)
