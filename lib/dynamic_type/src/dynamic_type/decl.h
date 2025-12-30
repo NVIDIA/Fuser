2a056e15c759c32554fa60536a79a70e24b21ebc
Christian Sarofeen <csarofeen@nvidia.com>
2025-12-30 11:14:33 -0500

    M8 Task 4: Move operator<< to impl.h

--- lib/dynamic_type/src/dynamic_type/decl.h
+++ lib/dynamic_type/src/dynamic_type/decl.h
@@ -780,30 +780,14 @@ constexpr auto can_print = [](auto x) constexpr {
   }
   return false;
 };
+
+// Declaration only - implementation in impl.h
 template <
     typename DT,
     typename = std::enable_if_t<
         is_dynamic_type_v<DT> &&
         any_check(can_print, DT::type_identities_as_tuple)>>
-std::ostream& operator<<(std::ostream& os, const DT& dt) {
-  bool printed = false;
-  DT::for_all_types([&printed, &os, &dt](auto _) {
-    using T = typename decltype(_)::type;
-    if constexpr (opcheck<std::ostream&> << opcheck<T>) {
-      if constexpr (std::is_same_v<
-                        decltype(os << std::declval<T>()),
-                        std::ostream&>) {
-        if (dt.template is<T>()) {
-          os << dt.template as<T>();
-          printed = true;
-        }
-      }
-    }
-  });
-  DYNAMIC_TYPE_CHECK(
-      printed, "Can not print ", dt.type().name(), " : incompatible type");
-  return os;
-}
+std::ostream& operator<<(std::ostream& os, const DT& dt);
 
 #define DEFINE_LEFT_PPMM(opname, op)                                           \
   /*TODO: we should inline the definition of opname##_helper into enable_if,*/ \
--- lib/dynamic_type/src/dynamic_type/impl.h
+++ lib/dynamic_type/src/dynamic_type/impl.h
@@ -10,9 +10,32 @@
 // Heavy operator implementations for DynamicType.
 // This file will contain operator implementations that can be
 // instantiated once via explicit template instantiation.
-//
-// Currently all implementations are still in decl.h.
-// Future tasks will move operators here incrementally.
 
 #include "decl.h"
+
+namespace dynamic_type {
+
+// Stream output operator implementation
+template <typename DT, typename>
+std::ostream& operator<<(std::ostream& os, const DT& dt) {
+  bool printed = false;
+  DT::for_all_types([&printed, &os, &dt](auto _) {
+    using T = typename decltype(_)::type;
+    if constexpr (opcheck<std::ostream&> << opcheck<T>) {
+      if constexpr (std::is_same_v<
+                        decltype(os << std::declval<T>()),
+                        std::ostream&>) {
+        if (dt.template is<T>()) {
+          os << dt.template as<T>();
+          printed = true;
+        }
+      }
+    }
+  });
+  DYNAMIC_TYPE_CHECK(
+      printed, "Can not print ", dt.type().name(), " : incompatible type");
+  return os;
+}
+
+} // namespace dynamic_type
 
