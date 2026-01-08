727c2b0e7b3e5d00e172a7db8bf614f478c0360a
Xiang Gao <xgao@nvidia.com>
2026-01-07 16:39:43 -0800

    fix

--- csrc/runtime/executor.cpp
+++ csrc/runtime/executor.cpp
@@ -539,8 +539,8 @@ std::vector<GlobalBufferInfo> KernelExecutor::getIntermediateBufferInfo(
     info.resets_to_zero = alloc->resetsToZero();
     // TODO: Allocation size needs to consider both expanded domains
     // as well as halo. Currently, halo support has bene removed so we only need
-    // to worry about the expand case which is handled in inferShapeofOutputs.
-    // There used to also be a inferShapeOfIntermediate function before this
+    // to worry about the expand case which is handled in inferShapeAndContiguousStrides.
+    // There used to also be a inferShapeAndContiguousStrideOfIntermediate function before this
     // commit, but that was safely removed with halo support. This will need to
     // be revisited when halo support is added again.
     auto [sizes, strides] = inferShapeAndContiguousStrides(tv, expr_eval);
