f71693db07e6fcab25a20284e51eb113b3b04706
Michael Davis <michael.l.davis@hotmail.co.uk>
2026-01-12 16:30:59 -0800

    Move FlatBuffers and Cutlass version checking into Submodule cmake file.

--- cmake/Submodules.cmake
+++ cmake/Submodules.cmake
@@ -34,3 +34,27 @@ else()
   set_property(TARGET benchmark PROPERTY IMPORTED_LOCATION ${BENCHMARK_LIBRARY})
 endif()
 
+# -----------------------------------------------------------------------------
+# FlatBuffer
+# -----------------------------------------------------------------------------
+
+set(FlatBuffers_Src_Dir ${PROJECT_SOURCE_DIR}/third_party/flatbuffers)
+
+option(FLATBUFFERS_BUILD_TESTS "Enable the build of tests and samples." OFF)
+option(FLATBUFFERS_BUILD_FLATC "Enable the build of the flatbuffers compiler" ON)
+option(FLATBUFFERS_STATIC_FLATC "Build flatbuffers compiler with -static flag" OFF)
+option(FLATBUFFERS_BUILD_FLATHASH "Enable the build of flathash" OFF)
+
+# Add FlatBuffers directly to our build. This defines the `flatbuffers` target.
+add_subdirectory(${FlatBuffers_Src_Dir})
+
+# -----------------------------------------------------------------------------
+# CUTLASS
+# -----------------------------------------------------------------------------
+
+if(CMAKE_CUDA_COMPILER_VERSION VERSION_LESS 12.8)
+  message(WARNING "CUTLASS Support DISABLED: Requires CUDA 12.8+")
+  set(NVFUSER_USE_CUTLASS FALSE)
+else()
+  set(NVFUSER_USE_CUTLASS TRUE)
+endif()
