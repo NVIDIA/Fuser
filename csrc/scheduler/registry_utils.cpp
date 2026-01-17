8a6d209ff3bcf4cd29dc1a94034c4a7304ecccf0
jjsjann123 <jiej@nvidia.com>
2026-01-16 17:43:49 -0800

    reverting unwanted changes

--- csrc/scheduler/registry_utils.cpp
+++ csrc/scheduler/registry_utils.cpp
@@ -856,13 +856,6 @@ bool hasNonTerminalBlockQuantizeOp(Fusion* fusion) {
       if (!block_scales->isFusionOutput()) {
         return true;
       }
-    } else if (expr->isA<GroupedBlockQuantizationOp>()) {
-      auto block_scales = expr->as<GroupedBlockQuantizationOp>()
-                              ->blockScales()
-                              ->as<TensorView>();
-      if (!block_scales->isFusionOutput()) {
-        return true;
-      }
     }
   }
   return false;
