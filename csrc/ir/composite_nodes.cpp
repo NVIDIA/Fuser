7c62ef38dfdd156b1967d60c1476356e86f832bd
Xiang <qasdfgtyuiop@gmail.com>
2026-01-21 12:54:43 -0800

    save

--- csrc/ir/composite_nodes.cpp
+++ csrc/ir/composite_nodes.cpp
@@ -89,20 +89,16 @@ std::vector<PolymorphicValue> MatmulOp::evaluate(
     matmul_out = matmul_out.unsqueeze(rfactor_did_idx);
   }
 
-  // Without InferContiguity, we mistakenly assume the output is contiguous.
-  if (!isOptionEnabled(EnableOption::InferContiguity)) {
-    const auto& [sizes, strides] = inferShapeAndContiguousStrides(out(), ee);
-    auto meta_out = at::detail::empty_strided_meta(sizes, strides, a.dtype());
+  const auto& [sizes, strides] = inferShapeAndContiguousStrides(out(), ee);
+  auto meta_out = at::detail::empty_strided_meta(sizes, strides, a.dtype());
 
-    if (meta_out.is_contiguous()) {
-      return {matmul_out};
-    }
-
-    auto strided_matmul_out = at::empty_strided(sizes, strides, a.options());
-    strided_matmul_out = strided_matmul_out.copy_(matmul_out);
-    return {strided_matmul_out};
+  if (meta_out.is_contiguous()) {
+    return {matmul_out};
   }
-  return {matmul_out};
+
+  auto strided_matmul_out = at::empty_strided(sizes, strides, a.options());
+  strided_matmul_out = strided_matmul_out.copy_(matmul_out);
+  return {strided_matmul_out};
 }
 
 LinearOp::LinearOp(
