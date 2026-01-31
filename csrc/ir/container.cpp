ed19c8cf86c959c42f6a78ad89b4495fcd227fda
Michael Davis <michael.l.davis@hotmail.co.uk>
2026-01-30 16:42:49 -0800

    Clean up commented interface definitions.

--- csrc/ir/container.cpp
+++ csrc/ir/container.cpp
@@ -24,48 +24,5 @@ IrContainer::IrContainer() : ir_storage_(std::make_unique<IrStorage>()) {
 
 IrContainer::~IrContainer() {}
 
-// void IrContainer::swap(IrContainer& a, IrContainer& b) noexcept {
-//   // We need to be careful to call IrStorage swap not unique_ptr swap, which
-//   // will only swap the ptrs NOT the contents.
-//   IrStorage::swap(*(a.ir_storage()), *(b.ir_storage()));
-//
-//   // Fix parent pointers after swapping containers
-//   // After swap, each IrContainer owns a different IrStorage, so we must
-//   update
-//   // the parent backpointers in those containers to point to their new owners
-//   if (a.ir_storage_) {
-//     // Also update all Statement ir_container_ pointers to point to new owner
-//     // Note: IrContainer is now in impl namespace, but
-//     Statement::ir_container_
-//     // is Fusion*. Since only Fusion (and its derived classes) inherit from
-//     // impl::IrContainer, this cast is safe.
-//     auto* fusion_a = static_cast<Fusion*>(&a);
-//     a.ir_storage()->parent_ = fusion_a;
-//     for (auto val : a.vals()) {
-//       val->ir_container_ = fusion_a;
-//     }
-//     for (auto expr : a.deterministic_exprs()) {
-//       expr->ir_container_ = fusion_a;
-//     }
-//   }
-//   if (b.ir_storage_) {
-//     // Also update all Statement ir_container_ pointers to point to new owner
-//     auto* fusion_b = static_cast<Fusion*>(&b);
-//     b.ir_storage()->parent_ = fusion_b;
-//     for (auto val : b.vals()) {
-//       val->ir_container_ = fusion_b;
-//     }
-//     for (auto expr : b.deterministic_exprs()) {
-//       expr->ir_container_ = fusion_b;
-//     }
-//   }
-// }
-
-// IrCloner IrContainer::copy(const IrContainer* from, IrContainer* to) {
-//   auto ir_cloner = IrStorage::copy(from->ir_storage(), to->ir_storage());
-//
-//   return ir_cloner;
-// }
-
 } // namespace impl
 } // namespace nvfuser
